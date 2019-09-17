f768fee217137b4edb0ba804c7c323ac6d299d7c
Jo Jinsik <kunggom@gmail.com>
2019-09-17 14:29:45 +0900

    feat(RequestHandler): getRequestHandler 구현

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,13 +1,12 @@
 package webserver;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.Socket;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.*;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.util.Objects;
 
 public class RequestHandler implements Runnable {
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
@@ -23,6 +22,7 @@ public class RequestHandler implements Runnable {
                 connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
+            logger.info(getRequestHeader(in));
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
             DataOutputStream dos = new DataOutputStream(out);
             byte[] body = "Hello World".getBytes();
@@ -31,6 +31,19 @@ public class RequestHandler implements Runnable {
         } catch (IOException e) {
             logger.error(e.getMessage());
         }
+    }
+
+    private String getRequestHeader(final InputStream inputStream) throws IOException {
+        final BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8));
+        final StringBuilder builder = new StringBuilder();
+        while (true) {
+            String line = reader.readLine();
+            if (Objects.isNull(line) || "".equals(line)) {
+                break;
+            }
+            builder.append(line).append("\n");
+        }
+        return builder.toString();
     }
 
     private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
