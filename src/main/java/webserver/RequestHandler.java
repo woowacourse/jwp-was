cb421c85ab22a58a39b74235269b075c89a06c21
yk1028 <wongeunsong94@gmail.com>
2019-09-17 14:22:58 +0900

    feat: Request Header 출력하기

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,13 +1,12 @@
 package webserver;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.Socket;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.*;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 
 public class RequestHandler implements Runnable {
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
@@ -23,7 +22,10 @@ public class RequestHandler implements Runnable {
                 connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+            BufferedReader buffer = new BufferedReader(new InputStreamReader(in, "UTF-8"));
+            List<String> lines = findRequestHeader(buffer);
+            printRequestHeader(lines);
+
             DataOutputStream dos = new DataOutputStream(out);
             byte[] body = "Hello World".getBytes();
             response200Header(dos, body.length);
@@ -31,6 +33,28 @@ public class RequestHandler implements Runnable {
         } catch (IOException e) {
             logger.error(e.getMessage());
         }
+    }
+
+    private void printRequestHeader(List<String> lines) {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("\n============================================\n");
+        for (String line : lines) {
+            stringBuilder.append(line).append("\n");
+        }
+        stringBuilder.append("============================================\n");
+        logger.debug(stringBuilder.toString());
+    }
+
+    private List<String> findRequestHeader(BufferedReader buffer) throws IOException {
+        List<String> lines = new ArrayList<>();
+        String line;
+        while (!"".equals(line = buffer.readLine())) {
+            if (line == null) {
+                break;
+            }
+            lines.add(line);
+        }
+        return lines;
     }
 
     private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
