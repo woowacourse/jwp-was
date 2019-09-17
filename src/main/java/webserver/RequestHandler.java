99be244eb4a6291d324e72a066ca04f0568c0734
imkimheejoo <jujudamdadi@naver.com>
2019-09-17 16:48:41 +0900

    feat: 요청한 파일을 응답하는 기능 구현

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,13 +1,12 @@
 package webserver;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.Socket;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import utils.FileIoUtils;
+
+import java.io.*;
+import java.net.Socket;
+import java.net.URISyntaxException;
 
 public class RequestHandler implements Runnable {
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
@@ -24,12 +23,32 @@ public class RequestHandler implements Runnable {
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+            BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
+            String line = br.readLine();
+            logger.debug(line);
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World".getBytes();
+            byte[] body = new byte[]{};
+
+            String path = line.split(" ")[1];
+            if(path.contains(".")) {
+                body = FileIoUtils.loadFileFromClasspath("./templates/index.html");
+            }
+
+            while (!"".equals(line)) {
+                logger.debug(line);
+                if (line == null) {
+                    return;
+                }
+                line = br.readLine();
+
+            }
             response200Header(dos, body.length);
             responseBody(dos, body);
         } catch (IOException e) {
             logger.error(e.getMessage());
+        } catch (URISyntaxException e) {
+            logger.error(e.getMessage());
+            e.printStackTrace();
         }
     }
 
---
+++ src/test/java/webserver/RequestHandlerTest.java
@@ -0,0 +1,31 @@
+package webserver;
+
+import org.apache.commons.io.IOUtils;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class RequestHandlerTest {
+    private RequestHandler requestHandler;
+
+    @Test
+    void InputStream_올바른경우() throws IOException {
+        Socket socket = mock(Socket.class);
+        String request = "GET /index2.html HTTP/1.1\n" +
+                "Host: localhost:8080\n" +
+                "Connection: keep-alive\n" +
+                "Accept: */*";
+        InetAddress mockInetAddress = mock(InetAddress.class);
+        when(socket.getInetAddress()).thenReturn(mockInetAddress);
+        when(socket.getPort()).thenReturn(8080);
+        when(socket.getInputStream()).thenReturn(IOUtils.toInputStream(request, "UTF-8"));
+        requestHandler = new RequestHandler(socket);
+
+        requestHandler.run();
+    }
+}
