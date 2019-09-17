9d9aebaf164e56b4da7dc334d42144defb85171b
imkimheejoo <jujudamdadi@naver.com>
2019-09-17 16:52:18 +0900

    test: 요청한 경로에 파일이 존재하는지 확인하는 테스트 구현

--- src/test/java/utils/FileIoUtilsTest.java
+++ src/test/java/utils/FileIoUtilsTest.java
@@ -4,12 +4,15 @@ import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class FileIoUtilsTest {
     private static final Logger log = LoggerFactory.getLogger(FileIoUtilsTest.class);
 
     @Test
     void loadFileFromClasspath() throws Exception {
         byte[] body = FileIoUtils.loadFileFromClasspath("./templates/index.html");
+        assertThat(body).isNotNull();
         log.debug("file : {}", new String(body));
     }
 }
