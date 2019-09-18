213aec17b2c573dc7c3e5520263e7cd56a4ec18b
hyogeon <men1219@hanmail.net>
2019-09-18 15:17:28 +0900

    test: http.domain 테스트

--- src/test/java/http/model/HttpUriTest.java
+++ src/test/java/http/model/HttpUriTest.java
@@ -1,7 +1,21 @@
 package http.model;
 
+import http.supoort.IllegalHttpRequestException;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Java6Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 class HttpUriTest {
+    HttpUri httpUri;
 
+    @Test
+    void HTTP_URI_가_아닌경우() {
+        assertThatThrownBy(()->new HttpUri("something")).isInstanceOf(IllegalHttpRequestException.class);
+    }
+
+    @Test
+    void HTTP_URI_가_맞는경우() {
+        assertDoesNotThrow(()->new HttpUri("/index.html"));
+    }
 }
