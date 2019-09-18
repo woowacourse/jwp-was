102498a632135edbd628de900490f8d005d90ac4
강연욱 <fiasboc23@gmail.com>
2019-09-18 14:35:40 +0900

    add test code - create user

---
+++ src/test/java/webserver/UserControllerTest.java
@@ -0,0 +1,31 @@
+package webserver;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.web.reactive.function.BodyInserters;
+
+public class UserControllerTest {
+
+    private WebTestClient webTestClient;
+
+    @BeforeEach
+    void setup() {
+        webTestClient = WebTestClient.bindToServer().baseUrl("http://localhost:8080").build();
+    }
+
+    @Test
+    void create() {
+        webTestClient.post()
+                .uri(UserController.USER_CREATE_URL)
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .body(BodyInserters.fromFormData("userId", "john123")
+                .with("password", "p@ssW0rd")
+                .with("name", "john")
+                .with("email", "john@example.com"))
+                .exchange()
+                .expectStatus().is3xxRedirection()
+                .expectHeader().valueMatches("Location", "/index.html");
+    }
+}
