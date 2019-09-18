91a9d966bb2fc2ae87a8b11e1788bda7df6386e7
ep1stas1s <masloz@konkuk.ac.kr>
2019-09-19 02:42:09 +0900

    feat: request sample

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,13 +1,15 @@
 package webserver;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import utils.FileIoUtils;
+
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import java.net.URISyntaxException;
 
 public class RequestHandler implements Runnable {
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
@@ -18,25 +20,48 @@ public class RequestHandler implements Runnable {
         this.connection = connectionSocket;
     }
 
+    @Override
     public void run() {
         logger.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
                 connection.getPort());
 
-        try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+        try (InputStream inputStream = connection.getInputStream();
+             OutputStream out = connection.getOutputStream()) {
+            RequestHeader requestHeader = RequestHeader.of(inputStream);
+
+            String path;
+            String uri = requestHeader.getValue("Uri");
+            if (requestHeader.getValue("Accept").contains("text/html")
+                    || uri.contains("favicon")) {
+                path = "./templates" + uri;
+            } else {
+                path = "./static" + uri;
+            }
+
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World".getBytes();
-            response200Header(dos, body.length);
-            responseBody(dos, body);
-        } catch (IOException e) {
+            byte[] responseBody = FileIoUtils.loadFileFromClasspath(path);
+
+            String contentType = "text/html; charset=utf-8";
+            if (uri.contains(".js")) {
+                contentType = "text/javascript; charset=utf-8";
+            }
+            if (uri.contains(".css")) {
+                contentType = "text/css; charset=utf-8";
+            }
+            if (uri.contains(".ico")) {
+                contentType = "image/x-icon";
+            }
+            response200Header(dos, responseBody.length, contentType);
+            responseBody(dos, responseBody);
+        } catch (IOException | URISyntaxException e) {
             logger.error(e.getMessage());
         }
     }
 
-    private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
+    private void response200Header(DataOutputStream dos, int lengthOfBodyContent, String contentType) {
         try {
             dos.writeBytes("HTTP/1.1 200 OK \r\n");
-            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
+            dos.writeBytes(String.format("Content-Type: %s\r\n", contentType));
             dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
             dos.writeBytes("\r\n");
         } catch (IOException e) {
