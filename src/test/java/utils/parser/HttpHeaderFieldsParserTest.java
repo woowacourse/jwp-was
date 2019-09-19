21d791e861b0b05ae392db89095c74787f4d2b7c
hyojaekim <hj00686@naver.com>
2019-09-19 21:45:02 +0900

    test: Http header fields parser

---
+++ src/test/java/utils/parser/HttpHeaderFieldsParserTest.java
@@ -0,0 +1,34 @@
+package utils.parser;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class HttpHeaderFieldsParserTest {
+    @Test
+    @DisplayName("헤더에 대한 내용을 key value 값으로 설정한다.")
+    void httpHeaderFieldsParser() {
+        HttpHeaderFieldsParser httpHeaderFieldsParser = new HttpHeaderFieldsParser();
+        Map<String, String> result = httpHeaderFieldsParser.toMap("Host: localhost:8080\r\n" +
+                                                                        "Connection: keep-alive\r\n" +
+                                                                        "Accept: */*");
+
+        assertThat(result.get("Host")).isEqualTo("localhost:8080");
+        assertThat(result.get("Connection")).isEqualTo("keep-alive");
+        assertThat(result.get("Accept")).isEqualTo("*/*");
+    }
+
+    @Test
+    @DisplayName("줄바꿈이 없는 경우 헤더에 대한 내용을 key value 값으로 설정하는데 실패한다.")
+    void FailHttpHeaderFieldsParser() {
+        HttpHeaderFieldsParser httpHeaderFieldsParser = new HttpHeaderFieldsParser();
+
+        Map<String, String> result = httpHeaderFieldsParser.toMap(
+                "Host: localhost:8080 Connection: keep-alive Accept: */*");
+
+        assertThat(result.size()).isEqualTo(0);
+    }
+}
