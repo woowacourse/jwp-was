cc93f5b9f9a52a014abb2b47a948ec90fad002fb
Jo Jinsik <kunggom@gmail.com>
2019-09-19 14:26:20 +0900

    fix(RequestDispatcher): 회원가입 경로 수정, NullPointerException 발생 문제 수정

--- src/main/java/webserver/RequestDispatcher.java
+++ src/main/java/webserver/RequestDispatcher.java
@@ -11,10 +11,12 @@ public class RequestDispatcher {
     private static final Map<String, Function<Request, byte[]>> requestUrls = new HashMap<>();
 
     static {
-        requestUrls.put("/create", UserController::createUser);
+        requestUrls.put("/user/create", UserController::createUser);
     }
 
     public static byte[] forward(final Request request) {
-        return requestUrls.get(request.getPath()).apply(request);
+        // TODO: 404 띄우기
+        return requestUrls.getOrDefault(request.getPath(),
+                (req) -> null).apply(request);
     }
 }
