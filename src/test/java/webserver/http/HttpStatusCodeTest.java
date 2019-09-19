d53a527f544b6dd47de0800c7c848389fb5d52c2
hyojaekim <hj00686@naver.com>
2019-09-19 21:52:45 +0900

    test: Create http status code

---
+++ src/test/java/webserver/http/HttpStatusCodeTest.java
@@ -0,0 +1,23 @@
+package webserver.http;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class HttpStatusCodeTest {
+    @Test
+    @DisplayName("존재하는 상태코드를 찾는다.")
+    void findHttpStatusCode() {
+        assertThat(HttpStatusCode.of(200).get()).isEqualTo(HttpStatusCode.OK);
+    }
+
+    @Test
+    @DisplayName("존재하지 않는 상태코드를 ")
+    void notFindHttpStatusCode() {
+        assertThrows(IllegalArgumentException.class, () ->
+            HttpStatusCode.of(199).orElseThrow(IllegalArgumentException::new)
+        );
+    }
+}
