d41e676008c235a48d39f01b2ac0968c0c6e176e
hyojaekim <hj00686@naver.com>
2019-09-19 21:51:44 +0900

    test: Find all http method

---
+++ src/test/java/webserver/http/HttpMethodTest.java
@@ -0,0 +1,19 @@
+package webserver.http;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class HttpMethodTest {
+    @Test
+    @DisplayName("HttpMethod 모든 메소드를 찾는다.")
+    void findHttpMethod() {
+        HttpMethod[] httpMethods = HttpMethod.values();
+
+        for (HttpMethod httpMethod : httpMethods) {
+            String methodName = httpMethod.name();
+            assertThat(HttpMethod.of(methodName).get()).isEqualTo(httpMethod);
+        }
+    }
+}
