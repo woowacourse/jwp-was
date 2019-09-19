5b097d05dc4c0d1564c7198dc3d39c9dc146338b
hyogeon <men1219@hanmail.net>
2019-09-19 11:48:31 +0900

    refactor: WebServer 메서드 추출

--- src/main/java/webserver/WebServer.java
+++ src/main/java/webserver/WebServer.java
@@ -14,30 +14,23 @@ import org.slf4j.LoggerFactory;
 
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class WebServer {
     private static final Logger logger = LoggerFactory.getLogger(WebServer.class);
     private static final int DEFAULT_PORT = 8080;
+    private static AtomicInteger counter = new AtomicInteger(0);
 
     public static void main(String args[]) throws Exception {
-        int port = 0;
-        if (args == null || args.length == 0) {
-            port = DEFAULT_PORT;
-        } else {
-            port = Integer.parseInt(args[0]);
-        }
-        HttpRequestHandlers httpRequestHandlers = new HttpRequestHandlers();
-        HttpRequestHandler fileHandler = new FileResourceRequestHandler();
-        HttpRequestHandler userRequestHandler = new UserRequestHandler();
+        int port = getPort(args);
 
-        httpRequestHandlers.addHandler(new RequestMapping(HttpMethod.GET, "/*"), fileHandler);
-        httpRequestHandlers.addHandler(new RequestMapping(HttpMethod.GET, "/user/create"), userRequestHandler);
-        httpRequestHandlers.addHandler(new RequestMapping(HttpMethod.POST, "/user/create"), userRequestHandler);
+        HttpRequestHandlers httpRequestHandlers = initRequestHandlers();
+        ViewHandler viewHandler = initViewHandlers();
 
-
-        ViewHandler viewHandler = new ViewHandler();
-        viewHandler.addResolver(new ViewResolver());
-        viewHandler.addResolver(new ModelResolver());
+        ExecutorService es = Executors.newFixedThreadPool(100);
 
         // 서버소켓을 생성한다. 웹서버는 기본적으로 8080번 포트를 사용한다.
         try (ServerSocket listenSocket = new ServerSocket(port)) {
@@ -46,9 +39,38 @@ public class WebServer {
             // 클라이언트가 연결될때까지 대기한다.
             Socket connection;
             while ((connection = listenSocket.accept()) != null) {
-                Thread thread = new Thread(new RequestHandler(connection, httpRequestHandlers, viewHandler));
-                thread.start();
+                es.execute(new RequestHandler(connection,httpRequestHandlers,viewHandler));
             }
         }
+        es.shutdown();
+        es.awaitTermination(100, TimeUnit.SECONDS);
+    }
+
+    private static int getPort(String[] args) {
+        int port = 0;
+        if (args == null || args.length == 0) {
+            port = DEFAULT_PORT;
+        } else {
+            port = Integer.parseInt(args[0]);
+        }
+        return port;
+    }
+
+    private static HttpRequestHandlers initRequestHandlers() {
+        HttpRequestHandlers httpRequestHandlers = new HttpRequestHandlers();
+        HttpRequestHandler fileHandler = new FileResourceRequestHandler();
+        HttpRequestHandler userRequestHandler = new UserRequestHandler();
+
+        httpRequestHandlers.addHandler(new RequestMapping(HttpMethod.GET, "/*"), fileHandler);
+        httpRequestHandlers.addHandler(new RequestMapping(HttpMethod.GET, "/user/create"), userRequestHandler);
+        httpRequestHandlers.addHandler(new RequestMapping(HttpMethod.POST, "/user/create"), userRequestHandler);
+        return httpRequestHandlers;
+    }
+
+    private static ViewHandler initViewHandlers() {
+        ViewHandler viewHandler = new ViewHandler();
+        viewHandler.addResolver(new ViewResolver());
+        viewHandler.addResolver(new ModelResolver());
+        return viewHandler;
     }
 }
