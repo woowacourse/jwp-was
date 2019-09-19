0820ef2d8344f6190742bd6d168625fc655dd681
hyojaekim <hj00686@naver.com>
2019-09-19 21:45:55 +0900

    test: Plain string parser

---
+++ src/test/java/utils/parser/PlainStringParserTest.java
@@ -0,0 +1,22 @@
+package utils.parser;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class PlainStringParserTest {
+    @Test
+    @DisplayName("'/'로 줄을 나누고, '='으로 key value를 설정한다.")
+    void toMap() {
+        PlainStringParser plainStringParser = new PlainStringParser("/", "=");
+        String inputData = "one=1/two=2/three=3";
+        Map<String, String> result = plainStringParser.toMap(inputData);
+
+        assertThat(result.get("one")).isEqualTo("1");
+        assertThat(result.get("two")).isEqualTo("2");
+        assertThat(result.get("three")).isEqualTo("3");
+    }
+}
