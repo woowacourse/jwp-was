b1ea49b5f097f1543e59223297a7fee982e8fe30
Jo Jinsik <kunggom@gmail.com>
2019-09-19 17:36:43 +0900

    feat(HttpStatus): HTTP Status Code를 나타내는 enum 추가

---
+++ src/main/java/webserver/domain/HttpStatus.java
@@ -0,0 +1,28 @@
+package webserver.domain;
+
+public enum HttpStatus {
+    OK(200, "OK"),
+    FOUND(302, "Found"),
+    NOT_FOUND(404, "Not Found");
+
+    private final int code;
+    private final String phrase;
+
+    HttpStatus(final int code, final String phrase) {
+        this.code = code;
+        this.phrase = phrase;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public String getPhrase() {
+        return phrase;
+    }
+
+    @Override
+    public String toString() {
+        return this.code + " " + this.phrase;
+    }
+}
