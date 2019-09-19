13d455c0f9ca5fd559aa2c83e416520079ec0cdd
Seongmo Jeon <oeeen3@gmail.com>
2019-09-19 15:32:52 +0900

    feat: HttpMethod, HttpStatus 생성

---
+++ src/main/java/http/HttpStatus.java
@@ -0,0 +1,32 @@
+package http;
+
+import java.util.Arrays;
+
+public enum HttpStatus {
+    OK("200", "Ok"),
+    FOUND("302", "Found");
+
+    private String statusCode;
+    private String reasonPhrase;
+
+    HttpStatus(String statusCode, String reasonPhrase) {
+        this.statusCode = statusCode;
+        this.reasonPhrase = reasonPhrase;
+    }
+
+    public static HttpStatus of(String code) {
+        return Arrays.stream(values())
+                .filter(value -> value.statusCode.equals(code))
+                .findFirst()
+                .orElseThrow(IllegalArgumentException::new);
+    }
+
+    public String getReasonPhrase() {
+        return reasonPhrase;
+    }
+
+    @Override
+    public String toString() {
+        return statusCode + " " + reasonPhrase;
+    }
+}
