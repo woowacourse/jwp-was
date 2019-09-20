2a7f0046f1574c622debb2e14a601c71f0dff73e
school0bhy <house1217@ajou.ac.kr>
2019-09-20 17:33:47 +0900

    test: URL test 구현

---
+++ src/test/java/http/common/URLTest.java
@@ -0,0 +1,29 @@
+package http.common;
+
+import http.common.exception.InvalidURLException;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class URLTest {
+    @Test
+    void 생성_성공() {
+        String path = "/test";
+        String queryString = "id=1";
+        URL url = URL.of(String.format("%s?%s", path, queryString));
+
+        assertThat(url.getPath()).isEqualTo(path);
+        assertThat(url.getQueryString()).isEqualTo(queryString);
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    @ValueSource(strings = {"  ", "\t", "\n", "?id=1"})
+    void 생성_실패(String input) {
+        assertThrows(InvalidURLException.class, () -> URL.of(input));
+    }
+}
