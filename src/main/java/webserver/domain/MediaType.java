9207a5a42c13e023b08261874e328390f669ad7f
Jo Jinsik <kunggom@gmail.com>
2019-09-20 21:35:15 +0900

    fix(MediaType): 전송된 페이지의 인코딩 문제 해결

--- src/main/java/webserver/domain/MediaType.java
+++ src/main/java/webserver/domain/MediaType.java
@@ -5,16 +5,17 @@ import java.util.Collections;
 import java.util.List;
 
 public enum MediaType {
-    TEXT_PLAIN("text/plain", "txt", "text"),
-    TEXT_HTML("text/html", "html", "htm"),
-    TEXT_CSS("text/css", "css"),
-    TEXT_CSV("text/csv", "csv"),
+    TEXT_PLAIN("text/plain; charset=utf-8", "txt", "text"),
+    TEXT_HTML("text/html; charset=utf-8", "html", "htm"),
+    TEXT_CSS("text/css; charset=utf-8", "css"),
+    TEXT_CSV("text/csv; charset=utf-8", "csv"),
     IMAGE_PNG("image/png", "png"),
     IMAGE_JPEG("image/jpeg", "jpg", "jpeg"),
     IMAGE_GIF("image/gif", "gif"),
-    APPLICATION_JS("application/javascript", "js"),
-    APPLICATION_JSON("application/json", "json"),
-    APPLICATION_XML("application/xml", "xml"),
+    IMAGE_ICON("image/x-icon", "ico"),
+    APPLICATION_JS("application/javascript; charset=utf-8", "js"),
+    APPLICATION_JSON("application/json; charset=utf-8", "json"),
+    APPLICATION_XML("application/xml; charset=utf-8", "xml"),
     APPLICATION_ZIP("application/zip", "zip"),
     APPLICATION_PDF("application/pdf", "pdf"),
     APPLICATION_BINARY("application/octet-stream", "*", "bin");
