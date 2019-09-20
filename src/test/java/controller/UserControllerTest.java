083c6714c2f49dded5951a3ac4b1acb7c744b23a
강연욱 <fiasboc23@gmail.com>
2019-09-20 16:49:47 +0900

    test: userController 테스트 변경

---
+++ src/test/java/controller/UserControllerTest.java
@@ -0,0 +1,30 @@
+package controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import webserver.RequestDispatcher;
+import webserver.RequestParser;
+import webserver.Response;
+import webserver.Status;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class UserControllerTest {
+
+    private static final String TEST_DIRECTORY = "./src/test/resources";
+
+    @Test
+    @DisplayName("회원가입 성공 통합테스트")
+    void create() throws IOException {
+        InputStream in = new FileInputStream(new File(TEST_DIRECTORY + "/PostSignUp.txt"));
+        Response res = RequestDispatcher.handle(RequestParser.parse(in));
+
+        assertThat(res.getStatus()).isEqualTo(Status.FOUND);
+        assertThat(res.getHeader("Location")).isEqualTo("/index.html");
+    }
+}
