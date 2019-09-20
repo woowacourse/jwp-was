f3b76a4ba320c45a8102e526b83be52cdf5f367b
choihz <choixhz@gmail.com>
2019-09-20 13:57:26 +0900

    feat: add description field and toString()

--- src/main/java/http/common/HttpStatus.java
+++ src/main/java/http/common/HttpStatus.java
@@ -1,13 +1,20 @@
 package http.common;
 
 public enum HttpStatus {
-    OK(200),
-    FOUND(302),
-    NOT_FOUND(404);
+    OK(200, "OK"),
+    FOUND(302, "FOUND"),
+    NOT_FOUND(404, "NOT FOUND");
 
     private int code;
+    private String description;
 
-    HttpStatus(int code) {
+    HttpStatus(int code, String description) {
         this.code = code;
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return code + " " + description;
     }
 }
