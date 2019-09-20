ec9f6c36e2ef1923831b5ecce25326984425b2f3
andole87 <andole87@gmail.com>
2019-09-20 09:54:36 +0900

    refactor: ViewResolver 수정
    - 헤더정보 핸들링 변경

--- src/main/java/http/view/ViewResolver.java
+++ src/main/java/http/view/ViewResolver.java
@@ -1,34 +1,56 @@
 package http.view;
 
-import http.model.ContentType;
-import http.model.HttpProtocols;
-import http.model.HttpResponse;
-import http.model.HttpStatus;
+import http.model.*;
 import utils.FileIoUtils;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class ViewResolver implements Resolver {
     private static final String HTML_PATH = "./templates";
     private static final String STATIC_PATH = "./static";
     private static final String REDIRECT_PREFIX = "redirect:";
     private static final String EXTENSION_SEPARATOR = ".";
+    private static final String CONTENT_TYPE = "Content-Type";
 
 
     @Override
     public HttpResponse resolve(ModelAndView modelAndView) {
         String resource = modelAndView.getViewLocation();
+        HttpHeaders headers = defaultHeaders(resource);
         if (resource.startsWith(REDIRECT_PREFIX)) {
-            return new HttpResponse(HttpProtocols.DEFAULT, HttpStatus.FOUND, ContentType.HTML);
+            appendLocationHeader(headers, extractLocation(resource));
         }
-        ContentType contentType = getContentType(modelAndView);
-        if (contentType == ContentType.HTML) {
-            return new HttpResponse(HttpProtocols.DEFAULT, HttpStatus.OK, contentType, FileIoUtils.loadFileFromClasspath(HTML_PATH + modelAndView.getViewLocation()));
-        }
-        return new HttpResponse(HttpProtocols.DEFAULT, HttpStatus.OK, contentType, FileIoUtils.loadFileFromClasspath(STATIC_PATH + modelAndView.getViewLocation()));
+        return resolveResponse(resource, headers);
     }
 
-    private ContentType getContentType(ModelAndView modelAndView) {
-        String resourceName = modelAndView.getViewLocation();
-        String extension = resourceName.substring(resourceName.lastIndexOf(EXTENSION_SEPARATOR) + 1);
+    private HttpResponse resolveResponse(String resource, HttpHeaders headers) {
+        ContentType contentType = ContentType.from(headers.getHeader(CONTENT_TYPE));
+        if (contentType.isHTML()) {
+            return new HttpResponse(HttpProtocols.DEFAULT, HttpStatus.OK,
+                    headers, FileIoUtils.loadFileFromClasspath(HTML_PATH + resource));
+        }
+        return new HttpResponse(HttpProtocols.DEFAULT, HttpStatus.OK,
+                headers, FileIoUtils.loadFileFromClasspath(STATIC_PATH + resource));
+    }
+
+    private String extractLocation(String resource) {
+        return resource.substring(resource.indexOf(REDIRECT_PREFIX) - 1);
+    }
+
+    private void appendLocationHeader(HttpHeaders headers, String redirectUri) {
+        headers.addHeader("Location", redirectUri);
+    }
+
+    private HttpHeaders defaultHeaders(String resource) {
+        Map<String, String> headers = new HashMap<>();
+        headers.put(CONTENT_TYPE, getContentType(resource).getType());
+        headers.put("Server", "ANDOLE MACHINE");
+        return new HttpHeaders(headers);
+    }
+
+    private ContentType getContentType(String resource) {
+        String extension = resource.substring(resource.lastIndexOf(EXTENSION_SEPARATOR) + 1);
         return ContentType.of(extension);
     }
 }
