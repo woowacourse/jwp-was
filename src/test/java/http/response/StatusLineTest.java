eb68f3e278abbc171e3ac042f4a8a0a613fe0d5f
school0bhy <house1217@ajou.ac.kr>
2019-09-20 21:18:16 +0900

    test: StatusLine test 구현

---
+++ src/test/java/http/response/StatusLineTest.java
@@ -0,0 +1,24 @@
+package http.response;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class StatusLineTest {
+    private String version = "HTTP/1.1";
+
+    @Test
+    void 생성_오류() {
+        assertThrows(InvalidStatusLineException.class, () -> new StatusLine(null, ResponseStatus.OK));
+        assertThrows(InvalidStatusLineException.class, () -> new StatusLine(version, null));
+    }
+
+    @Test
+    void serialize_확인() {
+        StatusLine statusLine = new StatusLine(version, ResponseStatus.OK);
+
+        assertThat(statusLine.serialize()).isEqualTo(String.format("%s 200 OK", version));
+    }
+
+}
