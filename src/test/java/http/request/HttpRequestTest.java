f837698fe706d3fa73c30b608fb986dfd9a27155
choihz <choixhz@gmail.com>
2019-09-20 17:19:53 +0900

    feat: update HttpRequest field names

--- src/test/java/http/request/HttpRequestTest.java
+++ src/test/java/http/request/HttpRequestTest.java
@@ -1,6 +1,5 @@
 package http.request;
 
-import http.common.HttpMethod;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -12,49 +11,49 @@ import static org.assertj.core.api.Assertions.assertThat;
 class HttpRequestTest {
     private static final String TEST_DIRECTORY = "./src/test/resources/";
 
-    private HttpRequest request;
+    private HttpRequest httpRequest;
 
     @BeforeEach
-    void setUp() throws Exception {
+    public void setUp() throws Exception {
         InputStream in = new FileInputStream(new File(TEST_DIRECTORY + "Http_GET.txt"));
         BufferedReader br = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
-        request = new HttpRequest(br);
+        httpRequest = new RequestHandler(br).create();
     }
 
     @Test
     public void request_GET() {
-        assertThat(request.getMethod()).isEqualTo(HttpMethod.GET);
-        assertThat(request.getPath()).isEqualTo("/user/create");
-        assertThat(request.getHeader("Connection")).isEqualTo("keep-alive");
-        assertThat(request.getQuery("userId")).isEqualTo("aiden");
+        assertThat(httpRequest.isGet()).isTrue();
+        assertThat(httpRequest.getPath()).isEqualTo("/user/create");
+        assertThat(httpRequest.getHeader("Connection")).isEqualTo("keep-alive");
+        assertThat(httpRequest.getQuery("userId")).isEqualTo("aiden");
     }
 
     @Test
     public void getUrlTest() {
-        assertThat(request.getUrl()).isEqualTo("http://localhost:8080/user/create");
+        assertThat(httpRequest.getUrl()).isEqualTo("http://localhost:8080/user/create");
     }
 
     @Test
     public void getQueryTest() {
-        assertThat(request.getQuery("userId")).isEqualTo("aiden");
-        assertThat(request.getQuery("password")).isEqualTo("password");
-        assertThat(request.getQuery("name")).isEqualTo("aiden");
-        assertThat(request.getQuery("email")).isEqualTo("aiden@aiden.com");
+        assertThat(httpRequest.getQuery("userId")).isEqualTo("aiden");
+        assertThat(httpRequest.getQuery("password")).isEqualTo("password");
+        assertThat(httpRequest.getQuery("name")).isEqualTo("aiden");
+        assertThat(httpRequest.getQuery("email")).isEqualTo("aiden@aiden.com");
     }
 
     @Test
     public void getPathTest() {
-        assertThat(request.getPath()).isEqualTo("/user/create");
+        assertThat(httpRequest.getPath()).isEqualTo("/user/create");
     }
 
     @Test
-    public void getMethodTest() {
-        assertThat(request.getMethod()).isEqualTo(HttpMethod.GET);
+    public void methodTest() {
+        assertThat(httpRequest.isGet()).isTrue();
+        assertThat(httpRequest.isPost()).isFalse();
     }
 
     @Test
     public void getHeaderTest() {
-<<<<<<< HEAD
         assertThat(httpRequest.getHeader("Host")).isEqualTo("localhost:8080");
         assertThat(httpRequest.getHeader("Connection")).isEqualTo("keep-alive");
         assertThat(httpRequest.getHeader("Accept")).isEqualTo("*/*");
@@ -63,10 +62,5 @@ class HttpRequestTest {
     @Test
     public void getEntityValueTest() {
         assertThat(httpRequest.getEntityValue("Host")).isEqualTo(null);
-=======
-        assertThat(request.getHeader("Host")).isEqualTo("localhost:8080");
-        assertThat(request.getHeader("Connection")).isEqualTo("keep-alive");
-        assertThat(request.getHeader("Accept")).isEqualTo("*/*");
->>>>>>> feat: create HttpRequest
     }
 }
