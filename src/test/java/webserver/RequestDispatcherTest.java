c256e593dfca4d31ac0302102b1c6c54c089a5d1
Jo Jinsik <kunggom@gmail.com>
2019-09-20 18:15:04 +0900

    test(RequestDispatcher): 수정된 프로덕트 코드에 따라 테스트 메서드 수정

--- src/test/java/webserver/RequestDispatcherTest.java
+++ src/test/java/webserver/RequestDispatcherTest.java
@@ -1,10 +1,9 @@
 package webserver;
 
-import model.User;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import webserver.domain.QueryParameter;
 import webserver.domain.Request;
+import webserver.domain.Response;
 import webserver.support.RequestHelper;
 
 import java.io.IOException;
@@ -17,15 +16,10 @@ class RequestDispatcherTest extends RequestHelper {
     @DisplayName("url 분기처리를 제대로 하는지 확인")
     void forward() throws IOException {
         final Request request = new Request(networkInputWithQueryString);
-
-        final QueryParameter queries = request.getQueryParameter();
-        final String userId = queries.getValue("userId");
-        final String password = queries.getValue("password");
-        final String name = queries.getValue("name");
-        final String email = queries.getValue("email");
-        final User user = new User(userId, password, name, email);
-
-        final byte[] response = RequestDispatcher.forward(request);
-        assertThat(response).isEqualTo(user.toString().getBytes());
+        final Response response = RequestDispatcher.forward(request);
+        assertThat(response.toBytes()).isEqualTo(new Response.Builder()
+                        .redirectUrl("/index.html")
+                        .body("")
+                        .build().toBytes());
     }
 }
