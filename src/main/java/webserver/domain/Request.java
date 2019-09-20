81edfbc493c337b7696a51d345f0ab8bb7a64c4e
Jo Jinsik <kunggom@gmail.com>
2019-09-20 17:16:06 +0900

    refactor(Request): 코드 리팩토링
    매직넘버 상수화 적용

--- src/main/java/webserver/domain/Request.java
+++ src/main/java/webserver/domain/Request.java
@@ -12,6 +12,21 @@ import static org.slf4j.LoggerFactory.getLogger;
 
 public class Request {
     private static final Logger LOG = getLogger(Request.class);
+    private static final String KEY_VALUE_DELIMETER = ":";
+    private static final String EMPTY = "";
+    private static final String CONTENT_LENGTH = "content-length";
+    private static final String ZERO_LENGTH = "0";
+    private static final String SPACE_DELIMITER = " ";
+    private static final String QUERY_DELIMITER = "\\?";
+    private static final int KEY_INDEX = 0;
+    private static final int VALUE_INDEX = 1;
+    private static final int URL_INDEX = 1;
+    private static final int PATH_INDEX = 0;
+    private static final int QUERY_INDEX = 0;
+    private static final int METHOD_INDEX = 0;
+    private static final int PROTOCOL_INDEX = 2;
+    private static final int ZERO = 0;
+    private static final int NO_QUERY = 1;
 
     // TODO: 잘못된 요청을 받았을 때 문제가 없는지 검토
     // TODO: 래퍼 클래스로 감싸기
@@ -24,17 +39,17 @@ public class Request {
     private final Map<String, String> requestFields;
 
     public Request(final NetworkInput networkInput) throws IOException {
-        final String[] httpMethodAndPath = networkInput.iterator().next().split(" ");
-        final String[] pathAndQuery = httpMethodAndPath[1].split("\\?");
+        final String[] httpMethodAndPath = networkInput.iterator().next().split(SPACE_DELIMITER);
+        final String[] pathAndQuery = httpMethodAndPath[URL_INDEX].split(QUERY_DELIMITER);
 
-        this.httpMethod = httpMethodAndPath[0];
-        this.protocol = httpMethodAndPath[2];
-        this.path = pathAndQuery[0];
-        this.queryParameter = new QueryParameter((pathAndQuery.length == 1) ? "" : pathAndQuery[1]);
+        this.httpMethod = HttpMethod.valueOf(httpMethodAndPath[METHOD_INDEX]);
+        this.protocol = httpMethodAndPath[PROTOCOL_INDEX];
+        this.path = pathAndQuery[PATH_INDEX];
+        this.queryParameter = new QueryParameter((pathAndQuery.length == NO_QUERY) ? EMPTY : pathAndQuery[QUERY_INDEX]);
         this.requestFields = makeFields(networkInput);
 
-        final int contentLength = Integer.parseInt(requestFields.getOrDefault("content-length", "0"));
-        this.body = (contentLength > 0) ? networkInput.readBody(contentLength) : "";
+        final int contentLength = Integer.parseInt(requestFields.getOrDefault(CONTENT_LENGTH, ZERO_LENGTH));
+        this.body = (contentLength > ZERO) ? networkInput.readBody(contentLength) : EMPTY;
         this.queryParameter.putByRawQueries(this.body);
 
         LOG.debug("Request - protocol: {}, method: {}, path: {}, parameter: {}\nbody: {}",
@@ -54,11 +69,11 @@ public class Request {
     }
 
     private String makeKey(final String rawField) {
-        return rawField.split(":")[0].trim().toLowerCase();
+        return rawField.split(KEY_VALUE_DELIMETER)[KEY_INDEX].trim().toLowerCase();
     }
 
     private String makeValue(final String rawField) {
-        return rawField.split(":")[1].trim();
+        return rawField.split(KEY_VALUE_DELIMETER)[VALUE_INDEX].trim();
     }
 
     public String getHttpMethod() {
@@ -82,6 +97,6 @@ public class Request {
     }
 
     public String getFieldsValue(final String key) {
-        return requestFields.getOrDefault(key, "");
+        return requestFields.getOrDefault(key, EMPTY);
     }
 }
