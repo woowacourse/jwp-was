adb01a912521a1874f2249bec41087c93767f570
starkim06 <starkim06@naver.com>
2019-09-20 16:57:05 +0900

    feat: HttpRequestFactory 추가

--- src/main/java/webserver/HttpRequestFactory.java
+++ src/main/java/webserver/HttpRequestFactory.java
@@ -1,24 +1,42 @@
-//package webserver;
-//
-//import java.io.BufferedReader;
-//import java.io.IOException;
-//import java.io.InputStream;
-//import java.io.InputStreamReader;
-//
-//public class HttpRequestFactory {
-//
-//    public HttpRequest getHttpRequest(InputStream in) throws IOException {
-//        BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
-//
-//        HttpStartLine httpStartLine = HttpStartLine.of(br);
-//        HttpRequestHeader httpRequestHeader = HttpRequestHeader.of(br);
-//
-//        if (httpStartLine.isGet()) {
-//            return new HttpRequest(httpStartLine, httpRequestHeader);
-//        } else if (httpStartLine.isPost()) {
-//            return new HttpRequestWithBody(new HttpRequest(httpStartLine, httpRequestHeader), new HttpRequestBody(br, httpRequestHeader.getContentLength()));
-//        }
-//
-//        throw new IllegalArgumentException();
-//    }
-//}
+package webserver;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import webserver.httpRequest.HttpRequestBody;
+import webserver.httpRequest.HttpRequestHeader;
+import webserver.httpRequest.HttpStartLine;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+
+public class HttpRequestFactory {
+    private static final Logger log = LoggerFactory.getLogger(HttpRequestFactory.class);
+    private static final String TAG = "HttpRequestFactory";
+
+    public HttpRequest getHttpRequest(InputStream in) {
+        BufferedReader br = null;
+        try {
+            br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
+        } catch (UnsupportedEncodingException e) {
+            log.error("{} 클래스 에러 발생", TAG);
+        }
+
+        HttpStartLine httpStartLine = HttpStartLine.of(br);
+        HttpRequestHeader httpRequestHeader = HttpRequestHeader.of(br);
+
+        HttpRequestBody httpRequestBody = null;
+        if (httpStartLine.isGet()) {
+            httpRequestBody = HttpRequestBody.Empty();
+        }
+
+        if (httpStartLine.isPost()) {
+            httpRequestBody = HttpRequestBody.of(br, httpRequestHeader.getContentLength());
+        }
+
+        return new HttpRequest(httpStartLine, httpRequestHeader, httpRequestBody);
+    }
+
+
+}
