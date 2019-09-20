d323e6094fdc5d5f3581f9eb14015d805467fef6
school0bhy <house1217@ajou.ac.kr>
2019-09-20 17:33:37 +0900

    feat: URL 에러 처리

--- src/main/java/http/common/URL.java
+++ src/main/java/http/common/URL.java
@@ -1,5 +1,8 @@
 package http.common;
 
+import http.common.exception.InvalidURLException;
+import utils.StringUtils;
+
 import java.util.Objects;
 
 public class URL {
@@ -7,15 +10,31 @@ public class URL {
     private final String queryString;
 
     public URL(String path, String queryString) {
+        checkValidatePath(path);
+        checkEmptyUrl(queryString);
         this.path = path;
         this.queryString = queryString;
     }
 
+    //todo: path 검증 로직 추가해서 생성_실패 테스트 케이스에서 확인
+    private void checkValidatePath(String path) {
+        if (StringUtils.isEmpty(path)) {
+            throw new InvalidURLException();
+        }
+    }
+
     public static URL of(String url) {
+        checkEmptyUrl(url);
         int index = getIndex(url);
         String path = url.substring(0, index);
         String queryString = getQueryString(url, index + 1);
         return new URL(path, queryString);
+    }
+
+    private static void checkEmptyUrl(String url) {
+        if (StringUtils.isEmpty(url)) {
+            throw new InvalidURLException();
+        }
     }
 
     private static String getQueryString(String url, int index) {
--- src/main/java/utils/StringUtils.java
+++ src/main/java/utils/StringUtils.java
@@ -5,7 +5,7 @@ public class StringUtils {
     public static final String WHITE_SPACE = " ";
 
     public static String[] split(String str, String delimiter) {
-        if (str == null || WHITE_SPACE.equals(str)) {
+        if (isNull(str) || WHITE_SPACE.equals(str)) {
             return null;
         }
 
@@ -13,10 +13,14 @@ public class StringUtils {
     }
 
     public static boolean isEmpty(String str) {
-        return str == null || BLANK.equals(str);
+        return isNull(str) || BLANK.equals(str);
     }
 
     public static boolean isNotEmpty(String str) {
         return !isEmpty(str);
+    }
+
+    public static boolean isNull(String str) {
+        return str == null;
     }
 }
