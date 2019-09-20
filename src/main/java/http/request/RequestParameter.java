d8cf7c67d7890961999c9611ff4c2c525658c853
seokhyeonchoi <lkamna12@gmail.com>
2019-09-20 15:40:09 +0900

    feat: RequestParameter 에러 상황 처리

--- src/main/java/http/request/RequestParameter.java
+++ src/main/java/http/request/RequestParameter.java
@@ -1,18 +1,29 @@
 package http.request;
 
-import org.springframework.util.StringUtils;
+import http.request.exception.InvalidQueryStringException;
+import http.request.exception.ParameterNotFoundException;
+import utils.StringUtils;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
 
 public class RequestParameter {
     private final Map<String, String> requestParameters = new HashMap<>();
 
     public RequestParameter(String queryString) {
+        checkValidQueryString(queryString);
         parse(queryString);
+    }
+
+    private void checkValidQueryString(String queryString) {
+        if (queryString == null) {
+            throw new InvalidQueryStringException();
+        }
     }
 
     private void parse(String queryString) {
@@ -36,6 +47,20 @@ public class RequestParameter {
     }
 
     public String getParameter(String key) {
-        return requestParameters.get(key);
+        return Optional.ofNullable(requestParameters.get(key))
+                .orElseThrow(ParameterNotFoundException::new);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        RequestParameter that = (RequestParameter) o;
+        return Objects.equals(requestParameters, that.requestParameters);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(requestParameters);
     }
 }
