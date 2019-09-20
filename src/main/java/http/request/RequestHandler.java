a2a1aec29cbf1eff34b916e824acc8d37b039842
choihz <choixhz@gmail.com>
2019-09-20 17:24:34 +0900

    feat: create RequestHandler

---
+++ src/main/java/http/request/RequestHandler.java
@@ -0,0 +1,50 @@
+package http.request;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import utils.IOUtils;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class RequestHandler {
+    private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
+
+    private final BufferedReader br;
+
+    public RequestHandler(BufferedReader br) {
+        this.br = br;
+    }
+
+    public HttpRequest create() throws IOException {
+        RequestStartLine requestStartLine = createRequestStartLine();
+        RequestHeader requestHeader = createRequestHeader();
+        RequestBody requestBody = createRequestBody(requestHeader);
+
+        return HttpRequest.of(requestStartLine, requestHeader, requestBody);
+    }
+
+    private RequestBody createRequestBody(RequestHeader requestHeader) throws IOException {
+        String body = "";
+        if (requestHeader.getHeader("Content-Length") != null) {
+            body = IOUtils.readData(br, Integer.parseInt(requestHeader.getHeader("Content-Length")));
+        }
+        return RequestBody.of(body);
+    }
+
+    private RequestHeader createRequestHeader() throws IOException {
+        List<String> header = new ArrayList<>();
+        String line;
+        while (!(line = br.readLine()).equals("")) {
+            header.add(line);
+        }
+        return RequestHeader.of(header);
+    }
+
+    private RequestStartLine createRequestStartLine() throws IOException {
+        String startLine = br.readLine();
+        return RequestStartLine.of(startLine);
+    }
+}
