96e728943663641a2b92bae5ba5d56591cffd2c9
mrkwon <erased11@gmail.com>
2019-09-22 00:25:10 +0900

    test: 테스트를 리팩토링한다.

--- src/test/java/http/HttpRequestHeaderTest.java
+++ src/test/java/http/HttpRequestHeaderTest.java
@@ -1,26 +1,43 @@
 package http;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;
 import java.util.Map;
 
+import http.excption.NotFoundHeaderException;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class HttpRequestHeaderTest {
 
-    @DisplayName("성공적으로 HttpRequestHeader 를 생성한다.")
-    @Test
-    void constructHttpRequestHeader() {
+    private HttpRequestHeader httpRequestHeader;
+
+    @BeforeEach
+    void setUp() {
         Map<String, String> headers = new HashMap<>();
+
         headers.put("Host", "localhost:8080");
         headers.put("Connection", "keep-alive");
         headers.put("Accept", "*/*");
 
-        HttpRequestHeader httpRequestHeader = new HttpRequestHeader(headers);
+        httpRequestHeader = new HttpRequestHeader(headers);
+    }
+
+    @DisplayName("성공적으로 HttpRequestHeader 를 생성한다.")
+    @Test
+    void constructHttpRequestHeader() {
         assertEquals(httpRequestHeader.get("Host"), "localhost:8080");
         assertEquals(httpRequestHeader.get("Connection"), "keep-alive");
         assertEquals(httpRequestHeader.get("Accept"), "*/*");
+    }
+
+    @DisplayName("존재하지 않는 header 값을 찾을 경우 에러가 발생한다.")
+    @Test
+    void name() {
+        assertThrows(NotFoundHeaderException.class, () -> httpRequestHeader.get("HELLO-MOTO"));
     }
 }
