fb16ba0a34fa3ff7bc1d7ea25574a729476ca1ed
김강민 <kangminals5610@gmail.com>
2019-09-21 16:22:58 +0900

    [refactor] update Readme.md

--- src/main/java/factory/UserFactory.java
+++ src/main/java/factory/UserFactory.java
@@ -1,9 +1,14 @@
 package factory;
 
 import model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.HashMap;
 
 public class UserFactory {
+    private static final Logger logger = LoggerFactory.getLogger(UserFactory.class);
+
     public static User of(String[] signUpData) {
         HashMap<String, String> signUpDataSet = new HashMap<>();
         signUpDataSet.put("userId", signUpData[0]);
@@ -12,7 +17,7 @@ public class UserFactory {
         signUpDataSet.put("email", signUpData[3]);
 
         for (String key : signUpDataSet.keySet()) {
-
+            logger.debug(signUpDataSet.get(key));
         }
 
         return new User(signUpDataSet.get("userId"),signUpDataSet.get("password"),signUpDataSet.get("name"), signUpDataSet.get("email"));
