12b2272a423a8a3e9fc0220627423302dfdb974e
DaejunBae <dpudpu11@gmail.com>
2019-09-21 12:52:40 +0900

    refactor: WebServer, Application 분리

--- src/main/java/webserver/WebServer.java
+++ src/main/java/webserver/WebServer.java
@@ -1,33 +1,37 @@
 package webserver;
 
+import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class WebServer {
+public class WebServer implements Runnable {
     private static final Logger logger = LoggerFactory.getLogger(WebServer.class);
     private static final int DEFAULT_PORT = 8080;
 
-    public static void main(String args[]) throws Exception {
-        int port = 0;
-        if (args == null || args.length == 0) {
-            port = DEFAULT_PORT;
-        } else {
-            port = Integer.parseInt(args[0]);
-        }
+    private final int port;
 
-        // 서버소켓을 생성한다. 웹서버는 기본적으로 8080번 포트를 사용한다.
+    public WebServer() {
+        this(DEFAULT_PORT);
+    }
+
+    public WebServer(int port) {
+        this.port = port;
+    }
+
+    public void run() {
         try (ServerSocket listenSocket = new ServerSocket(port)) {
             logger.info("Web Application Server started {} port.", port);
 
-            // 클라이언트가 연결될때까지 대기한다.
             Socket connection;
             while ((connection = listenSocket.accept()) != null) {
                 Thread thread = new Thread(new RequestHandler(connection));
                 thread.start();
             }
+        } catch (IOException e) {
+            throw new IllegalArgumentException();
         }
     }
 }
