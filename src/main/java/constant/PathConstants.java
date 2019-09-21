e8dbff9bc95775a1d27943a322d3f79fa85167b5
wbluke <pwb0127@naver.com>
2019-09-22 00:18:05 +0900

    fix: 잘못된 요청에 대한 NPE 문제를 error.html로 연결하여 수정

---
+++ src/main/java/constant/PathConstants.java
@@ -0,0 +1,5 @@
+package constant;
+
+public class PathConstants {
+    public static final String ERROR_PAGE_PATH = "./templates/error.html";
+}
--- src/main/java/utils/FileIoUtils.java
+++ src/main/java/utils/FileIoUtils.java
@@ -2,13 +2,26 @@ package utils;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+import static constant.PathConstants.ERROR_PAGE_PATH;
+
 public class FileIoUtils {
+
     public static byte[] loadFileFromClasspath(String filePath) throws IOException, URISyntaxException {
-        Path path = Paths.get(FileIoUtils.class.getClassLoader().getResource(filePath).toURI());
+        URL resource = getResource(filePath);
+        if (resource == null) {
+            resource = getResource(ERROR_PAGE_PATH);
+        }
+
+        Path path = Paths.get(resource.toURI());
         return Files.readAllBytes(path);
+    }
+
+    private static URL getResource(final String filePath) {
+        return FileIoUtils.class.getClassLoader().getResource(filePath);
     }
 }
