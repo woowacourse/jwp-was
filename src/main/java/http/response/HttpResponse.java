a7300d6ceb7a529e0c308221c7045dab017c8f74
Seongmo Jeon <oeeen3@gmail.com>
2019-09-21 17:08:42 +0900

    feat: RequestHandler 에서 Response로 분리

--- src/main/java/http/response/HttpResponse.java
+++ src/main/java/http/response/HttpResponse.java
@@ -1,14 +1,16 @@
 package http.response;
 
+import http.MediaType;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+
 public class HttpResponse {
     private HttpStatusLine httpStatusLine;
     private HttpResponseHeader httpResponseHeader;
     private HttpResponseBody httpResponseBody;
 
-    public HttpResponse(HttpStatusLine httpStatusLine, HttpResponseHeader httpResponseHeader, HttpResponseBody httpResponseBody) {
-        this.httpStatusLine = httpStatusLine;
-        this.httpResponseHeader = httpResponseHeader;
-        this.httpResponseBody = httpResponseBody;
+    public HttpResponse() {
     }
 
     public HttpStatusLine getHttpStatusLine() {
@@ -21,5 +23,22 @@ public class HttpResponse {
 
     public HttpResponseBody getHttpResponseBody() {
         return httpResponseBody;
+    }
+
+    public void sendRedirect(String uri) {
+        this.httpStatusLine = new HttpStatusLine("HTTP/1.1 302 FOUND \r\n");
+        this.httpResponseHeader = new HttpResponseHeader(
+                "Content-Type: text/html;charset=utf-8\r\n"
+                        + "Location: /" + uri + "\r\n");
+    }
+
+    public void send200Ok(String uri) throws IOException, URISyntaxException {
+        String contentType = MediaType.getContentType(uri);
+
+        this.httpStatusLine = new HttpStatusLine("HTTP/1.1 200 OK \r\n");
+        this.httpResponseBody = new HttpResponseBody(uri);
+        this.httpResponseHeader = new HttpResponseHeader(
+                "Content-Type: " + contentType + ";charset=utf-8\r\n"
+                        + "Content-Length: " + httpResponseBody.getBodyLength());
     }
 }
