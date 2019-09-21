555b308d090a511c8c190fd2a6d1504a31261211
jinwook <jin942002@naver.com>
2019-09-22 08:52:33 +0900

    refactor: request header 첫 줄 log 추가

--- src/main/java/http/request/RequestFirstLine.java
+++ src/main/java/http/request/RequestFirstLine.java
@@ -1,14 +1,19 @@
 package http.request;
 
 import http.RequestMethod;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class RequestFirstLine {
+    private static final Logger log = LoggerFactory.getLogger(RequestFirstLine.class);
     private final RequestMethod requestMethod;
     private String path;
     private String query;
     private final String version;
 
     public RequestFirstLine(String firstLine) {
+        log.info("request header contents: {}", firstLine);
+
         String[] tokens = firstLine.split(" ");
         this.requestMethod = RequestMethod.valueOf(tokens[0]);
         urlParse(tokens[1]);
