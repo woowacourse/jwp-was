84c3353a28779dca8698627af50fee3b01397742
이두호 <ddu0422@naver.com>
2019-09-22 04:04:53 +0900

    feat: ExcutorService 를 이용한 쓰레드 풀 구현

--- src/main/java/webserver/WebServer.java
+++ src/main/java/webserver/WebServer.java
@@ -5,29 +5,33 @@ import org.slf4j.LoggerFactory;
 
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class WebServer {
     private static final Logger logger = LoggerFactory.getLogger(WebServer.class);
     private static final int DEFAULT_PORT = 8080;
+    private static final int THREADS_COUNT = 10;
 
-    public static void main(String args[]) throws Exception {
+    public static void main(String[] args) throws Exception {
+        ExecutorService executor = Executors.newFixedThreadPool(THREADS_COUNT);
         int port = 0;
+
         if (args == null || args.length == 0) {
             port = DEFAULT_PORT;
         } else {
             port = Integer.parseInt(args[0]);
         }
 
-        // 서버소켓을 생성한다. 웹서버는 기본적으로 8080번 포트를 사용한다.
         try (ServerSocket listenSocket = new ServerSocket(port)) {
             logger.info("Web Application Server started {} port.", port);
 
-            // 클라이언트가 연결될때까지 대기한다.
             Socket connection;
             while ((connection = listenSocket.accept()) != null) {
-                Thread thread = new Thread(new RequestHandler(connection));
-                thread.start();
+                executor.execute(new RequestHandler(connection));
             }
         }
+
+        executor.shutdown();
     }
 }
