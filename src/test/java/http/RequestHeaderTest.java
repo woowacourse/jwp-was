f8f8c2349b8b5f599c140c2e15f4a7fd00251a67
김성훈 <seonghun127@gmail.com>
2019-09-21 20:31:25 +0900

    test: 테스트 추가

--- src/test/java/http/RequestHeaderTest.java
+++ src/test/java/http/RequestHeaderTest.java
@@ -3,11 +3,13 @@ package http;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import webserver.exception.NotFoundHeaderException;
 
 import java.util.HashMap;
 import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class RequestHeaderTest {
     private RequestHeader requestHeader;
@@ -39,5 +41,12 @@ public class RequestHeaderTest {
         assertThat(requestHeader.get("host")).isEqualTo("localhost:8080");
         assertThat(requestHeader.get("connection")).isEqualTo("keep-alive");
         assertThat(requestHeader.get("accept")).isEqualTo("*/*");
+    }
+
+    @Test
+    @DisplayName("Header 정보에서 없는 key값으로 value를 가져올 경우 예외 처리")
+    public void getException() {
+        String notExistKey = "none";
+        assertThrows(NotFoundHeaderException.class, () -> requestHeader.get(notExistKey));
     }
 }
