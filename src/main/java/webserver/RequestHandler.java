2d2c0390efcfd9a272073ac8c564e4dfaee07132
김강민 <kangminals5610@gmail.com>
2019-09-22 20:17:42 +0900

    [refactor] change RequestHandler

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,16 +1,18 @@
 package webserver;
 
+import webserver.controller.AbstractController;
+import webserver.controller.response.HttpResponse;
 import db.DataBase;
 import factory.UserFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import utils.FileIoUtils;
+import utils.IOUtils;
+import webserver.controller.request.*;
 
-import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
 import java.net.URISyntaxException;
@@ -29,48 +31,13 @@ public class RequestHandler implements Runnable {
         logger.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
             connection.getPort());
 
-        try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
-
-            String line = br.readLine();
-            String[] splitedLine = line.split(" ");
-
-            if (splitedLine[0].equals("GET")) {
-                byte[] bytes = FileIoUtils.loadFileFromClasspath("./templates" + splitedLine[1]);
-                DataOutputStream dos = new DataOutputStream(out);
-                if (splitedLine[1].contains(".html")) {
-                    response200Header(dos, bytes.length, "html");
-                }
-                responseBody(dos, bytes);
-
-                if (splitedLine[1].contains("/user/create?")) {
-                    String[] signUpUrl = splitedLine[1].split("\\?");
-                    DataBase.addUser(UserFactory.of(URLDecoder.decode(signUpUrl[1], "UTF-8").split("&")));
-                }
-            }
-        } catch (IOException | URISyntaxException e) {
-            logger.error(e.getMessage());
-        }
-    }
-
-
-    private void response200Header(DataOutputStream dos, int lengthOfBodyContent, String contentsType) {
-        try {
-            dos.writeBytes("HTTP/1.1 200 OK \r\n");
-            dos.writeBytes("Content-Type: text/" + contentsType + ";charset=utf-8\r\n");
-            dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
-            dos.writeBytes("\r\n");
+        try (InputStream inputStream = connection.getInputStream(); OutputStream outputStream = connection.getOutputStream()) {
+            HttpRequest httpRequest = new HttpRequest(inputStream);
+            HttpResponse httpResponse = new HttpResponse(outputStream);
+            RequestMapper requestMapper = new RequestMapper();
+            requestMapper.executeMapping(httpRequest, httpResponse);
         } catch (IOException e) {
-            logger.error(e.getMessage());
-        }
-    }
-
-    private void responseBody(DataOutputStream dos, byte[] body) {
-        try {
-            dos.write(body, 0, body.length);
-            dos.flush();
-        } catch (IOException e) {
-            logger.error(e.getMessage());
+            e.printStackTrace();
         }
     }
 }
