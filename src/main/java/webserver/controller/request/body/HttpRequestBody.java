ebb8733b7497204b248bec7a9042d6ac8e1c4c63
김강민 <kangminals5610@gmail.com>
2019-09-22 20:21:11 +0900

    [feat] impliment RequestMapper, HttpRequestBody, HttpResponse

---
+++ src/main/java/webserver/controller/request/body/HttpRequestBody.java
@@ -0,0 +1,28 @@
+package webserver.controller.request.body;
+
+import org.checkerframework.checker.units.qual.K;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+public class HttpRequestBody {
+    private final HashMap <String, String> bodyDataSet;
+
+    public HttpRequestBody(String requestParams) throws UnsupportedEncodingException {
+        bodyDataSet = new HashMap<>();
+        URLDecoder.decode(requestParams, "UTF-8");
+        String[] splitedParams = requestParams.split("&");
+
+        for(String splitedParam : splitedParams) {
+            String[] queryParam = splitedParam.split("=");
+            bodyDataSet.put(queryParam[0], queryParam[1]);
+        }
+    }
+
+    public Map<String, String> getBodyDataSet() {
+        return Collections.unmodifiableMap(bodyDataSet);
+    }
+}
---
+++ src/main/java/webserver/controller/response/HttpResponse.java
@@ -0,0 +1,53 @@
+package webserver.controller.response;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import webserver.controller.request.HttpRequest;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.URISyntaxException;
+
+public class HttpResponse {
+    private static final Logger logger = LoggerFactory.getLogger(HttpResponse.class);
+    private final DataOutputStream dataOutputStream;
+
+    public HttpResponse(OutputStream outputStream) {
+        this.dataOutputStream = new DataOutputStream(outputStream);
+    }
+
+    public void response200Header(int lengthOfBodyContent, ContentType contentType) {
+        try {
+            dataOutputStream.writeBytes("HTTP/1.1 200 OK \r\n");
+            dataOutputStream.writeBytes("Content-Type: text/"  + contentType + ";charset=utf-8\r\n");
+            dataOutputStream.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
+            dataOutputStream.writeBytes("\r\n");
+        } catch (IOException e) {
+            logger.error(e.getMessage());
+        }
+    }
+
+    public void responseBody(byte[] body) {
+        try {
+            dataOutputStream.write(body, 0, body.length);
+            dataOutputStream.flush();
+        } catch (IOException e) {
+            logger.error(e.getMessage());
+        }
+    }
+
+    public void redirect302Found(String redirectUrl) {
+        logger.debug("redirect302Found >>>");
+        try {
+            dataOutputStream.writeBytes("HTTP/1.1 302 FOUND \r\n");
+            dataOutputStream.writeBytes("Location: " + redirectUrl + "\r\n");
+            dataOutputStream.writeBytes("Connection: close \r\n");
+            dataOutputStream.writeBytes("\r\n");
+            dataOutputStream.flush();
+        } catch (IOException e) {
+            logger.error(e.getMessage());
+        }
+    }
+}
