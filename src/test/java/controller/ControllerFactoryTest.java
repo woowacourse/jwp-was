dc55e9d59c10e9b7453ad0a8ed5cd1a49175feee
KIMJUNGMIN <kimjungmin.developer@gmail.com>
2019-09-23 01:35:40 +0900

    test: add ControllerFactoryTest

---
+++ src/test/java/controller/ControllerFactoryTest.java
@@ -0,0 +1,57 @@
+package controller;
+
+import controller.creator.GetFileControllerCreator;
+import http.request.Request;
+import http.request.RequestInformation;
+import http.request.RequestMethod;
+import http.request.RequestUrl;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ControllerFactoryTest {
+
+    @Test
+    @DisplayName("get request 넣었을 시 원하는 Controller가 나오는지 테스트")
+    void createGetFileController() {
+        RequestMethod method = RequestMethod.GET;
+        RequestUrl url = RequestUrl.from("/index.html");
+        Map<String, String> information = new HashMap<>();
+        information.put("Request-Line:", "GET /index.html HTTP/1.1");
+        information.put("Host:", "localhost:8080");
+        information.put("Connection:", "keep-alive");
+        RequestInformation requestInformation = new RequestInformation(information);
+
+        Request request = new Request(method, url, requestInformation);
+
+        ControllerFactory factory = new ControllerFactory();
+        assertThat(factory.createController(request).getClass()).isEqualTo(GetFileController.class);
+    }
+
+    @Test
+    @DisplayName("POST /user/create request를 보낼시 원하는 Controller가 나오는지 테스트")
+    void createPostUserController() {
+        RequestMethod method = RequestMethod.POST;
+        RequestUrl url = RequestUrl.from("/user/create");
+        Map<String, String> information = new HashMap<>();
+        information.put("Request-Line:", "POST /user/create HTTP/1.1");
+        information.put("Host:", "localhost:8080");
+        information.put("Connection:", "keep-alive");
+        information.put("Content-Length:", "59");
+        information.put("Content-Type:", "application/x-www-form-urlencoded");
+        information.put("Accept:", "*/*");
+        information.put("Query-Parameters", "userId=javajigi&password=password&name=%EB%B0%95%EC%9E%AC%EC%84%B1&email=javajigi%40slipp.net");
+
+
+        RequestInformation requestInformation = new RequestInformation(information);
+
+        Request request = new Request(method, url, requestInformation);
+
+        ControllerFactory factory = new ControllerFactory();
+        assertThat(factory.createController(request).getClass()).isEqualTo(PostUserController.class);
+    }
+}
