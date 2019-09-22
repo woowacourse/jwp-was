d8543eb1b22752e572cd90db092637d1bcdb70ed
backlo <hjun4512@naver.com>
2019-09-22 16:45:50 +0900

    feat: webServer Executor Service 구현

--- src/main/java/webserver/WebServer.java
+++ src/main/java/webserver/WebServer.java
@@ -2,6 +2,8 @@ package webserver;
 
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,6 +11,7 @@ import org.slf4j.LoggerFactory;
 public class WebServer {
     private static final Logger logger = LoggerFactory.getLogger(WebServer.class);
     private static final int DEFAULT_PORT = 8080;
+    private static final ExecutorService es = Executors.newFixedThreadPool(100);
 
     public static void main(String args[]) throws Exception {
         int port = 0;
@@ -25,8 +28,7 @@ public class WebServer {
             // 클라이언트가 연결될때까지 대기한다.
             Socket connection;
             while ((connection = listenSocket.accept()) != null) {
-                Thread thread = new Thread(new RequestHandler(connection));
-                thread.start();
+                es.execute(new RequestHandler(connection));
             }
         }
     }
