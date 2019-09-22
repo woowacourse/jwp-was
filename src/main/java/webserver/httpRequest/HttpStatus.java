a42e82612117ab1d17f2e8c261a5ab3e8dfc9c41
starkim06 <starkim06@naver.com>
2019-09-22 20:48:55 +0900

    feat: 404, 405 상태코드 추가

--- src/main/java/webserver/httpRequest/HttpStatus.java
+++ src/main/java/webserver/httpRequest/HttpStatus.java
@@ -2,7 +2,9 @@ package webserver.httpRequest;
 
 public enum HttpStatus {
     OK(200, "OK"),
-    FOUND(302, "Found");
+    FOUND(302, "Found"),
+    NOT_FOUND(404, "Not Found"),
+    METHOD_NOT_ALLOWED(405, "Method Not Allowed");
 
     private final int code;
     private final String message;
