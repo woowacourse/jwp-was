3cc608282ed33ae5e89f41a490e94a549c9271cc
yunheehyeon <ok94life@daum.net>
2019-09-22 14:12:56 +0900

    refactor : ControllerContainer 책임 분리

--- src/main/java/webserver/ControllerContainer.java
+++ src/main/java/webserver/ControllerContainer.java
@@ -3,12 +3,7 @@ package webserver;
 import com.google.common.collect.Maps;
 import controller.AbstractController;
 import controller.UserCreateController;
-import http.request.HttpRequest;
-import http.response.HttpResponse;
-import http.response.view.DefaultView;
 
-import java.io.IOException;
-import java.net.URISyntaxException;
 import java.util.Map;
 import java.util.Optional;
 
@@ -19,16 +14,7 @@ public class ControllerContainer {
         controllers.put("/user/create", new UserCreateController());
     }
 
-    private static Optional<AbstractController> findController(String path) {
+    public static Optional<AbstractController> findController(String path) {
         return Optional.ofNullable(controllers.get(path));
-    }
-
-    public static void service(HttpRequest httpRequest, HttpResponse httpResponse) throws IOException, URISyntaxException {
-        Optional<AbstractController> maybeController = findController(httpRequest.getPath());
-        if (maybeController.isPresent()) {
-            maybeController.get().service(httpRequest, httpResponse);
-            return;
-        }
-        httpResponse.render(new DefaultView(httpRequest.getPath()));
     }
 }
