c96e9ba98bae6b78f3c4544c6ae3590262a7210a
yeji <yjll1019@naver.com>
2019-09-22 15:48:06 +0900

    refactor: rename response class(Response -> HttpResponse)

---
+++ src/main/java/model/HttpResponse.java
@@ -0,0 +1,43 @@
+package model;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import webserver.RequestHandler;
+
+public class HttpResponse {
+	private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
+	private final Map<String, String> header;
+
+	public HttpResponse(final Map<String, String> header) {
+		this.header = header;
+	}
+
+	public void sendRedirect(DataOutputStream dos) throws IOException {
+		dos.writeBytes(header.get("Http") + " " + header.get("Code") + " "
+				+ header.get("Description") + "\r\n");
+		dos.writeBytes("Location: " + header.get("Location") + "\r\n");
+		dos.writeBytes("\r\n");
+		dos.flush();
+	}
+
+	public void forward(byte[] body, DataOutputStream dos) throws IOException {
+		for (String key : header.keySet()) {
+			dos.writeBytes(key + ": " + header.get(key));
+		}
+		dos.writeBytes("\r\n");
+		sendResponseBody(body, dos);
+	}
+
+	private void sendResponseBody(byte[] body, DataOutputStream dos) {
+		try {
+			dos.write(body, 0, body.length);
+			dos.flush();
+		} catch (IOException e) {
+			logger.error(e.getMessage());
+		}
+	}
+}
