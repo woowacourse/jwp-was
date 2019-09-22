4d1cd5e9a3d544ae2ffae48fe2dd0f81ffa9347c
starkim06 <starkim06@naver.com>
2019-09-22 20:49:31 +0900

    refactor: ResourceViewProcessor로 정적파일 처리 통합

--- src/main/java/webserver/HttpResponseProcessor.java
+++
@@ -1,7 +0,0 @@
-package webserver;
-
-import java.io.DataOutputStream;
-
-public interface HttpResponseProcessor {
-    void process(DataOutputStream dos, String resolve, HttpResponse httpResponse);
-}
