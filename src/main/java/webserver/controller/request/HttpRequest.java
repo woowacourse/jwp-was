2cf4f20454aa25b8034de97c2beaad306afab226
김강민 <kangminals5610@gmail.com>
2019-09-22 20:19:21 +0900

    [feat] impliment HttpRequest

---
+++ src/main/java/webserver/controller/request/HttpRequest.java
@@ -0,0 +1,39 @@
+package webserver.controller.request;
+
+import webserver.controller.request.header.HttpBeginningHeader;
+import webserver.controller.request.header.HttpHeaderFields;
+import webserver.controller.response.ContentType;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URISyntaxException;
+
+public class HttpRequest {
+    private final BufferedReader bufferedReader;
+    private final HttpBeginningHeader httpBeginningHeader;
+    private final HttpHeaderFields httpHeaderFields;
+
+    public HttpRequest(final InputStream inputStream) throws IOException {
+        this.bufferedReader = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"));
+        this.httpBeginningHeader = new HttpBeginningHeader(bufferedReader);
+        this.httpHeaderFields = new HttpHeaderFields(bufferedReader);
+    }
+
+    public byte[] getResponseBody(ContentType contentType) throws IOException, URISyntaxException {
+        return httpBeginningHeader.getResponseBody(contentType);
+    }
+
+    public ContentType getContentType() {
+        return httpBeginningHeader.getContentType();
+    }
+
+    public String readData() throws IOException {
+        return httpHeaderFields.readData(bufferedReader);
+    }
+
+    public String getPath() {
+        return httpBeginningHeader.getUrl();
+    }
+}
