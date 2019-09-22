4cd396458c7be38b4d322c5ed8882ad662964bcb
김성훈 <seonghun127@gmail.com>
2019-09-23 01:43:30 +0900

    feat: 응답 작성 클래스 분리

---
+++ src/main/java/webserver/support/ResponseWriter.java
@@ -0,0 +1,51 @@
+package webserver.support;
+
+import http.response.Response;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+public class ResponseWriter {
+    private static final Logger logger = LoggerFactory.getLogger(ResponseWriter.class);
+
+    public static void write(DataOutputStream dos, Response response) {
+        if (response.isOk()) {
+            writeHeader(dos, response200Header(response));
+            writeBody(dos, response);
+            return;
+        }
+        writeHeader(dos, response302Header(response));
+    }
+
+    private static String response200Header(Response response) {
+        return response.getHttpVersion() + " " + response.getStatusCode() + " " + response.getReasonPhrase() + "\r\n" +
+                "Content-Type: " + response.getContentType() + "\r\n" +
+                "Content-Length: " + response.getContentLength() + "\r\n" +
+                "\r\n";
+    }
+
+    private static String response302Header(Response response) {
+        return response.getHttpVersion() + " " + response.getStatusCode() + " " + response.getReasonPhrase() + "\r\n" +
+                "Location: " + response.getLocation() + "\r\n" +
+                "\r\n";
+    }
+
+    private static void writeHeader(DataOutputStream dos, String message) {
+        try {
+            dos.writeBytes(message);
+        } catch (IOException e) {
+            logger.error(e.getMessage());
+        }
+    }
+
+    private static void writeBody(DataOutputStream dos, Response response) {
+        try {
+            dos.write(response.getContentBody(), 0, response.getContentLength());
+            dos.flush();
+        } catch (IOException e) {
+            logger.error(e.getMessage());
+        }
+    }
+}
