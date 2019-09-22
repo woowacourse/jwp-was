60b3b285a536f8f89046aeab49317af77469e9b2
김강민 <kangminals5610@gmail.com>
2019-09-22 20:17:00 +0900

    [refactor] change UserFactory

--- src/main/java/factory/UserFactory.java
+++ src/main/java/factory/UserFactory.java
@@ -5,21 +5,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class UserFactory {
     private static final Logger logger = LoggerFactory.getLogger(UserFactory.class);
 
-    public static User of(String[] signUpData) {
-        HashMap<String, String> signUpDataSet = new HashMap<>();
-        signUpDataSet.put("userId", signUpData[0]);
-        signUpDataSet.put("password", signUpData[1]);
-        signUpDataSet.put("name", signUpData[2]);
-        signUpDataSet.put("email", signUpData[3]);
-
-        for (String key : signUpDataSet.keySet()) {
-            logger.debug(signUpDataSet.get(key));
-        }
-
+    public static User of(Map<String, String> signUpDataSet) {
         return new User(signUpDataSet.get("userId"),signUpDataSet.get("password"),signUpDataSet.get("name"), signUpDataSet.get("email"));
     }
 }
