cd070b115d7e954285c1b4c283507c98d178153e
DaejunBae <dpudpu11@gmail.com>
2019-09-23 15:21:36 +0900

    refactor: MimeType enum으로 변경

--- src/main/java/webserver/http/MimeType.java
+++ src/main/java/webserver/http/MimeType.java
@@ -1,37 +1,50 @@
 package webserver.http;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
-public class MimeType {
-    public final static Map<String, String> MAP = new HashMap<>();
+public enum MimeType {
+
+    CSS("css", "text/css;charset=utf-8"),
+    JS("js", "text/javascript;charset=UTF-8:"),
+    HTM("htm", "text/html;charset=utf-8"),
+    HTML("html", "text/html;charset=utf-8"),
+    GIF("gif", "image/gif"),
+    PNG("png", "image/png"),
+    JPG("jpg", "image/jpeg"),
+    JPEG("jpeg", "image/jpeg"),
+    BMP("bmp", "image/bmp"),
+    WEPB("webp", "image/webp"),
+    TTF("ttf", "font/ttf"),
+    WOFF("woff", "font/woff"),
+    ICO("ico", "image/vnd.microsoft.icon");
+
+    public final static Map<String, String> mimeTypes = new HashMap<>();
 
     static {
-        MAP.put("css", "text/css;charset=utf-8");
-        MAP.put("js", "text/javascript;charset=UTF-8:");
-        MAP.put("htm", "text/html;charset=utf-8");
-        MAP.put("html", "text/html;charset=utf-8");
-        MAP.put("gif", "image/gif");
-        MAP.put("png", "image/png");
-        MAP.put("jpg", "image/jpeg");
-        MAP.put("jpeg", "image/jpeg");
-        MAP.put("bmp", "image/bmp");
-        MAP.put("webp", "image/webp");
-        MAP.put("ttf", "font/ttf");
-        MAP.put("woff", "font/woff");
-        MAP.put("ico", "image/vnd.microsoft.icon");
+        Arrays.stream(values())
+                .forEach(mimeType -> mimeTypes.put(mimeType.extensions, mimeType.type));
+    }
+
+    private final String extensions;
+    private final String type;
+
+    MimeType(final String extensions, final String type) {
+        this.extensions = extensions;
+        this.type = type;
     }
 
     public static String getType(String extension) {
-        return MAP.get(extension);
+        return mimeTypes.get(extension);
     }
 
     public static boolean contains(final String extension) {
-        return MAP.containsKey(extension);
+        return mimeTypes.containsKey(extension);
     }
 
     public static Set<String> getExtensions() {
-        return MAP.keySet();
+        return mimeTypes.keySet();
     }
 }
