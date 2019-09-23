abc01240b5e77b71c36bdc21b37887c2069a83e4
gch01410 <gch01410@naver.com>
2019-09-23 16:07:40 +0900

    refactor: httpRequest 를 객체로 분리

---
+++ src/main/java/webserver/request/HttpRequest.java
@@ -0,0 +1,62 @@
+package webserver.request;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class HttpRequest {
+    private final RequestLine requestLine;
+    private final RequestHeader requestHeader;
+    private final RequestBody requestBody;
+
+    private HttpRequest(RequestLine requestLine, RequestHeader requestHeader, RequestBody requestBody) {
+        this.requestLine = requestLine;
+        this.requestHeader = requestHeader;
+        this.requestBody = requestBody;
+    }
+
+    public static HttpRequest of(InputStream in) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(in));
+        RequestLine requestLine = RequestLine.of(br);
+        RequestHeader requestHeader = RequestHeader.of(br);
+        RequestBody requestBody = RequestBody.of(br, requestHeader.getContentLength());
+        return new HttpRequest(requestLine, requestHeader, requestBody);
+    }
+
+    public String getMethod() {
+        return requestLine.getMethod();
+    }
+
+    public RequestLine getRequestLine() {
+        return requestLine;
+    }
+
+    public RequestHeader getRequestHeader() {
+        return requestHeader;
+    }
+
+    public RequestBody getRequestBody() {
+        return requestBody;
+    }
+
+    public boolean isGet() {
+        return requestLine.isGet();
+    }
+
+    public boolean hasParameters() {
+        return requestLine.hasParameters();
+    }
+
+    public boolean isPost() {
+        return requestLine.isPost();
+    }
+
+    public String getHttpVersion() {
+        return requestLine.getHttpVersion();
+    }
+
+    public String getSource() {
+        return requestLine.getPath();
+    }
+}
