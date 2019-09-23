596d06f6729e7ef1c51523d04e5e5906c6dc2fa8
choihz <choixhz@gmail.com>
2019-09-23 17:32:55 +0900

    refactor: handle exception

--- src/main/java/http/controller/StaticFileController.java
+++ src/main/java/http/controller/StaticFileController.java
@@ -6,7 +6,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import utils.FileIoUtils;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.URISyntaxException;
 
@@ -23,11 +22,9 @@ public class StaticFileController extends AbstractController {
         try {
             byte[] body = FileIoUtils.loadFileFromClasspath(dir + httpRequest.getPath());
             httpResponse.setResponseBody(body, httpRequest.getPath());
-        } catch (FileNotFoundException e) {
-            logger.debug(e.getMessage());
-            httpResponse.sendNotFound();
         } catch (URISyntaxException | IOException e) {
             logger.debug(e.getMessage());
+            httpResponse.sendNotFound();
         }
     }
 }
