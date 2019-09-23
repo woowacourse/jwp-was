4dab0b67c6bbd2335f14784cf11b570b16b22e42
gch01410 <gch01410@naver.com>
2019-09-23 16:06:33 +0900

    refactor: httpRequestHeader 를 객체로 분리

---
+++ src/main/java/webserver/request/RequestHeader.java
@@ -0,0 +1,41 @@
+package webserver.request;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class RequestHeader {
+    private final Map<String, String> headerAttributes;
+
+    private RequestHeader(Map<String, String> headerAttributes) {
+        this.headerAttributes = headerAttributes;
+    }
+
+    public static RequestHeader of(BufferedReader br) throws IOException {
+        Map<String, String> headerAttributes = new HashMap<>();
+        String line = br.readLine();
+
+        while (!"".equals(line) && line != null) {
+            String[] attribute = line.split(": ");
+            headerAttributes.put(attribute[0], attribute[1]);
+            line = br.readLine();
+        }
+        return new RequestHeader(headerAttributes);
+    }
+
+    public String get(String attributeName) {
+        return headerAttributes.get(attributeName);
+    }
+
+    public int getContentLength() {
+        if (headerAttributes.containsKey("Content-Length")) {
+            return Integer.parseInt(headerAttributes.get("Content-Length"));
+        }
+        return 0;
+    }
+
+    public String getHost() {
+        return headerAttributes.get("Host");
+    }
+}
