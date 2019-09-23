f7a68e5c81683dfb511ab98784cf2488f7308516
gch01410 <gch01410@naver.com>
2019-09-23 16:09:33 +0900

    refactor: 응답 상태라인을 객체로 분리

---
+++ src/main/java/webserver/response/ResponseStatusLine.java
@@ -0,0 +1,24 @@
+package webserver.response;
+
+import webserver.request.HttpRequest;
+
+public class ResponseStatusLine {
+    private final String httpVersion;
+    private final String statusCode;
+    private final String statusText;
+
+    private ResponseStatusLine(String httpVersion, String statusCode, String statusText) {
+        this.httpVersion = httpVersion;
+        this.statusCode = statusCode;
+        this.statusText = statusText;
+    }
+
+    public static ResponseStatusLine of(HttpRequest httpRequest, String statusCode, String statusText) {
+        String version = httpRequest.getHttpVersion();
+        return new ResponseStatusLine(version, statusCode, statusText);
+    }
+
+    public String response() {
+        return httpVersion + " " + statusCode + " " + statusText + "\r\n";
+    }
+}
