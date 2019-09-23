899cdca5bbe347526c08932330326794de4c3c6f
gch01410 <gch01410@naver.com>
2019-09-23 16:05:27 +0900

    refactor: HttpRequestLine 객체로 분리

---
+++ src/main/java/webserver/request/RequestLine.java
@@ -0,0 +1,86 @@
+package webserver.request;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class RequestLine {
+    private final String method;
+    private final String target;
+    private final String httpVersion;
+
+    private RequestLine(String method, String target, String httpVersion) {
+        this.method = method;
+        this.target = target;
+        this.httpVersion = httpVersion;
+    }
+
+    public static RequestLine of(BufferedReader br) throws IOException {
+        String requestLine = br.readLine();
+        String[] splitRequestLine = requestLine.split(" ");
+        String method = splitRequestLine[0];
+        String target = splitRequestLine[1];
+        String httpVersion = splitRequestLine[2];
+
+        return new RequestLine(method, target, httpVersion);
+    }
+
+    public boolean isGet() {
+        return "GET".equals(method);
+    }
+
+    public boolean isPost() {
+        return "POST".equals(method);
+    }
+
+    public boolean isPut() {
+        return "PUT".equals(method);
+    }
+
+    public boolean isDelete() {
+        return "DELETE".equals(method);
+    }
+
+    public boolean hasParameters() {
+        return target.split("\\?").length == 2;
+    }
+
+    public Map<String, String> getParameters() {
+        Map<String, String> parameters = new HashMap<>();
+        String[] split = target.split("\\?")[1].split("&");
+        for (String pair : split) {
+            String[] split1 = pair.split("=");
+            String key = split1[0];
+            String value = "";
+            if (split1.length == 2) {
+                value = split1[1];
+            }
+            parameters.put(key, value);
+        }
+        return parameters;
+    }
+
+    public String getPath() {
+        return target.split("\\?")[0];
+    }
+
+    public String getContentType() {
+        System.out.println("getPath() = " + getPath());
+        String[] split = getPath().split("\\.");
+        int length = split.length;
+        return split[length - 1];
+    }
+
+    public String getMethod() {
+        return method;
+    }
+
+    public String getTarget() {
+        return target;
+    }
+
+    public String getHttpVersion() {
+        return httpVersion;
+    }
+}
