e9be660ef9f4cc6c9bab975912feb55008fe1c10
aiden <cnk9109@gmail.com>
2019-09-23 15:25:28 +0900

    feat: create MethodNotAllowedException

---
+++ src/main/java/http/exception/MethodNotAllowedException.java
@@ -0,0 +1,23 @@
+package http.exception;
+
+public class MethodNotAllowedException extends RuntimeException {
+    public MethodNotAllowedException() {
+        super();
+    }
+
+    public MethodNotAllowedException(String message) {
+        super(message);
+    }
+
+    public MethodNotAllowedException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public MethodNotAllowedException(Throwable cause) {
+        super(cause);
+    }
+
+    protected MethodNotAllowedException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
