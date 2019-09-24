ad2ac2e2d4c6ae38225097266bf6a39a4aee9728
gch01410 <gch01410@naver.com>
2019-09-24 21:51:28 +0900

    feat: HttpResponseHeader의 속성을 추가하는 메서드 구현

--- src/main/java/webserver/response/ResponseHeader.java
+++ src/main/java/webserver/response/ResponseHeader.java
@@ -1,5 +1,6 @@
 package webserver.response;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -10,7 +11,15 @@ public class ResponseHeader {
         this.attributes = new HashMap<>();
     }
 
-    public String response(String attribute) {
-        return attribute + ": " + "text/html;" + " " + "charset=utf-8\r\n";
+    public boolean addAttribute(String key, String value) {
+        if (attributes.containsKey(key)) {
+            return false;
+        }
+        attributes.put(key, value);
+        return true;
+    }
+
+    public Map<String, String> getAttributes() {
+        return Collections.unmodifiableMap(attributes);
     }
 }
