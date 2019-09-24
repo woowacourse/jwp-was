570f4c7e43ad4e75fea2ec5a2b93448a2632f601
mrkwon <erased11@gmail.com>
2019-09-24 23:07:14 +0900

    feat: UserController 를 구현합니다.

---
+++ src/main/java/dev/luffy/controller/UserController.java
@@ -0,0 +1,33 @@
+package dev.luffy.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import dev.luffy.annotation.Controller;
+import dev.luffy.annotation.RequestMapping;
+import dev.luffy.http.request.HttpRequest;
+import dev.luffy.http.response.HttpResponse;
+import dev.luffy.model.User;
+
+@Controller
+public class UserController {
+
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+
+    @RequestMapping("/user/create")
+    public static void createUser(HttpRequest request, HttpResponse response) {
+
+        logger.debug("request : {} & response : {}", request, response);
+
+        User user = new User(
+                request.getBodyParameter("userId"),
+                request.getBodyParameter("password"),
+                request.getBodyParameter("name"),
+                request.getBodyParameter("email")
+        );
+
+        logger.debug("Generated User : {}", user);
+
+        response.send302("/index.html");
+    }
+}
