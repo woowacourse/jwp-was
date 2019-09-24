76194bcd53cae1d22754f3bcf2b374bb0929827c
hyunsoojung <jhs695@naver.com>
2019-09-24 20:27:53 +0900

    refactor: 상수 분리

--- src/main/java/http/request/factory/HttpRequestHeaderFactory.java
+++ src/main/java/http/request/factory/HttpRequestHeaderFactory.java
@@ -1,6 +1,5 @@
 package http.request.factory;
 
-import http.request.HttpRequest;
 import http.request.HttpRequestHeader;
 
 import java.util.HashMap;
@@ -8,12 +7,14 @@ import java.util.List;
 import java.util.Map;
 
 public class HttpRequestHeaderFactory {
+    private static final int KEY = 0;
+    private static final int VALUE = 1;
 
     public static HttpRequestHeader create(List<String> lines) {
         Map<String, String> fields = new HashMap<>();
         lines.forEach(line -> {
             String[] element = line.split(": ");
-            fields.put(element[0], element[1]);
+            fields.put(element[KEY], element[VALUE]);
         });
 
         return new HttpRequestHeader(fields);
