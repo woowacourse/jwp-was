158a6f935cb9ee112458cc4f44d0bbdd8c567522
starkim06 <starkim06@naver.com>
2019-09-24 14:48:10 +0900

    refactor: ViewProcessor에 파라미터로 HttpResponse 추가

--- src/main/java/webserver/ViewProcessor.java
+++ src/main/java/webserver/ViewProcessor.java
@@ -5,5 +5,5 @@ import java.io.DataOutputStream;
 public interface ViewProcessor {
     boolean isSupported(String viewName);
 
-    void process(DataOutputStream dos, String viewName);
+    void process(DataOutputStream dos, String viewName, HttpResponse httpResponse);
 }
