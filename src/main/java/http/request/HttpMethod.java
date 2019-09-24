5e0d0e7874c6736908810ebb1d120326c448ef5b
이두호 <ddu0422@naver.com>
2019-09-24 20:58:48 +0900

    refactor: HttpMethod 가 존재하는지 확인하는 메서드 추가

--- src/main/java/http/request/HttpMethod.java
+++ src/main/java/http/request/HttpMethod.java
@@ -1,5 +1,30 @@
 package http.request;
 
+import controller.exception.NotAllowedMethodException;
+
+import java.util.Arrays;
+
 public enum HttpMethod {
-    GET, POST, PUT, DELETE
+    GET("GET"),
+    POST("POST"),
+    PUT("PUT"),
+    DELETE("DELETE");
+
+    private final String method;
+
+    HttpMethod(final String method) {
+        this.method = method;
+    }
+
+    public static HttpMethod of(final String value) {
+        return Arrays.stream(HttpMethod.values())
+                .filter(httpMethod -> httpMethod.match(value))
+                .findFirst()
+                .orElseThrow(NotAllowedMethodException::new)
+                ;
+    }
+
+    private boolean match(final String value) {
+        return this.method.equals(value);
+    }
 }
