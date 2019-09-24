cc7e960abf93e59bda8e080136b881c3e9ba42f7
gch01410 <gch01410@naver.com>
2019-09-24 17:15:29 +0900

    refactor: http 요청라인에서 파라미터를 갖고 있도록 수정

--- src/main/java/webserver/request/RequestLine.java
+++ src/main/java/webserver/request/RequestLine.java
@@ -2,6 +2,7 @@ package webserver.request;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -9,11 +10,13 @@ public class RequestLine {
     private final String method;
     private final String target;
     private final String httpVersion;
+    private final Map<String, String> parameters;
 
-    private RequestLine(String method, String target, String httpVersion) {
+    private RequestLine(String method, String target, String httpVersion, Map<String, String> parameters) {
         this.method = method;
         this.target = target;
         this.httpVersion = httpVersion;
+        this.parameters = parameters;
     }
 
     public static RequestLine of(BufferedReader br) throws IOException {
@@ -23,7 +26,39 @@ public class RequestLine {
         String target = splitRequestLine[1];
         String httpVersion = splitRequestLine[2];
 
-        return new RequestLine(method, target, httpVersion);
+        return new RequestLine(method, target, httpVersion, getParameters(target));
+    }
+
+    private static Map<String, String> getParameters(String target) {
+        if (hasNotParameters(target)) {
+            return Collections.emptyMap();
+        }
+
+        Map<String, String> parameters = new HashMap<>();
+        String[] split = target.split("\\?")[1].split("&");
+        for (String pair : split) {
+            addAttribute(parameters, pair);
+        }
+        return parameters;
+    }
+
+    private static boolean hasNotParameters(String target) {
+        return target.split("\\?").length == 2;
+    }
+
+    private static void addAttribute(Map<String, String> parameters, String pair) {
+        String[] split1 = pair.split("=");
+        String key = split1[0];
+        String value = determineValue(split1);
+        parameters.put(key, value);
+    }
+
+    private static String determineValue(String[] split1) {
+        String value = "";
+        if (split1.length == 2) {
+            value = split1[1];
+        }
+        return value;
     }
 
     public boolean isGet() {
@@ -46,20 +81,6 @@ public class RequestLine {
         return target.split("\\?").length == 2;
     }
 
-    public Map<String, String> getParameters() {
-        Map<String, String> parameters = new HashMap<>();
-        String[] split = target.split("\\?")[1].split("&");
-        for (String pair : split) {
-            String[] split1 = pair.split("=");
-            String key = split1[0];
-            String value = "";
-            if (split1.length == 2) {
-                value = split1[1];
-            }
-            parameters.put(key, value);
-        }
-        return parameters;
-    }
 
     public String getPath() {
         return target.split("\\?")[0];
@@ -82,5 +103,9 @@ public class RequestLine {
 
     public String getHttpVersion() {
         return httpVersion;
+    }
+
+    public String get(String attributeName) {
+        return parameters.get(attributeName);
     }
 }
