abed507ff8b49a2e53b22bf1113b26ea0d52463d
starkim06 <starkim06@naver.com>
2019-09-24 21:35:29 +0900

    refactor: 템플릿 처리 로직 추가

--- src/main/java/webserver/viewProcessor/HtmlViewProcessor.java
+++ src/main/java/webserver/viewProcessor/HtmlViewProcessor.java
@@ -1,6 +1,9 @@
 package webserver.viewProcessor;
 
-import utils.FileIoUtils;
+import com.github.jknack.handlebars.Handlebars;
+import com.github.jknack.handlebars.Template;
+import com.github.jknack.handlebars.io.ClassPathTemplateLoader;
+import com.github.jknack.handlebars.io.TemplateLoader;
 import webserver.HttpResponse;
 import webserver.MimeType;
 import webserver.ResponseProcessor;
@@ -8,11 +11,13 @@ import webserver.ViewProcessor;
 
 import java.io.DataOutputStream;
 import java.io.IOException;
-import java.net.URISyntaxException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class HtmlViewProcessor implements ViewProcessor {
 
-    private static final String HTML_ROUTE = "./templates";
+    private static final String HTML_ROUTE = "/templates";
+    public static final String HTML_SUFFIX = ".html";
 
     @Override
     public boolean isSupported(String viewName) {
@@ -22,19 +27,39 @@ public class HtmlViewProcessor implements ViewProcessor {
     @Override
     public void process(DataOutputStream dos, String viewName, HttpResponse httpResponse) {
         ResponseProcessor responseProcessor = ResponseProcessor.getInstance();
-        String filePath = HTML_ROUTE + viewName;
-        byte[] bytes = null;
-        try {
-            bytes = FileIoUtils.loadFileFromClasspath(filePath);
-        } catch (IOException | URISyntaxException | IllegalArgumentException e) {
-            throw new IllegalArgumentException();
-        }
-        setResponseBody(httpResponse, filePath, bytes);
+
+        String view = viewName.split("\\.")[0];
+        String key = httpResponse.getBodyKey();
+        Object data = httpResponse.getBodyValue();
+        byte[] bytes = getTemplateData(view, key, data);
+
+        setResponseBody(httpResponse, viewName, bytes);
         responseProcessor.forward(dos, bytes, httpResponse);
     }
 
-    private void setResponseBody(HttpResponse httpResponse, String filePath, byte[] bytes) {
-        httpResponse.setContentType(MimeType.values(filePath));
+    private byte[] getTemplateData(String view, String key, Object value) {
+        TemplateLoader loader = new ClassPathTemplateLoader();
+        loader.setPrefix(HTML_ROUTE);
+        loader.setSuffix(HTML_SUFFIX);
+        Handlebars handlebars = new Handlebars(loader);
+
+        Template template = null;
+        try {
+            template = handlebars.compile(view);
+        } catch (IOException e) {
+            throw new IllegalArgumentException("올바르지 않은 뷰 입니다.");
+        }
+        Map<String, Object> templateData = new HashMap<>();
+        templateData.put(key, value);
+        try {
+            return template.apply(templateData).getBytes();
+        } catch (IOException e) {
+            throw new IllegalArgumentException("올바르지 않는 뷰 모델 입니다.");
+        }
+    }
+
+    private void setResponseBody(HttpResponse httpResponse, String viewName, byte[] bytes) {
+        httpResponse.setContentType(MimeType.values(viewName));
         httpResponse.setContentLength(bytes.length);
     }
 }
