ef79eb78674667f1d4827a652587614fb1f9fbaf
hyunsoojung <jhs695@naver.com>
2019-09-24 19:45:45 +0900

    refactor: RequestParser 메소드명 변경

--- src/main/java/utils/RequestParser.java
+++ src/main/java/utils/RequestParser.java
@@ -8,8 +8,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class RequestParser {
-
-    public static List<String> parse(InputStream inputStream) throws IOException {
+    public static List<String> lineParse(InputStream inputStream) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
         List<String> lines = new ArrayList<>();
 
