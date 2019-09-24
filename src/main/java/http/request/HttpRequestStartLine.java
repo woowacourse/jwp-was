20455e22635f1e2461c7cfa79fc7791af0113220
hyunsoojung <jhs695@naver.com>
2019-09-24 19:30:50 +0900

    feat: HttpRequestLine -> HttpRequestStartLine 이름 변경

---
+++ src/main/java/http/request/HttpRequestStartLine.java
@@ -0,0 +1,58 @@
+package http.request;
+
+import http.HttpRequestMethod;
+import http.HttpVersion;
+
+import java.util.Objects;
+
+public class HttpRequestStartLine {
+    private HttpRequestMethod httpRequestMethod;
+    private HttpRequestTarget httpRequestTarget;
+    private HttpVersion httpVersion;
+
+    public HttpRequestStartLine(HttpRequestMethod httpRequestMethod, HttpRequestTarget httpRequestTarget, HttpVersion httpVersion) {
+        this.httpRequestMethod = httpRequestMethod;
+        this.httpRequestTarget = httpRequestTarget;
+        this.httpVersion = httpVersion;
+    }
+
+    public boolean hasBody() {
+        return httpRequestMethod.hasBody();
+    }
+
+    public String getQueryString() {
+        return httpRequestTarget.getQueryString();
+    }
+
+    public HttpRequestMethod getHttpRequestMethod() {
+        return httpRequestMethod;
+    }
+
+    public String getUri() {
+        return httpRequestTarget.getUri();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        HttpRequestStartLine that = (HttpRequestStartLine) o;
+        return httpRequestMethod == that.httpRequestMethod &&
+                httpRequestTarget.equals(that.httpRequestTarget) &&
+                httpVersion == that.httpVersion;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(httpRequestMethod, httpRequestTarget, httpVersion);
+    }
+
+    @Override
+    public String toString() {
+        return "HttpRequestStartLine{" +
+                "httpRequestMethod=" + httpRequestMethod +
+                ", httpRequestTarget=" + httpRequestTarget +
+                ", httpVersion=" + httpVersion +
+                '}';
+    }
+}
