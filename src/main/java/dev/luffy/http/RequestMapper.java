170e283fda63be70bcee76b2e7494163f263af7a
mrkwon <erased11@gmail.com>
2019-09-24 23:06:00 +0900

    style: RequestMapper 로 이름을 변경합니다.

--- src/main/java/dev/luffy/http/RequestMapper.java
+++ src/main/java/dev/luffy/http/RequestMapper.java
@@ -4,7 +4,7 @@ import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
 
-public class ControllerMapper {
+public class RequestMapper {
 
     private static final Map<String, Method> map;
 
