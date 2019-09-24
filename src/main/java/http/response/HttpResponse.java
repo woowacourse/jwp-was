d055d662f0ec2e70b00c13c79666b7c4d82682c9
seokhyeonchoi <lkamna12@gmail.com>
2019-09-24 19:57:28 +0900

    refactor: Response 생성 변경

--- src/main/java/http/response/HttpResponse.java
+++ src/main/java/http/response/HttpResponse.java
@@ -1,44 +1,17 @@
 package http.response;
 
-import http.common.ContentType;
 import http.common.HttpHeader;
-import http.response.exception.InvalidHttpResponseException;
-
-import java.util.Arrays;
+import http.common.HttpVersion;
 
 public class HttpResponse {
     private static final String LINE_FEED_AND_CARRIAGE_RETURN = "\r\n";
     private static final String RESPONSE_HEADER_SERIALIZE_FORMAT = "%s" + LINE_FEED_AND_CARRIAGE_RETURN + "%s";
-    private final StatusLine statusLine;
+    private StatusLine statusLine;
     private final HttpHeader httpHeader;
-    private final byte[] body;
+    private byte[] body;
 
-    public HttpResponse(StatusLine statusLine, HttpHeader httpHeader, byte[] body) {
-        checkValidHttpResponse(statusLine, httpHeader);
-        this.statusLine = statusLine;
-        this.httpHeader = httpHeader;
-        this.body = body;
-    }
-
-    private void checkValidHttpResponse(StatusLine statusLine, HttpHeader httpHeader) {
-        if (statusLine == null || httpHeader == null) {
-            throw new InvalidHttpResponseException();
-        }
-    }
-
-    public static HttpResponse found(String location) {
-        StatusLine statusLine = new StatusLine("HTTP/1.1", ResponseStatus.FOUND);
-        HttpHeader httpHeader = new HttpHeader(Arrays.asList("Location: " + location));
-        return new HttpResponse(statusLine, httpHeader, new byte[0]);
-    }
-
-    public static HttpResponse ok(byte[] body, String extension) {
-        StatusLine statusLine = new StatusLine("HTTP/1.1", ResponseStatus.OK);
-        HttpHeader httpHeader = new HttpHeader(Arrays.asList(
-                "Content-Type: " + ContentType.getContentType(extension) + ";charset=utf-8",
-                "Content-Length: " + body.length
-        ));
-        return new HttpResponse(statusLine, httpHeader, body);
+    public HttpResponse() {
+        this.httpHeader = new HttpHeader();
     }
 
     public byte[] serialize() {
@@ -51,5 +24,17 @@ public class HttpResponse {
         }
 
         return responseHeader.getBytes();
+    }
+
+    public void setResponseStatus(ResponseStatus responseStatus) {
+        this.statusLine = new StatusLine(HttpVersion.HTTP_1_1, responseStatus);
+    }
+
+    public void addHeaderAttribute(String key, String value) {
+        this.httpHeader.add(key, value);
+    }
+
+    public void setBody(byte[] body) {
+        this.body = body;
     }
 }
