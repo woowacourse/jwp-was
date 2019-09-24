919436d3e0e869b0716a2ed4f8f06b6cdde8cdc3
hyunsoojung <jhs695@naver.com>
2019-09-25 00:45:15 +0900

    refactor: response에 데이터 set하도록 변경

--- src/main/java/controller/FileController.java
+++ src/main/java/controller/FileController.java
@@ -1,7 +1,11 @@
 package controller;
 
+import http.HttpStatusCode;
 import http.request.HttpRequest;
 import http.response.HttpResponse;
+import http.response.HttpResponseBody;
+import http.response.HttpResponseHeader;
+import http.response.HttpResponseStatusLine;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -17,13 +21,26 @@ public class FileController extends AbstractController {
     }
 
     @Override
-    void doGet(HttpRequest httpRequest, HttpResponse httpResponse) {
+    void doGet(HttpRequest httpRequest, HttpResponse httpResponse) throws IOException, URISyntaxException {
         String uri = httpRequest.getUri();
+
+        HttpResponseStatusLine httpResponseStatusLine;
+        HttpResponseHeader HttpResponseHeader;
+        HttpResponseBody httpResponseBody;
         try {
+            httpResponseStatusLine = new HttpResponseStatusLine(httpRequest.getHttpVersion(), HttpStatusCode.of("200"));
+            HttpResponseHeader = new HttpResponseHeader("Location: /" + httpRequest.getUri());
+            httpResponseBody = new HttpResponseBody(uri);
             httpResponse.send200Ok(uri);
         } catch (IOException | URISyntaxException e) {
+//            httpResponseStatusLine = new HttpResponseStatusLine(httpRequest.getHttpVersion(), HttpStatusCode.of("404"));
+//            HttpResponseHeader = new HttpResponseHeader("");
+//            httpResponseBody = new HttpResponseBody("");
             logger.error(e.getMessage());
         }
 
+//        httpResponse.setHttpResponseStatusLine(httpResponseStatusLine);
+//        httpResponse.setHttpResponseHeader(HttpResponseHeader);
+//        httpResponse.setHttpResponseBody(httpResponseBody);
     }
 }
