6d26e80d27812e73004615a8b0f7d6b08f59319b
hyunsoojung <jhs695@naver.com>
2019-09-25 00:44:32 +0900

    refactor: setter 추가

--- src/main/java/http/response/HttpResponse.java
+++ src/main/java/http/response/HttpResponse.java
@@ -2,41 +2,61 @@ package http.response;
 
 import http.MediaType;
 
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.URISyntaxException;
 
 public class HttpResponse {
-    private HttpStatusLine httpStatusLine;
-    private HttpResponseHeader httpResponseHeader;
-    private HttpResponseBody httpResponseBody;
+	private HttpResponseStatusLine httpResponseStatusLine;
+	private HttpResponseHeader httpResponseHeader;
+	private HttpResponseBody httpResponseBody;
 
-    public HttpResponse() {
-    }
+	public HttpResponse() {
+	}
 
-    public HttpStatusLine getHttpStatusLine() {
-        return httpStatusLine;
-    }
+	public HttpResponseStatusLine getHttpResponseStatusLine() {
+		return httpResponseStatusLine;
+	}
 
-    public HttpResponseHeader getHttpResponseHeader() {
-        return httpResponseHeader;
-    }
+	public HttpResponseHeader getHttpResponseHeader() {
+		return httpResponseHeader;
+	}
 
-    public HttpResponseBody getHttpResponseBody() {
-        return httpResponseBody;
-    }
+	public HttpResponseBody getHttpResponseBody() {
+		return httpResponseBody;
+	}
 
-    public void sendRedirect(String uri) {
-        this.httpStatusLine = new HttpStatusLine("HTTP/1.1 302 FOUND \r\n");
-        this.httpResponseHeader = new HttpResponseHeader("Location: /" + uri + "\r\n");
-    }
+	public void setHttpResponseStatusLine(HttpResponseStatusLine httpResponseStatusLine) {
+		this.httpResponseStatusLine = httpResponseStatusLine;
+	}
 
-    public void send200Ok(String uri) throws IOException, URISyntaxException {
-        String contentType = MediaType.getContentType(uri);
+	public void setHttpResponseHeader(HttpResponseHeader httpResponseHeader) {
+		this.httpResponseHeader = httpResponseHeader;
+	}
 
-        this.httpStatusLine = new HttpStatusLine("HTTP/1.1 200 OK \r\n");
-        this.httpResponseBody = new HttpResponseBody(uri);
-        this.httpResponseHeader = new HttpResponseHeader(
-                "Content-Type: " + contentType + ";charset=utf-8\r\n"
-                        + "Content-Length: " + httpResponseBody.getBodyLength());
-    }
+	public void setHttpResponseBody(HttpResponseBody httpResponseBody) {
+		this.httpResponseBody = httpResponseBody;
+	}
+
+	public void sendResponse(DataOutputStream dataOutputStream) throws IOException {
+		dataOutputStream.writeBytes(httpResponseStatusLine.toString());
+		dataOutputStream.writeBytes(httpResponseHeader.toString());
+		dataOutputStream.write(httpResponseBody.getBody(), 0, httpResponseBody.getBody().length);
+		dataOutputStream.flush();
+	}
+
+	public void sendRedirect(String uri) {
+		this.httpResponseStatusLine = new HttpResponseStatusLine("HTTP/1.1 302 FOUND \r\n");
+		this.httpResponseHeader = new HttpResponseHeader("Location: /" + uri + "\r\n");
+	}
+
+	public void send200Ok(String uri) throws IOException, URISyntaxException {
+		String contentType = MediaType.getContentType(uri);
+
+		this.httpResponseStatusLine = new HttpResponseStatusLine("HTTP/1.1 200 OK \r\n");
+		this.httpResponseBody = new HttpResponseBody(uri);
+		this.httpResponseHeader = new HttpResponseHeader(
+				"Content-Type: " + contentType + ";charset=utf-8\r\n"
+						+ "Content-Length: " + httpResponseBody.getBodyLength());
+	}
 }
