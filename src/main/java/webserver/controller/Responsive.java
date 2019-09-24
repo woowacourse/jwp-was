9fab022d632c78de67a3fd4c52719b9d2324a2ae
ep1stas1s <masloz@konkuk.ac.kr>
2019-09-25 00:23:41 +0900

    refactor: controller 구조 변경

--- src/main/java/webserver/controller/Responsive.java
+++ src/main/java/webserver/controller/Responsive.java
@@ -3,9 +3,7 @@ package webserver.controller;
 import webserver.request.HttpRequest;
 import webserver.response.HttpResponse;
 
-import java.util.function.Function;
+import java.util.function.BiConsumer;
 
-public interface Responsive extends Function<HttpRequest, HttpResponse> {
-    @Override
-    HttpResponse apply(HttpRequest httpRequest);
+public interface Responsive extends BiConsumer<HttpRequest, HttpResponse> {
 }
