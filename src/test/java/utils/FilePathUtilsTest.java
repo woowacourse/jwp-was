5a9586f90caefb0aee4fb16cafbf7284af69ad94
ep1stas1s <masloz@konkuk.ac.kr>
2019-09-25 00:24:42 +0900

    test: refactoring 에 따른 test 수정

--- src/test/java/utils/FilePathUtilsTest.java
+++ src/test/java/utils/FilePathUtilsTest.java
@@ -1,5 +1,6 @@
 package utils;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -9,5 +10,14 @@ class FilePathUtilsTest {
     void getExtension() {
         String path = "/index.html";
         assertThat(FilePathUtils.getExtension(path)).isEqualTo("html");
+    }
+
+    @DisplayName("prefix 확인 후 path 반환")
+    @Test
+    void getResourcePath() {
+        String pathWithPrefix = "/index.html";
+        String pathWithoutPrefix = "index.html";
+        assertThat(FilePathUtils.getResourcePath(pathWithPrefix))
+                .isEqualTo(FilePathUtils.getResourcePath(pathWithoutPrefix));
     }
 }
