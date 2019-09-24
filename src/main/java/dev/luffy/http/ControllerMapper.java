3be6a96fc5afe959dd4e5c0443e63f21571b1ddf
mrkwon <erased11@gmail.com>
2019-09-24 23:02:58 +0900

    feat: ControllerMapper 클래스를 구현합니다.

---
+++ src/main/java/dev/luffy/http/ControllerMapper.java
@@ -0,0 +1,23 @@
+package dev.luffy.http;
+
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ControllerMapper {
+
+    private static final Map<String, Method> map;
+
+    static {
+        map = new HashMap<>();
+
+    }
+
+    public static void add(String path, Method method) {
+        map.put(path, method);
+    }
+
+    public static Method get(String path) {
+        return map.get(path);
+    }
+}
