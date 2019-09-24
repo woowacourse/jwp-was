70bd893c158e79123cf1fca71ce8c59e552c1dce
Seongmo Jeon <oeeen3@gmail.com>
2019-09-24 14:03:33 +0900

    test: getMethod, getUri 테스트 추가

--- src/test/java/http/request/HttpRequestLineTest.java
+++ src/test/java/http/request/HttpRequestLineTest.java
@@ -1,5 +1,6 @@
 package http.request;
 
+import http.HttpMethod;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -11,5 +12,19 @@ class HttpRequestLineTest {
         HttpRequestLine httpRequestLine = new HttpRequestLine("GET http://localhost:8080 HTTP/1.1");
 
         assertThat(httpRequestLine.toString()).isEqualTo("GET http://localhost:8080 HTTP/1.1\r\n");
+    }
+
+    @Test
+    void getMethod() {
+        HttpRequestLine httpRequestLine = new HttpRequestLine("GET http://localhost:8080 HTTP/1.1");
+
+        assertThat(httpRequestLine.getMethod()).isEqualTo(HttpMethod.GET);
+    }
+
+    @Test
+    void getUri() {
+        HttpRequestLine httpRequestLine = new HttpRequestLine("GET http://localhost:8080 HTTP/1.1");
+
+        assertThat(httpRequestLine.getUri()).isEqualTo("http://localhost:8080");
     }
 }
