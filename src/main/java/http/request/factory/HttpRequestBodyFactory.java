1392d0476a8d14062b322422bc89780b86639684
hyunsoojung <jhs695@naver.com>
2019-09-24 20:28:40 +0900

    refactor: 메소드 인자 변경

--- src/main/java/http/request/factory/HttpRequestBodyFactory.java
+++ src/main/java/http/request/factory/HttpRequestBodyFactory.java
@@ -2,10 +2,8 @@ package http.request.factory;
 
 import http.request.HttpRequestBody;
 
-import java.util.List;
-
 public class HttpRequestBodyFactory {
-    public static HttpRequestBody create(List<String> lines) {
-        return new HttpRequestBody(lines);
+    public static HttpRequestBody create(String line) {
+        return new HttpRequestBody(line);
     }
 }
