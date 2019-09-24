47712531e87cd7dfb903104a0ba8883f6a2a371c
ep1stas1s <masloz@konkuk.ac.kr>
2019-09-25 00:24:05 +0900

    feat: add status code

--- src/main/java/webserver/response/ResponseStatus.java
+++ src/main/java/webserver/response/ResponseStatus.java
@@ -2,7 +2,9 @@ package webserver.response;
 
 public enum ResponseStatus {
     OK(200),
-    FOUND(302);
+    FOUND(302),
+    NOT_FOUND(404),
+    INTERNAL_SERVER_ERROR(500);
 
     private int code;
 
