5e46a61764500afabca25c9cb38ba12ab77e6a23
mrkwon <erased11@gmail.com>
2019-09-24 23:07:01 +0900

    feat: IndexController 를 구현합니다.

---
+++ src/main/java/dev/luffy/controller/IndexController.java
@@ -0,0 +1,23 @@
+package dev.luffy.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import dev.luffy.annotation.Controller;
+import dev.luffy.annotation.RequestMapping;
+import dev.luffy.http.request.HttpRequest;
+import dev.luffy.http.response.HttpResponse;
+
+@Controller
+public class IndexController {
+
+    private static final Logger logger = LoggerFactory.getLogger(IndexController.class);
+
+    @RequestMapping("/index.html")
+    public static void index(HttpRequest request, HttpResponse response) {
+
+        logger.debug("request : {} & response : {}", request, response);
+
+        response.send200(request);
+    }
+}
