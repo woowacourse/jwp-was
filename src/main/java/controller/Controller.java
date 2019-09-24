7ac7057edffc6feb32ee24d876c1ffb77b89ed2c
hyunsoojung <jhs695@naver.com>
2019-09-25 00:45:58 +0900

    refactor: IOException, URISyntaxException 추가

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -3,6 +3,9 @@ package controller;
 import http.request.HttpRequest;
 import http.response.HttpResponse;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+
 public interface Controller {
-    void service(HttpRequest request, HttpResponse response);
+    void service(HttpRequest request, HttpResponse response) throws IOException, URISyntaxException;
 }
