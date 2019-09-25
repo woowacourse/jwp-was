d2bcb1a494643b75c99e01309f9964c31d032efc
DaejunBae <dpudpu11@gmail.com>
2019-09-25 20:14:15 +0900

    feat: HttpSession

---
+++ src/main/java/webserver/http/HttpSessionManager.java
@@ -0,0 +1,29 @@
+package webserver.http;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HttpSessionManager {
+    private final Map<String, HttpSession> map = new HashMap<>();
+
+    private HttpSessionManager() {
+    }
+
+    public HttpSession getSession(final String sessionId) {
+        return map.get(sessionId);
+    }
+
+    public HttpSession getSession() {
+        final HttpSession httpSession = new HttpSession();
+        map.put(httpSession.getId(), httpSession);
+        return httpSession;
+    }
+
+    public static HttpSessionManager getInstance() {
+        return LazyHolder.INSTANCE;
+    }
+
+    private static class LazyHolder {
+        private static final HttpSessionManager INSTANCE = new HttpSessionManager();
+    }
+}
---
+++ src/test/java/webserver/http/HttpSessionManagerTest.java
@@ -0,0 +1,25 @@
+package webserver.http;
+
+import org.junit.jupiter.api.Test;
+import webserver.http.HttpSession;
+import webserver.http.HttpSessionManager;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class HttpSessionManagerTest {
+    private HttpSessionManager manager = HttpSessionManager.getInstance();
+
+    @Test
+    void session_생성후_조회_확인() {
+        // given
+        final HttpSession session = manager.getSession();
+
+        // when & then
+        assertThat(session).isEqualTo(manager.getSession(session.getId()));
+    }
+
+    @Test
+    void 싱글톤_테스트() {
+        assertThat(manager == HttpSessionManager.getInstance()).isTrue();
+    }
+}
