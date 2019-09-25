abd286ff1d5f6e409c41bdd44ec4023aaea83101
gch01410 <gch01410@naver.com>
2019-09-25 15:16:04 +0900

    test: 요청 메시지를 갖고 있는 BaseTest 추가

---
+++ src/test/java/webserver/request/BaseTest.java
@@ -0,0 +1,25 @@
+package webserver.request;
+
+public class BaseTest {
+    public static final String GET_REQUEST_MESSAGE =
+            "GET /index.html HTTP/1.1\n" +
+                    "Host: localhost:8080\n" +
+                    "Connection: keep-alive\n" +
+                    "Accept: */*";
+
+    public static final String POST_REQUEST_MESSAGE =
+            "POST /user/create HTTP/1.1\n" +
+                    "Host: localhost:8080\n" +
+                    "Connection: keep-alive\n" +
+                    "Content-Length: 59\n" +
+                    "Content-Type: application/x-www-form-urlencoded\n" +
+                    "Accept: */*\n" +
+                    "\n" +
+                    "userId=javajigi&password=password&name=%EB%B0%95%EC%9E%AC%EC%84%B1&email=javajigi%40slipp.net";
+
+    public static final String GET_REQUEST_MESSAGE_WITH_QUERY_STRING =
+            "GET /user/create?userId=done&password=12345678&name=%EB%B0%95%EC%9E%AC%EC%84%B1&email=done%40slipp.net HTTP/1.1\n" +
+                    "Host: localhost:8080\n" +
+                    "Connection: keep-alive\n" +
+                    "Accept: */*";
+}
--- src/test/java/webserver/request/RequestLineTest.java
+++ src/test/java/webserver/request/RequestLineTest.java
@@ -11,29 +11,7 @@ import java.io.InputStreamReader;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class RequestLineTest {
-    private static final String GET_REQUEST_MESSAGE =
-            "GET /index.html HTTP/1.1\n" +
-                    "Host: localhost:8080\n" +
-                    "Connection: keep-alive\n" +
-                    "Accept: */*";
-
-    private static final String POST_REQUEST_MESSAGE =
-            "POST /user/create HTTP/1.1\n" +
-                    "Host: localhost:8080\n" +
-                    "Connection: keep-alive\n" +
-                    "Content-Length: 59\n" +
-                    "Content-Type: application/x-www-form-urlencoded\n" +
-                    "Accept: */*\n" +
-                    "\n" +
-                    "userId=javajigi&password=password&name=%EB%B0%95%EC%9E%AC%EC%84%B1&email=javajigi%40slipp.net";
-
-    private static final String GET_REQUEST_MESSAGE_WITH_QUERY_STRING =
-            "GET /user/create?userId=done&password=12345678&name=%EB%B0%95%EC%9E%AC%EC%84%B1&email=done%40slipp.net HTTP/1.1\n" +
-                    "Host: localhost:8080\n" +
-                    "Connection: keep-alive\n" +
-                    "Accept: */*";
-
+public class RequestLineTest extends BaseTest {
     private RequestLine requestLineOfGetMessage;
     private RequestLine requestLineOfPostMessage;
     private RequestLine requestLineWithQueryString;
