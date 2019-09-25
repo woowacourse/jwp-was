1c830e0572e083a4f48d7c10f212af180c3bf939
heebong <heebg2@naver.com>
2019-09-25 16:33:46 +0900

    feat: 회원가입된 사용자를 데이터베이스에 저장

--- README.md
+++ README.md
@@ -72,3 +72,13 @@
 - [x] mappingHandler의 servlets Map의 value 타입을 Object -> HttpServlet으로 바꾸기
 - [x] response 생성 방식 변경 (requestHandler에서 generateErrorResponse 함수를 제거해보자)
 - [x] response 생성 방식 변경 (ok, found)
+
+요구사항 1
+
+- [x] 회원가입에 DataBase.addUser() 로직 추가
+- [ ] LoginServelt 만들기(post, /user/login, userId=&password=)
+- [ ] User에 아이디와 비밀번호가 일치하는지 확인하는 함수 추가
+- [ ] loginservlet에서 아이디/비번이 일치하면 /index.html 리다이렉트
+- [ ] loginservlet에서 아이디/비번이 일치하면 Set-Cookie: logined=true; Path=/
+- [ ] loginservlet에서 아이디/비번이 일치하면 /user/login_failed.html 리다이렉트
+- [ ] loginservlet에서 아이디/비번이 일치하지 않으면 Set-Cookie: logined=false; Path=/
--- src/main/java/webserver/servlet/UserCreateServlet.java
+++ src/main/java/webserver/servlet/UserCreateServlet.java
@@ -1,5 +1,6 @@
 package webserver.servlet;
 
+import db.DataBase;
 import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,7 +15,9 @@ public class UserCreateServlet extends RequestServlet {
     @Override
     public HttpResponse doPost(HttpRequest httpRequest) {
         User user = new User(httpRequest.getBody("userId"), httpRequest.getBody("password"), httpRequest.getBody("name"), httpRequest.getBody("email"));
+        DataBase.addUser(user);
         logger.debug(">>> User : {}", user);
+        logger.debug(">>> Saved User : {}", DataBase.findUserById(user.getUserId()));
         ResponseHeader header = new ResponseHeader();
         header.setLocation("/index.html");
         return HttpResponse.found(header);
