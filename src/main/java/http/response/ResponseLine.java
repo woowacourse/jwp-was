d7c2c7c6582b124b1719eefde9617d67429e4d7b
aiden <cnk9109@gmail.com>
2019-09-26 01:17:03 +0900

    feat: update constant

--- src/main/java/http/response/ResponseLine.java
+++ src/main/java/http/response/ResponseLine.java
@@ -3,6 +3,8 @@ package http.response;
 import http.common.HttpStatus;
 
 public class ResponseLine {
+    private static final String HTTP_VERSION = "HTTP/1.1";
+
     private String httpVersion;
     private HttpStatus httpStatus;
 
@@ -12,7 +14,7 @@ public class ResponseLine {
     }
 
     public static ResponseLine of() {
-        return new ResponseLine("HTTP/1.1", HttpStatus.OK);
+        return new ResponseLine(HTTP_VERSION, HttpStatus.OK);
     }
 
     public void setHttpStatus(HttpStatus httpStatus) {
