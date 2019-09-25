ec411c60599d4cc013eb10ded287919948028343
seokhyeonchoi <lkamna12@gmail.com>
2019-09-25 22:13:18 +0900

    feat: 유저 리스트 동적 페이지 응답 구현

---
+++ src/main/java/controller/ModelAndView.java
@@ -0,0 +1,21 @@
+package controller;
+
+import java.util.Map;
+
+public class ModelAndView {
+    private final String viewName;
+    private final Map<String, Object> modelMap;
+
+    public ModelAndView(String viewName, Map<String, Object> modelMap) {
+        this.viewName = viewName;
+        this.modelMap = modelMap;
+    }
+
+    public String getViewName() {
+        return viewName;
+    }
+
+    public Map<String, Object> getModelMap() {
+        return modelMap;
+    }
+}
