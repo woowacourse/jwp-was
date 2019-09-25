6eec3dd000f95d3952e475d170fadfdd98b95318
hyunsoojung <jhs695@naver.com>
2019-09-25 13:03:00 +0900

    refactor: 사용하지 않는 메서드 제거

--- src/main/java/http/response/HttpResponseStatusLine.java
+++ src/main/java/http/response/HttpResponseStatusLine.java
@@ -4,18 +4,11 @@ import http.HttpStatusCode;
 import http.HttpVersion;
 
 public class HttpResponseStatusLine {
-    private HttpVersion httpVersion1;
-    private String httpVersion;
+    private HttpVersion httpVersion;
     private HttpStatusCode httpStatusCode;
 
-    public HttpResponseStatusLine(String line) {
-        String[] statusLine = line.split(" ");
-        this.httpVersion = statusLine[0];
-        this.httpStatusCode = HttpStatusCode.of(statusLine[1]);
-    }
-
     public HttpResponseStatusLine(HttpVersion httpVersion, HttpStatusCode httpStatusCode) {
-        this.httpVersion1 = httpVersion;
+        this.httpVersion = httpVersion;
         this.httpStatusCode = httpStatusCode;
     }
 
