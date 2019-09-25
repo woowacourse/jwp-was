585fa6dadb20406a714fe517bdeb894d6a247e3e
aiden <cnk9109@gmail.com>
2019-09-26 01:13:53 +0900

    feat: update findUserByIdAndPassword()

--- src/main/java/db/DataBase.java
+++ src/main/java/db/DataBase.java
@@ -1,11 +1,11 @@
 package db;
 
+import com.google.common.collect.Maps;
+import db.exception.NotFoundException;
+import model.User;
+
 import java.util.Collection;
 import java.util.Map;
-
-import com.google.common.collect.Maps;
-
-import model.User;
 
 public class DataBase {
     private static Map<String, User> users = Maps.newHashMap();
@@ -16,6 +16,15 @@ public class DataBase {
 
     public static User findUserById(String userId) {
         return users.get(userId);
+    }
+
+    public static User findUserByIdAndPassword(String userId, String password) {
+        User user = users.get(userId);
+        if (user == null) {
+            throw new NotFoundException();
+        }
+
+        return user.getCheckUserIdAndPassword(userId, password);
     }
 
     public static Collection<User> findAll() {
