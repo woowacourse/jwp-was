4beb12200b530df535187d2a9a1b93b03034dbcc
aiden <cnk9109@gmail.com>
2019-09-26 01:17:54 +0900

    feat: update constant and add constant

--- src/main/java/http/response/HttpResponse.java
+++ src/main/java/http/response/HttpResponse.java
@@ -1,27 +1,40 @@
 package http.response;
 
+import http.common.HttpCookie;
 import http.common.HttpStatus;
 import http.common.MimeType;
 
 public class HttpResponse {
     private static final String CRLF = "\r\n";
+    private static final String SET_COOKIE_NAME = "Set-Cookie";
 
     private ResponseLine responseLine;
     private ResponseHeader responseHeader;
     private ResponseBody responseBody;
+    private HttpCookie httpCookie;
 
     private HttpResponse(ResponseLine responseLine,
                          ResponseHeader responseHeader,
-                         ResponseBody responseBody) {
+                         ResponseBody responseBody,
+                         HttpCookie httpCookie) {
+
         this.responseLine = responseLine;
         this.responseHeader = responseHeader;
         this.responseBody = responseBody;
+        this.httpCookie = httpCookie;
     }
 
-    public static HttpResponse of() {
-        return new HttpResponse(ResponseLine.of(),
+    public static HttpResponse of(HttpCookie httpCookie) {
+        return new HttpResponse(
+                ResponseLine.of(),
                 ResponseHeader.of(),
-                ResponseBody.of());
+                ResponseBody.of(),
+                httpCookie
+        );
+    }
+
+    public HttpCookie getHttpCookie() {
+        return httpCookie;
     }
 
     public void putHeader(String key, String value) {
@@ -30,13 +43,13 @@ public class HttpResponse {
 
     public void setResponseBody(byte[] body, String path) {
         this.responseBody = ResponseBody.of(body);
-        putHeader("Content-Type", MimeType.findByPath(path).getContentType());
-        putHeader("Content-Length", String.valueOf(body.length));
+        putHeader(ResponseHeader.CONTENT_TYPE_NAME, MimeType.findByPath(path).getContentType());
+        putHeader(ResponseHeader.CONTENT_LENGTH_NAME, String.valueOf(body.length));
     }
 
     public void redirect(String path) {
         responseLine.setHttpStatus(HttpStatus.FOUND);
-        putHeader("Location", path);
+        putHeader(ResponseHeader.LOCATION_NAME, path);
     }
 
     public byte[] getBody() {
@@ -47,9 +60,19 @@ public class HttpResponse {
         responseLine.setHttpStatus(HttpStatus.NOT_FOUND);
     }
 
+    public void sendNotAllowed() {
+        responseLine.setHttpStatus(HttpStatus.METHOD_NOT_ALLOWED);
+    }
+
     @Override
     public String toString() {
-        return responseLine + CRLF +
-                responseHeader + CRLF;
+        StringBuilder sb = new StringBuilder();
+        sb.append(responseLine).append(CRLF);
+        sb.append(responseHeader);
+        if (!httpCookie.toString().equals("")) {
+            sb.append(String.format("%s: %s", SET_COOKIE_NAME, httpCookie));
+        }
+        sb.append(CRLF);
+        return sb.toString();
     }
 }
