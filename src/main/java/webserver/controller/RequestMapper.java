9c5127fb6deb893cdb77a8deed0dde29644b5caf
ep1stas1s <masloz@konkuk.ac.kr>
2019-09-26 02:02:49 +0900

    refactor: error 로직 수정

--- src/main/java/webserver/controller/RequestMapper.java
+++ src/main/java/webserver/controller/RequestMapper.java
@@ -4,15 +4,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import web.controller.IndexController;
 import web.controller.UserController;
-import webserver.utils.FilePathUtils;
 import webserver.exception.MethodNotAllowedException;
 import webserver.request.HttpRequest;
 import webserver.response.HttpResponse;
-import webserver.response.MediaType;
 
 import static webserver.controller.RequestMapping.getMapping;
 import static webserver.controller.RequestMapping.postMapping;
-import static webserver.response.ResponseStatus.*;
 
 public class RequestMapper {
     private RequestMapper() {
@@ -37,48 +34,35 @@ public class RequestMapper {
         post("/user/create", UserController.getInstance().createUser());
     }
 
-    public static void get(String uri, Responsive responsive) {
+    private static void get(String uri, Responsive responsive) {
         CONTROLLER_HANDLER.put(getMapping(uri), responsive);
     }
 
-    public static void post(String uri, Responsive responsive) {
+    private static void post(String uri, Responsive responsive) {
         CONTROLLER_HANDLER.put(postMapping(uri), responsive);
     }
 
     public HttpResponse service(HttpRequest request) {
         HttpResponse response = new HttpResponse();
         RequestMapping requestMapping = request.getRequestMapping();
+
+        Responsive responsive;
         try {
-            CONTROLLER_HANDLER.get(requestMapping).accept(request, response);
+            responsive = CONTROLLER_HANDLER.get(requestMapping);
         } catch (MethodNotAllowedException e) {
             logger.error("path: {}, {}", requestMapping, e.getMessage());
-            return HttpResponse.sendErrorResponse(METHOD_NOT_ALLOWED);
+            return HttpResponse.methodNotAllowed();
         } catch (Exception e) {
             logger.error("path: {}, {}", requestMapping, e.getMessage());
-            return HttpResponse.sendErrorResponse(INTERNAL_SERVER_ERROR);
+            return HttpResponse.internalServerError();
         }
 
-        if (response.hasNotContent() ||
-                FilePathUtils.isTemplateFile(FilePathUtils.getResourcePath(response.getViewPath()))) {
+        try {
+            responsive.accept(request, response);
             return response;
+        } catch (NullPointerException e) {
+            logger.error("path: {}, 존재하지 않는 path 요청", request.getPath());
+            return HttpResponse.notFound();
         }
-
-        String path = response.getViewPath();
-
-        if (FilePathUtils.isStaticFile(path)) {
-            return getStaticFileResponse(request, response);
-        }
-
-        logger.error("path: {}, 존재하지 않는 path 요청", path);
-        return HttpResponse.sendErrorResponse(NOT_FOUND);
-    }
-
-    private HttpResponse getStaticFileResponse(HttpRequest httpRequest, HttpResponse httpResponse) {
-        String path = httpRequest.getPath();
-        httpResponse.forward(path);
-
-        String extension = FilePathUtils.getExtension(path);
-        httpResponse.setContentType(MediaType.of(extension));
-        return httpResponse;
     }
 }
