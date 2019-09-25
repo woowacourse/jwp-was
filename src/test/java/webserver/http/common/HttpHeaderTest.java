012777591947cbaec2898f1037bfef34e871c68e
이두호 <ddu0422@naver.com>
2019-09-25 13:55:16 +0900

    test: HttpHeader / HttpVersion 테스트 추가

--- src/test/java/webserver/http/common/HttpHeaderTest.java
+++ src/test/java/webserver/http/common/HttpHeaderTest.java
@@ -3,30 +3,54 @@ package webserver.http.common;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+import webserver.http.common.exception.InvalidHeaderLines;
 
 import java.util.Arrays;
+import java.util.Collections;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 class HttpHeaderTest {
 
     @ParameterizedTest
     @CsvSource(value = {"Host|localhost:8080", "Connection|keep-alive", "Accept|*/*"}, delimiter = '|')
     void 생성(String key, String value) {
-        HttpHeader httpHeader = HttpHeader.of(
-                Arrays.asList(
-                        "Host: localhost:8080",
-                        "Connection: keep-alive",
-                        "Accept: */*"
-                )
-        );
+        HttpHeader httpHeader = HttpHeader.of(Arrays.asList(key + ": " + value));
 
         assertEquals(httpHeader.get(key), value);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"Host localhost:8080", "Connectoin keep-alive", "Accept */*"})
+    void 형식에_맞지_않는_헤더_라인(String value) {
+        HttpHeader httpHeader = HttpHeader.of(Arrays.asList(value));
+
+        assertFalse(httpHeader.getHeaders().containsKey(value.split(" ")[0]));
+    }
+
+    @Test
+    void 헤더에_빈_칸이_들어가는_경우() {
+        HttpHeader httpHeader = HttpHeader.of(Arrays.asList(""));
+
+        assertFalse(httpHeader.getHeaders().containsKey(""));
     }
 
     @Test
     void Location_add_확인() {
         HttpHeader httpHeader = HttpHeader.of(Arrays.asList("Location: /index.html"));
         assertEquals("/index.html", httpHeader.get("Location"));
+    }
+
+    @Test
+    void 헤더에_null이_들어가는_경우() {
+        assertThrows(InvalidHeaderLines.class, () -> {
+            HttpHeader.of(null);
+        });
+    }
+
+    @Test
+    void 헤더_사이즈가_0인_경우() {
+        assertEquals(HttpHeader.of(Arrays.asList()).getHeaders(), Collections.emptyMap());
     }
 }
--- src/test/java/webserver/http/common/HttpVersionTest.java
+++ src/test/java/webserver/http/common/HttpVersionTest.java
@@ -2,13 +2,24 @@ package webserver.http.common;
 
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+import webserver.http.common.exception.IllegalHttpVersionException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class HttpVersionTest {
     @ParameterizedTest
     @CsvSource(value = {"HTTP_1_0|HTTP/1.0", "HTTP_1_1|HTTP/1.1", "HTTP_2_0|HTTP/2.0"}, delimiter = '|')
     void 네트워크_버전_이름_확인(HttpVersion httpVersion, String name) {
         assertEquals(httpVersion, HttpVersion.of(name));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"HTTP/0.9", "HTTP/3.0"})
+    void 지원하지_않는_네트워크_버전_에러_확인(String name) {
+        assertThrows(IllegalHttpVersionException.class, () -> {
+            HttpVersion.of(name);
+        });
     }
 }
