2caf90761339126adb792091f59c79f323ebb19c
hyunsoojung <jhs695@naver.com>
2019-09-25 13:02:37 +0900

    refactor: 사용하지 않는 메서드 제거

--- src/main/java/http/response/HttpResponse.java
+++ src/main/java/http/response/HttpResponse.java
@@ -1,30 +1,12 @@
 package http.response;
 
-import http.MediaType;
-
 import java.io.DataOutputStream;
 import java.io.IOException;
-import java.net.URISyntaxException;
 
 public class HttpResponse {
 	private HttpResponseStatusLine httpResponseStatusLine;
 	private HttpResponseHeader httpResponseHeader;
 	private HttpResponseBody httpResponseBody;
-
-	public HttpResponse() {
-	}
-
-	public HttpResponseStatusLine getHttpResponseStatusLine() {
-		return httpResponseStatusLine;
-	}
-
-	public HttpResponseHeader getHttpResponseHeader() {
-		return httpResponseHeader;
-	}
-
-	public HttpResponseBody getHttpResponseBody() {
-		return httpResponseBody;
-	}
 
 	public void setHttpResponseStatusLine(HttpResponseStatusLine httpResponseStatusLine) {
 		this.httpResponseStatusLine = httpResponseStatusLine;
@@ -43,20 +25,5 @@ public class HttpResponse {
 		dataOutputStream.writeBytes(httpResponseHeader.toString());
 		dataOutputStream.write(httpResponseBody.getBody(), 0, httpResponseBody.getBody().length);
 		dataOutputStream.flush();
-	}
-
-	public void sendRedirect(String uri) {
-		this.httpResponseStatusLine = new HttpResponseStatusLine("HTTP/1.1 302 FOUND \r\n");
-		this.httpResponseHeader = new HttpResponseHeader("Location: /" + uri + "\r\n");
-	}
-
-	public void send200Ok(String uri) throws IOException, URISyntaxException {
-		String contentType = MediaType.getContentType(uri);
-
-		this.httpResponseStatusLine = new HttpResponseStatusLine("HTTP/1.1 200 OK \r\n");
-		this.httpResponseBody = new HttpResponseBody(uri);
-		this.httpResponseHeader = new HttpResponseHeader(
-				"Content-Type: " + contentType + ";charset=utf-8\r\n"
-						+ "Content-Length: " + httpResponseBody.getBodyLength());
 	}
 }
