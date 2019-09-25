81593559ff918dfa1288fa392c689bf843d07037
imkimheejoo <jujudamdadi@naver.com>
2019-09-25 10:53:25 +0900

    refactor: ResourceResolverRegistration 의 resolve 로직을 ResourceResolver 에 위임

--- src/main/java/utils/FileIoUtils.java
+++ src/main/java/utils/FileIoUtils.java
@@ -11,12 +11,7 @@ import java.util.Optional;
 
 public class FileIoUtils {
     public static byte[] loadFileFromClasspath(String filePath) throws IOException, URISyntaxException {
-        try {
-            Path path = Paths.get(FileIoUtils.class.getClassLoader().getResource(filePath).toURI());
-            return Optional.of(Files.readAllBytes(path)).orElseThrow(NotFoundException::new);
-        } catch (Exception e) {
-            throw new NotFoundException();
-        }
-
+        Path path = Paths.get(FileIoUtils.class.getClassLoader().getResource(filePath).toURI());
+        return Optional.of(Files.readAllBytes(path)).orElseThrow(NotFoundException::new);
     }
 }
