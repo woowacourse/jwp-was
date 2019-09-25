1ff5d5706329e7d11833c585102bbad26b29d78e
김성훈 <seonghun127@gmail.com>
2019-09-25 17:59:23 +0900

    test: handlebars 테스트 코드 추가

--- src/test/java/utils/HandlebarsTest.java
+++ src/test/java/utils/HandlebarsTest.java
@@ -9,11 +9,16 @@ import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class HandlebarsTest {
     private static final Logger log = LoggerFactory.getLogger(HandlebarsTest.class);
 
     @Test
-    void name() throws Exception {
+    public void name() throws Exception {
         TemplateLoader loader = new ClassPathTemplateLoader();
         loader.setPrefix("/templates");
         loader.setSuffix(".html");
@@ -23,6 +28,25 @@ public class HandlebarsTest {
 
         User user = new User("javajigi", "password", "이바", "javajigi@gmail.com");
         String profilePage = template.apply(user);
+        log.debug("ProfilePage : {}", profilePage);
+    }
+
+    @Test
+    public void userLists() throws Exception {
+        TemplateLoader loader = new ClassPathTemplateLoader();
+        loader.setPrefix("/templates");
+        loader.setSuffix(".html");
+        Handlebars handlebars = new Handlebars(loader);
+
+        Template template = handlebars.compile("user/list");
+
+        User user1 = new User("javajigi", "password", "자바지기", "javajigi@gmail.com");
+        User user2 = new User("seonghun127", "password", "이바", "seonghun127@gmail.com");
+        Map<String, Object> map = new HashMap<>();
+        List<User> users = Arrays.asList(user1, user2);
+        map.put("users", users);
+
+        String profilePage = template.apply(map);
         log.debug("ProfilePage : {}", profilePage);
     }
 }
