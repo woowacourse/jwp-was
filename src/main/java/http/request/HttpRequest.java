30a1ac67838597f8b9041ca8fc19f17accf30f7a
aiden <cnk9109@gmail.com>
2019-09-26 01:12:22 +0900

    feat: update of() and remove getEntityValue()

--- src/main/java/http/request/HttpRequest.java
+++ src/main/java/http/request/HttpRequest.java
@@ -1,38 +1,61 @@
 package http.request;
 
+import http.common.HttpCookie;
 import http.common.HttpMethod;
 
+import java.io.IOException;
+
 public class HttpRequest {
+    public static final String CONTENT_LENGTH_NAME = "Content-Length";
+    public static final String CONTENT_TYPE_NAME = "Content-Type";
+    public static final String HOST_NAME = "Host";
+    public static final String COOKIE_NAME = "Cookie";
+
     private RequestLine requestLine;
     private RequestHeader requestHeader;
     private RequestBody requestBody;
+    private HttpCookie httpCookie;
+    private HttpRequestParams httpRequestParams;
 
     private HttpRequest(RequestLine requestLine,
                         RequestHeader requestHeader,
-                        RequestBody requestBody) {
-            this.requestLine = requestLine;
-            this.requestHeader = requestHeader;
-            this.requestBody = requestBody;
+                        RequestBody requestBody,
+                        HttpCookie httpCookie,
+                        HttpRequestParams httpRequestParams) {
+        this.requestLine = requestLine;
+        this.requestHeader = requestHeader;
+        this.requestBody = requestBody;
+        this.httpCookie = httpCookie;
+        this.httpRequestParams = httpRequestParams;
     }
 
     public static HttpRequest of(RequestLine requestLine,
                                  RequestHeader requestHeader,
-                                 RequestBody requestBody) {
-        return new HttpRequest(requestLine,
-                requestHeader,
-                requestBody);
+                                 RequestBody requestBody) throws IOException {
+
+        HttpCookie httpCookie = HttpCookie.of(requestHeader.getHeader(COOKIE_NAME));
+
+        HttpRequestParams httpRequestParams = HttpRequestParams.of();
+        httpRequestParams.putAll(requestLine.getQueryString());
+        httpRequestParams.putAll(requestBody.getFormData(requestHeader));
+
+        return new HttpRequest(requestLine, requestHeader, requestBody, httpCookie, httpRequestParams);
     }
 
     public String getUrl() {
         String path = requestLine.getPath();
         String protocol = requestLine.getProtocol();
-        String host = requestHeader.getHeader("Host");
+        String host = requestHeader.getHeader(HOST_NAME);
 
         return String.format("%s://%s%s", protocol, host, path);
     }
 
-    public String getQuery(String key) {
-        return requestLine.getQuery().get(key);
+    public String getParameter(String name) {
+        return httpRequestParams.getParameter(name);
+    }
+
+    public HttpCookie getHttpCookie() {
+        return httpCookie;
     }
 
     public String getPath() {
@@ -49,9 +72,5 @@ public class HttpRequest {
 
     public String getHeader(String headerKey) {
         return requestHeader.getHeader(headerKey);
-    }
-
-    public String getEntityValue(String entityKey) {
-        return requestBody.getEntityValue(entityKey);
     }
 }
