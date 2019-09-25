188aca13e32736b125b2a270745cacf244f7098a
kangmin46 <kangminals5610@gmail.com>
2019-09-26 01:41:33 +0900

    [feat] impliment Controller, UserController

--- src/main/java/webserver/controller/IndexController.java
+++ src/main/java/webserver/controller/IndexController.java
@@ -1,26 +1,21 @@
 package webserver.controller;
 
-import db.DataBase;
-import factory.UserFactory;
-import model.User;
+import exception.UnregisteredURLException;
+import exception.UnsupportedMethodException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import webserver.controller.request.HttpRequest;
 import webserver.controller.response.HttpResponse;
 
-import java.util.Map;
+public class IndexController extends AbstractController {
+    private IndexController() { }
 
-public class UserController extends AbstractController {
-    public static final String SAVE_REDIRECT_URL = "/index.html";
-
-    private UserController() { }
-
-    public static UserController getInstance() {
+    public static IndexController getInstance() {
         return LazyHolder.INSTANCE;
     }
 
     private static class LazyHolder {
-        private static final UserController INSTANCE = new UserController();
+        private static final IndexController INSTANCE = new IndexController();
     }
 
     @Override
@@ -34,9 +29,6 @@ public class UserController extends AbstractController {
 
     @Override
     public void doPost(HttpRequest httpRequest, HttpResponse httpResponse) {
-        Map<String, String> requestBodyFields = httpRequest.getBodyFields();
-        User user = UserFactory.of(requestBodyFields);
-        DataBase.addUser(user);
-        httpResponse.sendRedirect(SAVE_REDIRECT_URL);
+        throw new UnregisteredURLException();
     }
 }
