d96dc4dedd081170e6efa27429947df96aaa7864
kangmin46 <kangminals5610@gmail.com>
2019-09-26 01:45:33 +0900

    [refactor] change HttpRequestBody Structure

--- src/main/java/webserver/controller/request/body/HttpRequestBody.java
+++ src/main/java/webserver/controller/request/body/HttpRequestBody.java
@@ -1,33 +1,18 @@
 package webserver.controller.request.body;
 
 
-import java.net.URLDecoder;
-import java.nio.charset.StandardCharsets;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
 public class HttpRequestBody {
-    private static final String KEY_VALUE_DELEMETER = "=";
-    private static final String FIELD_SEPERATE_DELEMETER = "&";
-    private final HashMap<String, String> bodyDataSet;
+    private final HashMap<String, String> bodyFields;
 
-    public HttpRequestBody(String requestParams) {
-        bodyDataSet = new HashMap<>();
-        requestParams = URLDecoder.decode(requestParams, StandardCharsets.UTF_8);
-        String[] splitedParams = requestParams.split(FIELD_SEPERATE_DELEMETER);
-
-        mappingRequestParams(splitedParams);
+    public HttpRequestBody(HashMap<String, String> bodyFields) {
+        this.bodyFields = bodyFields;
     }
 
-    private void mappingRequestParams(String[] splitedParams) {
-        for (String splitedParam : splitedParams) {
-            String[] queryParam = splitedParam.split(KEY_VALUE_DELEMETER);
-            bodyDataSet.put(queryParam[0], queryParam[1]);
-        }
-    }
-
-    public Map<String, String> getBodyDataSet() {
-        return Collections.unmodifiableMap(bodyDataSet);
+    public Map<String, String> getBodyFields() {
+        return Collections.unmodifiableMap(bodyFields);
     }
 }
