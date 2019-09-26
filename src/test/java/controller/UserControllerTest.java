7c77cc4b27f991ff3acb56556787326659ce2dd3
KIMJUNGMIN <kimjungmin.developer@gmail.com>
2019-09-26 20:44:14 +0900

    test: UserControllerTest 리팩토링

--- src/test/java/controller/UserControllerTest.java
+++ src/test/java/controller/UserControllerTest.java
@@ -10,6 +10,7 @@ import http.response.Response;
 import model.User;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import test.BaseTest;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
@@ -18,25 +19,19 @@ import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class UserControllerTest {
+public class UserControllerTest extends BaseTest {
 
     @Test
     @DisplayName("POST /user/create 요청 보낼 시 RedirectResponse 생성 확인")
     void createPostUserResponse() throws IOException, URISyntaxException {
         RequestMethod method = RequestMethod.POST;
         RequestUrl url = RequestUrl.from("/user/create");
-        Map<String, String> information = new HashMap<>();
-        information.put("Request-Line:", "POST /user/create HTTP/1.1");
-        information.put("Host:", "localhost:8080");
-        information.put("Connection:", "keep-alive");
-        information.put("Content-Length:", "59");
-        information.put("Content-Type:", "application/x-www-form-urlencoded");
-        information.put("Accept:", "*/*");
-        information.put("Query-Parameters:", "userId=javajigi&password=password&name=이인권&email=podo@gmail.com");
+        Map<String, String> header = makePostHeader( "POST /user/create HTTP/1.1",
+                "59", "application/x-www-form-urlencoded",
+                "userId=javajigi&password=password&name=이인권&email=podo@gmail.com");
 
 
-        RequestInformation requestInformation = new RequestInformation(information);
-
+        RequestInformation requestInformation = new RequestInformation(header);
         Request request = new Request(method, url, requestInformation);
         Response response = new Response();
 
