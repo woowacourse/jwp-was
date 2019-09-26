85182f2d14d81b91117ce3a2409646bb157edd7a
김성훈 <seonghun127@gmail.com>
2019-09-26 15:05:21 +0900

    feat: user list 출력 시 cookie 값 확인

--- src/main/java/webserver/controller/UserListController.java
+++ src/main/java/webserver/controller/UserListController.java
@@ -5,11 +5,13 @@ import http.response.Response;
 import model.User;
 import model.UserService;
 import webserver.exception.InvalidRequestMethodException;
+import webserver.support.CookieParser;
 import webserver.support.ModelAndView;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.util.List;
+import java.util.Map;
 
 public class UserListController extends HttpController {
 
@@ -22,9 +24,18 @@ public class UserListController extends HttpController {
 
     @Override
     protected void doGet(Request request, Response response) throws IOException, URISyntaxException {
-        List<User> users = new UserService().findAll();
-        ModelAndView modelAndView = new ModelAndView("/user/list.html");
-        modelAndView.setModels("users", users);
+        ModelAndView modelAndView;
+        try {
+            Map<String, String> cookies = CookieParser.parse(request.extractHeader("Cookie"));
+            if (!"true".equals(cookies.get("logined"))) {
+                throw new IllegalArgumentException();
+            }
+            List<User> users = new UserService().findAll();
+            modelAndView = new ModelAndView("user/list");
+            modelAndView.setModels("users", users);
+        } catch (RuntimeException e) {
+            modelAndView = new ModelAndView("/user/login.html");
+        }
         response.configureOkResponse(modelAndView);
     }
 
