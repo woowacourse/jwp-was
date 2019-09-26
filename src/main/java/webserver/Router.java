bf762a3681eed59c00e7bb32b84b68b5027eb6d7
kangmin46 <kangminals5610@gmail.com>
2019-09-26 21:14:26 +0900

    [refactor] change router structure

--- src/main/java/webserver/Router.java
+++ src/main/java/webserver/Router.java
@@ -1,11 +1,15 @@
 package webserver;
 
+import exception.UnregisteredURLException;
 import webserver.controller.AbstractController;
 import webserver.controller.LoginController;
 import webserver.controller.UserController;
+import webserver.controller.UserListController;
 import webserver.controller.request.HttpRequest;
 import webserver.controller.response.HttpResponse;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -15,13 +19,23 @@ public class Router {
     static {
         controllers.put("/user/create", UserController.getInstance());
         controllers.put("/user/login", LoginController.getInstance());
+        controllers.put("/user/list", UserListController.getInstance());
     }
 
-    public static void route(HttpRequest httpRequest, HttpResponse httpResponse) {
+    public static void route(HttpRequest httpRequest, HttpResponse httpResponse) throws IOException, URISyntaxException {
         String path = httpRequest.getPath();
+        String extension = httpRequest.getMimeType().getExtension();
+
+        if(!extension.equals("")) {
+            httpResponse.setResponseBody(httpRequest);
+            return;
+        }
 
         if (controllers.containsKey(path)) {
             controllers.get(path).service(httpRequest, httpResponse);
+            return;
         }
+
+        throw new UnregisteredURLException();
     }
 }
