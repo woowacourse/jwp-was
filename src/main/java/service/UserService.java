189618e94a92c786b055e28b59d1c08739a6c687
backlo <hjun4512@naver.com>
2019-09-27 03:12:31 +0900

    refactor : Response 책임을 RequestHandler 에게 분산

--- src/main/java/service/UserService.java
+++ src/main/java/service/UserService.java
@@ -2,20 +2,20 @@ package service;
 
 import db.DataBase;
 import model.User;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.util.Map;
+import webserver.http.request.HttpRequest;
 
 public class UserService {
-    private static final Logger log = LoggerFactory.getLogger(UserService.class);
     public static UserService getInstance() {
         return UserServiceHolder.INSTANCE;
     }
 
-    public void createUser(Map<String, String> data) {
-        User user = new User(data.get("userId"), data.get("password"), data.get("name"), data.get("email"));
-        log.debug("UserServiceData : {}, {}, {}, {}", data.get("userId"), data.get("password"), data.get("name"), data.get("email"));
+    public void createUser(HttpRequest httpRequest) {
+        User user = new User(
+                httpRequest.getBodyValue("userId"),
+                httpRequest.getBodyValue("password"),
+                httpRequest.getBodyValue("name"),
+                httpRequest.getBodyValue("email")
+        );
         DataBase.addUser(user);
     }
 
--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,10 +1,16 @@
 package webserver;
 
-import http.request.HttpRequest;
-import http.request.HttpRequestFactory;
-import http.response.HttpResponseFactory;
+import controller.ControllerFactory;
+import controller.core.AbstractController;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import webserver.http.HttpHeaderField;
+import webserver.http.HttpVersion;
+import webserver.http.request.HttpRequest;
+import webserver.http.request.HttpRequestFactory;
+import webserver.http.request.core.RequestPath;
+import webserver.http.response.HttpResponse;
+import webserver.http.response.core.ResponseContentType;
 
 import java.io.DataOutputStream;
 import java.io.IOException;
@@ -12,14 +18,24 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
 import java.net.URISyntaxException;
+import java.util.stream.Stream;
 
 public class RequestHandler implements Runnable {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
 
     private Socket connection;
 
-    public RequestHandler(Socket connectionSocket) {
+    RequestHandler(Socket connectionSocket) {
         this.connection = connectionSocket;
+    }
+
+    private static void sendResponse(OutputStream out, HttpResponse httpResponse, HttpRequest httpRequest) throws IOException, URISyntaxException {
+        DataOutputStream dos = new DataOutputStream(out);
+        byte[] body = httpResponse.responseBody(httpRequest.getRequestPath().getFullPath());
+        httpResponse.addHeader(HttpHeaderField.CONTENT_LENGTH, String.valueOf(body.length));
+        dos.writeBytes(httpResponse.doResponse());
+        dos.write(body, 0, body.length);
+        dos.flush();
     }
 
     public void run() {
@@ -27,13 +43,25 @@ public class RequestHandler implements Runnable {
                 connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            DataOutputStream dos = new DataOutputStream(out);
+            HttpRequest httpRequest = HttpRequestFactory.create(in);
+            HttpResponse httpResponse = new HttpResponse(HttpVersion.HTTP_VERSION_1_1);
 
-            HttpRequest request = HttpRequestFactory.parseHttpRequest(in);
-            HttpResponseFactory.sendResponse(request, dos);
+            if (isStaticFile(httpRequest.getRequestPath())) {
+                httpResponse.addStatus();
+                httpResponse.addHeader(HttpHeaderField.of("Content-Type:"), ResponseContentType.of(httpRequest.getRequestPath()));
+                sendResponse(out, httpResponse, httpRequest);
+            } else {
+                AbstractController controller = ControllerFactory.mappingController(httpRequest, httpResponse);
+                controller.service(out, httpRequest, httpResponse);
+            }
 
         } catch (IOException | URISyntaxException e) {
             log.error(e.getMessage());
         }
+    }
+
+    private boolean isStaticFile(RequestPath path) {
+        return Stream.of(".css", ".js", ".png", ".ico", ".eot", ".svg", ".ttf", ".woff", ".woff2")
+                .anyMatch(extension -> path.getPath().contains(extension));
     }
 }
