a36be0f4a5da59cfcc6f5a3ef12b72718acb9ab8
starkim06 <starkim06@naver.com>
2019-09-26 22:01:23 +0900

    feat: AbstractController와 SessionManager 의존성 추가

--- src/main/java/webserver/controller/AbstractController.java
+++ src/main/java/webserver/controller/AbstractController.java
@@ -1,12 +1,17 @@
 package webserver.controller;
 
 import webserver.Controller;
-import webserver.HttpMethod;
-import webserver.HttpRequest;
-import webserver.HttpResponse;
+import webserver.SessionManager;
 import webserver.exception.NotSupportedHttpMethodException;
+import webserver.http.HttpMethod;
+import webserver.http.HttpRequest;
+import webserver.http.HttpResponse;
+import webserver.http.HttpSession;
 
 public abstract class AbstractController implements Controller {
+    public static final String REDIRECT_VIEW = "/redirect:";
+    public static final String ERROR_VIEW = "/error:";
+
     @Override
     public String service(HttpRequest request, HttpResponse response) {
         if (request.checkMethod(HttpMethod.GET)) {
@@ -26,5 +31,35 @@ public abstract class AbstractController implements Controller {
 
     protected String doPost(HttpRequest httpRequest, HttpResponse response) {
         throw new NotSupportedHttpMethodException();
+    }
+
+    //TODO 이 메서드가 여기 있어도 괜찮은가
+    protected HttpSession getSession(HttpRequest httpRequest, HttpResponse httpResponse) {
+        //TODO SessionManager와 의존하는 부분을 이렇게 놔두어도 괜찮을까?
+        SessionManager sessionManager = SessionManager.getInstance();
+        String sessionId = httpRequest.getSessionId();
+        if (checkSession(sessionManager, sessionId)) {
+            sessionId = sessionManager.createSession();
+        }
+        //TODO 이 메서드에서 set까지 해버리는건 이상하지 않을까?
+        httpResponse.setSession(sessionId);
+        return sessionManager.getSession(sessionId);
+    }
+
+    private boolean checkSession(SessionManager sessionManager, String sessionId) {
+        return hasSession(sessionId) || validateSession(sessionManager, sessionId);
+    }
+
+    private boolean hasSession(String sessionId) {
+        return sessionId == null;
+    }
+
+    private boolean validateSession(SessionManager sessionManager, String sessionId) {
+        return sessionManager.getSession(sessionId) == null;
+    }
+
+    protected void setSession(HttpRequest httpRequest, HttpResponse httpResponse, String name, Object value) {
+        HttpSession session = getSession(httpRequest, httpResponse);
+        session.setAttributes(name, value);
     }
 }
