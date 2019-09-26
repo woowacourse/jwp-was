d9443a6541616c191f37e811f5405950e133a741
aiden <cnk9109@gmail.com>
2019-09-26 20:38:07 +0900

    test: test RequestHandler

--- src/test/java/http/request/RequestHandlerTest.java
+++ src/test/java/http/request/RequestHandlerTest.java
@@ -1,5 +1,6 @@
 package http.request;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.io.*;
@@ -10,17 +11,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 class RequestHandlerTest {
     private static final String TEST_DIRECTORY = "./src/test/resources/";
 
-    private HttpRequest httpRequest;
-
     @Test
+    @DisplayName("Request 생성 테스트")
     public void createTest() throws Exception {
         InputStream in = new FileInputStream(new File(TEST_DIRECTORY + "Http_GET.txt"));
         BufferedReader br = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
         RequestHandler requestHandler = new RequestHandler(br);
-        httpRequest = requestHandler.create();
 
-        assertThat(httpRequest.getUrl()).isEqualTo("http://localhost:8080/user/create");
-        assertThat(httpRequest.getHeader("Host")).isEqualTo("localhost:8080");
-        assertThat(httpRequest.getParameter("userId")).isEqualTo("aiden");
+        assertThat(requestHandler.create()).isInstanceOf(HttpRequest.class);
     }
 }
