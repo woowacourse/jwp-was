d70ebe29da9b093943f5059e8daab317f3efcaec
seokhyeonchoi <lkamna12@gmail.com>
2019-09-26 14:03:32 +0900

    feat: Session 구현

---
+++ src/main/java/http/session/Session.java
@@ -0,0 +1,34 @@
+package http.session;
+
+import com.google.common.collect.Maps;
+
+import java.util.Map;
+
+public class Session {
+    private Map<String, Object> attributes = Maps.newHashMap();
+    private final String sessionId;
+
+    public Session(String sessionId) {
+        this.sessionId = sessionId;
+    }
+
+    public void setAttribute(String name, Object value) {
+        attributes.put(name, value);
+    }
+
+    public Object getAttribute(String name) {
+        return attributes.get(name);
+    }
+
+    public void removeAttribute(String name) {
+        attributes.remove(name);
+    }
+
+    public String getId() {
+        return sessionId;
+    }
+
+    public void invalidate() {
+        attributes = Maps.newHashMap();
+    }
+}
---
+++ src/main/java/http/session/SessionRepository.java
@@ -0,0 +1,35 @@
+package http.session;
+
+import com.google.common.collect.Maps;
+
+import java.util.Map;
+import java.util.UUID;
+
+public class SessionRepository {
+    private final Map<String, Session> sessions = Maps.newHashMap();
+
+    private SessionRepository() {
+    }
+
+    private static class SessionRepositoryLazyHolder {
+        private static final SessionRepository INSTANCE = new SessionRepository();
+    }
+
+    public static SessionRepository getInstance() {
+        return SessionRepositoryLazyHolder.INSTANCE;
+    }
+
+    public Session getSession(String sessionId) {
+        return sessions.get(sessionId);
+    }
+
+    public Session createSession() {
+        String uuid = UUID.randomUUID().toString();
+
+        while (sessions.get(uuid) != null) {
+            uuid = UUID.randomUUID().toString();
+        }
+
+        return new Session(uuid);
+    }
+}
