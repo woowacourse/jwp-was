f127c7881fd961e27290d52b6e51b7ee02e59ab2
aiden <cnk9109@gmail.com>
2019-09-26 20:57:06 +0900

    test: test RequestLine

--- src/test/java/http/request/RequestLineTest.java
+++ src/test/java/http/request/RequestLineTest.java
@@ -2,6 +2,7 @@ package http.request;
 
 import http.common.HttpMethod;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -15,21 +16,25 @@ class RequestLineTest {
     }
 
     @Test
+    @DisplayName("요청 method 테스트")
     public void getMethodTest() {
         assertThat(requestLine.getMethod()).isEqualTo(HttpMethod.GET);
     }
 
     @Test
+    @DisplayName("요청 path 테스트")
     public void getPathTest() {
         assertThat(requestLine.getPath()).isEqualTo("/user/create");
     }
 
     @Test
+    @DisplayName("요청 query 테스트")
     public void getQueryStringTest() {
         assertThat(requestLine.getQueryString()).isEqualTo("userId=aiden&password=password&name=aiden&email=aiden@aiden.com");
     }
 
     @Test
+    @DisplayName("요청 Protocol 테스트")
     public void getProtocolTest() {
         assertThat(requestLine.getProtocol()).isEqualTo("http");
     }
