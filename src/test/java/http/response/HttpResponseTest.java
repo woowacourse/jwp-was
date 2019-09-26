1a8f8a99c62c243ee8a5ecc6fe91476b15be2d3f
aiden <cnk9109@gmail.com>
2019-09-26 21:26:28 +0900

    test: test HttpResponse

--- src/test/java/http/response/HttpResponseTest.java
+++ src/test/java/http/response/HttpResponseTest.java
@@ -2,6 +2,7 @@ package http.response;
 
 import http.common.HttpCookie;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -16,6 +17,7 @@ class HttpResponseTest {
     }
 
     @Test
+    @DisplayName("Header 추가 테스트")
     public void putHeaderTest() {
         httpResponse.putHeader("Connection", "keep-alive");
 
@@ -23,19 +25,47 @@ class HttpResponseTest {
     }
 
     @Test
-    public void setResponseBodyTest() {
+    @DisplayName("Body 추가 테스트")
+    public void sendOKTest() {
         String body = "HELLO!";
         httpResponse.sendOk(body.getBytes(), "test.html");
 
         assertThat(httpResponse.getBody()).isEqualTo(body.getBytes());
+        assertThat(httpResponse.toString()).contains("200 OK");
         assertThat(httpResponse.toString()).contains("Content-Type: text/html;charset=utf-8");
     }
 
     @Test
+    @DisplayName("redirect 테스트")
     public void redirectTest() {
         httpResponse.redirect("index.html");
 
         assertThat(httpResponse.toString()).contains("302 FOUND");
         assertThat(httpResponse.toString()).contains("Location: index.html");
+    }
+
+    @Test
+    @DisplayName("body byte[] 테스트")
+    public void getByteTest() {
+        String body = "HELLO!";
+        httpResponse.sendOk(body.getBytes(), "test.html");
+
+        assertThat(httpResponse.getBody()).contains(body.getBytes());
+    }
+
+    @Test
+    @DisplayName("NotFound 테스트")
+    public void notFoundTest() {
+        httpResponse.sendNotFound();
+
+        assertThat(httpResponse.toString()).contains("404 NOT FOUND");
+    }
+
+    @Test
+    @DisplayName("NotAllowed 테스트")
+    public void sendNotAllowedTest() {
+        httpResponse.sendNotAllowed();
+
+        assertThat(httpResponse.toString()).contains("405 Method Not Allowed");
     }
 }
