687f6a8e91088c619728c7192f414e59ada13ebb
aiden <cnk9109@gmail.com>
2019-09-26 21:47:14 +0900

    test: test ResponseHeader

--- src/test/java/http/response/ResponseHeaderTest.java
+++ src/test/java/http/response/ResponseHeaderTest.java
@@ -1,6 +1,7 @@
 package http.response;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -14,6 +15,7 @@ class ResponseHeaderTest {
     }
 
     @Test
+    @DisplayName("Header put 테스트")
     public void putTest() {
         responseHeader.put("Content-Length", "1");
 
