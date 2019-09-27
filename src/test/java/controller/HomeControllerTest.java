97182fbf1338c042a6926c25691b07a887bff540
soojinroh <rsj3112@gmail.com>
2019-09-27 23:46:40 +0900

    test : root url mapping method test 코드 추가

--- src/test/java/controller/HomeControllerTest.java
+++ src/test/java/controller/HomeControllerTest.java
@@ -4,26 +4,25 @@ import model.http.HttpRequest;
 import model.http.HttpResponse;
 import org.junit.jupiter.api.Test;
 import utils.HttpStatus;
+import utils.RequestDispatcher;
 import utils.RequestHeaderParser;
 
-import java.io.*;
-import java.net.Socket;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class HomeControllerTest {
     @Test
     void index_페이지() throws IOException {
-        FileInputStream fileInputStream = new FileInputStream("src/test/java/data/http_request.txt");
+        FileInputStream fileInputStream = new FileInputStream("src/test/java/data/HttpRequest.txt");
         InputStreamReader inputStream = new InputStreamReader(fileInputStream);
         HttpRequest httpRequest = RequestHeaderParser.parseRequest(inputStream);
+        HttpResponse httpResponse = HttpResponse.of();
 
-//        Socket socket = new Socket();
-//        HttpResponse httpResponse = new HttpResponse(socket.getOutputStream());
-//
-//        Controller controller = new HomeController();
-//        controller.service(httpRequest, httpResponse);
-//
-//        assertThat(httpResponse.getHttpStatusCode()).isEqualTo(HttpStatus.OK);
+        RequestDispatcher.handle(httpRequest, httpResponse);
+        assertThat(httpResponse.getHttpStatus()).isEqualTo(HttpStatus.OK);
+        assertThat(httpResponse.getPath()).isEqualTo("./templates/index.html");
     }
 }
