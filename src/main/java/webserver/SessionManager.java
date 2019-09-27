a3a580637fc61cb4a11409f875c796658bd1aded
starkim06 <starkim06@naver.com>
2019-09-27 11:05:03 +0900

    refactor: 싱글톤에서 static 메서드로 변경

--- src/main/java/webserver/SessionManager.java
+++ src/main/java/webserver/SessionManager.java
@@ -7,28 +7,16 @@ import java.util.Map;
 import java.util.UUID;
 
 public class SessionManager {
-    private Map<UUID, HttpSession> sessions;
+    private static Map<UUID, HttpSession> sessions = new HashMap<>();
 
-    private SessionManager() {
-        this.sessions = new HashMap<>();
-    }
-
-    public static SessionManager getInstance() {
-        return LazyHolder.INSTANCE;
-    }
-
-    public String createSession() {
+    public static String createSession() {
         UUID uuid = UUID.randomUUID();
         HttpSession httpSession = new HttpSession(uuid);
         sessions.put(uuid, httpSession);
         return uuid.toString();
     }
 
-    public HttpSession getSession(String uuid) {
+    public static HttpSession getSession(String uuid) {
         return sessions.get(UUID.fromString(uuid));
-    }
-
-    private static final class LazyHolder {
-        private static final SessionManager INSTANCE = new SessionManager();
     }
 }
