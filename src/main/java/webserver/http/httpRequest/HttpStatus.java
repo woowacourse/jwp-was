d52846159e73e12684e15afa9ac282536fe41314
starkim06 <starkim06@naver.com>
2019-09-27 22:50:07 +0900

    refactor: 사용하지 않는 404상태코드 삭제

--- src/main/java/webserver/http/httpRequest/HttpStatus.java
+++ src/main/java/webserver/http/httpRequest/HttpStatus.java
@@ -3,7 +3,6 @@ package webserver.http.httpRequest;
 public enum HttpStatus {
     OK(200, "OK"),
     FOUND(302, "Found"),
-    NOT_FOUND(404, "Not Found"),
     METHOD_NOT_ALLOWED(405, "Method Not Allowed");
 
     private final int code;
@@ -12,6 +11,16 @@ public enum HttpStatus {
     HttpStatus(int code, String message) {
         this.code = code;
         this.message = message;
+    }
+
+    public static HttpStatus values(String inputCode) {
+        int code = Integer.parseInt(inputCode);
+        for (HttpStatus value : values()) {
+            if (value.code == code) {
+                return value;
+            }
+        }
+        throw new IllegalArgumentException("올바르지 않은 상태코드 입니다.");
     }
 
     public int getCode() {
@@ -24,15 +33,5 @@ public enum HttpStatus {
 
     public String getCodeAndMessage() {
         return code + " " + message;
-    }
-
-    public static HttpStatus values(String inputCode) {
-        int code = Integer.parseInt(inputCode);
-        for (HttpStatus value : values()) {
-            if (value.code == code) {
-                return value;
-            }
-        }
-        throw new IllegalArgumentException("올바르지 않은 상태코드 입니다.");
     }
 }
