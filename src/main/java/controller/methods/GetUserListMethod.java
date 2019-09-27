bf99292d7f930e625afcad850917cee37742005f
KIMJUNGMIN <kimjungmin.developer@gmail.com>
2019-09-27 22:56:38 +0900

    refactor:clean up code

--- src/main/java/controller/methods/GetUserListMethod.java
+++ src/main/java/controller/methods/GetUserListMethod.java
@@ -1,22 +1,12 @@
 package controller.methods;
 
-import com.github.jknack.handlebars.Handlebars;
-import com.github.jknack.handlebars.Template;
-import com.github.jknack.handlebars.io.ClassPathTemplateLoader;
-import com.github.jknack.handlebars.io.TemplateLoader;
 import controller.support.TemplateManager;
-import db.DataBase;
 import http.request.Request;
 import http.request.RequestMethod;
 import http.response.Response;
 import http.session.Session;
-import model.User;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 public class GetUserListMethod implements ControllerMethod {
 
