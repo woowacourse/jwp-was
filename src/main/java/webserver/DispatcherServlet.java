014e746ed43b4a2c81118041ab5574f849996403
kdg <kmdngyu@gmail.com>
2019-09-28 15:40:51 +0900

    refactor: RequestDispatcher의 프로세스 역할을 DispatcherSevlet으로 분리

---
+++ src/main/java/webserver/DispatcherServlet.java
@@ -0,0 +1,44 @@
+package webserver;
+
+import exceptions.MethodNotAllowedException;
+import exceptions.NotFoundFileException;
+import exceptions.NotFoundURIException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import webserver.handler.MappingHandler;
+import webserver.http.request.HttpRequest;
+import webserver.http.response.HttpResponse;
+import webserver.http.response.HttpStatus;
+import webserver.servlet.HttpServlet;
+import webserver.view.ModelAndView;
+import webserver.view.View;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+public class DispatcherServlet {
+    private static final Logger logger = LoggerFactory.getLogger(DispatcherServlet.class);
+    private HttpResponse httpResponse;
+
+    public DispatcherServlet(HttpResponse httpResponse) {
+        this.httpResponse = httpResponse;
+    }
+
+    public void dispatch(HttpRequest httpRequest) throws IOException {
+        try {
+            HttpServlet httpServlet = MappingHandler.getServlets(httpRequest.getUri());
+            ModelAndView modelAndView = httpServlet.run(httpRequest, httpResponse);
+            View view = modelAndView.getView();
+            view.render(modelAndView.getModelMap(), httpRequest, httpResponse);
+        } catch (NotFoundFileException | NotFoundURIException e) {
+            logger.error(e.getMessage());
+            httpResponse.error(HttpStatus.NOT_FOUND);
+        } catch (MethodNotAllowedException e) {
+            logger.error(e.getMessage());
+            httpResponse.error(HttpStatus.METHOD_NOT_ALLOW);
+        } catch (URISyntaxException e) {
+            logger.error(e.getMessage());
+            httpResponse.error(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
