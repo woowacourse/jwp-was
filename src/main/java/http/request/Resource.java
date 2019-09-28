7b33d4512d74d50caa0bcd5a68a31a89dea39ac3
hyunsoojung <jhs695@naver.com>
2019-09-28 22:25:18 +0900

    refactor: fileName -> target 이름 변경, getUri() 반환값 변경

--- src/main/java/http/request/Resource.java
+++ src/main/java/http/request/Resource.java
@@ -5,41 +5,41 @@ import http.MediaType;
 import java.util.Objects;
 
 public class Resource {
-    private String path;
-    private String fileName;
-    private MediaType mediaType;
+	private String path;
+	private String target;
+	private MediaType mediaType;
 
-    public Resource(String path, String fileName, MediaType mediaType) {
-        this.path = path;
-        this.fileName = fileName;
-        this.mediaType = mediaType;
-    }
+	public Resource(String path, String target, MediaType mediaType) {
+		this.path = path;
+		this.target = target;
+		this.mediaType = mediaType;
+	}
 
-    public String getUri() {
-        return path + fileName + mediaType.getExtensionPath();
-    }
+	public String getUri() {
+		return path + target;
+	}
 
-    @Override
-    public String toString() {
-        return "Resource{" +
-                "path='" + path + '\'' +
-                ", fileName='" + fileName + '\'' +
-                ", mediaType=" + mediaType +
-                '}';
-    }
+	@Override
+	public String toString() {
+		return "Resource{" +
+				"path='" + path + '\'' +
+				", fileName='" + target + '\'' +
+				", mediaType=" + mediaType +
+				'}';
+	}
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Resource resource = (Resource) o;
-        return path.equals(resource.path) &&
-                fileName.equals(resource.fileName) &&
-                mediaType == resource.mediaType;
-    }
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		Resource resource = (Resource) o;
+		return path.equals(resource.path) &&
+				target.equals(resource.target) &&
+				mediaType == resource.mediaType;
+	}
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(path, fileName, mediaType);
-    }
+	@Override
+	public int hashCode() {
+		return Objects.hash(path, target, mediaType);
+	}
 }
