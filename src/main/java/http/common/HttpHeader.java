f6a2cc25c7e8ad8a4ab15960c8cdf945aa2e44da
school0bhy <house1217@ajou.ac.kr>
2019-09-28 14:28:49 +0900

    feat: Cookie 구현

--- src/main/java/http/common/HttpHeader.java
+++ src/main/java/http/common/HttpHeader.java
@@ -1,6 +1,7 @@
 package http.common;
 
 import com.google.common.collect.Lists;
+import http.Cookie;
 import http.common.exception.InvalidHeaderKeyException;
 import http.common.exception.InvalidHttpHeaderException;
 import utils.StringUtils;
@@ -9,15 +10,18 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class HttpHeader {
+    public static final String EMPTY_HEADER_NAME_ERROR_MESSAGE = "헤더 이름의 값이 필요합니다.";
+
     private static final String HEADER_LINE_DELIMITER = ": ";
     public static final String HEADER_VALUES_DELIMITER = "; ";
+    private static final String COOKIE = "Cookie";
     private static final String HEADER_FIELD_FORMAT = "%s: %s\r\n";
     private static final int HTTP_HEADER_PARAMETER_SIZE = 2;
     private static final int HEADER_FIELD_KEY_INDEX = 0;
     private static final int HEADER_FIELD_VALUE_INDEX = 1;
-    public static final String EMPTY_HEADER_NAME_ERROR_MESSAGE = "헤더 이름의 값이 필요합니다.";
 
     private final Map<String, List<String>> httpHeader = new HashMap<>();
+    private final Cookie cookie = new Cookie();
 
     public HttpHeader() {
     }
@@ -25,6 +29,7 @@ public class HttpHeader {
     public HttpHeader(List<String> header) {
         if (header != null) {
             header.forEach(this::addHeader);
+            cookie.addAll(httpHeader.get(COOKIE));
         }
     }
 
