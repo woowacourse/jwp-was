30f4b7602a60cde89d682e852c3c6eab4853c242
hyunsoojung <jhs695@naver.com>
2019-09-28 22:14:06 +0900

    refactor: 지원하지 않는 메소드 에러 메세지 추가, controller에서 response 생성 제거

--- src/main/java/controller/CreateUserController.java
+++ src/main/java/controller/CreateUserController.java
@@ -1,12 +1,10 @@
 package controller;
 
+import controller.exception.NotSupportMethod;
 import db.DataBase;
 import http.HttpStatusCode;
 import http.request.HttpRequest;
 import http.response.HttpResponse;
-import http.response.HttpResponseBody;
-import http.response.HttpResponseHeader;
-import http.response.HttpResponseStatusLine;
 import model.User;
 
 import java.io.IOException;
@@ -14,43 +12,31 @@ import java.net.URISyntaxException;
 
 public class CreateUserController extends AbstractController {
 
-    @Override
-    void doGet(HttpRequest httpRequest, HttpResponse httpResponse) {
+	public static final String PATH = "/user/create";
 
-    }
+	@Override
+	void doGet(HttpRequest httpRequest, HttpResponse httpResponse) {
+		throw new NotSupportMethod("Not Support : " + httpRequest.getUri() + httpRequest.getMethod());
+	}
 
-    @Override
-    void doPost(HttpRequest httpRequest, HttpResponse httpResponse) throws IOException, URISyntaxException {
-        save(httpRequest.getParameter("userId"),
-                httpRequest.getParameter("password"),
-                httpRequest.getParameter("name"),
-                httpRequest.getParameter("email"));
+	@Override
+	void doPost(HttpRequest httpRequest, HttpResponse httpResponse) throws IOException, URISyntaxException {
+		save(httpRequest.getParameter("userId"),
+				httpRequest.getParameter("password"),
+				httpRequest.getParameter("name"),
+				httpRequest.getParameter("email"));
 
-        HttpResponseStatusLine httpResponseStatusLine;
-        HttpResponseHeader HttpResponseHeader;
-        HttpResponseBody httpResponseBody;
-        try {
-            httpResponseStatusLine = new HttpResponseStatusLine(httpRequest.getHttpVersion(), HttpStatusCode.of("302"));
-            HttpResponseHeader = new HttpResponseHeader("Location: /index.html");
-            httpResponseBody = new HttpResponseBody("");
-        } catch (IOException e) {
-            httpResponseStatusLine = new HttpResponseStatusLine(httpRequest.getHttpVersion(), HttpStatusCode.of("404"));
-            HttpResponseHeader = new HttpResponseHeader("");
-            httpResponseBody = new HttpResponseBody("");
-        }
+		httpResponse.setStatusCode(HttpStatusCode.FOUND);
+		httpResponse.redirect("index.html");
+	}
 
-        httpResponse.setHttpResponseStatusLine(httpResponseStatusLine);
-        httpResponse.setHttpResponseHeader(HttpResponseHeader);
-        httpResponse.setHttpResponseBody(httpResponseBody);
-    }
+	private void save(String userId, String password, String name, String email) {
+		if (DataBase.findUserById(userId) == null) {
+			User user = new User(userId, password, name, email);
+			DataBase.addUser(user);
+			return;
+		}
 
-    private void save(String userId, String password, String name, String email) {
-        if (DataBase.findUserById(userId) == null) {
-            User user = new User(userId, password, name, email);
-            DataBase.addUser(user);
-            return;
-        }
-
-        throw new IllegalArgumentException();
-    }
+		throw new IllegalArgumentException();
+	}
 }
--- src/main/java/controller/FileController.java
+++ src/main/java/controller/FileController.java
@@ -1,11 +1,9 @@
 package controller;
 
+import controller.exception.NotSupportMethod;
 import http.HttpStatusCode;
 import http.request.HttpRequest;
 import http.response.HttpResponse;
-import http.response.HttpResponseBody;
-import http.response.HttpResponseHeader;
-import http.response.HttpResponseStatusLine;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -13,33 +11,27 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 
 public class FileController extends AbstractController {
-    private static final Logger logger = LoggerFactory.getLogger(FileController.class);
+	private static final Logger logger = LoggerFactory.getLogger(FileController.class);
 
-    @Override
-    void doPost(HttpRequest httpRequest, HttpResponse httpResponse) {
+	public FileController() {
+	}
 
-    }
+	@Override
+	void doPost(HttpRequest httpRequest, HttpResponse httpResponse) {
+		throw new NotSupportMethod("Not Support : " + httpRequest.getUri() + " " + httpRequest.getMethod());
+	}
 
-    @Override
-    void doGet(HttpRequest httpRequest, HttpResponse httpResponse) throws IOException, URISyntaxException {
-        String uri = httpRequest.getUri();
+	@Override
+	void doGet(HttpRequest httpRequest, HttpResponse httpResponse) {
+		logger.debug("request file: {}", httpRequest.getUri());
 
-        HttpResponseStatusLine httpResponseStatusLine;
-        HttpResponseHeader HttpResponseHeader;
-        HttpResponseBody httpResponseBody;
-        try {
-            httpResponseStatusLine = new HttpResponseStatusLine(httpRequest.getHttpVersion(), HttpStatusCode.of("200"));
-            HttpResponseHeader = new HttpResponseHeader("Location: /" + httpRequest.getUri());
-            httpResponseBody = new HttpResponseBody(uri);
-        } catch (IOException | URISyntaxException e) {
-            httpResponseStatusLine = new HttpResponseStatusLine(httpRequest.getHttpVersion(), HttpStatusCode.of("404"));
-            HttpResponseHeader = new HttpResponseHeader("");
-            httpResponseBody = new HttpResponseBody("");
-            logger.error(e.getMessage());
-        }
-
-        httpResponse.setHttpResponseStatusLine(httpResponseStatusLine);
-        httpResponse.setHttpResponseHeader(HttpResponseHeader);
-        httpResponse.setHttpResponseBody(httpResponseBody);
-    }
+		try {
+			httpResponse.setStatusCode(HttpStatusCode.OK);
+			httpResponse.forward(httpRequest.getUri());
+		} catch (IOException | URISyntaxException e) {
+			httpResponse.setStatusCode(HttpStatusCode.NOT_FOUND);
+			e.printStackTrace();
+		}
+	}
 }
+
