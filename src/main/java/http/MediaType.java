cca4bfbd47c74c4c13ff55901eb20015a8746525
hyunsoojung <jhs695@naver.com>
2019-09-28 22:23:53 +0900

    refactor: 확장자 없는 경우, 폰트 확장자 추가

--- src/main/java/http/MediaType.java
+++ src/main/java/http/MediaType.java
@@ -1,63 +1,69 @@
 package http;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.Arrays;
 
 public enum MediaType {
-    HTML("html", "templates", "text/html"),
-    CSS("css", "static", "text/css"),
-    JS("js", "static", "application/js"),
-    ICO("ico", "static", "image/x-icon");
+	HTML("html", "templates", "text/html"),
+	CSS("css", "static", "text/css"),
+	JS("js", "static", "application/js"),
+	ICO("ico", "static", "image/x-icon"),
+	NONE("", "", ""),
+	WOFF("woff", "static", "application/x-font-woff"),
+	TTF("ttf", "static", "application/x-font-ttf");
 
-    private String extension;
-    private String path;
-    private String contentType;
+	private String extension;
+	private String path;
+	private String contentType;
 
-    MediaType(String extension, String path, String contentType) {
-        this.extension = extension;
-        this.path = path;
-        this.contentType = contentType;
-    }
+	MediaType(String extension, String path, String contentType) {
+		this.extension = extension;
+		this.path = path;
+		this.contentType = contentType;
+	}
 
-    public static MediaType of(String mediaType) {
-        return Arrays.stream(values())
-                .filter(value -> value.extension.equals(mediaType))
-                .findFirst()
-                .orElseThrow(IllegalArgumentException::new)
-                ;
-    }
+	public static MediaType of(String mediaType) {
+		return Arrays.stream(values())
+				.filter(value -> value.extension.equals(mediaType))
+				.findFirst()
+				.orElseThrow(IllegalArgumentException::new)
+				;
+	}
 
-    public String getExtension() {
-        return extension;
-    }
+	public static String getFullPath(String uri) {
+		Logger logger = LoggerFactory.getLogger(MediaType.class);
 
-    public String getExtensionPath() {
-        return extension.equals("") ? "" : "." + getExtension();
-    }
+		logger.debug("uri: {}", uri);
 
-    public static String getFullPath(String uri) {
-        return Arrays.stream(values())
-                .filter(value -> uri.contains(value.extension))
-                .map(value -> "./" + value.path + uri)
-                .findFirst()
-                .orElseThrow(IllegalArgumentException::new)
-                ;
-    }
+		return Arrays.stream(values())
+				.filter(value -> uri.contains(value.extension))
+				.map(value -> "./" + value.path + uri)
+				.findFirst()
+				.orElseThrow(IllegalArgumentException::new)
+				;
+	}
 
-    public static String getContentType(String uri) {
-        return Arrays.stream(values())
-                .filter(value -> uri.contains(value.extension))
-                .map(value -> value.contentType)
-                .findFirst()
-                .orElseThrow(IllegalArgumentException::new)
-                ;
-    }
+	public static String getContentType(String uri) {
+		return Arrays.stream(values())
+				.filter(value -> uri.contains(value.extension))
+				.map(value -> value.contentType)
+				.findFirst()
+				.orElseThrow(IllegalArgumentException::new)
+				;
+	}
 
-    public static boolean isContain(String extension) {
-        return Arrays.stream(values())
-                .map(value -> extension.contains(value.extension))
-                .filter(value -> value)
-                .findFirst()
-                .orElse(false)
-                ;
-    }
+	public static boolean isContain(String extension) {
+		if (!extension.contains(".")) {
+			return false;
+		}
+
+		return Arrays.stream(values())
+				.map(value -> extension.contains(value.extension))
+				.filter(value -> value)
+				.findFirst()
+				.orElse(false)
+				;
+	}
 }
