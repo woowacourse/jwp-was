25887ecb1e04676d3bfbd5f4089b4410e3f57eef
CODEMCD <psbum1609@gmail.com>
2019-09-28 10:10:00 +0900

    refactor: 파일 확장자를 가져오는 메서드 추가
    * FileIoUtils 클래스에 파일 확장자를 반환하는 메서드를 구현하였다.

--- src/main/java/utils/FileIoUtils.java
+++ src/main/java/utils/FileIoUtils.java
@@ -5,6 +5,7 @@ import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Optional;
 
 public class FileIoUtils {
     public static boolean isExistFile(String filePath) throws URISyntaxException {
@@ -18,5 +19,14 @@ public class FileIoUtils {
     public static byte[] loadFileFromClasspath(String filePath) throws IOException, URISyntaxException {
         Path path = Paths.get(FileIoUtils.class.getClassLoader().getResource(filePath).toURI());
         return Files.readAllBytes(path);
+    }
+
+    public static Optional<String> getExtension(String filePath) {
+        String[] pathTokens = filePath.split("\\.");
+        if (pathTokens.length > 1) {
+            return Optional.of(pathTokens[pathTokens.length - 1]);
+        }
+
+        return Optional.empty();
     }
 }
