8eac991a2cfd663db5aae176ea2edd310cee4028
CODEMCD <psbum1609@gmail.com>
2019-09-28 09:59:58 +0900

    refactor: HTTP RequestParser 객체 변경
    * 변경된 Request 객체를 파서하도록 로직을 구현하였다.

--- src/main/java/http/HttpRequestParser.java
+++ src/main/java/http/HttpRequestParser.java
@@ -8,91 +8,71 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.URLDecoder;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 public class HttpRequestParser {
     private static final Logger log = LoggerFactory.getLogger(HttpRequestParser.class);
+
+    private static final String START_LINE_DELIMITER = " ";
 
     public static HttpRequest parse(InputStream in) {
         HttpRequest.HttpRequestBuilder builder = new HttpRequest.HttpRequestBuilder();
 
         try {
-            List<String> message = new ArrayList<>();
             BufferedReader br = new BufferedReader(new InputStreamReader(in));
-            String line = br.readLine();
-            message.add(line);
-            HttpMethod method = parseStartLine(line, builder);
-            HttpHeader headers = parseHeaders(br, builder, message);
 
-            if (headers.getContentLength() > 0) {
-                String body = parseBody(br, method, headers, builder);
-                message.add(body);
-            }
+            HttpRequestLine httpRequestLine = parseRequestLine(br);
+            HttpHeader httpHeader = parseHeader(br);
+            HttpBody httpBody = parserBody(br, httpHeader);
 
-            log.info(String.join("\n", message));
+            printLogRequestMessage(httpRequestLine, httpHeader, httpBody);
 
-            return builder.build();
+            return builder
+                    .requestLine(httpRequestLine)
+                    .header(httpHeader)
+                    .body(httpBody)
+                    .build();
         } catch (IOException e) {
             log.error(e.getMessage());
             throw new IllegalArgumentException();
         }
     }
 
-    private static HttpMethod parseStartLine(String startLine, HttpRequest.HttpRequestBuilder builder) {
-        String[] startLineTokens = startLine.split(" ");
+    private static HttpRequestLine parseRequestLine(BufferedReader br) throws IOException {
+        String startLine = br.readLine();
+        String[] startLineTokens = startLine.split(START_LINE_DELIMITER);
         HttpMethod method = HttpMethod.of(startLineTokens[0]);
-        builder.method(method);
-        if (startLineTokens[1].contains("?")) {
-            String[] path = startLineTokens[1].split("\\?");
-            builder.uri(path[0])
-                    .requestParameter(new RequestParameter(parseQueryString(path[1])));
-            return method;
-        }
-
-        builder.uri(startLineTokens[1]);
-        return method;
+        HttpUri uri = new HttpUri(startLineTokens[1]);
+        HttpVersion version = HttpVersion.of(startLineTokens[2]);
+        return new HttpRequestLine(method, uri, version);
     }
 
-    private static HttpHeader parseHeaders(BufferedReader br, HttpRequest.HttpRequestBuilder builder, List<String> message) throws IOException {
+    private static HttpHeader parseHeader(BufferedReader br) throws IOException {
         List<String> headerLines = new ArrayList<>();
         String header = br.readLine();
         while (!"".equals(header)) {
             if (header == null) {
                 break;
             }
+
             headerLines.add(header);
             header = br.readLine();
         }
-        HttpHeader headers = new HttpHeader(headerLines);
-        builder.headers(headers);
 
-        message.addAll(headerLines);
-        message.add("");
-
-        return headers;
+        return new HttpHeader(headerLines);
     }
 
-    private static String parseBody(BufferedReader br, HttpMethod method, HttpHeader headers, HttpRequest.HttpRequestBuilder builder) throws IOException {
-        String body = IOUtils.readData(br, headers.getContentLength());
-        if (method.equals(HttpMethod.POST)
-                && headers.getValue("Content-Type").equals("application/x-www-form-urlencoded")) {
-            RequestParameter requestBody = new RequestParameter(parseQueryString(URLDecoder.decode(body, "UTF-8")));
-            builder.requestBody(requestBody);
+    private static HttpBody parserBody(BufferedReader br, HttpHeader httpHeader) throws IOException {
+        if (httpHeader.getContentLength() <= 0) {
+            return new HttpBody("");
         }
 
-        builder.body(body);
-        return body;
+        return new HttpBody(IOUtils.readData(br, httpHeader.getContentLength()));
     }
 
-    private static Map<String, String> parseQueryString(String queryString) {
-        return Arrays.stream(queryString.split("&"))
-                .map(query -> query.split("="))
-                .collect(Collectors.toMap(q -> q[0], q -> q[1]))
-                ;
+    private static void printLogRequestMessage(HttpRequestLine httpRequestLine, HttpHeader httpHeader, HttpBody httpBody) {
+        log.info("\n----------\n{}\n----------",
+                httpRequestLine.toString() + httpHeader.toString() + httpBody.toString());
     }
 }
