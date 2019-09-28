58b204d50578b384e1df800cf71e804cc2074d01
김성훈 <seonghun127@gmail.com>
2019-09-28 17:13:16 +0900

    refactor: 쓰이지 않는 클래스 삭제

--- src/main/java/http/exception/NotFoundCookieException.java
+++
@@ -1,7 +0,0 @@
-package http.exception;
-
-public class NotFoundCookieException extends RuntimeException {
-    public NotFoundCookieException() {
-        super("Not Found Cookie");
-    }
-}
--- src/main/java/http/exception/NotFoundHeaderException.java
+++
@@ -1,7 +0,0 @@
-package http.exception;
-
-public class NotFoundHeaderException extends RuntimeException {
-    public NotFoundHeaderException() {
-        super("Not Found Header");
-    }
-}
