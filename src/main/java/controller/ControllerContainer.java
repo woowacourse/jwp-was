54cd89d8bba1a81cf43735ee2637d15a9ec7bd2c
hyunsoojung <jhs695@naver.com>
2019-09-28 22:20:14 +0900

    refactor: getController() optional 추가

--- src/main/java/controller/ControllerContainer.java
+++ src/main/java/controller/ControllerContainer.java
@@ -1,17 +1,28 @@
 package controller;
 
+import controller.exception.ControllerNotFoundException;
+
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 public class ControllerContainer {
 	private static Map<String, Controller> controllers = new HashMap<>();
 
 	static {
-		controllers.put("/user/create", new CreateUserController());
-		controllers.put("/", new FileController());
+		controllers.put(IndexController.PATH, new IndexController());
+		controllers.put(CreateUserController.PATH, new CreateUserController());
+		controllers.put(LoginUserController.PATH, new LoginUserController());
+		controllers.put("file", new FileController());
 	}
 
-	public static Controller getController(String uri) {
-		return controllers.get(uri);
+	public static Controller getController(boolean isContainExtension, String uri) {
+		if (isContainExtension) {
+			return controllers.get("file");
+		}
+
+		return Optional.ofNullable(controllers.get(uri))
+				.orElseThrow(() -> new ControllerNotFoundException("not found" + uri))
+				;
 	}
 }
