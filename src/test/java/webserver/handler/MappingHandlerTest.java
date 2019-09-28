de7a6298abde86db5272a41ba013cce5d978caab
kdg <kmdngyu@gmail.com>
2019-09-29 05:09:17 +0900

    fix: mappingHandlerTest 보강

--- src/test/java/webserver/handler/MappingHandlerTest.java
+++ src/test/java/webserver/handler/MappingHandlerTest.java
@@ -3,10 +3,7 @@ package webserver.handler;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import webserver.http.request.RequestUri;
-import webserver.servlet.FileServlet;
-import webserver.servlet.HomeServlet;
-import webserver.servlet.HttpServlet;
-import webserver.servlet.UserCreateServlet;
+import webserver.servlet.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -16,8 +13,14 @@ class MappingHandlerTest {
     void getServlets_request_true() {
         HttpServlet homeServlet = MappingHandler.getServlets(new RequestUri("/"));
         HttpServlet userCreateServlet = MappingHandler.getServlets(new RequestUri("/user/create"));
+        HttpServlet userListServlet = MappingHandler.getServlets(new RequestUri("/user/list"));
+        HttpServlet userLoginServlet = MappingHandler.getServlets(new RequestUri("/user/login"));
         assertThat(homeServlet).isInstanceOf(HomeServlet.class);
         assertThat(userCreateServlet).isInstanceOf(UserCreateServlet.class);
+        assertThat(userListServlet).isInstanceOf(UserListServlet.class);
+        assertThat(userLoginServlet).isInstanceOf(UserLoginServlet.class);
+
+
     }
 
     @DisplayName("정적 파일 서블릿 매핑")
--- src/test/java/webserver/servlet/FileServletTest.java
+++ src/test/java/webserver/servlet/FileServletTest.java
@@ -16,16 +16,14 @@ class FileServletTest {
     @DisplayName("정적 html파일 가져오기")
     @Test
     void run_httpFileRequest_ok() throws IOException {
-        BufferedReader br = IOHelper.createBuffer(
-                "GET /index.html HTTP/1.1",
-                "Host: localhost:8080",
-                "Connection: keep-alive",
-                "Accept: text/html,*/*"
-        );
-        FileServlet fileServlet =  new FileServlet();
-        HttpRequest httpRequest = HttpRequestParser.parse(br);
-        HttpResponse httpResponse = new HttpResponse(new DataOutputStream(null), HttpVersion.HTTP1);
-
-
+//        BufferedReader br = IOHelper.createBuffer(
+//                "GET /index.html HTTP/1.1",
+//                "Host: localhost:8080",
+//                "Connection: keep-alive",
+//                "Accept: text/html,*/*"
+//        );
+//        FileServlet fileServlet =  new FileServlet();
+//        HttpRequest httpRequest = HttpRequestParser.parse(br);
+//        HttpResponse httpResponse = new HttpResponse(new DataOutputStream(null), HttpVersion.HTTP1);
     }
 }
