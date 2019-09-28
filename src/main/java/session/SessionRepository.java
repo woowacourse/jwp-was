f665b82924fd5704cf604d60aba248eba67c5aad
school0bhy <house1217@ajou.ac.kr>
2019-09-28 14:38:50 +0900

    feat: session 구현

---
+++ src/main/java/session/SessionRepository.java
@@ -0,0 +1,41 @@
+package session;
+
+import com.google.common.collect.Maps;
+
+import java.util.Map;
+import java.util.UUID;
+
+public class SessionRepository {
+    private final Map<String, Session> sessions = Maps.newHashMap();
+
+    private SessionRepository() {
+    }
+
+    private static class SessionRepositoryLazyHolder {
+        private static final SessionRepository INSTANCE = new SessionRepository();
+    }
+
+    public static SessionRepository getInstance() {
+        return SessionRepositoryLazyHolder.INSTANCE;
+    }
+
+    public Session getSession(String sessionId) {
+        return sessions.containsKey(sessionId) ? sessions.get(sessionId) : createSession();
+    }
+
+    private Session createSession() {
+        String sessionId = createUniqueSessionId();
+        Session session = new Session(sessionId);
+        sessions.put(sessionId, session);
+        return session;
+    }
+
+    private String createUniqueSessionId() {
+        String uuid = UUID.randomUUID().toString();
+        while (sessions.get(uuid) != null) {
+            uuid = UUID.randomUUID().toString();
+        }
+
+        return uuid;
+    }
+}
