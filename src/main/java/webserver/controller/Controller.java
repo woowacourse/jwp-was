5fef18621b6351d9deb3b0795c0202b2d215a585
kangmin46 <kangminals5610@gmail.com>
2019-09-29 17:11:28 +0900

    [refactor] change service method return type

--- src/main/java/webserver/controller/Controller.java
+++ src/main/java/webserver/controller/Controller.java
@@ -4,5 +4,5 @@ import webserver.controller.request.HttpRequest;
 import webserver.controller.response.HttpResponse;
 
 public interface Controller {
-    void service(HttpRequest httpRequest, HttpResponse httpResponse);
+    HttpResponse service(HttpRequest httpRequest);
 }
