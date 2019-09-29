f929d7a0a7a589d2d4bbccd5d83eca189dddee83
hyunsoojung <jhs695@naver.com>
2019-09-29 17:56:18 +0900

    test: test cookie가 있는 경우 추가

--- src/test/java/http/request/factory/HttpRequestHeaderFactoryTest.java
+++ src/test/java/http/request/factory/HttpRequestHeaderFactoryTest.java
@@ -1,25 +1,1 @@
-package http.request.factory;
-
-import http.request.HttpCookieStore;
-import http.request.HttpRequestHeader;
-import org.junit.jupiter.api.Test;
-
-import java.util.*;
-
-import static org.assertj.core.api.Assertions.assertThat;
-
-class HttpRequestHeaderFactoryTest {
-
-    @Test
-    void create() {
-        Map<String, String> map = new HashMap<>();
-        map.put("Host", "localhost:8080");
-        map.put("Connection", "keep-alive");
-        map.put("Content-Length", "345");
-        HttpRequestHeader httpRequestHeader = new HttpRequestHeader(map, new HttpCookieStore(Collections.EMPTY_LIST));
-
-        List<String> lines = Arrays.asList("Host: localhost:8080", "Connection: keep-alive", "Content-Length: 345");
-
-        assertThat(HttpRequestHeaderFactory.createHttpCookieStore(lines)).isEqualTo(httpRequestHeader);
-    }
-}
+package http.request.factory;import http.HttpCookie;import http.request.HttpCookieStore;import http.request.HttpRequestHeader;import org.junit.jupiter.api.Test;import java.util.*;import static org.assertj.core.api.Assertions.assertThat;class HttpRequestHeaderFactoryTest {	@Test	void create_쿠키가_없는_경우() {		Map<String, String> map = new HashMap<>();		map.put("Host", "localhost:8080");		map.put("Connection", "keep-alive");		map.put("Content-Length", "345");		HttpRequestHeader httpRequestHeader = new HttpRequestHeader(map, new HttpCookieStore(Collections.EMPTY_LIST));		List<String> lines = Arrays.asList("Host: localhost:8080", "Connection: keep-alive", "Content-Length: 345");		assertThat(HttpRequestHeaderFactory.create(lines)).isEqualTo(httpRequestHeader);	}    @Test    void create_쿠키가_있는_경우() {        Map<String, String> map = new HashMap<>();        map.put("Host", "localhost:8080");        map.put("Connection", "keep-alive");        map.put("Content-Length", "345");        List<HttpCookie> cookies = Arrays.asList(new HttpCookie("logined", "false"), new HttpCookie("cart", "empty"));        HttpRequestHeader httpRequestHeader = new HttpRequestHeader(map, new HttpCookieStore(cookies));        List<String> lines = Arrays.asList("Host: localhost:8080", "Connection: keep-alive", "Content-Length: 345", "Cookie: logined=false;cart=empty");        assertThat(HttpRequestHeaderFactory.create(lines)).isEqualTo(httpRequestHeader);    }}
