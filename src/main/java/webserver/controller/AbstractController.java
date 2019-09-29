92d8f65e540ee2fe1c2f1e5e52133d8161b95e96
starkim06 <starkim06@naver.com>
2019-09-29 20:16:03 +0900

    refactor: 사용하지 않는 메서드 삭제

--- src/main/java/webserver/controller/AbstractController.java
+++ src/main/java/webserver/controller/AbstractController.java
@@ -1,13 +1,10 @@
 package webserver.controller;
 
 import webserver.Controller;
-import webserver.SessionManager;
 import webserver.View;
 import webserver.exception.NotSupportedHttpMethodException;
 import webserver.http.HttpMethod;
 import webserver.http.HttpRequest;
-import webserver.http.HttpResponse;
-import webserver.http.HttpSession;
 
 public abstract class AbstractController implements Controller {
     public static final String REDIRECT_VIEW = "/redirect:";
@@ -32,34 +29,5 @@ public abstract class AbstractController implements Controller {
 
     protected View doPost(HttpRequest httpRequest) {
         throw new NotSupportedHttpMethodException();
-    }
-
-    //TODO 이 메서드가 여기 있어도 괜찮은가, 아니면 HttpRequest, HttpResponse와 각각 의존성을 맺는 게 더 좋을까?
-    protected HttpSession getSession(HttpRequest httpRequest, HttpResponse httpResponse) {
-        //TODO SessionManager와 의존하는 부분을 이렇게 놔두어도 괜찮을까?
-        String sessionId = httpRequest.getSessionId();
-        if (checkSession(sessionId)) {
-            sessionId = SessionManager.createSession();
-        }
-        //TODO 이 메서드에서 set까지 해버리는건 이상하지 않을까?
-        httpResponse.setSession(sessionId);
-        return SessionManager.getSession(sessionId);
-    }
-
-    private boolean checkSession(String sessionId) {
-        return hasSession(sessionId) || validateSession(sessionId);
-    }
-
-    private boolean hasSession(String sessionId) {
-        return sessionId == null;
-    }
-
-    private boolean validateSession(String sessionId) {
-        return SessionManager.getSession(sessionId) == null;
-    }
-
-    protected void setSession(HttpRequest httpRequest, HttpResponse httpResponse, String name, Object value) {
-        HttpSession session = getSession(httpRequest, httpResponse);
-        session.setAttributes(name, value);
     }
 }
