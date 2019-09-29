d9d9a108c3906bbc6e996b53ac3312f6f3d10406
hyogeon <men1219@hanmail.net>
2019-09-29 20:05:20 +0900

    feat: LoginController 구현 및 테스트

---
+++ src/main/java/http/controller/LoginController.java
@@ -0,0 +1,39 @@
+package http.controller;
+
+import db.DataBase;
+import http.model.*;
+import model.User;
+
+import static com.google.common.net.HttpHeaders.CONTENT_TYPE;
+import static com.google.common.net.HttpHeaders.SET_COOKIE;
+
+public class LoginController implements Controller {
+    @Override
+    public HttpResponse service(HttpRequest httpRequest) {
+        HttpParameters httpParameters = httpRequest.getParameters();
+
+        String userId = httpParameters.getParameter("userId");
+        String password = httpParameters.getParameter("password");
+
+        User user = DataBase.findUserById(userId);
+
+        if (!(user == null) && password.equals(user.getPassword())) {
+            HttpSession httpSession = httpRequest.getHttpSession();
+            httpSession.setAttributes("user", user);
+
+            return new HttpResponse.Builder()
+                    .sendRedirect("/index.html")
+                    .protocols(HttpProtocols.HTTP1_1)
+                    .status(HttpStatus.FOUND)
+                    .addHeader(CONTENT_TYPE, ContentType.HTML.getType())
+                    .addHeader(SET_COOKIE, "JSESSIONID=" + httpSession.getId() + "; Path=/")
+                    .build();
+        }
+        return new HttpResponse.Builder()
+                .sendRedirect("/user/login_failed.html")
+                .protocols(HttpProtocols.HTTP1_1)
+                .status(HttpStatus.FOUND)
+                .addHeader(CONTENT_TYPE, ContentType.HTML.getType())
+                .build();
+    }
+}
