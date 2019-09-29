ad769a82ad107dc2daa27b93b6a4fe7553dd47f9
yuyu154 <38104827+yuyu154@users.noreply.github.com>
2019-09-29 19:52:48 +0900

    refactor: HttpCookie 생성자 null, blank 체크

--- src/main/java/http/request/HttpCookie.java
+++ src/main/java/http/request/HttpCookie.java
@@ -1,20 +1,25 @@
 package http.request;
 
+import com.github.jknack.handlebars.internal.lang3.StringUtils;
+
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 public class HttpCookie {
-    Map<String, String> cookies = new HashMap<>();
+    public static final String DELIMITER_OF_COOKIES = ";";
+    public static final String DELIMITER_OF_COOKIE = "=";
+
+    private final Map<String, String> cookies = new HashMap<>();
 
     public HttpCookie() {
     }
 
     public HttpCookie(final String cookies) {
-        if (!cookies.equals("")) {
-            Arrays.stream(cookies.split(";"))
+        if (!StringUtils.isBlank(cookies)) {
+            Arrays.stream(cookies.split(DELIMITER_OF_COOKIES))
                     .map(line -> line.trim())
-                    .map(line -> Arrays.asList(line.split("=")))
+                    .map(line -> Arrays.asList(line.split(DELIMITER_OF_COOKIE)))
                     .forEach(line -> this.cookies.put(line.get(0), line.get(1)));
         }
     }
