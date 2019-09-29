91072f4f9a635ae96fe9249af698eae865f3d8ae
hyunsoojung <jhs695@naver.com>
2019-09-29 15:09:28 +0900

    feat: UserListController 추가

---
+++ src/main/java/controller/UserListController.java
@@ -0,0 +1,30 @@
+package controller;
+
+import controller.exception.NotSupportMethod;
+import http.HttpStatusCode;
+import http.request.HttpRequest;
+import http.response.HttpResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+public class UserListController extends AbstractController {
+	private static final Logger LOGGER = LoggerFactory.getLogger(UserListController.class);
+
+	public static final String PATH = "/user/list";
+
+	@Override
+	void doPost(HttpRequest httpRequest, HttpResponse httpResponse) {
+		throw new NotSupportMethod("Not support" + httpRequest.getMethod());
+	}
+
+	@Override
+	void doGet(HttpRequest httpRequest, HttpResponse httpResponse) throws IOException, URISyntaxException {
+		LOGGER.debug("userList request get: {}", httpRequest.getUri());
+		httpResponse.setStatusCode(HttpStatusCode.OK);
+		httpResponse.forward(httpRequest.getPath() + "list.html");
+
+	}
+}
