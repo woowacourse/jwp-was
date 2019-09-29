77fe769dea3afa29c1d4239285c0aa5948d41944
kangmin46 <kangminals5610@gmail.com>
2019-09-29 17:15:06 +0900

    [refactor] change LoginController structure

--- src/main/java/webserver/controller/LoginController.java
+++ src/main/java/webserver/controller/LoginController.java
@@ -4,51 +4,45 @@ import db.DataBase;
 import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import utils.FileIoUtils;
 import webserver.controller.request.HttpRequest;
 import webserver.controller.response.HttpResponse;
 
 import java.util.Map;
 import java.util.Optional;
 
-public class LoginController extends AbstractController{
+public class LoginController extends AbstractController {
     private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
     private static final String USER_ID = "userId";
     private static final String PASSWORD = "password";
     private static final String LOGIN_SUCCESS_INDEX = "/index.html";
     private static final String LOGIN_FAILED_INDEX = "/user/login_failed.html";
 
-    private LoginController () {
-    }
-
-    public static LoginController getInstance() {
-        return LazyHolder.INSTANCE;
-    }
-
-    private static class LazyHolder {
-
-        private static final LoginController INSTANCE = new LoginController();
+    @Override
+    public HttpResponse doGet(HttpRequest httpRequest) {
+        String path = NON_STATIC_FILE_PATH + httpRequest.getPath();
+        Optional<byte[]> maybeBody = FileIoUtils.loadFileFromClasspath(path);
+        return HttpResponse.ok(httpRequest,maybeBody.get());
     }
 
     @Override
-    public void doPost(HttpRequest httpRequest, HttpResponse httpResponse) {
+    public HttpResponse doPost(HttpRequest httpRequest) {
         Map<String, String> requestBodyFields = httpRequest.getBodyFields();
         String userId = requestBodyFields.get(USER_ID);
         String password = requestBodyFields.get(PASSWORD);
         logger.debug(">>> {} , {}", userId, password);
-        sendLoginPage(httpResponse, userId, password);
+        return sendLoginPage(httpRequest, userId, password);
     }
 
-    private void sendLoginPage(HttpResponse httpResponse, String userId, String password) {
+    private HttpResponse sendLoginPage(HttpRequest httpRequest,String userId, String password) {
         Optional<User> maybeUser = DataBase.findUserById(userId);
 
-        if(maybeUser.isPresent()) {
+        if (maybeUser.isPresent()) {
             User user = maybeUser.get();
             boolean isMatchPassword = user.isMatchPassword(password);
-            httpResponse.sendRedirect(LOGIN_SUCCESS_INDEX, isMatchPassword);
-            return;
+            return HttpResponse.sendRedirect(httpRequest, LOGIN_SUCCESS_INDEX, isMatchPassword);
         }
 
-        String redirectUrl = LOGIN_FAILED_INDEX;
-        httpResponse.sendRedirect(redirectUrl, false);
+        return HttpResponse.sendRedirect(httpRequest, LOGIN_FAILED_INDEX, false);
     }
 }
