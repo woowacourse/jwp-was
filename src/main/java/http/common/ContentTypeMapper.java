2a10dcbc2b517116f62af854a0d41ce92d64a8eb
school0bhy <house1217@ajou.ac.kr>
2019-09-29 18:15:20 +0900

    refactor: 확장자로 ContentType 찾는 로직 수정

--- src/main/java/http/common/ContentTypeMapper.java
+++ src/main/java/http/common/ContentTypeMapper.java
@@ -2,31 +2,49 @@ package http.common;
 
 import http.common.exception.InvalidContentTypeException;
 
+import java.util.*;
+
 import static utils.StringUtils.BLANK;
 
 public enum ContentTypeMapper {
-    JS(ContentType.JS),
-    CSS(ContentType.CSS),
-    HTML(ContentType.HTML),
-    PNG(ContentType.PNG),
-    EOT(ContentType.EOT),
-    SVG(ContentType.SVG),
-    TTF(ContentType.TTF),
-    WOFF(ContentType.WOFF),
-    WOFF2(ContentType.WOFF2),
-    ICO(ContentType.ICO);
+    JS(ContentType.JS, Arrays.asList("js")),
+    CSS(ContentType.CSS, Arrays.asList("css")),
+    HTML(ContentType.HTML, Arrays.asList("html")),
+    PNG(ContentType.PNG, Arrays.asList("png")),
+    EOT(ContentType.EOT, Arrays.asList("eot")),
+    SVG(ContentType.SVG, Arrays.asList("svg")),
+    TTF(ContentType.TTF, Arrays.asList("ttf")),
+    WOFF(ContentType.WOFF, Arrays.asList("woff")),
+    WOFF2(ContentType.WOFF2, Arrays.asList("woff2")),
+    ICO(ContentType.ICO, Arrays.asList("ico")),
+    FORM_URLENCODED(ContentType.FORM_URLENCODED, Collections.emptyList());
 
+    private static Map<String, ContentTypeMapper> cache = new HashMap<>();
     private static final char DOT = '.';
     private static final int NOT_EXIST_EXTENSION_START_INDEX = 0;
-    private final String contentType;
 
-    ContentTypeMapper(String contentType) {
+    static {
+        Arrays.stream(values())
+                .filter(ContentTypeMapper::hasExtension)
+                .forEach(contentType -> contentType.extensions.stream()
+                        .forEach(ext -> cache.put(ext, contentType)));
+    }
+
+    private final String contentType;
+    private final List<String> extensions;
+
+    ContentTypeMapper(String contentType, List<String> extensions) {
         this.contentType = contentType;
+        this.extensions = extensions;
+    }
+
+    private boolean hasExtension() {
+        return extensions.size() > 0;
     }
 
     public static String getContentType(String path) {
         try {
-            return valueOf(extractExtension(path).toUpperCase()).contentType;
+            return cache.get(extractExtension(path)).contentType;
         } catch (IllegalArgumentException | NullPointerException e) {
             throw new InvalidContentTypeException();
         }
@@ -35,9 +53,5 @@ public enum ContentTypeMapper {
     private static String extractExtension(String path) {
         int extensionStartIndex = path.lastIndexOf(DOT) + 1;
         return (extensionStartIndex == NOT_EXIST_EXTENSION_START_INDEX) ? BLANK : path.substring(extensionStartIndex);
-    }
-
-    public String getContentType() {
-        return contentType;
     }
 }
