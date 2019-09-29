c04ff76a9d85f386d416cf2ce719bee2215b35a2
pkch93 <star_final@naver.com>
2019-09-29 10:32:38 +0900

    test: CreateUserController

---
+++ src/test/java/http/application/controller/CreateUserControllerTest.java
@@ -0,0 +1,44 @@
+package http.application.controller;
+
+import http.application.Controller;
+import http.common.HttpVersion;
+import http.request.HttpRequest;
+import http.request.HttpRequestParser;
+import http.response.HttpResponse;
+import http.response.HttpStatus;
+import http.response.StatusLine;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class CreateUserControllerTest {
+    private static final String TEST_RESOURCES = "./src/test/resources";
+
+    private Controller controller;
+    private InputStream in;
+
+    @BeforeEach
+    void setUp() {
+        controller = new CreateUserController();
+    }
+
+    @Test
+    void 유저_생성_정상_흐름() throws IOException {
+        InputStream in = new FileInputStream(TEST_RESOURCES + "/http_post.txt");
+
+        HttpRequest httpRequest = HttpRequestParser.parse(in);
+        HttpResponse httpResponse = new HttpResponse();
+        controller.service(httpRequest, httpResponse);
+
+        StatusLine statusLine = httpResponse.getStatusLine();
+        assertThat(statusLine.getHttpStatus()).isEqualTo(HttpStatus.FOUND);
+        assertThat(statusLine.getHttpVersion()).isEqualTo(HttpVersion.HTTP_1_1);
+
+        assertThat(httpResponse.getHttpHeader().get("Location")).isEqualTo("/index.html");
+    }
+}
