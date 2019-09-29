907931a402e10f58e78dfaa067771463f92b7327
이영현 <leey9494@icloud.com>
2019-09-29 15:05:47 +0900

    review : abstractcontroller 리뷰 반영

--- src/main/java/controller/AbstractController.java
+++ src/main/java/controller/AbstractController.java
@@ -9,29 +9,20 @@ import http.support.HttpStatus;
 import java.util.List;
 
 public abstract class AbstractController implements Controller {
-    private List<HttpMethod> supportMethod = Lists.newArrayList(HttpMethod.POST, HttpMethod.GET);
-
     @Override
     public void service(Request request, Response response) {
-        if (isNotSupportMethod(request.getMethod())) {
-            response.forward(request.getPath(), HttpStatus.METHOD_NOT_ALLOWED);
-            return;
-        }
-
-        if (request.getMethod().contains(HttpMethod.POST.name())) {
+        if (request.getMethod().equals(HttpMethod.POST.name())) {
             doPost(request, response);
             return;
         }
 
-        doGet(request, response);
-    }
+        if (request.getMethod().equals(HttpMethod.GET.name())) {
+            doGet(request, response);
+        }
 
-    private boolean isNotSupportMethod(String method) {
-        return supportMethod.stream()
-                .map(Enum::name)
-                .noneMatch(name -> name.equals(method));
+        response.forward(request.getPath(), HttpStatus.METHOD_NOT_ALLOWED);
     }
-
+    
     public void doPost(Request request, Response response) {
         response.forward(request.getPath(), HttpStatus.METHOD_NOT_ALLOWED);
     }
