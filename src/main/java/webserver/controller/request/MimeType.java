5c594bbec8cef5705719c1031d76ec5a308f27ac
kangmin46 <kangminals5610@gmail.com>
2019-09-29 18:23:55 +0900

    [refactor] change MimeType.None field

--- src/main/java/webserver/controller/request/MimeType.java
+++ src/main/java/webserver/controller/request/MimeType.java
@@ -10,7 +10,7 @@ public enum MimeType {
     ICO(".ico", "image/x-icon"),
     TTF(".ttf", "application/x-font-ttf"),
     WOFF(".woff", "application/x-font-woff"),
-    NONE("", "");
+    NONE("", "text/html");
 
     private String extension;
     private String mimeType;
