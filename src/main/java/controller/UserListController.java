8f433a61dd57e5ad2b081c8009488ec398f2d917
backlo <hjun4512@naver.com>
2019-09-30 02:33:41 +0900

    feat: 로그인 한 사람만 User List 보도록 기능 추가

--- src/main/java/controller/UserListController.java
+++ src/main/java/controller/UserListController.java
@@ -7,12 +7,15 @@ import webserver.http.request.HttpRequest;
 import webserver.http.response.HttpResponse;
 import webserver.http.response.core.ResponseContentType;
 import webserver.http.response.core.ResponseStatus;
+import webserver.http.session.HttpSession;
 
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.URISyntaxException;
 
 public class UserListController extends AbstractController {
+    private static final String SESSION_USER_KEY = "user";
+    private static final String USER_LIST_PATH = "user/list";
     private byte[] body;
 
     @Override
@@ -27,13 +30,14 @@ public class UserListController extends AbstractController {
 
     @Override
     protected void doGet(HttpRequest httpRequest, HttpResponse httpResponse) throws IOException {
-        if(httpRequest.getLogin() != null && httpRequest.getLogin().equals("true")) {
-            httpResponse.addStatus(ResponseStatus.of(200));
-            httpResponse.addHeader(HttpHeaderField.CONTENT_TYPE, ResponseContentType.of(httpRequest.getRequestPath()));
-            this.body = TemplateManager.getTemplatePage("user/list").getBytes();
+        HttpSession session = httpRequest.getSession();
+        if (session != null && session.getAttribute(SESSION_USER_KEY) != null) {
+            httpResponse.addStatus(ResponseStatus.of(200))
+                    .addHeader(HttpHeaderField.CONTENT_TYPE, ResponseContentType.of(httpRequest.getRequestPath()));
+            this.body = TemplateManager.getTemplatePage(USER_LIST_PATH).getBytes();
             return;
         }
-        httpResponse.addStatus(ResponseStatus.of(302));
-        httpResponse.addHeader(HttpHeaderField.LOCATION, LOGIN_PAGE);
+        httpResponse.addStatus(ResponseStatus.of(302))
+                .addHeader(HttpHeaderField.LOCATION, LOGIN_PAGE);
     }
 }
