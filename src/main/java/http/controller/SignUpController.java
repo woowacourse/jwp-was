1620d683be88a19f0646121f8314b333c0312dc4
hyogeon <men1219@hanmail.net>
2019-09-29 19:27:30 +0900

    refactor: sendredirect 수정

--- src/main/java/http/controller/SignUpController.java
+++ src/main/java/http/controller/SignUpController.java
@@ -7,8 +7,6 @@ import model.User;
 import static com.google.common.net.HttpHeaders.CONTENT_TYPE;
 
 public class SignUpController implements Controller {
-    private final static String ROOT_URI = "http://localhost:8080";
-
     @Override
     public HttpResponse service(HttpRequest httpRequest) {
         HttpParameters httpParameters = httpRequest.getParameters();
@@ -21,7 +19,7 @@ public class SignUpController implements Controller {
         DataBase.addUser(user);
 
         return new HttpResponse.Builder()
-                .sendRedirect(ROOT_URI + "/index.html")
+                .sendRedirect("/index.html")
                 .protocols(HttpProtocols.HTTP1_1)
                 .status(HttpStatus.FOUND)
                 .addHeader(CONTENT_TYPE, ContentType.HTML.getType())
