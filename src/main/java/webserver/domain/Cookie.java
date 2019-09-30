b705c96f2cb0b1a3c73f6c53b5cfbce1777a2367
Jo Jinsik <kunggom@gmail.com>
2019-09-30 20:45:33 +0900

    fix(Cookie): 쿠키 스트링 상의 순서를 반영

--- src/main/java/webserver/domain/Cookie.java
+++ src/main/java/webserver/domain/Cookie.java
@@ -22,27 +22,41 @@ public class Cookie {
     private static final int DEFAULT_COOKIE_STRING_LENGTH = 256;
     private static final String TRUE = "true";
     private static final String EMPTY = "";
+    private static final String ROOT = "/";
+    private static final String PATH = "Path";
     private static final String UTF_8 = "UTF-8";
+    private static final String DOMAIN = "Domain";
+    private static final String SECURE = "Secure";
     private static final String EXPIRES = "Expires";
+    private static final String SAME_SITE = "SameSite";
+    private static final String HTTP_ONLY = "HttpOnly";
     private static final String MAX_AGE = "Max-Age";
-    private static final String QUOTE_MARK = "\"";
     private static final String COOKIE_DELIMITER = "; ";
     private static final String KEY_VALUE_DELIMITER = "=";
     private static final Escaper URL_ESCAPER = UrlEscapers.urlFragmentEscaper();
     private static final DateTimeFormatter HTML_TIME_FORMATTER = DateTimeFormatter.RFC_1123_DATE_TIME.withZone(ZoneOffset.UTC);
 
+    // 각 필드들을 다른 자료구조 객체에 넣어보려 했지만, 오히려 복잡성이 증가하여 일단 이렇게 한다.
     private final Map<String, String> cookieJar;
-    private final Set<String> options;
+    private final Set<String> booleanOptions;
     private ZonedDateTime expires = null;
     private Integer maxAgeSecond = null;
+    private String domain = EMPTY;
+    private String path = ROOT; // 기본값 주의
+    private String sameSite = EMPTY;
+    private boolean secure = false;
+    private boolean httpOnly = false;
 
     public Cookie() {
-        this.cookieJar = new HashMap<>();
-        this.options = new HashSet<>();
+        cookieJar = new HashMap<>();
+        booleanOptions = new HashSet<>();
     }
 
     public Cookie(final String cookieString) {
         this();
+        if (Objects.isNull(cookieString)) {
+            return;
+        }
         final String[] fragments = cookieString.split(COOKIE_DELIMITER);
         for (final String fragment : fragments) {
             processFragment(fragment);
@@ -55,7 +69,18 @@ public class Cookie {
             processTuple(maybeTuple);
             return;
         }
-        options.add(maybeTuple[KEY_INDEX]);
+        final String option = maybeTuple[KEY_INDEX];
+        switch (option) {
+            case SECURE:
+                setSecure(true);
+                break;
+            case HTTP_ONLY:
+                setHttpOnly(true);
+                break;
+            default:
+                booleanOptions.add(option);
+                break;
+        }
     }
 
     private void processTuple(final String[] tuple) {
@@ -68,22 +93,30 @@ public class Cookie {
             case MAX_AGE:
                 setMaxAge(Integer.parseInt(value));
                 break;
+            case DOMAIN:
+                setDomain(value);
+                break;
+            case PATH:
+                setPath(value);
+                break;
+            case SAME_SITE:
+                setSameSite(value);
+                break;
             default:
                 set(urlDecode(key), urlDecode(value));
                 break;
         }
     }
 
-    // TODO: expires 및 max-age 관련해서 기능 추가
     public String get(final String keyOrOption) {
-        if (options.contains(keyOrOption)) {
+        if (booleanOptions.contains(keyOrOption)) {
             return TRUE;
         }
         return cookieJar.getOrDefault(keyOrOption, EMPTY);
     }
 
     public void set(final String option) {
-        options.add(option);
+        booleanOptions.add(option);
     }
 
     public void set(final String key, final String value) {
@@ -91,33 +124,33 @@ public class Cookie {
     }
 
     public String remove(final String keyOrOption) {
-        if (options.contains(keyOrOption)) {
-            options.remove(keyOrOption);
+        if (booleanOptions.contains(keyOrOption)) {
+            booleanOptions.remove(keyOrOption);
             return TRUE;
         }
-        return this.cookieJar.remove(keyOrOption);
+        return cookieJar.remove(keyOrOption);
     }
 
     public void clear() {
         cookieJar.clear();
-        options.clear();
+        booleanOptions.clear();
     }
 
     public boolean isEmpty() {
-        return cookieJar.isEmpty() && options.isEmpty();
+        return cookieJar.isEmpty() && booleanOptions.isEmpty();
     }
 
     public boolean containsKey(final String keyOrOption) {
-        return cookieJar.containsKey(keyOrOption) && options.contains(keyOrOption);
+        return cookieJar.containsKey(keyOrOption) || booleanOptions.contains(keyOrOption);
     }
 
     public int size() {
-        return cookieJar.size() + options.size();
+        return cookieJar.size() + booleanOptions.size();
     }
 
     public Map<String, String> entries() {
         final Map<String, String> result = new HashMap<>(cookieJar);
-        for (final String item : options) {
+        for (final String item : booleanOptions) {
             result.put(item, TRUE);
         }
         return Collections.unmodifiableMap(result);
@@ -132,6 +165,10 @@ public class Cookie {
         this.expires = expires;
     }
 
+    public void setExpires(final String expires) {
+        this.setExpires(parseExpires(expires));
+    }
+
     @Nullable
     public Integer getMaxAge() {
         return maxAgeSecond;
@@ -142,6 +179,50 @@ public class Cookie {
             this.maxAgeSecond = ONE_YEAR; // RFC 2616 Section 14.21에 따른 값
         }
         this.maxAgeSecond = maxAgeSecond;
+    }
+
+    public void setMaxAge(final String maxAgeSecond) {
+        this.setMaxAge(Integer.parseInt(maxAgeSecond));
+    }
+
+    public String getDomain() {
+        return domain;
+    }
+
+    public void setDomain(final String domain) {
+        this.domain = domain;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(final String path) {
+        this.path = path;
+    }
+
+    public String getSameSite() {
+        return sameSite;
+    }
+
+    public void setSameSite(final String sameSite) {
+        this.sameSite = sameSite;
+    }
+
+    public boolean isSecure() {
+        return secure;
+    }
+
+    public void setSecure(final boolean secure) {
+        this.secure = secure;
+    }
+
+    public boolean isHttpOnly() {
+        return httpOnly;
+    }
+
+    public void setHttpOnly(final boolean httpOnly) {
+        this.httpOnly = httpOnly;
     }
 
     private ZonedDateTime parseExpires(final String text) throws DateTimeParseException {
@@ -161,11 +242,20 @@ public class Cookie {
     public String toString() {
         final StringBuilder builder = new StringBuilder(DEFAULT_COOKIE_STRING_LENGTH);
         cookieJarToString(builder);
-        toStringHelper(builder, EXPIRES, expiresToString(expires));
-        toStringHelper(builder, MAX_AGE, maxAgeToString(maxAgeSecond));
-        itemsToString(builder, options);
+        optionsToString(builder);
         removeLastDelimiter(builder);
         return builder.toString();
+    }
+
+    private void optionsToString(final StringBuilder builder) {
+        toStringHelper(builder, EXPIRES, expiresToString(expires));
+        toStringHelper(builder, MAX_AGE, maxAgeToString(maxAgeSecond));
+        toStringHelper(builder, DOMAIN, domain);
+        toStringHelper(builder, PATH, path);
+        toStringHelper(builder, SECURE, secure);
+        toStringHelper(builder, HTTP_ONLY, httpOnly);
+        toStringHelper(builder, SAME_SITE, sameSite);
+        itemsToString(builder, booleanOptions);
     }
 
     private void cookieJarToString(final StringBuilder builder) {
@@ -173,9 +263,7 @@ public class Cookie {
             builder
                     .append(URL_ESCAPER.escape(key))
                     .append(KEY_VALUE_DELIMITER)
-                    .append(QUOTE_MARK)
                     .append(URL_ESCAPER.escape(cookieJar.get(key)))
-                    .append(QUOTE_MARK)
                     .append(COOKIE_DELIMITER);
         }
     }
@@ -191,9 +279,17 @@ public class Cookie {
                 .append(COOKIE_DELIMITER);
     }
 
+    private void toStringHelper(final StringBuilder builder, final String option, final boolean value) {
+        if (value) {
+            builder.append(option).append(COOKIE_DELIMITER);
+        }
+    }
+
     private void itemsToString(final StringBuilder builder, final Iterable<String> items) {
         for (final String item : items) {
-            builder.append(item).append(COOKIE_DELIMITER);
+            if (!EMPTY.equals(item)) {
+                toStringHelper(builder, item, true);
+            }
         }
     }
 
