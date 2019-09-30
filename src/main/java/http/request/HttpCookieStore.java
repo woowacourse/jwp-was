cfb7c7be173a90d50aad7df79c1a16311c4af4d4
hyunsoojung <jhs695@naver.com>
2019-09-30 17:03:41 +0900

    feat: containsValue() 추가

--- src/main/java/http/request/HttpCookieStore.java
+++ src/main/java/http/request/HttpCookieStore.java
@@ -9,51 +9,54 @@ import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class HttpCookieStore {
-	List<HttpCookie> cookies;
+    private List<HttpCookie> cookies;
 
-	public HttpCookieStore(List<HttpCookie> cookies) {
-		if (cookies.isEmpty()) {
-			this.cookies = new ArrayList<>();
-			return;
-		}
+    public HttpCookieStore(List<HttpCookie> cookies) {
+        if (cookies.isEmpty()) {
+            this.cookies = new ArrayList<>();
+            return;
+        }
 
-		this.cookies = cookies;
-	}
+        this.cookies = cookies;
+    }
 
-	public void addCookie(HttpCookie httpCookie) {
-		cookies.add(httpCookie);
-	}
+    public void addCookie(HttpCookie httpCookie) {
+        cookies.add(httpCookie);
+    }
 
-	public String getCookieValue(String key) {
-		return getCookie(key).getValue();
-	}
+    public String getCookieValue(String key) {
+        return getCookie(key).getValue();
+    }
 
-	public HttpCookie getCookie(String key) {
-		return cookies.stream()
-				.filter(httpCookie -> httpCookie.hasValueByKey(key))
-				.findAny()
-				.orElseThrow(NotFoundCookieException::new)
-				;
-	}
+    public boolean containsValue(String key) {
+        return cookies.contains(key);
+    }
 
-	@Override
-	public String toString() {
-		return "Cookie: " +
-				cookies.stream()
-				.map(HttpCookie::toString)
-				.collect(Collectors.joining(";"));
-	}
+    public HttpCookie getCookie(String key) {
+        return cookies.stream()
+                .filter(httpCookie -> httpCookie.hasValueByKey(key))
+                .findAny()
+                .orElseThrow(NotFoundCookieException::new);
+    }
 
-	@Override
-	public boolean equals(Object o) {
-		if (this == o) return true;
-		if (o == null || getClass() != o.getClass()) return false;
-		HttpCookieStore that = (HttpCookieStore) o;
-		return cookies.equals(that.cookies);
-	}
+    @Override
+    public String toString() {
+        return "Cookie: " +
+                cookies.stream()
+                        .map(HttpCookie::toString)
+                        .collect(Collectors.joining(";"));
+    }
 
-	@Override
-	public int hashCode() {
-		return Objects.hash(cookies);
-	}
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        HttpCookieStore that = (HttpCookieStore) o;
+        return cookies.equals(that.cookies);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(cookies);
+    }
 }
