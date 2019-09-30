bf922d273d23e1ca06a8ad6ac12d16b7d1f6dc0e
soojinroh <rsj3112@gmail.com>
2019-10-01 04:11:53 +0900

    refactor : ControllerMapper클래스의 mappingMethod return값 변경
    기존에 url에 맞는 method를 찾아 실행시키는 것까지 수행하던 것에서 method를 찾아 return하는 것으로 수정. 그에 따라 return 타입 변경

--- src/main/java/controller/ControllerMapper.java
+++ src/main/java/controller/ControllerMapper.java
@@ -1,31 +1,23 @@
 package controller;
 
-import controller.exception.NotFoundUrlException;
-import model.http.HttpRequest;
-import model.http.HttpResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import webserver.RequestHandler;
+import webserver.http.request.HttpRequest;
+import webserver.http.response.HttpResponse;
 
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 public class ControllerMapper {
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
 
-    public static void mapAndHandle(HttpRequest request, HttpResponse response) {
+    public static Method mappingMethod(HttpRequest request, HttpResponse response) {
         Method method = mappingResourceMethod(request);
         if (method == null) {
             method = Controllers.REQUEST_MAPPING_METHODS.get(request.getPath());
         }
         method.setAccessible(true);
-
-        try {
-            method.invoke(Controllers.REQUEST_MAPPING_CONTROLLERS.get(method), request, response);
-        } catch (IllegalAccessException | InvocationTargetException e) {
-            logger.error(e.getMessage());
-            throw new NotFoundUrlException(e);
-        }
+        return method;
     }
 
     private static Method mappingResourceMethod(HttpRequest request) {
@@ -44,7 +36,7 @@ public class ControllerMapper {
     }
 
     private static boolean isTemplateFileRequest(HttpRequest request, String extension) {
-        return isStaticFileRequest(request, extension);
+        return request.getPath().contains(extension) || request.getPath().equals("/");
     }
 
 }
