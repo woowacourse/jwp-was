73c302c6552763990e3e0051aa5e8111a3099d8d
hyunsoojung <jhs695@naver.com>
2019-09-30 20:25:46 +0900

    feat: try-catch문 추가 handlebars 추가

--- src/main/java/controller/UserListController.java
+++ src/main/java/controller/UserListController.java
@@ -1,11 +1,15 @@
 package controller;
 
 import controller.exception.NotSupportMethod;
+import db.DataBase;
 import http.HttpStatusCode;
+import http.exception.NotFoundCookieException;
 import http.request.HttpRequest;
 import http.response.HttpResponse;
+import http.response.HttpResponseBody;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import utils.HandlebarsHelper;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
@@ -23,13 +27,17 @@ public class UserListController extends AbstractController {
     @Override
     void doGet(HttpRequest httpRequest, HttpResponse httpResponse) throws IOException, URISyntaxException {
         LOGGER.debug("userList request get: {}", httpRequest.getUri());
-        if (httpRequest.getCookieValue("logined").equals("true")) {
-            httpResponse.setStatusCode(HttpStatusCode.OK);
-            httpResponse.forward(httpRequest.getPath() + "list.html");
-            return;
+        try {
+            if (httpRequest.getCookieValue("logined").equals("true")) {
+                httpResponse.setStatusCode(HttpStatusCode.OK);
+                httpResponse.forward(httpRequest.getPath() + "list.html");
+                httpResponse.setHttpResponseBody(new HttpResponseBody(HandlebarsHelper.apply(DataBase.findAll())));
+                return;
+            }
+        } catch (NotFoundCookieException e) {
+            httpResponse.setStatusCode(HttpStatusCode.FOUND);
+            httpResponse.redirect("login_failed.html");
         }
 
-        httpResponse.setStatusCode(HttpStatusCode.NOT_FOUND);
-        httpResponse.forward("/index.html");
     }
 }
