1b348c1811b676ba2d8b2f4590cbcf983b8157cd
hyunsoojung <jhs695@naver.com>
2019-09-30 17:06:12 +0900

    feat: request에서 sessionId을 받아와 response의 쿠키에 담아주는 로직 추가

--- src/main/java/http/response/HttpResponse.java
+++ src/main/java/http/response/HttpResponse.java
@@ -1,5 +1,6 @@
 package http.response;
 
+import http.HttpSessionStore;
 import http.HttpStatusCode;
 import http.MediaType;
 import http.request.HttpRequest;
@@ -12,66 +13,71 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 
 public class HttpResponse {
-	private static final Logger logger = LoggerFactory.getLogger(HttpResponse.class);
+    private static final Logger logger = LoggerFactory.getLogger(HttpResponse.class);
 
-	private HttpResponseStatusLine httpResponseStatusLine;
-	private HttpResponseHeader httpResponseHeader;
-	private HttpResponseBody httpResponseBody;
+    private HttpResponseStatusLine httpResponseStatusLine;
+    private HttpResponseHeader httpResponseHeader;
+    private HttpResponseBody httpResponseBody;
 
-	public static HttpResponse of(HttpRequest httpRequest) {
-		HttpResponseStatusLine httpResponseStatusLine = new HttpResponseStatusLine(httpRequest.getHttpVersion());
-		HttpResponseHeader httpResponseHeader = new HttpResponseHeader();
-		return new HttpResponse(httpResponseStatusLine, httpResponseHeader);
-	}
+    public static HttpResponse of(HttpRequest httpRequest) {
+        HttpResponseStatusLine httpResponseStatusLine = new HttpResponseStatusLine(httpRequest.getHttpVersion());
 
-	public HttpResponse(HttpResponseStatusLine httpResponseStatusLine, HttpResponseHeader httpResponseHeader) {
-		this.httpResponseStatusLine = httpResponseStatusLine;
-		this.httpResponseHeader = httpResponseHeader;
-	}
+        String sessionId = httpRequest.hasSession() ? httpRequest.getSessionId() : HttpSessionStore.getSession("").getId();
 
-	public void setCookie(String cookieValue) {
-		httpResponseHeader.addField("Set-cookie", cookieValue);
-	}
+        HttpResponseHeader httpResponseHeader = new HttpResponseHeader();
+        httpResponseHeader.addCookie("SessionId=" + sessionId);
 
-	public void setStatusCode(HttpStatusCode httpStatusCode) {
-		httpResponseStatusLine.setHttpStatusCode(httpStatusCode);
-	}
+        return new HttpResponse(httpResponseStatusLine, httpResponseHeader);
+    }
 
-	public void redirect(String uri) {
-		String contentType = MediaType.getContentType(uri);
-		httpResponseHeader.addField("Content-Type", contentType + ";charset=utf-8");
-		httpResponseHeader.setLocation(uri);
-	}
+    public HttpResponse(HttpResponseStatusLine httpResponseStatusLine, HttpResponseHeader httpResponseHeader) {
+        this.httpResponseStatusLine = httpResponseStatusLine;
+        this.httpResponseHeader = httpResponseHeader;
+    }
 
-	public void forward(String uri) throws IOException, URISyntaxException {
-		String contentType = MediaType.getContentType(uri);
+    public void setCookie(String cookieValue) {
+        httpResponseHeader.addField("Set-cookie", cookieValue);
+    }
 
-		logger.debug("request uri : {}", uri);
-		logger.debug("request file full path : {}", MediaType.getFullPath(uri));
-		logger.debug("request file contentType : {}", MediaType.getContentType(uri));
+    public void setStatusCode(HttpStatusCode httpStatusCode) {
+        httpResponseStatusLine.setHttpStatusCode(httpStatusCode);
+    }
 
-		this.httpResponseBody = new HttpResponseBody(FileIoUtils.loadFileFromClasspath(MediaType.getFullPath(uri)));
-		httpResponseHeader.addField("Content-Type", contentType + ";charset=utf-8");
-		httpResponseHeader.addField("Content-Length", String.valueOf(httpResponseBody.getBodyLength()));
-	}
+    public void redirect(String uri) {
+        String contentType = MediaType.getContentType(uri);
+        httpResponseHeader.addField("Content-Type", contentType + ";charset=utf-8");
+        httpResponseHeader.setLocation(uri);
+    }
 
-	public void writeResponse(DataOutputStream dataOutputStream) throws IOException {
-		logger.debug("response status line");
-		logger.debug("response header");
+    public void forward(String uri) throws IOException, URISyntaxException {
+        String contentType = MediaType.getContentType(uri);
+
+        logger.debug("request uri : {}", uri);
+        logger.debug("request file full path : {}", MediaType.getFullPath(uri));
+        logger.debug("request file contentType : {}", MediaType.getContentType(uri));
+
+        this.httpResponseBody = new HttpResponseBody(FileIoUtils.loadFileFromClasspath(MediaType.getFullPath(uri)));
+        httpResponseHeader.addField("Content-Type", contentType + ";charset=utf-8");
+        httpResponseHeader.addField("Content-Length", String.valueOf(httpResponseBody.getBodyLength()));
+    }
+
+    public void writeResponse(DataOutputStream dataOutputStream) throws IOException {
+        logger.debug("response status line");
+        logger.debug("response header");
 
 
-		dataOutputStream.writeBytes(httpResponseStatusLine.toString());
-		dataOutputStream.writeBytes(httpResponseHeader.toString());
+        dataOutputStream.writeBytes(httpResponseStatusLine.toString());
+        dataOutputStream.writeBytes(httpResponseHeader.toString());
 
-		logger.debug("response status line: {}", httpResponseStatusLine.toString());
-		logger.debug("response header: {}", httpResponseHeader.toString());
+        logger.debug("response status line: {}", httpResponseStatusLine.toString());
+        logger.debug("response header: {}", httpResponseHeader.toString());
 
-		if (httpResponseBody != null) {
-			logger.debug("response header: {}", this.httpResponseBody.getBody());
+        if (httpResponseBody != null) {
+            logger.debug("response header: {}", this.httpResponseBody.getBody());
 
-			dataOutputStream.write(this.httpResponseBody.getBody(), 0, this.httpResponseBody.getBodyLength());
-		}
+            dataOutputStream.write(this.httpResponseBody.getBody(), 0, this.httpResponseBody.getBodyLength());
+        }
 
-		dataOutputStream.flush();
-	}
+        dataOutputStream.flush();
+    }
 }
