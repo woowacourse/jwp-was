cffde02f05f3281a0e88ab0f68b8c29a8f3ff24e
hyunsoojung <jhs695@naver.com>
2019-09-30 16:53:04 +0900

    refactor: 자료구조 변경 Map<String, String> -> List<HttpResponseField>

--- src/main/java/http/response/HttpResponseHeader.java
+++ src/main/java/http/response/HttpResponseHeader.java
@@ -3,41 +3,53 @@ package http.response;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 
 public class HttpResponseHeader {
     private static final Logger logger = LoggerFactory.getLogger(HttpResponseHeader.class);
 
-    private Map<String, String> fields;
+    private List<HttpResponseField> fields;
 
     public HttpResponseHeader() {
-        fields = new HashMap<>();
+        fields = new ArrayList<>();
     }
 
     public HttpResponseHeader(final String headers) {
-        this.fields = new HashMap<>();
+        this.fields = new ArrayList<>();
         String[] field = headers.split("\n");
 
         for (String s : field) {
             String[] keyValue = s.split(": ");
-            this.fields.put(keyValue[0], keyValue[1]);
+            this.fields.add(new HttpResponseField(keyValue[0], keyValue[1]));
         }
     }
 
+    public void addCookie(String value) {
+        fields.add(new HttpResponseField("Set-Cookie", value));
+    }
+
     public void addField(String key, String value) {
-        fields.put(key, value);
+        HttpResponseField httpResponseField = new HttpResponseField(key, value);
+
+        if (fields.contains(httpResponseField)) {
+            int index = fields.indexOf(httpResponseField);
+            fields.get(index).updateValue(value);
+            return;
+        }
+
+        fields.add(httpResponseField);
     }
 
     public void setLocation(String uri) {
         logger.debug("redirect uri: {}", uri);
-        fields.put("Location", uri);
+        addField("Location", uri);
     }
 
     @Override
     public String toString() {
         StringBuilder stringBuilder = new StringBuilder();
-        fields.forEach((key, value) -> stringBuilder.append(key).append(": ").append(value).append("\r\n"));
+        fields.forEach(field -> stringBuilder.append(field.toString()).append("\r\n"));
         stringBuilder.append("\r\n");
 
         return stringBuilder.toString();
