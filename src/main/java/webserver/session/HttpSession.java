dfef89f1e270c2d14e053a04c687c97fb662dcde
Donggyu Kim <kmdngyu@gmail.com>
2019-09-30 15:13:18 +0900

    feat: 세션 만료시 메소드 작동 방지

--- src/main/java/webserver/session/HttpSession.java
+++ src/main/java/webserver/session/HttpSession.java
@@ -1,10 +1,17 @@
 package webserver.session;
 
+import webserver.session.exception.InvalidatedSessionException;
+
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
+import static java.lang.Boolean.FALSE;
+import static java.lang.Boolean.TRUE;
+
 public class HttpSession {
     private final String id;
+    private boolean isInvalidate = FALSE;
     private Map<String, Object> attributes = new HashMap<>();
 
     public HttpSession(String id) {
@@ -12,19 +19,29 @@ public class HttpSession {
     }
 
     public void setAttribute(String name, Object value) {
+        checkInvalidated();
         attributes.put(name, value);
     }
 
     public Object getAttribute(String name) {
+        checkInvalidated();
         return attributes.get(name);
     }
 
     public void removeAttribute(String name) {
+        checkInvalidated();
         attributes.remove(name);
     }
 
+    private void checkInvalidated() {
+        if (isInvalidate) {
+            throw new InvalidatedSessionException();
+        }
+    }
+
     public void invalidate() {
-        attributes = new HashMap<>();
+        attributes = Collections.emptyMap();
+        isInvalidate = TRUE;
     }
 
     public String getId() {
