281bb22d30353b272f3d06ff2e5852d0f17fc607
yk1028 <wongeunsong94@gmail.com>
2019-09-30 14:31:20 +0900

    feat: Thread Pool을 활용해 다수 사용자 요청 처리

--- src/main/java/webserver/WebServer.java
+++ src/main/java/webserver/WebServer.java
@@ -5,12 +5,17 @@ import org.slf4j.LoggerFactory;
 
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class WebServer {
     private static final Logger logger = LoggerFactory.getLogger(WebServer.class);
     private static final int DEFAULT_PORT = 8080;
+    private static final int NUMBER_OF_THREAD = 100;
 
     public static void main(String args[]) throws Exception {
+        ExecutorService es = Executors.newFixedThreadPool(NUMBER_OF_THREAD);
+
         int port = 0;
         if (args == null || args.length == 0) {
             port = DEFAULT_PORT;
@@ -18,15 +23,12 @@ public class WebServer {
             port = Integer.parseInt(args[0]);
         }
 
-        // 서버소켓을 생성한다. 웹서버는 기본적으로 8080번 포트를 사용한다.
         try (ServerSocket listenSocket = new ServerSocket(port)) {
             logger.info("Web Application Server started {} port.", port);
 
-            // 클라이언트가 연결될때까지 대기한다.
             Socket connection;
             while ((connection = listenSocket.accept()) != null) {
-                Thread thread = new Thread(new RequestHandler(connection));
-                thread.start();
+                es.execute(new RequestHandler((connection)));
             }
         }
     }
