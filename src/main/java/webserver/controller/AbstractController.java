db3ee0b092b6598008ede20459e9a0295a443ffd
gch01410 <gch01410@naver.com>
2019-10-01 20:47:35 +0900

    refactor: HttpResponse 클래스에 forward, sendRedirect 메서드 추가

--- src/main/java/webserver/controller/AbstractController.java
+++ src/main/java/webserver/controller/AbstractController.java
@@ -3,7 +3,7 @@ package webserver.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import utils.FileIoUtils;
-import webserver.HttpStatus;
+import webserver.ModelAndView;
 import webserver.controller.exception.MethodNotAllowedException;
 import webserver.request.HttpRequest;
 import webserver.response.HttpResponse;
@@ -11,23 +11,16 @@ import webserver.response.HttpResponse;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.util.Objects;
 
 public abstract class AbstractController implements Controller {
     private static final Logger log = LoggerFactory.getLogger(AbstractController.class);
 
-    protected static final String HEADER_FIELD_CONTENT_TYPE = "Content-Type";
-    protected static final String HEADER_FIELD_CONTENT_LENGTH = "Content-Length";
-    protected static final String HEADER_FIELD_LOCATION = "Location";
-    protected static final String HEADER_FIELD_HOST = "Host";
-    protected static final String CONTENT_TYPE_HTML = "text/html";
-    protected static final String CONTENT_TYPE_CSS = "text/css";
-    protected static final String HTTP_PROTOCOL = "http://";
-
+    private static final String CONTENT_TYPE_HTML = "text/html";
+    private static final String CONTENT_TYPE_CSS = "text/css";
     private static final String TEMPLATES_PATH = "./templates";
     private static final String HEADER_FIELD_ACCEPT = "Accept";
-    private static final String CHARSET_UTF8 = "charset=utf-8";
-    private static final String SEMICOLON = ";";
-    private static final String REDIRECT = "redirect:";
+    private static final String REDIRECT_PREFIX = "redirect:";
 
     @Override
     public void service(HttpRequest httpRequest, HttpResponse httpResponse) throws FileNotFoundException {
@@ -41,37 +34,27 @@ public abstract class AbstractController implements Controller {
     }
 
     private void resolveGet(HttpRequest httpRequest, HttpResponse httpResponse) throws FileNotFoundException {
-        byte[] staticFile = {};
-        String filePath = doGet(httpRequest, httpResponse);
+        ModelAndView modelAndView = doGet(httpRequest, httpResponse);
+        String filePath = modelAndView.getView();
+
+        if (Objects.nonNull(filePath) && filePath.startsWith(REDIRECT_PREFIX)) {
+            httpResponse.sendRedirect(httpRequest, filePath);
+        }
+
         if (httpRequest.containHeaderField(HEADER_FIELD_ACCEPT, CONTENT_TYPE_CSS)) {
-            staticFile = getStaticFile(httpRequest);
-            httpResponse.addHeader(HEADER_FIELD_CONTENT_TYPE, CONTENT_TYPE_CSS);
+            byte[] staticFile = getStaticFile(httpRequest);
+            httpResponse.forward(httpRequest, staticFile);
         }
-
         if (httpRequest.containHeaderField(HEADER_FIELD_ACCEPT, CONTENT_TYPE_HTML)) {
-            staticFile = getStaticFile(filePath);
-            httpResponse.addHeader(HEADER_FIELD_CONTENT_TYPE, CONTENT_TYPE_HTML + SEMICOLON + CHARSET_UTF8);
+            httpResponse.forward(httpRequest, modelAndView);
         }
-
-        forward(httpRequest, httpResponse, staticFile);
     }
 
     private void resolvePost(HttpRequest httpRequest, HttpResponse httpResponse) {
-        String file = doPost(httpRequest, httpResponse);
-        if (file.startsWith(REDIRECT)) {
-            redirect(httpRequest, httpResponse, file);
+        String path = doPost(httpRequest, httpResponse);
+        if (path.startsWith(REDIRECT_PREFIX)) {
+            httpResponse.sendRedirect(httpRequest, path);
         }
-    }
-
-    private void forward(HttpRequest httpRequest, HttpResponse httpResponse, byte[] staticFile) {
-        httpResponse.addStatusLine(httpRequest, HttpStatus.OK);
-        httpResponse.addHeader(HEADER_FIELD_CONTENT_LENGTH, String.valueOf(staticFile.length));
-        httpResponse.addBody(staticFile);
-    }
-
-    private void redirect(HttpRequest httpRequest, HttpResponse httpResponse, String file) {
-        httpResponse.addStatusLine(httpRequest, HttpStatus.FOUND);
-        httpResponse.addHeader(HEADER_FIELD_LOCATION, HTTP_PROTOCOL + httpRequest.getHeaderFieldValue(HEADER_FIELD_HOST) + file.substring(REDIRECT.length()));
     }
 
     protected byte[] getStaticFile(HttpRequest httpRequest) throws FileNotFoundException {
@@ -84,17 +67,7 @@ public abstract class AbstractController implements Controller {
         throw new FileNotFoundException("fail to find file.");
     }
 
-    protected byte[] getStaticFile(String fileName) throws FileNotFoundException {
-        try {
-            return FileIoUtils.loadFileFromClasspath(TEMPLATES_PATH + fileName);
-        } catch (IOException | URISyntaxException e) {
-            log.debug("fail to load file", e);
-        }
-        throw new FileNotFoundException("fail to find file.");
-    }
-
-
-    protected String doGet(HttpRequest httpRequest, HttpResponse httpResponse) {
+    protected ModelAndView doGet(HttpRequest httpRequest, HttpResponse httpResponse) {
         throw new MethodNotAllowedException();
     }
 
--- src/main/java/webserver/response/HttpResponse.java
+++ src/main/java/webserver/response/HttpResponse.java
@@ -1,11 +1,32 @@
 package webserver.response;
 
+import com.github.jknack.handlebars.Handlebars;
+import com.github.jknack.handlebars.Template;
+import com.github.jknack.handlebars.io.ClassPathTemplateLoader;
+import com.github.jknack.handlebars.io.TemplateLoader;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import webserver.HttpStatus;
+import webserver.ModelAndView;
 import webserver.request.HttpRequest;
 
+import java.io.IOException;
 import java.util.Objects;
 
 public class HttpResponse {
+    private static final Logger log = LoggerFactory.getLogger(HttpResponse.class);
+
+    private static final String HEADER_FIELD_CONTENT_TYPE = "Content-Type";
+    private static final String HEADER_FIELD_CONTENT_LENGTH = "Content-Length";
+    private static final String HEADER_FIELD_LOCATION = "Location";
+    private static final String HTTP_PROTOCOL = "http://";
+    private static final String CONTENT_TYPE_HTML = "text/html";
+    private static final String CONTENT_TYPE_CSS = "text/css";
+    private static final String HEADER_FIELD_HOST = "Host";
+    private static final String REDIRECT_PREFIX = "redirect:";
+    private static final String CHARSET_UTF8 = "charset=utf-8";
+    private static final String SEMICOLON = ";";
+
     private ResponseStatusLine responseStatusLine;
     private ResponseHeader responseHeader;
     private ResponseBody responseBody;
@@ -25,6 +46,43 @@ public class HttpResponse {
 
     public boolean addHeader(String key, String value) {
         return responseHeader.addAttribute(key, value);
+    }
+
+    public void sendRedirect(HttpRequest httpRequest, String path) {
+        addStatusLine(httpRequest, HttpStatus.FOUND);
+        addHeader(HEADER_FIELD_LOCATION, HTTP_PROTOCOL + httpRequest.getHeaderFieldValue(HEADER_FIELD_HOST) + path.substring(REDIRECT_PREFIX.length()));
+    }
+
+    public void forward(HttpRequest httpRequest, byte[] file) {
+        addStatusLine(httpRequest, HttpStatus.OK);
+        addHeader(HEADER_FIELD_CONTENT_TYPE, CONTENT_TYPE_CSS);
+        addBody(file);
+    }
+
+    public void forward(HttpRequest httpRequest, ModelAndView modelAndView) {
+        byte[] file = getFile(modelAndView);
+
+        addStatusLine(httpRequest, HttpStatus.OK);
+        addHeader(HEADER_FIELD_CONTENT_TYPE, CONTENT_TYPE_HTML + SEMICOLON + CHARSET_UTF8);
+        addHeader(HEADER_FIELD_CONTENT_LENGTH, String.valueOf(file.length));
+        addBody(file);
+    }
+
+    private byte[] getFile(ModelAndView modelAndView) {
+        TemplateLoader loader = new ClassPathTemplateLoader();
+        loader.setPrefix("/templates");
+        loader.setSuffix(".html");
+        Handlebars handlebars = new Handlebars(loader);
+
+        byte[] file = null;
+        try {
+            String view = modelAndView.getView();
+            Template template = handlebars.compile(view);
+            file = template.apply(modelAndView.getModel()).getBytes();
+        } catch (IOException e) {
+            log.debug("fail to compile {}", e.getMessage());
+        }
+        return file;
     }
 
     public String responseLine() {
