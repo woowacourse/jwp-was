081a9bf7e67aa6cf764ab2afab65e67a058220bb
school0bhy <house1217@ajou.ac.kr>
2019-10-01 21:10:50 +0900

    refactor: 디폴트 HttpRequestHandler 설정 구현에 따른 테스트 코드 변경 - HttpResourceRequestHandler를 디폴트 설정

--- src/test/java/webserver/HttpRequestHandlerMappingTest.java
+++ src/test/java/webserver/HttpRequestHandlerMappingTest.java
@@ -1,18 +1,12 @@
 package webserver;
 
-import http.common.HttpHeader;
-import http.request.HttpRequest;
-import http.request.RequestBody;
-import http.request.RequestLine;
-import http.response.HttpResponse;
 import org.junit.jupiter.api.Test;
 import webserver.httphandler.HttpRequestHandler;
 import webserver.httphandler.HttpRequestHandlerMapping;
-import webserver.httphandler.exception.ResourceNotFoundException;
+import webserver.httphandler.HttpResourceRequestHandler;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static utils.StringUtils.BLANK;
 
 class HttpRequestHandlerMappingTest {
     private HttpRequestHandlerMapping httpRequestHandlerMapping = HttpRequestHandlerMapping.getInstance();
@@ -23,14 +17,8 @@ class HttpRequestHandlerMappingTest {
     }
 
     @Test
-    void handler_존재하지_않는_경우() {
-        HttpRequest httpRequest = new HttpRequest(
-                new RequestLine("POST /user/login HTTP/1.1"),
-                new HttpHeader(),
-                new RequestBody(BLANK, BLANK));
-        HttpResponse httpResponse = new HttpResponse(httpRequest);
-
-        HttpRequestHandler handler = httpRequestHandlerMapping.getHandler(null);
-        assertThrows(ResourceNotFoundException.class, () -> handler.handleHttpRequest(httpRequest, httpResponse));
+    void handler_존재하지_않는_경우_디폴트가_HttpResourceRequestHandler() {
+        HttpRequestHandler httpRequestHandler = httpRequestHandlerMapping.getHandler(null);
+        assertThat(httpRequestHandler.getClass()).isEqualTo(HttpResourceRequestHandler.class);
     }
 }
