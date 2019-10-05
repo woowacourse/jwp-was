38e0675de459a6a148e00b563567a4f019f5cf67
soojinroh <rsj3112@gmail.com>
2019-10-05 19:01:51 +0900

    refactor : 컨벤션 수정 및 사용하지 않는 클래스 삭제
    UserBuilder 클래스 삭제

--- src/main/java/webserver/http/Cookie.java
+++ src/main/java/webserver/http/Cookie.java
@@ -97,8 +97,12 @@ public class Cookie {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
         Cookie cookie = (Cookie) o;
         return Objects.equals(name, cookie.name) &&
                 Objects.equals(value, cookie.value) &&
