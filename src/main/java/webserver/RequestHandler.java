7d8ba8f0a3e42961b5cdbb99d01249d25f922603
김강민 <kangminals5610@gmail.com>
2019-10-06 19:46:51 +0900

    [refactor] change RequestHandler structure

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,10 +1,16 @@
 package webserver;
 
+import exception.HttpMethodNotFoundException;
+import exception.InvalidSignUpDataException;
+import exception.UnregisteredURLException;
+import exception.UnsupportedMethodException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import webserver.controller.request.HttpRequest;
 import webserver.controller.response.HttpResponse;
 
+import java.io.DataOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
@@ -22,15 +28,32 @@ public class RequestHandler implements Runnable {
         logger.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
             connection.getPort());
         try (InputStream inputStream = connection.getInputStream(); OutputStream outputStream = connection.getOutputStream()) {
-            HttpRequest httpRequest = new HttpRequest(inputStream);
-
-            Router router = Router.getInstance();
-            HttpResponse httpResponse = router.serve(httpRequest);
+            DataOutputStream dataOutputStream = new DataOutputStream(outputStream);
+            HttpResponse httpResponse = getHttpResponse(inputStream);
 
             Renderer renderer = Renderer.getInstance();
-            renderer.render(outputStream, httpResponse);
-        } catch (Exception e) {
-            HttpResponse.badRequest(e.getMessage());
+            renderer.render(dataOutputStream, httpResponse);
+        } catch (IOException e) {
+            logger.debug(e.getMessage());
         }
+    }
+
+    private HttpResponse getHttpResponse(InputStream inputStream) throws IOException {
+        HttpResponse httpResponse;
+        try {
+            HttpRequest httpRequest = new HttpRequest(inputStream); //HttpMethodNotFoundException , UnsupportedMethodException
+            Router router = Router.getInstance();
+            httpResponse = router.serve(httpRequest);
+        } catch (HttpMethodNotFoundException | UnsupportedMethodException e) {
+            httpResponse = HttpResponse.badRequest(e.getMessage());
+            logger.debug("HttpMethodNotFoundException!!");
+        } catch (UnregisteredURLException e) {
+            httpResponse = HttpResponse.NotFound(e.getMessage());
+            logger.debug("not found!!");
+        } catch (InvalidSignUpDataException e) {
+            httpResponse = HttpResponse.InternerServerError(e.getMessage());
+            logger.debug("interner server error");
+        }
+        return httpResponse;
     }
 }
