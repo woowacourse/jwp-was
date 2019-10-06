a8ecceaeade7e97572807f1ee64147767cc1d22d
kangmin46 <kangmin789@naver.com>
2019-10-06 19:33:06 +0900

    [refactor]  seperate method in LoginController

--- src/main/java/webserver/controller/LoginController.java
+++ src/main/java/webserver/controller/LoginController.java
@@ -5,18 +5,20 @@ import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import webserver.ModelAndView;
+import webserver.controller.cookie.HttpCookie;
 import webserver.controller.request.HttpRequest;
 import webserver.controller.response.HttpResponse;
+import webserver.controller.session.HttpSessionManager;
 
 import java.io.IOException;
 import java.util.Map;
 import java.util.Optional;
 
 public class LoginController extends AbstractController {
+    public static final String LOGIN_SUCCESS_INDEX = "/index.html";
     private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
     private static final String USER_ID = "userId";
     private static final String PASSWORD = "password";
-    public static final String LOGIN_SUCCESS_INDEX = "/index.html";
     private static final String LOGIN_FAILED_INDEX = "/user/login_failed.html";
 
     @Override
@@ -31,13 +33,25 @@ public class LoginController extends AbstractController {
         Map<String, String> requestBodyFields = httpRequest.getBodyFields();
         String userId = requestBodyFields.get(USER_ID);
         String password = requestBodyFields.get(PASSWORD);
-        return sendLoginPage(httpRequest, userId, password);
+        Optional<User> maybeUser = DataBase.findUserById(userId);
+        return loginRedirect(httpRequest, maybeUser, password);
     }
 
-    private HttpResponse sendLoginPage(HttpRequest httpRequest, String userId, String password) {
-        Optional<User> maybeUser = DataBase.findUserById(userId);
-        return maybeUser.filter(user -> user.isMatchPassword(password))
-            .map(user -> HttpResponse.sendRedirect(httpRequest, LOGIN_SUCCESS_INDEX, true))
-            .orElse(HttpResponse.sendRedirect(httpRequest, LOGIN_FAILED_INDEX, false));
+    private HttpResponse loginRedirect(HttpRequest httpRequest, Optional<User> maybeUser, String password) {
+        if (maybeUser.isPresent()) {
+            return LoginSuccessRedirect(httpRequest, password, maybeUser);
+        }
+        return HttpResponse.sendRedirect(httpRequest, LOGIN_FAILED_INDEX);
+    }
+
+    private HttpResponse LoginSuccessRedirect(HttpRequest httpRequest, String password, Optional<User> maybeUser) {
+        User user = maybeUser.get();
+        if (user.isMatchPassword(password)) {
+            HttpSessionManager httpSessionManager = HttpSessionManager.getInstance();
+            String sessionId = httpSessionManager.setAttribute("user", user);
+            httpRequest.setSessionId(sessionId);
+            return HttpResponse.loginSuccessRedirect(httpRequest, LOGIN_SUCCESS_INDEX);
+        }
+        return HttpResponse.sendRedirect(httpRequest, LOGIN_FAILED_INDEX);
     }
 }
