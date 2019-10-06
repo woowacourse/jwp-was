5caa1c401b923d2bad8c361898d0afc69dcd0243
김강민 <kangminals5610@gmail.com>
2019-10-06 19:41:16 +0900

    [refactor] add renderCookie method

--- src/main/java/webserver/Renderer.java
+++ src/main/java/webserver/Renderer.java
@@ -6,6 +6,7 @@ import webserver.controller.response.HttpStatus;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.util.List;
 import java.util.Map;
 
 public class Renderer {
@@ -16,30 +17,37 @@ public class Renderer {
         return LazyHolder.INSTANCE;
     }
 
-    private static class LazyHolder {
-        private static final Renderer INSTANCE = new Renderer();
-    }
-
-    public void render(OutputStream outputStream, HttpResponse httpResponse) throws IOException {
+    public void render(DataOutputStream outputStream, HttpResponse httpResponse) throws IOException {
         DataOutputStream dataOutputStream = new DataOutputStream(outputStream);
         HttpStatus httpStatus = httpResponse.getHttpStatus();
-        Map<String, String> responseHeaderFields = httpResponse.getHeaderFields();
         renderResponseLine(httpResponse, dataOutputStream, httpStatus);
-        renderResponseFields(dataOutputStream, responseHeaderFields);
+
+        if(httpResponse.hasCookie()) {
+            renderResponseCookie(dataOutputStream, httpResponse.getCookieFields());
+        }
+
+        renderResponseFields(dataOutputStream, httpResponse.getHeaderFields());
+
         if (httpResponse.hasBody()) {
             renderResponseBody(httpResponse, dataOutputStream);
         }
+
         dataOutputStream.flush();
     }
 
-    private void renderResponseLine(HttpResponse httpResponse, DataOutputStream dataOutputStream, HttpStatus httpStatus) throws IOException {
+    private void renderResponseCookie(DataOutputStream dataOutputStream, List<String> cookieFields) throws IOException {
+        for (String cookieField : cookieFields) {
+            dataOutputStream.writeBytes("Set-Cookie: "+ cookieField +"; Path=/ \r\n");
+        }
+    }
 
+    private void renderResponseLine(HttpResponse httpResponse, DataOutputStream dataOutputStream, HttpStatus httpStatus) throws IOException {
         dataOutputStream.writeBytes(httpResponse.getVersion() + " " + httpStatus.getStatusCode() + " " + httpStatus.getStatus() + " \r\n");
     }
 
     private void renderResponseFields(DataOutputStream dataOutputStream, Map<String, String> responseHeaderFields) throws IOException {
         for (String key : responseHeaderFields.keySet()) {
-            dataOutputStream.writeBytes(key + ": " + responseHeaderFields.get(key) + "\r\n");
+            dataOutputStream.writeBytes(key + ": " + responseHeaderFields.get(key) + "\n");
         }
         dataOutputStream.writeBytes("\r\n");
     }
@@ -48,5 +56,9 @@ public class Renderer {
         byte[] body = httpResponse.getBody();
 
         dataOutputStream.write(body, 0, body.length);
+    }
+
+    private static class LazyHolder {
+        private static final Renderer INSTANCE = new Renderer();
     }
 }
