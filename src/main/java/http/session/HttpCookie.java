cfbbc9e19d30f805a51e27e7a7744d256f485b72
deocksoo <dusol92@snu.ac.kr>
2019-10-07 15:56:26 +0900

    feat: HttpCookie 추가

---
+++ src/main/java/http/session/HttpCookie.java
@@ -0,0 +1,58 @@
+package http.session;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import utils.LoggingUtils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HttpCookie {
+    private static final Logger logger = LoggerFactory.getLogger(HttpCookie.class);
+
+    private static final String COOKIE_DELIMITER = ";\\s*";
+    private static final String NAME_AND_VALUE_DELIMITER = "=";
+    private static final int COOKIE_NAME = 0;
+    private static final int VALUE = 1;
+
+    private Map<String, String> cookies;
+
+    private HttpCookie(Map<String, String> cookies) {
+        this.cookies = cookies;
+    }
+
+    public static HttpCookie parse(String cookieLine) {
+        HashMap<String, String> cookies = new HashMap<>();
+
+        if (isEmptyCookieHeader(cookieLine)) {
+            return new HttpCookie(new HashMap<>());
+        }
+
+        for (String cookie : cookieLine.split(COOKIE_DELIMITER)) {
+            addCookie(cookies, cookie);
+        }
+        return new HttpCookie(cookies);
+    }
+
+    private static boolean isEmptyCookieHeader(String cookieLine) {
+        return cookieLine == null || cookieLine.trim().isEmpty();
+    }
+
+    private static void addCookie(HashMap<String, String> cookies, String cookie) {
+        String[] nameAndValue = cookie.split(NAME_AND_VALUE_DELIMITER);
+
+        try {
+            cookies.put(nameAndValue[COOKIE_NAME], nameAndValue[VALUE]);
+        } catch (IndexOutOfBoundsException e) {
+            LoggingUtils.logStackTrace(logger, e);
+        }
+    }
+
+    public String getCookie(String cookieName) {
+        return cookies.get(cookieName);
+    }
+
+    public Map<String, String> getCookies() {
+        return cookies;
+    }
+}
---
+++ src/test/java/http/session/HttpCookieTest.java
@@ -0,0 +1,58 @@
+package http.session;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class HttpCookieTest {
+    @Test
+    @DisplayName("Http request 헤더에서 쿠키 정보를 받아서 쿠키 객체를 만든다.")
+    void parse() {
+        String cookiesInHeader = "logined=true; Path=/";
+
+        HttpCookie cookies = HttpCookie.parse(cookiesInHeader);
+
+        assertThat(cookies.getCookie("logined")).isEqualTo("true");
+        assertThat(cookies.getCookie("Path")).isEqualTo("/");
+    }
+
+    @Test
+    @DisplayName("쿠키 헤더가 없는 경우 빈 쿠키를 만든다.")
+    void returnEmptyCookie_ifCookieHeader_doesNotExsist() {
+        HttpCookie cookies = HttpCookie.parse(null);
+
+        assertThat(cookies.getCookies().size()).isEqualTo(0);
+    }
+
+    @Test
+    @DisplayName("쿠키 헤더는 있지만 쿠키가 하나도 없는 경우 빈 쿠키를 만든다.")
+    void returnEmptyCookie_ifNoCookie_inCookieHeader() {
+        HttpCookie cookies = HttpCookie.parse("");
+
+        assertThat(cookies.getCookies().size()).isEqualTo(0);
+    }
+
+    @Test
+    @DisplayName("쿠키 이름이 없는 경우 해당 쿠키는 만들지 않는다.")
+    void passParsing_ifCookieName_doesNotExist() {
+        String cookiesInHeader = "=true; Path=/";
+
+        HttpCookie cookies = HttpCookie.parse(cookiesInHeader);
+
+        assertThat(cookies.getCookie("logined")).isEqualTo(null);
+        assertThat(cookies.getCookie("Path")).isEqualTo("/");
+    }
+
+    @Test
+    @DisplayName("쿠키 값이 없는 경우 해당 쿠키는 만들지 않는다.")
+    void passParsing_ifCookieValue_doesNotExist() {
+        String cookiesInHeader = "logined=true; Path=";
+
+        HttpCookie cookies = HttpCookie.parse(cookiesInHeader);
+
+        assertThat(cookies.getCookie("logined")).isEqualTo("true");
+        assertThat(cookies.getCookie("Path")).isEqualTo(null);
+
+    }
+}
