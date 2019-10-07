4b648fae8dbab0f78a2927face4070ae71309735
pkch93 <star_final@naver.com>
2019-10-07 16:26:56 +0900

    test: 세션 만료 테스트

--- src/test/java/http/common/HttpSessionTest.java
+++ src/test/java/http/common/HttpSessionTest.java
@@ -1,5 +1,6 @@
 package http.common;
 
+import http.exception.SessionExpireException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -7,6 +8,7 @@ import java.time.LocalDateTime;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class HttpSessionTest {
 
@@ -22,5 +24,12 @@ public class HttpSessionTest {
     @Test
     void 생성시_UUID가_같은지_테스트() {
         assertThat(httpSession.getSessionId()).isEqualTo(uuid.toString());
+    }
+
+    @Test
+    void 세션에_등록된_시간이_먄료된_경우_세션_만료_예외처리() {
+        httpSession = new HttpSession(uuid, LocalDateTime.now());
+
+        assertThrows(SessionExpireException.class, () -> httpSession.getAttribute("anyone"));
     }
 }
