dccc2411e3173b4eb37fb8e5948247c65acb8510
박효진 <hgenie14@gmail.com>
2019-10-08 21:46:09 +0900

    feat: ModelAndView 클래스 추가

---
+++ src/main/java/webserver/view/ModelAndView.java
@@ -0,0 +1,34 @@
+package webserver.view;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ModelAndView {
+    private View view;
+    private Map<String, Object> models;
+
+    private ModelAndView() {
+    }
+
+    public ModelAndView(final View view) {
+        this(view, new HashMap<>());
+    }
+
+    public ModelAndView(final View view, final Map<String, Object> models) {
+        this.view = view;
+        this.models = models;
+    }
+
+    public View getView() {
+        return view;
+    }
+
+    public Map<String, Object> getModels() {
+        return Collections.unmodifiableMap(models);
+    }
+
+    public void addModel(final String key, final Object value) {
+        this.models.put(key, value);
+    }
+}
