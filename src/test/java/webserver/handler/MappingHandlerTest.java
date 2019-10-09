e4f04d57c5dc1b15220dbb0a6f4cabdb2b8e8958
kdg <kmdngyu@gmail.com>
2019-10-10 04:07:22 +0900

    fix: canMapping이 HttpRequest를 파라미터로 함에 따라 깨지는 테스트 수정

--- src/test/java/webserver/handler/MappingHandlerTest.java
+++ src/test/java/webserver/handler/MappingHandlerTest.java
@@ -2,31 +2,48 @@ package webserver.handler;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import webserver.http.request.RequestUri;
+import webserver.http.request.*;
 import webserver.servlet.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class MappingHandlerTest {
+    HttpRequest getRequest(String uri) {
+        return new HttpRequest(new RequestLine("GET", uri, "HTTP/1.1")
+                , new RequestHeader(new HashMap<>()),
+                new RequestBody(null),
+                new Cookie(null));
+    }
+
+    HttpRequest getRequestWithAccept(String uri) {
+        Map<String, String> requestHeader = new HashMap<>();
+        requestHeader.put("Accept","text/html");
+        return new HttpRequest(new RequestLine("GET", uri, "HTTP/1.1")
+                , new RequestHeader(requestHeader),
+                new RequestBody(null),
+                new Cookie(null));
+    }
+
     @DisplayName("리퀘스트 서블릿 매핑")
     @Test
     void getServlets_request_true() {
-        HttpServlet homeServlet = MappingHandler.getServlets(new RequestUri("/"));
-        HttpServlet userCreateServlet = MappingHandler.getServlets(new RequestUri("/user/create"));
-        HttpServlet userListServlet = MappingHandler.getServlets(new RequestUri("/user/list"));
-        HttpServlet userLoginServlet = MappingHandler.getServlets(new RequestUri("/user/login"));
+        HttpServlet homeServlet = MappingHandler.getServlets(getRequest("/"));
+        HttpServlet userCreateServlet = MappingHandler.getServlets(getRequest("/user/create"));
+        HttpServlet userListServlet = MappingHandler.getServlets(getRequest("/user/list"));
+        HttpServlet userLoginServlet = MappingHandler.getServlets(getRequest("/user/login"));
         assertThat(homeServlet).isInstanceOf(HomeServlet.class);
         assertThat(userCreateServlet).isInstanceOf(UserCreateServlet.class);
         assertThat(userListServlet).isInstanceOf(UserListServlet.class);
         assertThat(userLoginServlet).isInstanceOf(UserLoginServlet.class);
-
-
     }
 
     @DisplayName("정적 파일 서블릿 매핑")
     @Test
     void getServlets_staticFile_true() {
-        HttpServlet fileServlet = MappingHandler.getServlets(new RequestUri("/abc.html"));
+        HttpServlet fileServlet = MappingHandler.getServlets(getRequestWithAccept("/abc.html"));
         assertThat(fileServlet).isInstanceOf(FileServlet.class);
     }
 }
--- src/test/java/webserver/servlet/AbstractServletTest.java
+++ src/test/java/webserver/servlet/AbstractServletTest.java
@@ -17,57 +17,57 @@ public abstract class AbstractServletTest {
     User dummyUser = new User("id", "password", "name", "mail@mail");
     String dummySessionID = "dummySessionID";
 
-    protected HttpRequest getCommonGetRequest(String uri) {
+    HttpRequest getCommonGetRequest(String uri) {
         return new HttpRequest(new RequestLine("GET", uri, "HTTP/1.1")
                 , new RequestHeader(new HashMap<>()),
                 new RequestBody(null),
                 new Cookie(null));
     }
 
-    protected HttpRequest getCreateUserPostRequest() {
+    HttpRequest getCreateUserPostRequest() {
         return new HttpRequest(new RequestLine("POST", "/user/create", "HTTP/1.1")
                 , new RequestHeader(new HashMap<>()),
                 new RequestBody(createUserBody()),
                 new Cookie(null));
     }
 
-    protected HttpRequest getLoginPostRequest() {
+    HttpRequest getLoginPostRequest() {
         return new HttpRequest(new RequestLine("POST", "/user/login", "HTTP/1.1")
                 , new RequestHeader(new HashMap<>()),
                 new RequestBody(loginBody()),
                 new Cookie(null));
     }
 
-    protected HttpRequest getUserListRequest(String sessionId) {
+    HttpRequest getUserListRequest(String sessionId) {
         return new HttpRequest(new RequestLine("POST", "/user/list", "HTTP/1.1")
                 , new RequestHeader(new HashMap<>()),
                 new RequestBody(loginBody()),
                 new Cookie(Cookie.SESSION_ID_KEY + "=" + sessionId));
     }
 
-    protected String createUserBody() {
+    String createUserBody() {
         return "userId=" + dummyUser.getUserId() +
                 "&password=" + dummyUser.getPassword() +
                 "&name=" + dummyUser.getName() +
                 "&email=" + dummyUser.getEmail();
     }
 
-    protected String loginBody() {
+    String loginBody() {
         return "userId=" + dummyUser.getUserId() +
                 "&password=" + dummyUser.getPassword();
     }
 
 
-    protected void signUpAndLogin(User user, String sessionId) {
+    void signUpAndLogin(User user, String sessionId) {
         signUp(user);
         login(user, sessionId);
     }
 
-    protected void signUp(User user) {
+    void signUp(User user) {
         DataBase.addUser(user);
     }
 
-    protected void login(User user, String sessionId) {
+    void login(User user, String sessionId) {
         SessionStorage.create(StorageHelper.idGenerator(sessionId));
         SessionStorage.get(sessionId).setAttribute("user", user);
     }
