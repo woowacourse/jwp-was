235d0565e87b4abb627d4e726d8c6ac9db38f985
hyunsoojung <jhs695@naver.com>
2019-10-12 13:23:24 +0900

    refactor: httpResponse.forward() 로직 뷰로 이동

--- src/main/java/controller/IndexController.java
+++ src/main/java/controller/IndexController.java
@@ -7,17 +7,13 @@ import http.response.HttpResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
-import java.net.URISyntaxException;
-
 public class IndexController extends AbstractController {
     private static final Logger logger = LoggerFactory.getLogger(IndexController.class);
 
     public static final String PATH = "/";
 
     @Override
-    View doGet(HttpRequest httpRequest, HttpResponse httpResponse) throws IOException, URISyntaxException {
-        httpResponse.forward(httpRequest.getPath() + "index.html");
+    View doGet(HttpRequest httpRequest, HttpResponse httpResponse) {
 
         return new TemplateView("index.html");
     }
