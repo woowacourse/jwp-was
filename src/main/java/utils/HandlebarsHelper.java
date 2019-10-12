dc3f5a8039d9506152aeaf7f461ed501088ad3e5
hyunsoojung <jhs695@naver.com>
2019-10-12 16:39:42 +0900

    refactor: HandlebarsHelper 구조 변경

--- src/main/java/utils/HandlebarsHelper.java
+++ src/main/java/utils/HandlebarsHelper.java
@@ -5,18 +5,17 @@ import com.github.jknack.handlebars.Handlebars;
 import com.github.jknack.handlebars.Template;
 import com.github.jknack.handlebars.io.ClassPathTemplateLoader;
 import com.github.jknack.handlebars.io.TemplateLoader;
-import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
-import java.util.Collection;
 import java.util.Collections;
+import java.util.Map;
 
 public class HandlebarsHelper {
     private static final Logger LOGGER = LoggerFactory.getLogger(HandlebarsHelper.class);
 
-    public static byte[] apply(Collection<User> users) throws IOException {
+    public static byte[] apply(Map<String, ?> model) throws IOException {
         TemplateLoader templateLoader = new ClassPathTemplateLoader();
         templateLoader.setPrefix("/templates");
         templateLoader.setPrefix(".html");
@@ -25,6 +24,6 @@ public class HandlebarsHelper {
 
         Template template = handlebars.compile("user/list");
 
-        return template.apply(Collections.singletonMap("users", users)).getBytes();
+        return template.apply(Collections.singletonMap("users", model.values())).getBytes();
     }
 }
--- src/main/java/view/HandlebarView.java
+++ src/main/java/view/HandlebarView.java
@@ -3,10 +3,8 @@ package view;
 import http.request.HttpRequest;
 import http.response.HttpResponse;
 import http.response.HttpResponseBody;
-import model.User;
 import utils.HandlebarsHelper;
 
-import java.util.Collection;
 import java.util.Map;
 
 public class HandlebarView implements View {
@@ -19,11 +17,9 @@ public class HandlebarView implements View {
     @Override
     public void render(Map<String, ?> model,  HttpRequest request, HttpResponse response) throws Exception {
         response.forward(request.getPath() + path);
-        if (!model.isEmpty()) {
 
-            //todo get은 제거가 불가능..?
-            Collection<User> users = (Collection<User>) model.get("userList");
-            response.setHttpResponseBody(new HttpResponseBody(HandlebarsHelper.apply(users)));
+        if (!model.isEmpty()) {
+            response.setHttpResponseBody(new HttpResponseBody(HandlebarsHelper.apply(model)));
         }
     }
 }
