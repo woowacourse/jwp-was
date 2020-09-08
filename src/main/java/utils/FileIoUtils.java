6c5c188e86d3d9285638cb32ee5fbdfc0809fc0e
hongbin23 <khbasd@gmail.com>
2020-09-08 17:52:58 +0900

    feat: 요구사항1 적용
     - path해당하는 파일을 서버에서 응답합게 한다.

--- src/main/java/utils/FileIoUtils.java
+++ src/main/java/utils/FileIoUtils.java
@@ -7,8 +7,17 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class FileIoUtils {
+
+    private static final String DELIMITER = " ";
+    private static final String TEMPLATE_PATH = "./templates";
+
     public static byte[] loadFileFromClasspath(String filePath) throws IOException, URISyntaxException {
         Path path = Paths.get(FileIoUtils.class.getClassLoader().getResource(filePath).toURI());
         return Files.readAllBytes(path);
+    }
+
+    public static byte[] loadFileFromRequest(String line) throws IOException, URISyntaxException {
+        String path = line.split(DELIMITER)[1];
+        return loadFileFromClasspath(TEMPLATE_PATH + path);
     }
 }
--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,13 +1,19 @@
 package webserver;
 
+import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.net.URISyntaxException;
+import java.nio.charset.StandardCharsets;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import utils.FileIoUtils;
 
 public class RequestHandler implements Runnable {
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
@@ -20,15 +26,23 @@ public class RequestHandler implements Runnable {
 
     public void run() {
         logger.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
-                connection.getPort());
+            connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
+            String line = bufferedReader.readLine();
+            byte[] body = FileIoUtils.loadFileFromRequest(line);
+
+            while (!"".equals(line)) {
+                System.out.println(line);
+                line = bufferedReader.readLine();
+            }
+
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World".getBytes();
+
             response200Header(dos, body.length);
             responseBody(dos, body);
-        } catch (IOException e) {
+        } catch (IOException | URISyntaxException e) {
             logger.error(e.getMessage());
         }
     }
