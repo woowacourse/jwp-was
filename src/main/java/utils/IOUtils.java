25e57c49295cd3b70bb1ad17e7593498cae8036e
황준호 <42054054+hwanghe159@users.noreply.github.com>
2020-09-09 11:48:53 +0900

    feat: URL추출 메서드 및 request header 출력 메서드 작성

--- src/main/java/utils/IOUtils.java
+++ src/main/java/utils/IOUtils.java
@@ -1,14 +1,22 @@
 package utils;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class IOUtils {
+    private static final Logger logger = LoggerFactory.getLogger(IOUtils.class);
+
+    public static final String BLANK = " ";
+    public static final int URL_INDEX = 1;
+
     /**
-     * @param BufferedReader는
-     *            Request Body를 시작하는 시점이어야
-     * @param contentLength는
-     *            Request Header의 Content-Length 값이다.
+     * @param BufferedReader는 Request Body를 시작하는 시점이어야
+     * @param contentLength는  Request Header의 Content-Length 값이다.
      * @return
      * @throws IOException
      */
@@ -16,5 +24,27 @@ public class IOUtils {
         char[] body = new char[contentLength];
         br.read(body, 0, contentLength);
         return String.copyValueOf(body);
+    }
+
+    public static String extractURL(String requestLine) {
+        String[] tokens = requestLine.split(BLANK);
+        return tokens[URL_INDEX];
+    }
+
+    public static void printRequest(InputStream in) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(in));
+        if (br == null) {
+            return;
+        }
+        String result = "\n";
+        String line = " ";
+        while (!"".equals(line)) {
+            line = br.readLine();
+            if (line == null) {
+                break;
+            }
+            result += line + "\n";
+        }
+        logger.debug(result);
     }
 }
--- src/test/java/utils/IOUtilsTest.java
+++ src/test/java/utils/IOUtilsTest.java
@@ -1,11 +1,13 @@
 package utils;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.BufferedReader;
-import java.io.StringReader;
+import java.io.*;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class IOUtilsTest {
     private static final Logger logger = LoggerFactory.getLogger(IOUtilsTest.class);
@@ -17,5 +19,27 @@ public class IOUtilsTest {
         BufferedReader br = new BufferedReader(sr);
 
         logger.debug("parse body : {}", IOUtils.readData(br, data.length()));
+    }
+
+    @DisplayName("request line을 입력됐을 때 URL 추출")
+    @Test
+    void extractURLTest() {
+        String requestLine = "GET /index.html HTTP/1.1";
+
+        String url = IOUtils.extractURL(requestLine);
+
+        assertThat(url).isEqualTo("/index.html");
+    }
+
+    @DisplayName("request를 로그로 남김")
+    @Test
+    void printRequestTest() throws IOException {
+        String request = "GET /index.html HTTP/1.1\n" +
+                "Host: localhost:8080\n" +
+                "Connection: keep-alive\n" +
+                "Accept: */*";
+        InputStream in = new ByteArrayInputStream(request.getBytes());
+
+        IOUtils.printRequest(in);
     }
 }
