0fbfa7f313a1ba34a73411f730b752ffa6209104
kimjunseok <jnunseok@gmail.com>
2020-09-09 20:04:35 +0900

    feat: 요구사항 1 완료

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,13 +1,19 @@
 package webserver;
 
+import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.net.URISyntaxException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import http.Request;
+import utils.FileIoUtils;
 
 public class RequestHandler implements Runnable {
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
@@ -23,14 +29,25 @@ public class RequestHandler implements Runnable {
                 connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+            BufferedReader br = new BufferedReader(new InputStreamReader(in,"UTF-8"));
+            Request httpRequest = parse(br);
+            logger.debug("request lines[1] : {}", httpRequest.getLine());
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World".getBytes();
+            byte[] body = FileIoUtils.loadFileFromClasspath("./templates" + httpRequest.getLine());
             response200Header(dos, body.length);
             responseBody(dos, body);
-        } catch (IOException e) {
+        } catch (IOException | URISyntaxException e) {
             logger.error(e.getMessage());
         }
+    }
+
+    private Request parse(BufferedReader br) throws IOException {
+        String line = br.readLine();
+        logger.debug("request line : {}", line);
+        String[] lines = line.split(" ");
+        logger.debug("request lines[0] : {}", lines[0]);
+        logger.debug("request lines[1] : {}", lines[1]);
+        return new Request(lines[0], lines[1]);
     }
 
     private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
