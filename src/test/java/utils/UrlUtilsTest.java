e9e41c1c3ac2362ebdef09ad2332a266894e9dda
aegis <aegis1920@gmail.com>
2020-09-11 02:23:17 +0900

    feat: 테스트코드에 DisplayName 추가

--- src/test/java/utils/UrlUtilsTest.java
+++ src/test/java/utils/UrlUtilsTest.java
@@ -5,6 +5,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Stream;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -12,12 +13,47 @@ import org.junit.jupiter.params.provider.MethodSource;
 
 public class UrlUtilsTest {
 
+    @DisplayName("Uri에서 리소스 경로 추출")
+    @ParameterizedTest
+    @CsvSource(value = {"/user/create?userId=javajigi&password=password, /user/create",
+        "?userId=javajigi&password=password, /"})
+    void extractFilePath(String input, String expected) {
+        String result = UrlUtils.extractFilePath(input);
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @DisplayName("HttpHeader first line에서 http method 추출")
+    @ParameterizedTest
+    @CsvSource(value = {"GET / HTTP/1.1, GET", "POST /index.html HTTP/1.1, POST",
+        "GET /user/login.html HTTP/1.1, GET"})
+   void extractHttpMethod(String input, String expected) {
+        String resourcePath = UrlUtils.extractHttpMethod(input);
+        assertThat(resourcePath).isEqualTo(expected);
+    }
+
+    @DisplayName("HttpHeader first line에서 리소스 경로 추출")
     @ParameterizedTest
     @CsvSource(value = {"GET / HTTP/1.1, /index.html", "GET /index.html HTTP/1.1, /index.html",
         "GET /user/login.html HTTP/1.1, /user/login.html"})
     void extractResourcePath(String input, String expected) {
         String resourcePath = UrlUtils.extractResourcePath(input);
         assertThat(resourcePath).isEqualTo(expected);
+    }
+
+    @DisplayName("Request Params에서 Param 추출")
+    @ParameterizedTest
+    @MethodSource("provideStringsForExtractRequestParam")
+    void extractRequestParam(String input, Map<String, String> expected) {
+        Map<String, String> result = UrlUtils.extractRequestParam(input);
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @DisplayName("URL에서 Param 추출")
+    @ParameterizedTest
+    @MethodSource("provideStringsForExtractRequestParamFromUrl")
+    void extractRequestParamFromUrl(String input, Map<String, String> expected) {
+        Map<String, String> result = UrlUtils.extractRequestParamFromUrl(input);
+        assertThat(result).isEqualTo(expected);
     }
 
     private static Stream<Arguments> provideStringsForExtractRequestParam() {
@@ -38,21 +74,6 @@ public class UrlUtilsTest {
                         put("email", "javajigi%40slipp.net");
                     }})
         );
-    }
-
-    @ParameterizedTest
-    @MethodSource("provideStringsForExtractRequestParam")
-    void extractRequestParam(String input, Map<String, String> expected) {
-        Map<String, String> result = UrlUtils.extractRequestParam(input);
-        assertThat(result).isEqualTo(expected);
-    }
-
-    @ParameterizedTest
-    @CsvSource(value = {"GET / HTTP/1.1, GET", "POST /index.html HTTP/1.1, POST",
-        "GET /user/login.html HTTP/1.1, GET"})
-   void extractHttpMethod(String input, String expected) {
-        String resourcePath = UrlUtils.extractHttpMethod(input);
-        assertThat(resourcePath).isEqualTo(expected);
     }
 
     private static Stream<Arguments> provideStringsForExtractRequestParamFromUrl() {
@@ -77,20 +98,5 @@ public class UrlUtilsTest {
                         put("email", "javajigi%40slipp.net");
                     }})
         );
-    }
-
-    @ParameterizedTest
-    @MethodSource("provideStringsForExtractRequestParamFromUrl")
-    void extractRequestParamFromUrl(String input, Map<String, String> expected) {
-        Map<String, String> result = UrlUtils.extractRequestParamFromUrl(input);
-        assertThat(result).isEqualTo(expected);
-    }
-
-    @ParameterizedTest
-    @CsvSource(value = {"/user/create?userId=javajigi&password=password, /user/create",
-        "?userId=javajigi&password=password, /"})
-    void extractFilePath(String input, String expected) {
-        String result = UrlUtils.extractFilePath(input);
-        assertThat(result).isEqualTo(expected);
     }
 }
--- src/test/java/webserver/RequestHeaderTest.java
+++ src/test/java/webserver/RequestHeaderTest.java
@@ -8,6 +8,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class RequestHeaderTest {
@@ -15,6 +16,7 @@ public class RequestHeaderTest {
     private final String HTTP_GET_REQUEST = "GET /index.html HTTP/1.1\r\nHost: localhost:8080\r\nConnection: keep-alive\r\nCache-Control: max-age=0\r\n\r\n";
     private final String HTTP_POST_REQUEST = "POST /index.html HTTP/1.1\r\nHost: localhost:8080\r\nConnection: keep-alive\r\nCache-Control: max-age=0\r\nContent-Length: 33\r\n\r\nuserId=javajigi&password=password";
 
+    @DisplayName("RequestHeader 객체 생성 - GET REQUEST")
     @Test
     void constructor_isGet_RequestHeader() throws IOException {
         InputStream inputStream = new ByteArrayInputStream(HTTP_GET_REQUEST.getBytes());
@@ -27,6 +29,7 @@ public class RequestHeaderTest {
         assertThat(requestHeader.getFirstLine()).isEqualTo("GET /index.html HTTP/1.1");
     }
 
+    @DisplayName("RequestHeader 객체 생성 - POST REQUEST")
     @Test
     void constructor_isPost_RequestHeader() throws IOException {
         InputStream inputStream = new ByteArrayInputStream(HTTP_POST_REQUEST.getBytes());
