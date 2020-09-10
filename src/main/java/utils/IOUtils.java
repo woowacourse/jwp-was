8073166a693a508ba986f449f9c043c5119a9f0b
lxxjn0 <ljy3510@gmail.com>
2020-09-10 12:33:46 +0900

    feat: Map형식의 Request Header를 추출하는 기능 구현

--- src/main/java/utils/IOUtils.java
+++ src/main/java/utils/IOUtils.java
@@ -2,8 +2,16 @@ package utils;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class IOUtils {
+    private static final Logger logger = LoggerFactory.getLogger(IOUtils.class);
+    private static final int REQUEST_URL_INDEX = 1;
+
     /**
      * @param BufferedReader는
      *            Request Body를 시작하는 시점이어야
@@ -17,4 +25,22 @@ public class IOUtils {
         br.read(body, 0, contentLength);
         return String.copyValueOf(body);
     }
+
+    public static Map<String, String> readRequestHeader(BufferedReader bufferedReader) throws
+            IOException {
+        String line = null;
+        Map<String, String> requestHeaders = new HashMap<>();
+
+        while (!"".equals(line = bufferedReader.readLine())) {
+            if (line == null) {
+                break;
+            }
+            String[] split = line.split(": ");
+            requestHeaders.put(split[0], split[1]);
+            logger.debug("Request Header: {}", line);
+        }
+        
+        return requestHeaders;
+    }
+
 }
--- src/test/java/utils/IOUtilsTest.java
+++ src/test/java/utils/IOUtilsTest.java
@@ -1,11 +1,17 @@
 package utils;
 
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.StringReader;
+import java.util.Map;
+
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.io.BufferedReader;
-import java.io.StringReader;
 
 public class IOUtilsTest {
     private static final Logger logger = LoggerFactory.getLogger(IOUtilsTest.class);
@@ -18,4 +24,31 @@ public class IOUtilsTest {
 
         logger.debug("parse body : {}", IOUtils.readData(br, data.length()));
     }
+
+    @DisplayName("Map형식의 Request Header를 추출한다.")
+    @Test
+    void readRequestUrl_NonParams() throws IOException {
+        // given
+        String requestHeader = "Host: localhost:8080\n"
+                + "Connection: keep-alive\n"
+                + "Content-Length: 59\n"
+                + "Content-Type: application/x-www-form-urlencoded\n"
+                + "Accept: */*";
+
+        StringReader stringReader = new StringReader(requestHeader);
+        BufferedReader bufferedReader = new BufferedReader(stringReader);
+        // when
+        Map<String, String> actual = IOUtils.readRequestHeader(bufferedReader);
+
+        // then
+        assertAll(
+                () -> assertThat(actual.get("Host")).isEqualTo("localhost:8080"),
+                () -> assertThat(actual.get("Connection")).isEqualTo("keep-alive"),
+                () -> assertThat(actual.get("Content-Length")).isEqualTo("59"),
+                () -> assertThat(actual.get("Content-Type")).isEqualTo(
+                        "application/x-www-form-urlencoded"),
+                () -> assertThat(actual.get("Accept")).isEqualTo("*/*")
+        );
+    }
+
 }
