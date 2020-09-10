4d3d89e2dc2c6d2538b02fbc240264435e21e107
toneyparky <qkrtmddhks95@gmail.com>
2020-09-10 21:18:35 +0900

    chore: 불필요한 유틸 클래스 제거

--- src/main/java/utils/StringSplitUtils.java
+++
@@ -1,7 +0,0 @@
-package utils;
-
-public class StringSplitUtils {
-	public static String splitAndFetch(String target, String delimiter, int fetchingIndex) {
-		return target.split(delimiter)[fetchingIndex];
-	}
-}
