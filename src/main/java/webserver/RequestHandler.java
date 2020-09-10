1e39843793d264a5584a7e46409f649dbdcf533b
juyeongjung <juyeong.dev@gmail.com>
2020-09-10 23:07:26 +0900

    feat: request를 읽고 원하는 파일을 제공 하는 기능 구현

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,15 +1,23 @@
 package webserver;
 
+import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
-
+import java.net.URISyntaxException;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import utils.FileIoUtils;
+import utils.Request;
 
 public class RequestHandler implements Runnable {
+
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
 
     private Socket connection;
@@ -20,17 +28,32 @@ public class RequestHandler implements Runnable {
 
     public void run() {
         logger.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
-                connection.getPort());
+            connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+            Request request = requestParser(in);
+            byte[] fileData = fileDataFinder(request);
+
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World".getBytes();
-            response200Header(dos, body.length);
-            responseBody(dos, body);
-        } catch (IOException e) {
+
+            response200Header(dos, fileData.length);
+            responseBody(dos, fileData);
+        } catch (IOException | URISyntaxException e) {
             logger.error(e.getMessage());
         }
+    }
+
+    private Request requestParser(InputStream in) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
+        String line;
+        List<String> lines = new ArrayList<>();
+
+        while (!"".equals(line = br.readLine()) && line != null) {
+            logger.debug(line);
+            lines.add(line);
+        }
+
+        return new Request(lines);
     }
 
     private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
@@ -51,5 +74,9 @@ public class RequestHandler implements Runnable {
         } catch (IOException e) {
             logger.error(e.getMessage());
         }
+    }
+
+    private byte[] fileDataFinder(Request request) throws IOException, URISyntaxException {
+        return FileIoUtils.loadFileFromClasspath("./templates" + request.getHeader("filePath"));
     }
 }
