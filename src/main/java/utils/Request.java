55b4c6260c10a9343986d3874832f14321374bc5
juyeongjung <juyeong.dev@gmail.com>
2020-09-10 23:30:34 +0900

    feat: request를 읽고 params를 저장하는 기능 추가

--- src/main/java/utils/Request.java
+++ src/main/java/utils/Request.java
@@ -3,21 +3,35 @@ package utils;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Stream;
 
 public class Request {
 
     private Map<String, Object> headers;
+    private Map<String, String> params;
 
     public Request(List<String> requestHeaders) {
         headers = new HashMap<>();
-        parseHttpMethods(requestHeaders.get(0));
+        params = new HashMap<>();
+        parseRequestFirstLine(requestHeaders.get(0));
     }
 
-    private void parseHttpMethods(String headerFirstLine) {
+    private void parseRequestFirstLine(String headerFirstLine) {
         String[] splitHeaderFirstLine = headerFirstLine.split(" ");
+
         headers.put("method", HttpMethod.of(splitHeaderFirstLine[0]));
         headers.put("filePath", splitHeaderFirstLine[1]);
         headers.put("httpVersion", splitHeaderFirstLine[2]);
+
+        parseParams(splitHeaderFirstLine[1]);
+    }
+
+    private void parseParams(String filePath) {
+        String paramsSequence = filePath.split("\\?")[1];
+
+        Stream.of(paramsSequence.split("&"))
+            .map(param -> param.split("="))
+            .forEach(pair -> params.put(pair[0], pair[1]));
     }
 
     public Object getHeader(String name) {
