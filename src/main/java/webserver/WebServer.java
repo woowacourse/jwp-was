f1e570c6fb18cb07ae03ee12e7d64c03b9bd2038
minuyim <minu_yim@naver.com>
2020-09-12 00:53:37 +0900

    refactor: WebServer port 번호를 받는 부분 Optional 활용

--- src/main/java/webserver/WebServer.java
+++ src/main/java/webserver/WebServer.java
@@ -2,6 +2,8 @@ package webserver;
 
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Objects;
+import java.util.Optional;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,13 +12,11 @@ public class WebServer {
     private static final Logger logger = LoggerFactory.getLogger(WebServer.class);
     private static final int DEFAULT_PORT = 8080;
 
-    public static void main(String args[]) throws Exception {
-        int port = 0;
-        if (args == null || args.length == 0) {
-            port = DEFAULT_PORT;
-        } else {
-            port = Integer.parseInt(args[0]);
-        }
+    public static void main(String[] args) throws Exception {
+        int port = Optional.ofNullable(args)
+            .filter(WebServer::isArgsNotEmpty)
+            .map(el -> Integer.parseInt(el[0]))
+            .orElse(DEFAULT_PORT);
 
         // 서버소켓을 생성한다. 웹서버는 기본적으로 8080번 포트를 사용한다.
         try (ServerSocket listenSocket = new ServerSocket(port)) {
@@ -24,10 +24,14 @@ public class WebServer {
 
             // 클라이언트가 연결될때까지 대기한다.
             Socket connection;
-            while ((connection = listenSocket.accept()) != null) {
+            while (Objects.nonNull(connection = listenSocket.accept())) {
                 Thread thread = new Thread(new RequestHandler(connection));
                 thread.start();
             }
         }
+    }
+
+    private static boolean isArgsNotEmpty(String[] args) {
+        return args.length != 0;
     }
 }
