4b750975e7c11a9adacd91a03d69d8b10ec83811
SeYun <ksy90101@naver.com>
2020-09-11 17:39:05 +0900

    feat: 302 response 구현

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,5 +1,7 @@
 package webserver;
 
+import static http.ResponseHeader.*;
+
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
@@ -10,6 +12,7 @@ import java.net.Socket;
 import java.net.URISyntaxException;
 import java.util.Map;
 
+import db.DataBase;
 import exception.IllegalRequestException;
 import http.Request;
 import http.RequestLine;
@@ -36,8 +39,8 @@ public class RequestHandler implements Runnable {
                 connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
             BufferedReader br = new BufferedReader(new InputStreamReader(in));
+            DataOutputStream dos = new DataOutputStream(out);
             byte[] body = null;
             Request request = new Request(br);
             RequestLine requestLine = request.getRequestLine();
@@ -46,30 +49,21 @@ public class RequestHandler implements Runnable {
                 String requestUrl = requestLine.getUrl();
                 if (requestUrl.endsWith(".html")) {
                     body = FileIoUtils.loadFileFromClasspath("./templates" + requestUrl);
+                    response200Header(dos, body.length);
                 } else {
                     QueryParams queryParams = new QueryParams(requestUrl);
                     Map<String, String> queryParamsMap = queryParams.getQueryParams();
                     User user = new User(queryParamsMap.get("userId"), queryParamsMap.get("password"), queryParamsMap.get("name"), queryParamsMap.get("email"));
+                    DataBase.addUser(user);
                 }
             } else if (requestLine.getMethod() == RequestMethod.POST) {
                 Map<String, String> requestBodies = request.getRequestBody().getRequestBodies();
                 User user = new User(requestBodies.get("userId"), requestBodies.get("password"), requestBodies.get("name"), requestBodies.get("email"));
+                DataBase.addUser(user);
+                response302Header(dos, "/index.html");
             }
-            DataOutputStream dos = new DataOutputStream(out);
-            response200Header(dos, body.length);
             responseBody(dos, body);
         } catch (Exception e) {
-            logger.error(e.getMessage());
-        }
-    }
-
-    private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
-        try {
-            dos.writeBytes("HTTP/1.1 200 OK \r\n");
-            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
-            dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
-            dos.writeBytes("\r\n");
-        } catch (IOException e) {
             logger.error(e.getMessage());
         }
     }
