4f2bb3debeb3322eba061e9381d5319ba5035feb
ordinCode <ysh1346@naver.com>
2020-09-11 10:55:39 +0900

    feat: 요구사항1 구현
     - index.html로 이동 가능

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,15 +1,22 @@
 package webserver;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.util.StringUtils;
+import utils.FileIoUtils;
+
+import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import java.net.URISyntaxException;
+import java.nio.charset.StandardCharsets;
 
 public class RequestHandler implements Runnable {
+    private static final String BASE_URL = "./templates";
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
 
     private Socket connection;
@@ -23,14 +30,24 @@ public class RequestHandler implements Runnable {
                 connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+            BufferedReader br = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
+            String requestUrl = findRequestUrl(br);
+            byte[] body = FileIoUtils.loadFileFromClasspath(BASE_URL + requestUrl);
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World".getBytes();
             response200Header(dos, body.length);
             responseBody(dos, body);
-        } catch (IOException e) {
+        } catch (IOException | URISyntaxException e) {
             logger.error(e.getMessage());
         }
+    }
+
+    private String findRequestUrl(BufferedReader br) throws IOException {
+        String line = br.readLine();
+        logger.debug("요청 정보 : {}", line);
+        if (StringUtils.isEmpty(line)) {
+            throw new IOException("요청 정보를 찾을 수 없습니다.");
+        }
+        return line.split(" ")[1];
     }
 
     private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
