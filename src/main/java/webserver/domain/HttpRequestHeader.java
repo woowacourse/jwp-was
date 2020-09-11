d82ae12d5075306c359033e7e43d5a89c513d92f
yejiahn <yeppilisa@naver.com>
2020-09-11 16:56:39 +0900

    refactor: RequestLine 클래스 분리 및 쓰이는 Pattern 객체 상수화

--- src/main/java/webserver/domain/HttpRequestHeader.java
+++ src/main/java/webserver/domain/HttpRequestHeader.java
@@ -12,31 +12,18 @@ import utils.StaticFileType;
 @Getter
 public class HttpRequestHeader {
 
-    public static final String NEW_LINE = "\n";
-    public static final String SPACE = " ";
-    public static final String QUESTION_MARK = "?";
-    public static final String COLON = ": ";
+    private static final String NEW_LINE = System.lineSeparator();
+    private static final String SPACE = " ";
+    private static final String QUESTION_MARK = "?";
+    private static final String COLON = ": ";
 
-    private final RequestMethod requestMethod;
-    private final String path;
-    private final Map<String, String> queryParams;
+    private final RequestLine requestLine;
     private final Map<String, String> headerParams;
 
     public HttpRequestHeader(String header) throws UnsupportedEncodingException {
         String[] lines = header.split(NEW_LINE);
-        String firstLineOfHeader = lines[0];
-        this.requestMethod = RequestMethod.from(firstLineOfHeader.split(SPACE)[0]);
-
-        this.queryParams = new HashMap<>();
-        String url = firstLineOfHeader.split(SPACE)[1];
-        int delimiterIndex = url.indexOf(QUESTION_MARK);
-        if (delimiterIndex != -1) {
-            this.path = url.substring(0, delimiterIndex);
-            String query = url.substring(delimiterIndex + 1);
-            IOUtils.addParameters(query, this.queryParams);
-        } else {
-            this.path = url;
-        }
+        String requestLine = lines[0];
+        this.requestLine = new RequestLine(requestLine);
 
         this.headerParams = new HashMap<>();
         for (int i = 1; i < lines.length; i++) {
@@ -46,11 +33,15 @@ public class HttpRequestHeader {
     }
 
     public boolean isStaticFile() {
-        return Pattern.matches("^.*[.][a-z]+$", this.path);
+        return this.requestLine.isStaticFile();
     }
 
     public boolean hasEqualPathWith(String target) {
-        return this.path.equals(target);
+        return this.requestLine.hasEqualPathWith(target);
+    }
+
+    public StaticFileType findExtension() {
+        return this.requestLine.findExtension();
     }
 
     public int findContentLength() {
@@ -59,9 +50,11 @@ public class HttpRequestHeader {
         return Integer.parseInt(length);
     }
 
-    public StaticFileType findExtension() {
-        String[] split = this.path.split("\\.");
-        String extensionName = split[split.length - 1];
-        return StaticFileType.valueOf(extensionName.toUpperCase());
+    public RequestMethod getRequestMethod() {
+        return requestLine.getRequestMethod();
+    }
+
+    public String getPath() {
+        return requestLine.getPath();
     }
 }
