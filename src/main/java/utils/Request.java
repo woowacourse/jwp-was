f5667c474be9fb625d0a7dcfc07619cabd25fb8c
juyeongjung <juyeong.dev@gmail.com>
2020-09-12 22:09:34 +0900

    refactor: 미사용 메소드 제거

--- src/main/java/utils/Request.java
+++ src/main/java/utils/Request.java
@@ -10,13 +10,11 @@ import java.util.Optional;
 public class Request {
 
     private Map<String, Object> headers;
-    private Map<String, String> params;
     private AcceptType type;
     private String body;
 
     public Request(List<String> requestHeaders, BufferedReader bufferedReader) throws IOException {
         headers = new HashMap<>();
-        params = new HashMap<>();
         parseRequestFirstLine(requestHeaders.remove(0));
         parseHeaders(requestHeaders);
         parseBody(bufferedReader);
@@ -38,8 +36,6 @@ public class Request {
         headers.put("method", HttpMethod.of(splitHeaderFirstLine[0]));
         headers.put("filePath", splitHeaderFirstLine[1].split("\\?")[0]);
         headers.put("httpVersion", splitHeaderFirstLine[2]);
-
-        parseParams(splitHeaderFirstLine[1]);
     }
 
     private void parseHeaders(List<String> requestHeaders) {
@@ -53,20 +49,6 @@ public class Request {
         String filePath = (String) getHeader("filePath");
         String fileExtension = IOUtils.extractExtension(filePath);
         this.type = AcceptType.of(fileExtension);
-    }
-
-    private void parseParams(String filePath) {
-        if (!filePath.contains("?")) {
-            return;
-        }
-        String paramsSequence = filePath.split("\\?")[1];
-
-        parseParam(paramsSequence);
-    }
-
-    private void parseParam(String paramsSequence) {
-        IOUtils.parseParamsSequence(paramsSequence)
-            .forEach(pair -> params.put(pair[0], pair[1]));
     }
 
     public boolean isGetRequest() {
