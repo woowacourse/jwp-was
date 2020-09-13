876865b74a140b75197d1a7a85c99d5e427e595e
KIMSIYOUNG <tldud2404@gmail.com>
2020-09-13 16:46:03 +0900

    feat : request body에 대한 테스트 추가

--- src/main/java/webserver/RequestBody.java
+++ src/main/java/webserver/RequestBody.java
@@ -2,38 +2,39 @@ package webserver;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.Objects;
 
 public class RequestBody {
     private final Map<String, String> attribute;
 
-    public RequestBody(Map<String, String> attribute) {
+    private RequestBody(Map<String, String> attribute) {
         this.attribute = attribute;
     }
 
     public static RequestBody of(String line) {
-        try {
-            Map<String, String> attributes = getBody(line);
+        Map<String, String> attributes = getBody(line);
 
-            return new RequestBody(attributes);
-        } catch (Exception e) {
+        return new RequestBody(attributes);
+    }
+
+    private static Map<String, String> getBody(String line) {
+        Map<String, String> attributes = new LinkedHashMap<>();
+        if (Objects.isNull(line) || line.isEmpty()) {
+            return attributes;
+        }
+        try {
+            String[] params = line.split("&");
+            for (String param : params) {
+                String[] attribute = param.split("=");
+                attributes.put(attribute[0], attribute[1]);
+            }
+            return attributes;
+        } catch (IndexOutOfBoundsException e) {
             throw new InvalidRequestBodyException();
         }
     }
 
     public Map<String, String> getAttribute() {
         return attribute;
-    }
-
-    private static Map<String, String> getBody(String line) {
-        Map<String, String> attributes = new LinkedHashMap<>();
-        if (line.isEmpty()) {
-            return attributes;
-        }
-        String[] params = line.split("&");
-        for (String param : params) {
-            String[] attribute = param.split("=");
-            attributes.put(attribute[0], attribute[1]);
-        }
-        return attributes;
     }
 }
