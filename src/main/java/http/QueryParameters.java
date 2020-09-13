b49dbb224812fdd68b33866f668f5f46caa77d8d
K.S.KIM <nullable@kakao.com>
2020-09-14 03:08:38 +0900

    feat: QueryParameters 추가

---
+++ src/main/java/http/QueryParameters.java
@@ -0,0 +1,38 @@
+package http;
+
+import static java.util.stream.Collectors.*;
+
+import java.util.Map;
+import java.util.stream.Stream;
+
+public class QueryParameters {
+    private static final String QUERY_DELIMITER = "&";
+    private static final String QUERY_KEY_VALUE_DELIMITER = "=";
+
+    private final Map<String, String> parameters;
+
+    public QueryParameters(final Map<String, String> parameters) {
+        this.parameters = parameters;
+    }
+
+    public static QueryParameters from(final String queries) {
+        return Stream.of(queries.split(QUERY_DELIMITER))
+                .map(query -> query.split(QUERY_KEY_VALUE_DELIMITER))
+                .collect(collectingAndThen(toMap(query -> query[0], query -> query[1]), QueryParameters::new));
+    }
+
+    public String getParameter(final String key) {
+        return parameters.get(key);
+    }
+
+    public Map<String, String> getParameters() {
+        return parameters;
+    }
+
+    @Override
+    public String toString() {
+        return "QueryParameters{" +
+                "parameters=" + parameters +
+                '}';
+    }
+}
