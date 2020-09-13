232f4c4d73435c3cb0de999c3c63872e5c970a52
ParkDooWon <pass345@naver.com>
2020-09-13 18:04:28 +0900

    feat : GET으로 요청된 URL에서 파라미터를 추출

---
+++ src/test/java/utils/ExtractUtilsTest.java
@@ -0,0 +1,27 @@
+package utils;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Map;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class ExtractUtilsTest {
+	private static final String PATH = "/user/create?userId=a&password=1&name=asd&email=test%40test.com";
+
+	@DisplayName("URL의 Param을 분리")
+	@Test
+	void extractUserInfoTest() throws UnsupportedEncodingException {
+		Map<String, String> userInfo = ExtractUtils.extractUserInfo(PATH);
+
+		assertAll(
+			() -> assertThat(userInfo.get("userId")).isEqualTo("a"),
+			() -> assertThat(userInfo.get("password")).isEqualTo("1"),
+			() -> assertThat(userInfo.get("name")).isEqualTo("asd"),
+			() -> assertThat(userInfo.get("email")).isEqualTo("test@test.com")
+		);
+	}
+}
