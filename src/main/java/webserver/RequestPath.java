109fd3da159e4549bc4fcb323deb87a560a62842
Sony <sonypark0204@gmail.com>
2020-09-14 15:37:16 +0900

    feat: RequestPath 클래스 생성

---
+++ src/main/java/webserver/RequestPath.java
@@ -0,0 +1,35 @@
+package webserver;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RequestPath {
+
+    private static final String PATH_PARAM_DELIMITER = "\\?";
+    private static final String PARAM_DELIMITER = "&";
+    private static final String PARAM_KEY_VALUE_DELIMITER = "=";
+    private String path;
+    private Map<String, String> parameters = new HashMap<>();
+
+    public RequestPath(String path) {
+        String[] splitPath = path.split(PATH_PARAM_DELIMITER);
+        this.path = splitPath[0];
+        if (path.equals(this.path)) {
+            return;
+        }
+        String params = splitPath[1];
+        String[] splitParams = params.split(PARAM_DELIMITER);
+        for (String param : splitParams) {
+            String[] token = param.split(PARAM_KEY_VALUE_DELIMITER);
+            parameters.put(token[0], token[1]);
+        }
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public String getParameters(String key) {
+        return parameters.get(key);
+    }
+}
