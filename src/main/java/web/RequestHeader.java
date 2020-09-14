d63db37b7c41a5e66fcaa11b5421b1c9a2f8923c
Sony <sonypark0204@gmail.com>
2020-09-14 11:00:29 +0900

    feat: RequestHeader headers 추가

--- src/main/java/web/RequestHeader.java
+++ src/main/java/web/RequestHeader.java
@@ -2,19 +2,30 @@ package web;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 
 public class RequestHeader {
 
+    private final Map<String, String> headers = new HashMap<>();
+
     public RequestHeader(BufferedReader bufferedReader) throws IOException {
         String line = bufferedReader.readLine();
 
-        while (!line.equals("")) {
-            line = bufferedReader.readLine();
-            if (Objects.isNull(line)) {
-                break;
-            }
+        while (!isEmpty(line)) {
             System.out.println(line);
+            String[] headerToken = line.split(": ");
+            headers.put(headerToken[0], headerToken[1]);
+            line = bufferedReader.readLine();
         }
+    }
+
+    private boolean isEmpty(String line) {
+        return Objects.isNull(line) || Objects.equals(line, "");
+    }
+
+    public Map<String, String> getHeaders() {
+        return headers;
     }
 }
