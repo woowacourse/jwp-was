24029508d5534c8b96766b27447697e3eee1fc2a
kimseonggyu1 <luckykimsk@gmail.com>
2020-09-14 16:39:02 +0900

    refactor: Params 일급 컬렉션 분리
    상수화 일부 진행

---
+++ src/test/java/stringprocessor/ParamsTest.java
@@ -0,0 +1,42 @@
+package stringprocessor;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class ParamsTest {
+
+    private static final String PARAM_BUNDLE = "userId=javajigi&password=password&name=박재성&email=javajigi@slipp.net";
+    private static final Map<String, String> PARAMS = new HashMap<String, String>() {{
+        put("userId", "javajigi");
+        put("password", "password");
+        put("name", "박재성");
+        put("email", "javajigi@slipp.net");
+    }};
+
+    @Test
+    public void from() {
+        Params params = Params.from(PARAM_BUNDLE);
+        assertThat(params.getParams()).isEqualTo(PARAMS);
+    }
+
+    @ParameterizedTest
+    @CsvSource({"userId,javajigi", "notExist,"})
+    public void findValueBy(String key, String expected) {
+        Params params = Params.from(PARAM_BUNDLE);
+        assertThat(params.findValueBy(key)).isEqualTo(nullToEmpty(expected));
+
+    }
+
+    private String nullToEmpty(String input) {
+        if (input == null) {
+            return "";
+        }
+        return input;
+    }
+}
