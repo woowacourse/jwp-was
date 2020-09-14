9c127ad97121a084d4c73890521235f0e903a548
lowoon <sangbo0229@gmail.com>
2020-09-14 11:45:19 +0900

    feat: read ans show request

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,18 +1,28 @@
 package webserver;
 
+import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.net.URISyntaxException;
+import java.nio.charset.StandardCharsets;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import utils.FileIoUtils;
+import utils.RequestUtils;
+
 public class RequestHandler implements Runnable {
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
+    public static final String EMPTY = "";
+    public static final int OFFSET = 0;
+    public static final String FILE_PATH = "./templates/";
 
-    private Socket connection;
+    private final Socket connection;
 
     public RequestHandler(Socket connectionSocket) {
         this.connection = connectionSocket;
@@ -20,17 +30,28 @@ public class RequestHandler implements Runnable {
 
     public void run() {
         logger.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
-                connection.getPort());
+            connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+            BufferedReader br = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
+            String fileName = readRequest(br);
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World".getBytes();
+            byte[] body = FileIoUtils.loadFileFromClasspath(FILE_PATH + fileName);
             response200Header(dos, body.length);
             responseBody(dos, body);
-        } catch (IOException e) {
+        } catch (IOException | URISyntaxException e) {
             logger.error(e.getMessage());
         }
+    }
+
+    private String readRequest(BufferedReader br) throws IOException {
+        String line = br.readLine();
+        String fileName = RequestUtils.extractFileName(line);
+        while (!EMPTY.equals(line) && line != null) {
+            logger.debug("header : {}", line);
+            line = br.readLine();
+        }
+        return fileName;
     }
 
     private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
@@ -46,7 +67,7 @@ public class RequestHandler implements Runnable {
 
     private void responseBody(DataOutputStream dos, byte[] body) {
         try {
-            dos.write(body, 0, body.length);
+            dos.write(body, OFFSET, body.length);
             dos.flush();
         } catch (IOException e) {
             logger.error(e.getMessage());
