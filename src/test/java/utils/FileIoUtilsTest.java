f5129a3e96412d30aa9e15141fceb0bbe8f390a3
김예림 <kiel0103@naver.com>
2020-09-14 16:40:19 +0900

    test: utils 테스트 클래스의 무의미한 public 키워드 삭제

--- src/test/java/utils/FileIoUtilsTest.java
+++ src/test/java/utils/FileIoUtilsTest.java
@@ -4,7 +4,8 @@ import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class FileIoUtilsTest {
+class FileIoUtilsTest {
+
     private static final Logger log = LoggerFactory.getLogger(FileIoUtilsTest.class);
 
     @Test
