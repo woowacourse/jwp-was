a616009bdb18672a1b8165552d462ee18f33a5c9
kimseonggyu1 <luckykimsk@gmail.com>
2020-09-14 16:46:51 +0900

    refactor: 상수화

--- src/main/java/http/request/RequestEntity.java
+++ src/main/java/http/request/RequestEntity.java
@@ -6,6 +6,13 @@ import java.io.IOException;
 import exception.RequestEntityCreateFailException;
 
 public class RequestEntity {
+    private static final String START_LINE_SEPERATOR = " ";
+    private static final int METHOD_INDEX = 0;
+    private static final int URL_INDEX = 1;
+    private static final int VERSION_INDEX = 2;
+    private static final String CONTENT_LENGTH = "Content-Length";
+    private static final String CONTENT_TYPE = "Content-Type";
+
     private HttpMethod httpMethod;
     private HttpUrl httpUrl;
     private String httpVersion;
@@ -33,16 +40,17 @@ public class RequestEntity {
 
     public static RequestEntity from(BufferedReader bufferedReader) {
         try {
-            String[] splittedStartLine = bufferedReader.readLine().split(" ");
-            HttpMethod httpMethod = HttpMethod.valueOf(splittedStartLine[0]);
-            HttpUrl httpUrl = HttpUrl.from(splittedStartLine[1]);
-            String httpVersion = splittedStartLine[2];
+            String[] splittedStartLine = bufferedReader.readLine().split(START_LINE_SEPERATOR);
+            HttpMethod httpMethod = HttpMethod.valueOf(splittedStartLine[METHOD_INDEX]);
+            HttpUrl httpUrl = HttpUrl.from(splittedStartLine[URL_INDEX]);
+            String httpVersion = splittedStartLine[VERSION_INDEX];
 
             RequestHeader requestHeader = RequestHeader.from(bufferedReader);
-            String contentLength = requestHeader.findOrEmpty("Content-Length");
+            String contentLength = requestHeader.findOrEmpty(CONTENT_LENGTH);
             if (!contentLength.isEmpty()) {
-                ContentType contentType = ContentType.findOrDefault(requestHeader.findOrEmpty("Content-Type"));
-                RequestBody requestBody = RequestBody.from(bufferedReader, contentType, Integer.parseInt(contentLength));
+                ContentType contentType = ContentType.findOrDefault(requestHeader.findOrEmpty(CONTENT_TYPE));
+                RequestBody requestBody = RequestBody.from(bufferedReader, contentType,
+                    Integer.parseInt(contentLength));
                 return new RequestEntity(httpMethod, httpUrl, httpVersion, requestHeader, requestBody);
             }
             return new RequestEntity(httpMethod, httpUrl, httpVersion, requestHeader);
--- src/main/java/http/request/RequestHeader.java
+++ src/main/java/http/request/RequestHeader.java
@@ -10,6 +10,12 @@ import exception.RequestHeaderCreateFailException;
 import utils.IOUtils;
 
 public class RequestHeader {
+    private static final String HEADER_SEPERATOR = ": ";
+    private static final int SPLIT_SIZE = 2;
+    private static final int KEY_INDEX = 0;
+    private static final int VALUE_INDEX = 1;
+    private static final String DEFAUlT_VALUE = "";
+
     private Map<String, String> headers;
 
     private RequestHeader(Map<String, String> headers) {
@@ -31,13 +37,13 @@ public class RequestHeader {
     private static void extractHeaders(BufferedReader bufferedReader, Map<String, String> headers) throws IOException {
         List<String> lines = IOUtils.readDataUntilEmpty(bufferedReader);
         for (String line : lines) {
-            String[] keyValues = line.split(": ", 2);
-            headers.put(keyValues[0], keyValues[1]);
+            String[] keyValues = line.split(HEADER_SEPERATOR, SPLIT_SIZE);
+            headers.put(keyValues[KEY_INDEX], keyValues[VALUE_INDEX]);
         }
     }
 
     public String findOrEmpty(String input) {
-        return headers.getOrDefault(input, "");
+        return headers.getOrDefault(input, DEFAUlT_VALUE);
     }
 
     public Map<String, String> getHeaders() {
