c288193c69f6dce1af4e2cbdb72f6b62582cb61d
icyMojito <dev.icymojito@gmail.com>
2020-09-14 20:25:24 +0900

    feat: HTTP Request의 첫째 줄을 관리하는 RequestLine 생성
    정해둔 규격(첫째 줄은 공백 기준으로 세 개의 요소가 존재)에 맞지 않는 Request Line 값으로 객체를 만들려할 때
    발생할 예외(InvalidRequestLineException) 만듦

---
+++ src/main/java/webserver/RequestLine.java
@@ -0,0 +1,44 @@
+package webserver;
+
+import exception.InvalidRequestLineException;
+import utils.StringUtils;
+
+public class RequestLine {
+    private static final String REQUEST_LINE_DELIMITER = " ";
+    private static final int REQUEST_LINE_ELEMENT_COUNT = 3;
+
+    private final HttpMethod httpMethod;
+    private final HttpUri httpUri;
+    private final HttpVersion httpVersion;
+
+    private RequestLine(HttpMethod httpMethod, HttpUri httpUri, HttpVersion httpVersion) {
+        this.httpMethod = httpMethod;
+        this.httpUri = httpUri;
+        this.httpVersion = httpVersion;
+    }
+
+    public static RequestLine from(String line) {
+        StringUtils.validateNonNullAndNotEmpty(line);
+
+        String[] tokens = createTokens(line);
+
+        HttpMethod httpMethod = HttpMethod.from(tokens[0]);
+        HttpUri httpUri = new HttpUri(tokens[1]);
+        HttpVersion httpVersion = new HttpVersion(tokens[2]);
+
+        return new RequestLine(httpMethod, httpUri, httpVersion);
+    }
+
+    private static String[] createTokens(String line) {
+        String[] tokens = line.split(REQUEST_LINE_DELIMITER);
+        if (tokens.length != REQUEST_LINE_ELEMENT_COUNT) {
+            throw new InvalidRequestLineException(line);
+        }
+
+        return tokens;
+    }
+
+    public byte[] readFile() {
+        return this.httpUri.readFile();
+    }
+}
