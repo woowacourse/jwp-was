9f692ba50184d166eb8ba166181188c793b3e8a8
김예림 <kiel0103@naver.com>
2020-09-15 16:48:01 +0900

    refactor: displayName 적용

--- src/test/java/webserver/resource/ResourcesHandlerTest.java
+++ src/test/java/webserver/resource/ResourcesHandlerTest.java
@@ -5,6 +5,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import resource.ContentType;
 import resource.Resource;
@@ -15,6 +16,7 @@ class ResourcesHandlerTest {
     private ResourcesHandler resourceHandler = new ResourcesHandler();
 
     @Test
+    @DisplayName("uri 로 resource 얻기")
     void convertUriToResource() throws IOException, URISyntaxException {
         Resource resource = resourceHandler.convertUriToResource("/css/styles.css");
         assertThat(resource).isInstanceOf(Resource.class);
@@ -22,6 +24,7 @@ class ResourcesHandlerTest {
     }
 
     @Test
+    @DisplayName("uri 로 resource 얻기 - uri 의 확장자가, 웹서버가 지원하지 않는 확장자인 경우 예외처리")
     void convertUriToResource_IfExtensionOfRequestFileIsNotSupported() {
         assertThatThrownBy(() -> resourceHandler.convertUriToResource("/css/styles.exe"))
             .isInstanceOf(IllegalArgumentException.class)
