b55ed00c5f59a686682f6d3458ebb653847bd023
KimGyeong <taekwondo714@naver.com>
2020-09-15 23:48:06 +0900

    refactor: 상수 분리

--- src/main/java/utils/RequestPathUtil.java
+++ src/main/java/utils/RequestPathUtil.java
@@ -1,12 +1,19 @@
 package utils;
 
 public class RequestPathUtil {
+
+	public static final String PATH_DELIMITER = " ";
+	public static final String HTML_FILE_TYPE = ".html";
+	public static final String ICO_FILE_TYPE = ".ico";
+	public static final String TEMPLATES_PATH = "./templates";
+	public static final String STATIC_PATH = "./static";
+
 	public static String extract(String request) {
-		String path = request.split(" ")[1];
-		if (path.endsWith(".html") || path.endsWith(".ico")) {
-			return "./templates" + path;
+		String path = request.split(PATH_DELIMITER)[1];
+		if (path.endsWith(HTML_FILE_TYPE) || path.endsWith(ICO_FILE_TYPE)) {
+			return TEMPLATES_PATH + path;
 		}
 
-		return "./static" + path;
+		return STATIC_PATH + path;
 	}
 }
