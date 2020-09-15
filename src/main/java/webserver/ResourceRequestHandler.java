c4ed0d582e27a68e48780845635c52ec1e3cb06d
K.S.KIM <nullable@kakao.com>
2020-09-16 04:33:59 +0900

    feat: 리소스 요청 처리를 위한 ResourceRequestHandler 추가

---
+++ src/main/java/webserver/ResourceRequestHandler.java
@@ -0,0 +1,21 @@
+package webserver;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+import http.HttpRequest;
+import http.HttpResponse;
+import http.StaticResourceType;
+import utils.FileIoUtils;
+
+public class ResourceRequestHandler implements Handler {
+    @Override
+    public void handleRequest(final HttpRequest httpRequest, final HttpResponse httpResponse) throws
+            IOException,
+            URISyntaxException {
+        StaticResourceType resourceType = StaticResourceType.findByUri(httpRequest.getRequestLine().getUri());
+        byte[] body = FileIoUtils.loadFileFromClasspath(resourceType.getResourceLocation() + httpRequest.getPath());
+        httpResponse.response200Header(resourceType.getContentType(), body.length);
+        httpResponse.responseBody(body);
+    }
+}
