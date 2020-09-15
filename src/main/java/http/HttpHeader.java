10dee58382799ff50ca4f9084d2aae0e83781cdb
kimseonggyu1 <luckykimsk@gmail.com>
2020-09-15 17:30:31 +0900

    feat: 응답 객체 구현

--- src/main/java/http/HttpHeader.java
+++ src/main/java/http/HttpHeader.java
@@ -15,11 +15,16 @@ public class HttpHeader {
     private static final int KEY_INDEX = 0;
     private static final int VALUE_INDEX = 1;
     private static final String DEFAUlT_VALUE = "";
+    private static final String JOIN_SEPERATOR = ",";
 
     private Map<String, String> headers;
 
     private HttpHeader(Map<String, String> headers) {
         this.headers = headers;
+    }
+
+    public static HttpHeader empty() {
+        return new HttpHeader(new HashMap<>());
     }
 
     public static HttpHeader from(BufferedReader bufferedReader) {
@@ -39,6 +44,15 @@ public class HttpHeader {
         for (String line : lines) {
             String[] keyValues = line.split(HEADER_SEPERATOR, SPLIT_SIZE);
             headers.put(keyValues[KEY_INDEX], keyValues[VALUE_INDEX]);
+        }
+    }
+
+    public void addHeader(String key, String value) {
+        if (headers.containsKey(key)) {
+            String old = headers.get(key);
+            headers.replace(key, old + JOIN_SEPERATOR + value);
+        } else {
+            headers.put(key, value);
         }
     }
 
--- src/main/java/http/response/ResponseEntity.java
+++ src/main/java/http/response/ResponseEntity.java
@@ -1,4 +1,68 @@
 package http.response;
 
+import java.util.Map;
+
+import http.HttpBody;
+import http.HttpHeader;
+import http.request.ContentType;
+
 public class ResponseEntity {
+
+    private String httpVersion;
+    private HttpStatus httpStatus;
+    private HttpHeader httpHeader;
+    private HttpBody httpBody;
+
+    public ResponseEntity(HttpStatus httpStatus) {
+        this(null, httpStatus, HttpHeader.empty(), null);
+    }
+
+    public ResponseEntity(String httpVersion, HttpStatus httpStatus, HttpHeader httpHeader, HttpBody httpBody) {
+        this.httpVersion = httpVersion;
+        this.httpStatus = httpStatus;
+        this.httpHeader = httpHeader;
+        this.httpBody = httpBody;
+    }
+
+    public static ResponseEntity status(HttpStatus httpStatus) {
+        return new ResponseEntity(httpStatus);
+    }
+
+    public ResponseEntity version(String version) {
+        this.httpVersion = version;
+        return this;
+    }
+
+    public ResponseEntity addHeaders(Map<String, String> headers) {
+        for (Map.Entry<String, String> entry : headers.entrySet()) {
+            this.httpHeader.addHeader(entry.getKey(), entry.getValue());
+        }
+        return this;
+    }
+
+    public ResponseEntity addHeader(String key, String value) {
+        this.httpHeader.addHeader(key, value);
+        return this;
+    }
+
+    public ResponseEntity body(String body, ContentType contentType) {
+        this.httpBody = new HttpBody(body, contentType);
+        return this;
+    }
+
+    public String getHttpVersion() {
+        return httpVersion;
+    }
+
+    public HttpStatus getHttpStatus() {
+        return httpStatus;
+    }
+
+    public HttpHeader getHttpHeader() {
+        return httpHeader;
+    }
+
+    public HttpBody getHttpBody() {
+        return httpBody;
+    }
 }
