42a5e8ce0e806d85450ddafae999a33a6faeaa8e
toneyparky <qkrtmddhks95@gmail.com>
2020-09-16 07:53:06 +0900

    feat: 중복되는 MappedRequest에 대한 예외처리

--- src/main/java/http/request/MappedRequest.java
+++ src/main/java/http/request/MappedRequest.java
@@ -3,8 +3,15 @@ package http.request;
 import java.util.Objects;
 
 public class MappedRequest {
-    private HttpMethod httpMethod;
-    private String path;
+    private HttpMethod httpMethod = HttpMethod.GET;
+    private String path = "";
+
+    public MappedRequest() {
+    }
+
+    public MappedRequest(HttpMethod httpMethod) {
+        this.httpMethod = httpMethod;
+    }
 
     public MappedRequest(HttpMethod httpMethod, String path) {
         this.httpMethod = httpMethod;
--- src/main/java/webserver/RequestMapper.java
+++ src/main/java/webserver/RequestMapper.java
@@ -5,11 +5,16 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 
 import http.request.MappedRequest;
+import webserver.exception.DuplicatedMappedRequestException;
 
 public class RequestMapper {
     private static final Map<MappedRequest, Method> requestMapper = new LinkedHashMap<>();
 
     public static void add(MappedRequest mappedRequest, Method method) {
+        if (requestMapper.containsKey(mappedRequest)) {
+            throw new DuplicatedMappedRequestException(
+                String.format("중복되는 Mapped Request가 있습니다 : %s와 %s", method, requestMapper.get(mappedRequest)));
+        }
         requestMapper.put(mappedRequest, method);
     }
 
