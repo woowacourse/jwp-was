2e9a3609c69dfd4704baa8cffc9375521e41fa46
Sony <sonypark0204@gmail.com>
2020-09-16 23:21:55 +0900

    refactor: StringUtils 라이브러리 사용

--- src/main/java/web/RequestHeader.java
+++ src/main/java/web/RequestHeader.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
-import utils.StringUtils;
+import org.springframework.util.StringUtils;
 
 public class RequestHeader {
 
