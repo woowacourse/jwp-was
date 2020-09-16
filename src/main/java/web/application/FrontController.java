ec07570f381669d3c9ff043c4c3b392ac84dbab5
hongbin23 <khbasd@gmail.com>
2020-09-16 17:12:09 +0900

    refactor: FrontController에서 path과 method를 통한 컨트롤러 매핑
     - Path와 method를 가진 RequestVo 생성

--- src/main/java/web/application/FrontController.java
+++ src/main/java/web/application/FrontController.java
@@ -2,26 +2,30 @@ package web.application;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.BiConsumer;
 
-import web.application.controller.Controller;
 import web.application.controller.CreateUserController;
 import web.application.controller.RootController;
-import web.application.exception.UrlNotFoundException;
+import web.application.exception.ControllerNotFoundException;
+import web.application.vo.RequestVo;
+import web.server.domain.request.HttpRequest;
+import web.server.domain.request.RequestMethod;
+import web.server.domain.response.HttpResponse;
 
 public class FrontController {
 
-    private static final Map<String, Controller> mapper;
+    private static final Map<RequestVo, BiConsumer<HttpRequest, HttpResponse>> mapper;
 
     static {
         mapper = new HashMap<>();
-        mapper.put("/user/create", CreateUserController.getInstance());
-        mapper.put("/", RootController.getInstance());
+        mapper.put(RequestVo.of("/user/create", RequestMethod.POST), CreateUserController.getInstance()::doPost);
+        mapper.put(RequestVo.of("/", RequestMethod.GET), RootController.getInstance()::doGet);
     }
 
-    public static Controller findMatchingController(String path) {
-        if (mapper.containsKey(path)) {
-            return mapper.get(path);
+    public static BiConsumer<HttpRequest, HttpResponse> findMatchingController(RequestVo requestVo) {
+        if (mapper.containsKey(requestVo)) {
+            return mapper.get(requestVo);
         }
-        throw new UrlNotFoundException(path);
+        throw new ControllerNotFoundException(requestVo);
     }
 }
--- src/main/java/web/application/exception/UrlNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package web.application.exception;
-
-public class UrlNotFoundException extends IllegalArgumentException {
-
-    public UrlNotFoundException(String path) {
-        super("등록되지 않은 경로입니다. Path = " + path);
-    }
-}
