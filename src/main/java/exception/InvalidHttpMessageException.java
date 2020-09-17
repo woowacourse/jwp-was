0c3ef74ac8b71d915356bdf13a8a601348dd8ebe
icyMojito <dev.icymojito@gmail.com>
2020-09-18 04:44:14 +0900

    feat: InvalidHttpMessageException 예외를 일으킨 매개변수를 모두 보여주도록 수정

--- src/main/java/exception/InvalidHttpMessageException.java
+++ src/main/java/exception/InvalidHttpMessageException.java
@@ -1,7 +1,9 @@
 package exception;
 
+import java.util.Arrays;
+
 public class InvalidHttpMessageException extends RuntimeException {
-    public InvalidHttpMessageException(Object object) {
-        super("잘못된 형식의 HTTP Message입니다! -> " + object);
+    public InvalidHttpMessageException(Object... object) {
+        super("잘못된 형식의 HTTP Message입니다! -> " + Arrays.toString(object));
     }
 }
