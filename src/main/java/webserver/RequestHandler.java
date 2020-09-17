b799e91ee0b51c782456e6002ecf0aaff1ef73a6
icyMojito <dev.icymojito@gmail.com>
2020-09-18 04:55:06 +0900

    refactor: HTTP Response 관련 객체들로 HTTP Request에 응답 처리

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,12 +1,16 @@
 package webserver;
 
-import db.DataBase;
-import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import webserver.http.HttpMessage;
-import webserver.http.request.HttpUri;
+import webserver.http.body.DefaultHttpBody;
 import webserver.http.body.HttpBody;
+import webserver.http.header.HttpHeader;
+import webserver.http.message.HttpMessage;
+import webserver.http.message.HttpRequestMessage;
+import webserver.http.request.HttpUri;
+import webserver.http.response.HttpStatus;
+import webserver.http.response.StatusLine;
+import webserver.service.UserService;
 
 import java.io.*;
 import java.net.Socket;
@@ -14,7 +18,6 @@ import java.nio.charset.StandardCharsets;
 
 public class RequestHandler implements Runnable {
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
-    private static final String NEW_LINE = System.lineSeparator();
 
     private Socket connection;
 
@@ -30,53 +33,41 @@ public class RequestHandler implements Runnable {
              BufferedReader br = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
              DataOutputStream dos = new DataOutputStream(connection.getOutputStream())) {
 
-            HttpMessage httpMessage = HttpMessage.from(br);
-            HttpBody httpBody = httpMessage.getHttpBody();
+            HttpRequestMessage httpRequestMessage = HttpRequestMessage.from(br);
+            HttpBody httpBody = httpRequestMessage.getHttpBody();
 
             UserService.addUser(httpBody);
 
-            HttpUri httpUri = httpMessage.getRequestLine().getHttpUri();
-            try {
-                byte[] fileBytes = httpUri.readFile();
-                String contentType = httpUri.getContentType();
-                response200Header(dos, contentType, fileBytes.length);
-                responseBody(dos, fileBytes);
-            } catch (Exception e) {
-                String indexUrl = "/index.html";
-                response302Header(dos, indexUrl);
-            }
+            HttpUri httpUri = httpRequestMessage.getRequestLine().getHttpUri();
+            HttpMessage httpResponseMessage = createHttpMessage(httpUri);
+
+            response(dos, httpResponseMessage.toHttpMessage().getBytes());
         } catch (IOException e) {
             logger.error(e.getMessage());
         }
     }
 
-    private void response200Header(DataOutputStream dos, String contentType, int lengthOfBodyContent) {
+    private HttpMessage createHttpMessage(HttpUri httpUri) {
         try {
-            dos.writeBytes("HTTP/1.1 200 OK ");
-            dos.writeBytes(NEW_LINE);
-            dos.writeBytes("Content-Type: " + contentType + ";charset=utf-8");
-            dos.writeBytes(NEW_LINE);
-            dos.writeBytes("Content-Length: " + lengthOfBodyContent);
-            dos.writeBytes(NEW_LINE);
-            dos.writeBytes(NEW_LINE);
-        } catch (IOException e) {
-            logger.error(e.getMessage());
+            byte[] fileBytes = httpUri.readFile();
+            StatusLine statusLine = new StatusLine(HttpStatus.OK);
+            HttpHeader httpHeader = new HttpHeader.Builder()
+                    .addHeader("Content-Type", httpUri.getContentType() + ";charset=utf-8")
+                    .addHeader("Content-Length", String.valueOf(fileBytes.length))
+                    .build();
+            HttpBody httpbody = new DefaultHttpBody(new String(fileBytes, 0, fileBytes.length));
+            return new HttpMessage(statusLine, httpHeader, httpbody);
+        } catch (Exception e) {
+            StatusLine statusLine = new StatusLine(HttpStatus.FOUND);
+            String redirectUrl = "/index.html";
+            HttpHeader httpHeader = new HttpHeader.Builder()
+                    .addHeader("Location", redirectUrl)
+                    .build();
+            return new HttpMessage(statusLine, httpHeader);
         }
     }
 
-    private void response302Header(DataOutputStream dos, String redirectUrl) {
-        try {
-            dos.writeBytes("HTTP/1.1 302 Found ");
-            dos.writeBytes(NEW_LINE);
-            dos.writeBytes("Location: " + redirectUrl);
-            dos.writeBytes(NEW_LINE);
-            dos.writeBytes(NEW_LINE);
-        } catch (IOException e) {
-            logger.error(e.getMessage());
-        }
-    }
-
-    private void responseBody(DataOutputStream dos, byte[] body) {
+    private void response(DataOutputStream dos, byte[] body) {
         try {
             dos.write(body, 0, body.length);
             dos.flush();
