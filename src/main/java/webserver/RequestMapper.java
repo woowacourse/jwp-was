cc47ae0c385fba5c38b18e3ebe084c874fceeff5
toneyparky <qkrtmddhks95@gmail.com>
2020-09-17 21:28:40 +0900

    refactor: RequestMapper의 add 메서드를 Reflection하는 로직 제거

--- src/main/java/webserver/RequestMapper.java
+++ src/main/java/webserver/RequestMapper.java
@@ -1,6 +1,5 @@
 package webserver;
 
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.LinkedHashMap;
 import java.util.Map;
@@ -19,40 +18,31 @@ import webserver.http.request.MappedRequest;
 public class RequestMapper {
     private static final Map<MappedRequest, Method> requestMapper = new LinkedHashMap<>();
     private static final String DEFAULT_CONTROLLER_DIRECTORY = "application/controller";
-    private static final String ADD_METHOD_NAME = "add";
 
-    public static void scanRequestMappingAnnotatedMethod() throws NoSuchMethodException {
+    public static void scanRequestMappingAnnotatedMethod() {
         Reflections reflections = new Reflections(DEFAULT_CONTROLLER_DIRECTORY);
         Set<Class<?>> controllerAnnotatedClasses = reflections.getTypesAnnotatedWith(Controller.class);
 
-        Method addToRequestMapper = RequestMapper.class.getMethod(ADD_METHOD_NAME, MappedRequest.class, Method.class);
-
         controllerAnnotatedClasses.stream()
-            .map(findRequestMappingAnnotatedMethods())
-            .forEach(applyAnnotatedMethodsTo(addToRequestMapper));
+            .map(scanRequestMappingAnnotatedMethods())
+            .forEach(applyAnnotatedMethods());
     }
 
-    private static Function<Class<?>, Set<Method>> findRequestMappingAnnotatedMethods() {
+    private static Function<Class<?>, Set<Method>> scanRequestMappingAnnotatedMethods() {
         return aClass ->
             new Reflections(aClass.getName(), new MethodAnnotationsScanner())
                 .getMethodsAnnotatedWith(RequestMapping.class);
     }
 
-    private static Consumer<Set<Method>> applyAnnotatedMethodsTo(Method addToRequestMapper) {
-        return classes -> {
-            classes.forEach(addAnnotatedMethodToRequestMapper(addToRequestMapper));
-        };
+    private static Consumer<Set<Method>> applyAnnotatedMethods() {
+        return classes -> classes.forEach(addAnnotatedMethodToRequestMapper());
     }
 
-    private static Consumer<Method> addAnnotatedMethodToRequestMapper(Method addToRequestMapper) {
+    private static Consumer<Method> addAnnotatedMethodToRequestMapper() {
         return annotatedMethod -> {
             RequestMapping annotation = annotatedMethod.getAnnotation(RequestMapping.class);
             MappedRequest mappedRequest = new MappedRequest(annotation.method(), annotation.path());
-            try {
-                addToRequestMapper.invoke(null, mappedRequest, annotatedMethod);
-            } catch (IllegalAccessException | InvocationTargetException e) {
-                e.printStackTrace();
-            }
+            add(mappedRequest, annotatedMethod);
         };
     }
 
