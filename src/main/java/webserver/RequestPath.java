17b626e4d50df38bb647f88cd2de633cfc79aac9
aegis <aegis1920@gmail.com>
2020-09-18 09:33:01 +0900

    feat: RequestPath 클래스 추가

---
+++ src/main/java/webserver/RequestPath.java
@@ -0,0 +1,13 @@
+package webserver;
+
+public class RequestPath {
+    private final String requestPath;
+
+    public RequestPath(String requestPath) {
+        this.requestPath = requestPath;
+    }
+
+    public String getRequestPath() {
+        return requestPath;
+    }
+}
