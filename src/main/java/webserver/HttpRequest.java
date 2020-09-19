9b10aba3b3df7a8da32dd29823beeef1a27e4e87
moonyoungCHAE <xpf_fl@naver.com>
2020-09-20 01:47:27 +0900

    feat: 회원가입 시 입력한 정보 파싱

--- src/main/java/webserver/HttpRequest.java
+++ src/main/java/webserver/HttpRequest.java
@@ -1,27 +1,56 @@
 package webserver;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.net.URLDecoder;
+import java.util.HashMap;
+import java.util.Map;
 
 public class HttpRequest {
     private final HttpMethod httpMethod;
     private final String resourcePath;
+    private final Map<String, String> parameters;
 
-    public HttpRequest(HttpMethod httpMethod, String resourcePath) {
+    public HttpRequest(HttpMethod httpMethod, String resourcePath, Map<String, String> parameters) {
         this.httpMethod = httpMethod;
         this.resourcePath = resourcePath;
+        this.parameters = parameters;
+    }
+
+    public HttpRequest(HttpMethod httpMethod, String resourcePath) {
+        this(httpMethod, resourcePath, new HashMap<>());
     }
 
     public HttpRequest(BufferedReader bufferedReader) throws IOException {
         String[] requestLine = bufferedReader.readLine().split(" ");
         this.httpMethod = HttpMethod.valueOf(requestLine[0]);
-        this.resourcePath = requestLine[1];
+        this.resourcePath = parseResourcePath(requestLine[1]);
+        this.parameters = parseParameters(requestLine[1]);
     }
 
     public HttpRequest(InputStream inputStream) throws IOException {
         this(new BufferedReader(new InputStreamReader(inputStream)));
+    }
+
+    private String parseResourcePath(String line) {
+        return line.split("\\?")[0];
+    }
+
+    private Map<String, String> parseParameters(String line) throws UnsupportedEncodingException {
+        String resourcePath = parseResourcePath(line);
+        if (resourcePath.equals(line)) {
+            return new HashMap<>();
+        }
+
+        line = line.substring(resourcePath.length() + 1);
+        line = URLDecoder.decode(line, "UTF-8");
+        String[] queryStrings = line.split("&");
+
+        Map<String, String> parameters = new HashMap<>();
+        for (String queryString : queryStrings) {
+            parameters.put(queryString.split("=")[0],
+                    queryString.split("=")[1]);
+        }
+        return parameters;
     }
 
     public boolean isResourcePath(String resourcePath) {
@@ -30,5 +59,9 @@ public class HttpRequest {
 
     public HttpMethod getHttpMethod() {
         return httpMethod;
+    }
+
+    public Map<String, String> getParameters() {
+        return parameters;
     }
 }
--- src/test/java/webserver/HttpRequestTest.java
+++ src/test/java/webserver/HttpRequestTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -23,5 +24,24 @@ class HttpRequestTest {
 
         assertThat(httpRequest.getHttpMethod()).isEqualTo(HttpMethod.GET);
         assertThat(httpRequest.isResourcePath("/user/form.html")).isTrue();
+    }
+
+    @DisplayName("QueryString(Parameter) 파싱 테스트")
+    @Test
+    void queryParsingTest() throws IOException {
+        StringBuilder request = new StringBuilder();
+        request.append("GET /user/create?userId=javajigi&password=password&name=%EB%B0%95%EC%9E%AC%EC%84%B1&email=javajigi%40slipp.net HTTP/1.1 \n");
+        request.append("Host: localhost:8080 \n");
+        request.append("Connection: keep-alive \n");
+        request.append("Accept: */* \n");
+
+        HttpRequest httpRequest = new HttpRequest(new ByteArrayInputStream(request.toString().getBytes()));
+        Map<String, String> actual = httpRequest.getParameters();
+
+        assertThat(actual).hasSize(4);
+        assertThat(actual.get("userId")).isEqualTo("javajigi");
+        assertThat(actual.get("password")).isEqualTo("password");
+        assertThat(actual.get("name")).isEqualTo("박재성");
+        assertThat(actual.get("email")).isEqualTo("javajigi@slipp.net");
     }
 }
