6afc493bdd52579d3f22cf71ffbab72122ca1dbf
aegis <aegis1920@gmail.com>
2020-09-20 21:25:21 +0900

    feat: RequestPath 루트 디렉토리일 경우, index.html로 가는 기능 구현 및 테스트 추가

--- src/main/java/webserver/RequestPath.java
+++ src/main/java/webserver/RequestPath.java
@@ -1,6 +1,10 @@
 package webserver;
 
 public class RequestPath {
+    private static final String ROOT_PATH = "/";
+    private static final String INDEX_PATH = "/index";
+    private static final String INDEX_SUFFIX = ".html";
+
     private final String requestPath;
 
     public RequestPath(String requestPath) {
@@ -8,6 +12,9 @@ public class RequestPath {
     }
 
     public String getRequestPath() {
+        if (ROOT_PATH.equals(requestPath)) {
+            return INDEX_PATH + INDEX_SUFFIX;
+        }
         return requestPath;
     }
 }
