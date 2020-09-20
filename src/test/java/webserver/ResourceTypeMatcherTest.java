ff02e2229d749256af6ca49dab11aec9bfcd3e01
ParkDooWon <pass345@naver.com>
2020-09-20 21:06:58 +0900

    test : woff 타입의 파일에 대한 테스트 추가

--- src/test/java/webserver/ResourceTypeMatcherTest.java
+++ src/test/java/webserver/ResourceTypeMatcherTest.java
@@ -7,13 +7,14 @@ import org.junit.jupiter.params.provider.CsvSource;
 
 public class ResourceTypeMatcherTest {
 	@ParameterizedTest
-	@CsvSource({"/a.html,HTML", "/b.js,JS", "/c.css,CSS"})
+	@CsvSource({"/a.html,HTML", "/b.js,JS", "/c.css,CSS", "/d.woff,WOFF"})
 	void findContentTypeTest(String file, ResourceTypeMatcher type) {
 		assertThat(ResourceTypeMatcher.findContentType(file)).isEqualTo(type);
 	}
 
 	@ParameterizedTest
-	@CsvSource({"/a.html,HTML,./templates/a.html", "/b.js,JS,./static/b.js", "/c.css,CSS,./static/c.css"})
+	@CsvSource({"/a.html,HTML,./templates/a.html", "/b.js,JS,./static/b.js", "/c.css,CSS,./static/c.css",
+		"/d.woff,WOFF,./static/d.woff"})
 	void parseFilePathTest(String file, ResourceTypeMatcher type, String path) {
 		assertThat(type.parseFilePath(file)).isEqualTo(path);
 	}
