6fc6776035cef8ae843daa96a6b237949901d978
Andy H. Jung <commaniakr@gmail.com>
2020-09-21 18:12:06 +0900

    feat: 다수의 사용자 요청에 대해 큐에 저장한 후 순차적으로 처리하는 기능 추가

--- src/main/java/webserver/WebServer.java
+++ src/main/java/webserver/WebServer.java
@@ -1,17 +1,19 @@
 package webserver;
 
-import http.RequestHeader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 public class WebServer {
     private static final Logger logger = LoggerFactory.getLogger(WebServer.class);
     private static final int DEFAULT_PORT = 8080;
+    private static final int CORES = Runtime.getRuntime().availableProcessors();
 
 
     public static void main(String args[]) throws Exception {
@@ -24,13 +26,16 @@ public class WebServer {
 
         // 서버소켓을 생성한다. 웹서버는 기본적으로 8080번 포트를 사용한다.
         try (ServerSocket listenSocket = new ServerSocket(port)) {
+            ExecutorService executorService = new ThreadPoolExecutor(
+                    CORES, CORES, 5000L,
+                    TimeUnit.MILLISECONDS, new ArrayBlockingQueue<>(100, true),
+                    new ThreadPoolExecutor.CallerRunsPolicy());
+
             logger.info("Web Application Server started {} port.", port);
-            ExecutorService executorService = Executors.newFixedThreadPool(100);
             // 클라이언트가 연결될때까지 대기한다.
             Socket connection;
             while ((connection = listenSocket.accept()) != null) {
-                RequestHandler requestHandler = new RequestHandler(connection);
-                executorService.execute(requestHandler);
+                executorService.execute(new RequestHandler(connection));
             }
         }
     }
