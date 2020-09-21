97a181c60be3811c48cc4c3ccdfd4a95a51394cf
juyeongjung <juyeong.dev@gmail.com>
2020-09-21 16:43:43 +0900

    test: 테스트를 위한 환경 설정

--- src/test/java/webserver/RequestTest.java
+++ src/test/java/webserver/RequestTest.java
@@ -4,33 +4,10 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static webserver.HttpMethod.GET;
 import static webserver.HttpMethod.POST;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.List;
 import org.junit.jupiter.api.Test;
 import webserver.request.Request;
 
-public class RequestTest {
-
-    private String testDirectory = "./src/test/resources/";
-
-    private Request generateRequest(String request) throws IOException {
-        InputStream in = new FileInputStream(new File(testDirectory + request));
-        BufferedReader br = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
-        String line;
-        List<String> lines = new ArrayList<>();
-
-        while (!"".equals(line = br.readLine()) && line != null) {
-            lines.add(line);
-        }
-        return new Request(lines, br);
-    }
+public class RequestTest extends WebServerTest {
 
     @Test
     public void requestTest_GET() throws Exception {
