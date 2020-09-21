b0955708604e7a8a4182feeecfa475e17f89646d
juyeongjung <juyeong.dev@gmail.com>
2020-09-21 16:44:01 +0900

    test: Response index.html 테스트 추가

---
+++ src/test/java/webserver/ResponseTest.java
@@ -0,0 +1,22 @@
+package webserver;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static webserver.Status.OK;
+
+import java.io.IOException;
+import org.junit.jupiter.api.Test;
+import webserver.response.Response;
+
+public class ResponseTest extends WebServerTest {
+
+
+    @Test
+    public void responseTest_GET() throws IOException {
+        Response response = generateResponse(generateRequest("http_GET_index.txt"));
+
+        assertThat(response.getStatus()).isEqualTo(OK);
+        assertThat(response.getVersion()).isEqualTo("HTTP/1.1");
+        assertThat(response.getHeaders().get("Content-Type")).contains("text/html");
+        assertThat(response.getBody()).hasSizeGreaterThan(1);
+    }
+}
