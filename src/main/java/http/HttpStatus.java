4019632f97ff466516602f25c5707468e1024e4a
lalize <printf@kakao.com>
2020-09-21 16:00:33 +0900

    feat: HttpResponse 추가

---
+++ src/main/java/http/HttpStatus.java
@@ -0,0 +1,28 @@
+package http;
+
+import java.util.Arrays;
+
+public enum HttpStatus {
+    OK(200, "OK"),
+    FOUND(304, "Found"),
+    NOT_FOUND(404, "Not Found");
+
+    private final int statusCode;
+    private final String reason;
+
+    HttpStatus(int statusCode, String reason) {
+        this.statusCode = statusCode;
+        this.reason = reason;
+    }
+
+    public static HttpStatus from(int statusCode) {
+        return Arrays.stream(values())
+            .filter(it -> it.statusCode == statusCode)
+            .findFirst()
+            .orElseThrow(() -> new IllegalArgumentException("지원하지 않는 상태 코드 입니다."));
+    }
+
+    public String build() {
+        return statusCode + " " + reason;
+    }
+}
