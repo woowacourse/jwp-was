faa6b68ee0a6a359b4366ced7c991249b420392a
aegis <aegis1920@gmail.com>
2020-09-21 18:50:42 +0900

    feat: HttpVersion Enum 추가 및 테스트 작성

---
+++ src/main/java/exception/NotFoundHttpVersionException.java
@@ -0,0 +1,8 @@
+package exception;
+
+public class NotFoundHttpVersionException extends RuntimeException {
+
+    public NotFoundHttpVersionException(String message) {
+        super(message);
+    }
+}
