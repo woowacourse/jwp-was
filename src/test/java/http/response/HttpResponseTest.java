f49c587f90cecc1edab0ae562de3f4862dc49b8c
lalize <printf@kakao.com>
2020-09-21 19:54:35 +0900

    refactor: HttpResponseTest 수정

--- src/test/java/http/response/HttpResponseTest.java
+++ src/test/java/http/response/HttpResponseTest.java
@@ -21,12 +21,27 @@ public class HttpResponseTest {
     }
 
     @Test
+    @DisplayName("HttpResponse 본문이 존재하는 경우 테스트")
+    void end_body() throws IOException {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        HttpResponse httpResponse = HttpResponse.from(outputStream, "HTTP/1.1");
+        String expected = "HTTP/1.1 200 OK\r\n"
+            + "Content-Length: 12\r\n"
+            + "\r\n"
+            + "Hello World!";
+        httpResponse.status(HttpStatus.OK).end("Hello World!".getBytes());
+        assertThat(outputStream.toString()).isEqualTo(expected);
+    }
+
+    @Test
     @DisplayName("HttpResponse sendRedirect 테스트")
     void sendRedirect() throws IOException {
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         HttpResponse httpResponse = HttpResponse.from(outputStream, "HTTP/1.1");
+        String expected = "HTTP/1.1 302 Found\r\n"
+            + "Location: /index.html\r\n"
+            + "\r\n";
         httpResponse.sendRedirect("/index.html");
-        assertThat(outputStream.toString()).isEqualTo("HTTP/1.1 302 Found\r\n"
-            + "Location: /index.html\r\n\r\n");
+        assertThat(outputStream.toString()).isEqualTo(expected);
     }
 }
