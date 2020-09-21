650bbd7fa8a41653cd8516a5fd003745c725d6d9
lalize <printf@kakao.com>
2020-09-21 19:07:29 +0900

    fix: MimeType 추가 css 파일을 지원하도록 수정

--- src/main/java/webserver/middleware/ServeStatic.java
+++ src/main/java/webserver/middleware/ServeStatic.java
@@ -6,6 +6,7 @@ import http.HttpStatus;
 import http.request.HttpRequest;
 import http.response.HttpResponse;
 import utils.FileIoUtils;
+import utils.MimeType;
 
 public class ServeStatic extends Middleware {
     private final String root;
@@ -18,8 +19,9 @@ public class ServeStatic extends Middleware {
     public void service(HttpRequest request, HttpResponse response) throws IOException {
         try {
             byte[] file = FileIoUtils.loadFileFromClasspath("./" + root + request.path());
+
             response.status(HttpStatus.OK)
-                .set("Content-Type", "text/html;charset=utf-8")
+                .set("Content-Type", MimeType.from(request.path()).getMimeType() + ";charset=utf-8")
                 .end(file);
         } catch (Exception ignored) {
             next(request, response);
