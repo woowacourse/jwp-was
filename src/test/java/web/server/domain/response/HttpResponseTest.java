00551847fba38b0889b262e9a3b021f487bee96d
hongbin23 <khbasd@gmail.com>
2020-09-22 18:07:16 +0900

    refactor: 테스트로 생기는 out파일 경로 변경 및 gitignore 추가

--- src/test/java/web/server/domain/response/HttpResponseTest.java
+++ src/test/java/web/server/domain/response/HttpResponseTest.java
@@ -22,9 +22,9 @@ public class HttpResponseTest {
     @DisplayName("정적 파일을 요청했을 때 HttpResponse에 Status Code 200과 정적파일 내용이 담기는지 확인한다.")
     @Test
     public void responseForward() throws Exception {
-        HttpResponse response = new HttpResponse(createOutputStream("http_forward.txt"));
+        HttpResponse response = new HttpResponse(createOutputStream("/out/http_forward.txt"));
         response.forward(INDEX_PAGE);
-        String actual = readFile("http_forward.txt");
+        String actual = readFile("/out/http_forward.txt");
         assertAll(
             () -> assertThat(actual).contains("200"),
             () -> assertThat(actual).contains("</div>")
@@ -34,9 +34,9 @@ public class HttpResponseTest {
     @DisplayName("다른 URL로 리다이렉트 요청이 왔을 때 HttpResponse에 Status Code 302와 해당 Location 정보가 담기는지 확인한다.")
     @Test
     public void responseRedirect() throws Exception {
-        HttpResponse response = new HttpResponse(createOutputStream("http_redirect.txt"));
+        HttpResponse response = new HttpResponse(createOutputStream("/out/http_redirect.txt"));
         response.sendRedirect(INDEX_PAGE);
-        String actual = readFile("http_redirect.txt");
+        String actual = readFile("/out/http_redirect.txt");
         assertAll(
             () -> assertThat(actual).contains("302"),
             () -> assertThat(actual).contains("Location: " + INDEX_PAGE)
@@ -46,13 +46,14 @@ public class HttpResponseTest {
     @DisplayName("없는 경로로 요청이 왔을 때 HttpResponse에 Status Code 404가 담기는지 확인한다.")
     @Test
     public void responsePageNotFound() throws Exception {
-        HttpResponse response = new HttpResponse(createOutputStream("http_url_not_found.txt"));
+        HttpResponse response = new HttpResponse(createOutputStream("/out/http_url_not_found.txt"));
         response.respondPageNotFound();
-        String actual = readFile("http_url_not_found.txt");
+        String actual = readFile("/out/http_url_not_found.txt");
         assertThat(actual).contains("404");
     }
 
     private DataOutputStream createOutputStream(String filename) throws FileNotFoundException {
+        new File(testDirectory + "/out").mkdirs();
         return new DataOutputStream(new FileOutputStream(new File(testDirectory + filename)));
     }
 
