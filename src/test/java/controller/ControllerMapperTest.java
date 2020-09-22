83c06f8ad79c121f1f877d4e2c8ebab7b06f4160
김예림 <kiel0103@naver.com>
2020-09-22 19:31:14 +0900

    test: findUserController 정상적인 사용상황 테스트에 GET 이외의 요청 추가

--- src/test/java/controller/ControllerMapperTest.java
+++ src/test/java/controller/ControllerMapperTest.java
@@ -2,23 +2,35 @@ package controller;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import request.HttpRequest;
 
 class ControllerMapperTest {
 
-    @Test
-    @DisplayName("/user/create 요청시 UserController 반환")
-    void findUserController() {
+    @ParameterizedTest
+    @MethodSource("httpMethods")
+    @DisplayName("/user/create url로 요청시, Http Method 와 무관하게 UserController 반환")
+    void findUserController(String httpRequestFormat) {
         ControllerMapper controllerMapper = new ControllerMapper();
 
-        String httpRequestFormat = "GET /user/create?id=1 HTTP/1.1\n"
-            + "Host: localhost:8080\n";
         HttpRequest httpRequest = new HttpRequest(httpRequestFormat, "");
 
         assertThat(controllerMapper.findController(httpRequest))
             .isInstanceOf(UserController.class);
+    }
+
+    private static Stream<Arguments> httpMethods() {
+        return Stream.of(
+            Arguments.of("GET /user/create?id=1 HTTP/1.1\n"),
+            Arguments.of("POST /user/create?id=1 HTTP/1.1\nHost: localhost:8080\n"),
+            Arguments.of("PUT /user/create?id=1 HTTP/1.1\n"),
+            Arguments.of("DELETE /user/create?id=1 HTTP/1.1\nHost: localhost:8080\n")
+        );
     }
 
     @Test
