a1d583f6344af6bdb427bcdfd57468404f46995a
김예림 <kiel0103@naver.com>
2020-09-23 15:03:38 +0900

    test: UserController 테스트 구현

--- src/test/java/controller/UserControllerTest.java
+++ src/test/java/controller/UserControllerTest.java
@@ -1,4 +1,31 @@
 package controller;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import request.HttpRequest;
+import response.HttpResponse;
+
 class UserControllerTest {
+
+    private UserController userController = new UserController();
+
+    @Test
+    @DisplayName("회원가입 테스트")
+    void serviceForJoin() {
+        HttpRequest joinRequest = new HttpRequest("POST /join HTTP/1.1\n"
+            + "Host: localhost:8080\n"
+            + "Connection: keep-alive\n"
+            + "Cache-Control: max-age=0\n"
+            + "Upgrade-Insecure-Requests: 1\n"
+            + "Content-Length: 10\n",
+            "userId=tls123&password=passwordoftls&name=신&email=test@email.com");
+
+        HttpResponse response = userController.service(joinRequest);
+        String responseHeader = response.buildHeader();
+
+        assertThat(responseHeader.startsWith("HTTP/1.1 302 Found")).isTrue();
+        assertThat(responseHeader.contains("Location: /")).isTrue();
+    }
 }
