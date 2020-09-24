6402510354efd4719bee64cc0eba77345161c3c0
hongbin23 <khbasd@gmail.com>
2020-09-24 17:26:51 +0900

    refactor: RequestVo 매핑에서 URL로만 매핑하도록 변경

--- src/main/java/web/application/FrontController.java
+++ src/main/java/web/application/FrontController.java
@@ -2,24 +2,23 @@ package web.application;
 
 import java.util.HashMap;
 import java.util.Map;
+
 import web.application.controller.Controller;
 import web.application.controller.CreateUserController;
 import web.application.controller.PageNotFoundController;
 import web.application.controller.RootController;
 import web.application.controller.StaticController;
-import web.application.vo.RequestVo;
 import web.server.domain.request.HttpRequest;
-import web.server.domain.request.RequestMethod;
 import web.server.domain.response.HttpResponse;
 
 public class FrontController implements Controller {
 
-    private final Map<RequestVo, Controller> mapper;
+    private final Map<String, Controller> mapper;
 
     public FrontController() {
         mapper = new HashMap<>();
-        mapper.put(RequestVo.of("/user/create", RequestMethod.POST), CreateUserController.getInstance());
-        mapper.put(RequestVo.of("/", RequestMethod.GET), RootController.getInstance());
+        mapper.put("/user/create", CreateUserController.getInstance());
+        mapper.put("/", RootController.getInstance());
     }
 
     @Override
@@ -28,9 +27,8 @@ public class FrontController implements Controller {
             StaticController.getInstance().service(httpRequest, httpResponse);
             return;
         }
-        RequestVo requestVo = RequestVo.of(httpRequest.getPath(), httpRequest.getRequestMethod());
-        if (mapper.containsKey(requestVo)) {
-            mapper.get(requestVo).service(httpRequest, httpResponse);
+        if (mapper.containsKey(httpRequest.getPath())) {
+            mapper.get(httpRequest.getPath()).service(httpRequest, httpResponse);
             return;
         }
         PageNotFoundController.getInstance().service(httpRequest, httpResponse);
