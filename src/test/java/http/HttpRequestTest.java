b64480cd424775920258bef2d1a0d0259be2de94
K.S.KIM <nullable@kakao.com>
2020-09-28 22:10:01 +0900

    test: 프로젝트에서 사용되지 않는 테스트코드 제거

--- src/test/java/http/HttpRequestTest.java
+++ src/test/java/http/HttpRequestTest.java
@@ -2,14 +2,15 @@ package http;
 
 import static org.assertj.core.api.Assertions.*;
 
-import java.util.Arrays;
+import java.util.Collections;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class HttpRequestTest {
     private static final RequestLine REQUEST_LINE = RequestLine.from("GET / HTTP/1.1");
-    private static final RequestHeaders REQUEST_HEADERS = RequestHeaders.from(Arrays.asList("Host: localhost:8080"));
+    private static final RequestHeaders REQUEST_HEADERS = RequestHeaders.from(
+            Collections.singletonList("Host: localhost:8080"));
     private static final RequestBody REQUEST_BODY = RequestBody.from("userId=test@test.com&name=hello");
 
     @DisplayName("constructor: body가 없는 http 요청을 읽어 HttpRequest 생성")
