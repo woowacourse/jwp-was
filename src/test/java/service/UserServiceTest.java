129c1e3d062d455ab8a029cba2615658311547cd
chws <rebeccachoosir@gmail.com>
2020-09-28 19:27:33 +0900

    refactor: HttpRequest 클래스 추가에 따른 UserServiceTest 리팩토링 및 mockito 제거

--- src/test/java/service/UserServiceTest.java
+++ src/test/java/service/UserServiceTest.java
@@ -1,28 +1,30 @@
 package service;
 
 import db.DataBase;
-import http.RequestBody;
-import model.User;
+import http.HttpRequest;
 import org.junit.jupiter.api.Test;
 
-import java.io.BufferedReader;
-import java.io.IOException;
+import java.io.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 class UserServiceTest {
     @Test
-    void createUserTest() {
-        RequestBody requestBody = mock(RequestBody.class);
-        when(requestBody.getValue("userId")).thenReturn("javajigi");
-        when(requestBody.getValue("password")).thenReturn("password");
-        when(requestBody.getValue("name")).thenReturn("박재성");
-        when(requestBody.getValue("email")).thenReturn("javajigi@slipp.net");
+    void createUserTest() throws IOException {
+        String request = "POST /user/create HTTP/1.1\n" +
+                "Host: localhost:8080\n" +
+                "Connection: keep-alive\n" +
+                "Content-Length: 93\n" +
+                "Content-Type: application/x-www-form-urlencoded\n" +
+                "Accept: */*\n" +
+                "\n" +
+                "userId=javajigi&password=password&name=%EB%B0%95%EC%9E%AC%EC%84%B1&email=javajigi%40slipp.net";
+
+        InputStream in = new ByteArrayInputStream(request.getBytes());
+        BufferedReader br = new BufferedReader(new InputStreamReader(in));
 
         UserService userService = new UserService();
-        userService.createUser(requestBody);
+        userService.createUser(new HttpRequest(br));
 
         assertThat(DataBase.findUserById("javajigi")).isNotNull();
     }
