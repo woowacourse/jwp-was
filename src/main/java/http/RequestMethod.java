0956b869618cf6a732d9379c0055718def54e314
chws <rebeccachoosir@gmail.com>
2020-09-28 20:06:12 +0900

    refactor: RequestMethod 분기 처리를 BiFunction으로 변경

--- src/main/java/http/RequestMethod.java
+++ src/main/java/http/RequestMethod.java
@@ -1,17 +1,28 @@
 package http;
 
+import http.controller.Controller;
+
 import java.util.Arrays;
+import java.util.function.BiFunction;
 
 public enum RequestMethod {
-    GET,
-    POST,
-    PUT,
-    DELETE;
+    GET(Controller::get),
+    POST(Controller::post);
+
+    private final BiFunction<Controller, HttpRequest, HttpResponse> function;
+
+    RequestMethod(BiFunction<Controller, HttpRequest, HttpResponse> function) {
+        this.function = function;
+    }
 
     public static RequestMethod of(String method) {
         return Arrays.stream(values())
                 .filter(t -> t.name().equals(method))
                 .findFirst()
-                .orElseThrow(() -> new IllegalArgumentException("Invalid method"));
+                .orElseThrow(() -> new IllegalArgumentException("Unsupported method"));
+    }
+
+    public HttpResponse extractResponse(Controller controller, HttpRequest httpRequest) {
+        return this.function.apply(controller, httpRequest);
     }
 }
