8cb3f6a8253741cda9a701b736e4d38f78a4b35e
jamie9504 <jamie9504@kakao.com>
2020-09-29 05:13:52 +0900

    refactor: UserService ID 중복시 로깅 추가

--- src/main/java/jwp/was/webapplicationserver/service/UserService.java
+++ src/main/java/jwp/was/webapplicationserver/service/UserService.java
@@ -6,19 +6,29 @@ import jwp.was.webapplicationserver.configure.annotation.Service;
 import jwp.was.webapplicationserver.controller.dto.UserRequest;
 import jwp.was.webapplicationserver.db.DataBase;
 import jwp.was.webapplicationserver.model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Service
 public class UserService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(UserService.class);
 
     @Autowired
     private DataBase dataBase;
 
     public User createUser(UserRequest userRequest) {
         User user = userRequest.toUser();
-        if (Objects.nonNull(dataBase.findUserById(user.getUserId()))) {
-            throw new IllegalArgumentException("userId가 중복되었습니다.");
-        }
+        validateUserId(user.getUserId());
         dataBase.addUser(user);
         return user;
+    }
+
+    private void validateUserId(String userId) {
+        if (Objects.nonNull(dataBase.findUserById(userId))) {
+            String validateMessage = "userID가 중복되었습니다.";
+            LOGGER.error("{} : {}", validateMessage, userId);
+            throw new IllegalArgumentException(validateMessage);
+        }
     }
 }
