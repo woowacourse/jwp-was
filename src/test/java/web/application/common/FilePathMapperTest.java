3259d4929c4fc934e423329dc164b2a877b5c4ef
hongbin23 <khbasd@gmail.com>
2020-09-29 20:39:56 +0900

    feat: 요청파일 경로의 full path를 매핑해주는 FilePathMapper 클래스 추가

---
+++ src/test/java/web/application/common/FilePathMapperTest.java
@@ -0,0 +1,22 @@
+package web.application.common;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import web.server.utils.StaticFileType;
+
+class FilePathMapperTest {
+
+    @DisplayName("정적파일의 FullPath를 확인한다")
+    @ParameterizedTest
+    @CsvSource(value = {"/index.html, HTML, ./templates/index.html", "/style.css, CSS, ./static/style.css"})
+    void addPrefix(String path, StaticFileType staticFileType, String actual) {
+        FilePathMapper filePathMapper = FilePathMapper.getInstance();
+        String filePath = filePathMapper.addPrefix(path, staticFileType);
+
+        assertThat(filePath).isEqualTo(actual);
+    }
+}
