e69c5798cb7fdfb0454d3da84d8ca58af4ed9f05
chws <rebeccachoosir@gmail.com>
2020-09-30 20:54:34 +0900

    feat: 지원되지 않는 content-type으로 RequestBody가 주어질 경우의 테스트 추가

--- src/test/java/http/RequestBodyTest.java
+++ src/test/java/http/RequestBodyTest.java
@@ -40,4 +40,22 @@ class RequestBodyTest {
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("password");
     }
+
+    @Test
+    @DisplayName("[예외] content-type이 미지원되는 타입일 경우 테스트")
+    void unsupportedContentTypeTest() {
+        String request = "{" +
+                "userId : javajigi," +
+                "password: password," +
+                "name: %EB%B0%95%EC%9E%AC%EC%84%B1," +
+                "email: javajigi%40slipp.net " +
+                "}";
+
+        InputStream in = new ByteArrayInputStream(request.getBytes());
+        BufferedReader br = new BufferedReader(new InputStreamReader(in));
+
+        assertThatThrownBy(() -> new RequestBody(br, 85, "application/json"))
+                .isInstanceOf(UnsupportedOperationException.class)
+                .hasMessageContaining("content-type");
+    }
 }
