a9198980d628723418d8340e3b67ae041921f57f
icyMojito <dev.icymojito@gmail.com>
2020-10-02 04:25:31 +0900

    feat: HTTP Response Message의 출력을 담당하는 ResponseWriter 생성
    RequestHandler의 책임 중 하나(HTTP Response Message의 출력)를 분리하기 위해 만듦

---
+++ src/main/java/webserver/ResponseWriter.java
@@ -0,0 +1,35 @@
+package webserver;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import webserver.http.message.HttpMessage;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+public class ResponseWriter implements AutoCloseable {
+    private static final Logger logger = LoggerFactory.getLogger(ResponseWriter.class);
+
+    private final DataOutputStream dos;
+
+    public ResponseWriter(Socket connection) throws IOException {
+        this.dos = new DataOutputStream(connection.getOutputStream());
+    }
+
+    public void write(HttpMessage httpResponseMessage) {
+        byte[] message = httpResponseMessage.toHttpMessage().getBytes();
+
+        try {
+            dos.write(message, 0, message.length);
+            dos.flush();
+        } catch (IOException e) {
+            logger.error(e.getMessage());
+        }
+    }
+
+    @Override
+    public void close() throws IOException {
+        this.dos.close();
+    }
+}
