ee8ae89d15d6bd183c61e17eaa961821d153064d
Hamliet <hamliet@hanyang.ac.kr>
2020-10-05 19:38:51 +0900

    refactor: StringUtils 리팩토링

--- src/main/java/utils/StringUtils.java
+++ src/main/java/utils/StringUtils.java
@@ -11,30 +11,47 @@ import model.Request;
 
 public class StringUtils {
 
+    private static final String REQUEST_LINE_DELIMITER = " ";
+    private static final int METHOD_INDEX = 0;
+    private static final int LOCATION_INDEX = 1;
     private static final int VALID_REQUEST_SIZE = 3;
+    private static final String KEY_VALUE_LINK_LETTER = "=";
+    private static final String PARAMETER_DELIMITER = "&";
+    private static final int VALUE_RIGHT_SIDE_INDEX = 1;
+    private static final int VALUE_INDEX = 0;
     private static final String TEMPLATE_LOCATION = "./templates";
     private static final String STATIC_LOCATION = "./static";
+    private static final String EXTENSION_DELIMITER = "\\.";
+    private static final int NO_EXTENSION_SIZE = 1;
+    private static final int SIZE_CORRECTION_NUMBER = 1;
+    private static final String EXTENSION_LETTER = ".";
+    private static final int NO_PARAMETER_SIZE = 1;
+    private static final String QUERY_STRING_DELIMITER = "\\?";
+    private static final int PARAMETERS_RIGHT_SIDE_INDEX = 1;
+    private static final int PARAMETERS_INDEX = 0;
+    private static final int CONTENT_LENGTH_VALUE_INDEX = 1;
+    private static final String EMPTY_LIME = "";
 
     public static String extractRequestMethod(String line) {
-        String[] requestLine = line.split(" ");
+        String[] requestLine = line.split(REQUEST_LINE_DELIMITER);
         if (requestLine.length != VALID_REQUEST_SIZE) {
             throw new IllegalArgumentException("Invalid Request");
         }
-        return requestLine[0];
+        return requestLine[METHOD_INDEX];
     }
 
     public static String extractRequestLocation(String line) {
-        String[] requestLine = line.split(" ");
+        String[] requestLine = line.split(REQUEST_LINE_DELIMITER);
         if (requestLine.length != VALID_REQUEST_SIZE) {
             throw new IllegalArgumentException("Invalid Request");
         }
-        return requestLine[1];
+        return requestLine[LOCATION_INDEX];
     }
 
     public static String extractParameterValue(String params, String key) {
         return params
-            .split(key + "=")[1]
-            .split("&")[0];
+            .split(key + KEY_VALUE_LINK_LETTER)[VALUE_RIGHT_SIDE_INDEX]
+            .split(PARAMETER_DELIMITER)[VALUE_INDEX];
     }
 
     public static String generatePath(Request request) {
@@ -48,13 +65,13 @@ public class StringUtils {
     }
 
     public static String extractExtension(String line) {
-        String location = line.split(" ")[1];
-        String[] sections = location.split("\\.");
+        String location = line.split(REQUEST_LINE_DELIMITER)[LOCATION_INDEX];
+        String[] sections = location.split(EXTENSION_DELIMITER);
 
-        if (sections.length == 1) {
+        if (sections.length == NO_EXTENSION_SIZE) {
             return null;
         }
-        return "." + sections[sections.length - 1];
+        return EXTENSION_LETTER + sections[sections.length - SIZE_CORRECTION_NUMBER];
     }
 
     public static String getParameters(String line, String requestMethod,
@@ -70,12 +87,14 @@ public class StringUtils {
         throw new IllegalArgumentException("Not Implemented Method");
     }
 
-    private static String extractGetParameters(String line) throws IOException {
-        String[] sections = line.split("\\?");
-        if (sections.length == 1) {
+    private static String extractGetParameters(String line) {
+        String location = extractRequestLocation(line);
+        String[] sections = location.split(QUERY_STRING_DELIMITER);
+        if (sections.length == NO_PARAMETER_SIZE) {
             return null;
         }
-        return sections[1].split(" ")[0];
+        return sections[PARAMETERS_RIGHT_SIDE_INDEX]
+            .split(REQUEST_LINE_DELIMITER)[PARAMETERS_INDEX];
     }
 
     private static String extractPostParameters(BufferedReader bufferedReader) throws IOException {
@@ -83,9 +102,10 @@ public class StringUtils {
         while (!line.contains("Content-Length:")) {
             line = bufferedReader.readLine();
         }
-        int contentLength = Integer.parseInt(line.split("Content-Length: ")[1]);
+        int contentLength
+            = Integer.parseInt(line.split("Content-Length: ")[CONTENT_LENGTH_VALUE_INDEX]);
 
-        while (!line.equals("")) {
+        while (!line.equals(EMPTY_LIME)) {
             line = bufferedReader.readLine();
         }
         return IOUtils.readData(bufferedReader, contentLength);
