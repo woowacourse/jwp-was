ef589aa98f028d63e7f5f88d616fd07c65261139
icyMojito <dev.icymojito@gmail.com>
2020-10-09 16:57:26 +0900

    feat: 사용자 목록 출력을 동적으로 수행하기 위한 TemplateCreator 생성
    Handlebars가 적용되도록 사용자 목록 템플릿인 list.html 수정

---
+++ src/main/java/exception/InvalidTemplateException.java
@@ -0,0 +1,6 @@
+package exception;
+
+public class InvalidTemplateException extends RuntimeException {
+    public InvalidTemplateException() {
+    }
+}
---
+++ src/test/java/utils/TemplateCreatorTest.java
@@ -0,0 +1,35 @@
+package utils;
+
+import db.DataBase;
+import model.User;
+import model.Users;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import webserver.service.UserService;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class TemplateCreatorTest {
+    private static final Logger log = LoggerFactory.getLogger(TemplateCreator.class);
+
+    @DisplayName("출력을 원하는 템플릿 경로와 출력할 객체를 전달하면 동적으로 템플릿을 잘 만드는지 확인")
+    @Test
+    void createTemplateTest() {
+        String userId = "coollime";
+        String password = "1234";
+        String name = "쿨라임";
+        String email = "coollime@woowa.com";
+        DataBase.addUser(new User(userId, password, name, email));
+
+        String userListTemplateLocation = "/user/list";
+        Users users = UserService.findAll();
+
+        String template = TemplateCreator.createTemplate(userListTemplateLocation, users);
+
+        log.debug("UserListPage : {}", template);
+
+        assertThat(template).contains(userId, name, email);
+    }
+}
