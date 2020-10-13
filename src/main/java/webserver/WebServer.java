b789efa08136856dbd1d6fb7452de2f66657bb74
gunhee lee <dlrjsgml42@naver.com>
2020-10-13 14:22:39 +0900

    feat: 쓰레드풀 적용

--- src/main/java/webserver/WebServer.java
+++ src/main/java/webserver/WebServer.java
@@ -5,10 +5,13 @@ import org.slf4j.LoggerFactory;
 
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class WebServer {
     private static final Logger logger = LoggerFactory.getLogger(WebServer.class);
     private static final int DEFAULT_PORT = 8080;
+    private static final ExecutorService EXECUTOR_SERVICE = Executors.newFixedThreadPool(100);
 
     public static void main(String args[]) throws Exception {
         int port = 0;
@@ -25,8 +28,7 @@ public class WebServer {
             // 클라이언트가 연결될때까지 대기한다.
             Socket connection;
             while ((connection = listenSocket.accept()) != null) {
-                Thread thread = new Thread(new RequestHandler(connection));
-                thread.start();
+                EXECUTOR_SERVICE.execute(new RequestHandler(connection));
             }
         }
     }
