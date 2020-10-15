bcaf5c90fd1725db4b17f1948ec2adc6a27bd858
김예림 <kiel0103@naver.com>
2020-10-15 18:51:20 +0900

    test: ControllerMapperTest 보강

--- src/test/java/controller/ControllerMapperTest.java
+++ src/test/java/controller/ControllerMapperTest.java
@@ -15,7 +15,7 @@ class ControllerMapperTest {
 
     @ParameterizedTest
     @MethodSource("httpMethods")
-    @DisplayName("/user/create url로 요청시, Http Method 와 무관하게 UserController 반환")
+    @DisplayName("UserController 반환")
     void findUserController(String httpRequestFormat) {
         ControllerMapper controllerMapper = new ControllerMapper();
 
@@ -27,15 +27,21 @@ class ControllerMapperTest {
 
     private static Stream<Arguments> httpMethods() {
         return Stream.of(
+            // user/create
             Arguments.of("GET /user/create?id=1 HTTP/1.1\n"),
             Arguments.of("POST /user/create?id=1 HTTP/1.1\nHost: localhost:8080\n"),
             Arguments.of("PUT /user/create?id=1 HTTP/1.1\n"),
-            Arguments.of("DELETE /user/create?id=1 HTTP/1.1\nHost: localhost:8080\n")
+            Arguments.of("DELETE /user/create?id=1 HTTP/1.1\nHost: localhost:8080\n"),
+            // user/login
+            Arguments.of("GET /user/login?id=1 HTTP/1.1\n"),
+            Arguments.of("POST /user/login?id=1 HTTP/1.1\nHost: localhost:8080\n"),
+            Arguments.of("PUT /user/login?id=1 HTTP/1.1\n"),
+            Arguments.of("DELETE /user/login?id=1 HTTP/1.1\nHost: localhost:8080\n")
         );
     }
 
     @Test
-    @DisplayName("요청 path가 컨트롤러와 매핑되어있지 않을 때 StaticFileController 반환")
+    @DisplayName("정의된 uri 가 아닌 경우 StaticFileController 반환")
     void findStaticFileController() {
         ControllerMapper controllerMapper = new ControllerMapper();
 
