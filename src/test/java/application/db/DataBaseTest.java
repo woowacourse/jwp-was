8dbd0933cb8e4d1a86617221a03f1ae05060a789
김예림 <kiel0103@naver.com>
2020-10-15 20:29:53 +0900

    test: Database 테스트 작성

--- src/test/java/application/db/DataBaseTest.java
+++ src/test/java/application/db/DataBaseTest.java
@@ -1,18 +1,29 @@
 package application.db;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import application.controller.WrongUserIdPasswordException;
+import model.User;
 import org.junit.jupiter.api.Test;
 
 class DataBaseTest {
 
     @Test
-    void addUser() {
-    }
+    void addAndFindUser() throws WrongUserIdPasswordException {
+        DataBase.addUser(new User("id", "password", "name", "email"));
 
-    @Test
-    void findUserById() {
+        User user = DataBase.findUserById("id");
+
+        assertThat(user.getUserId()).isEqualTo("id");
+        assertThat(user.getEmail()).isEqualTo("email");
+        assertThat(user.getPassword()).isEqualTo("password");
     }
 
     @Test
     void findAll() {
+        DataBase.addUser(new User("id1", "password1", "name1", "email1"));
+        DataBase.addUser(new User("id2", "password2", "name2", "email2"));
+
+        assertThat(DataBase.findAll()).hasSize(2);
     }
 }
