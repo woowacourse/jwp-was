0a3ecaf32dbeea3dabd308b8d38cb2cd968dadb2
chanin <pci2676@gmail.com>
2020-10-15 19:23:33 +0900

    refactor : response 의 순서 보장

--- src/main/java/web/HttpResponse.java
+++ src/main/java/web/HttpResponse.java
@@ -17,30 +17,36 @@ public class HttpResponse {
         this.dataOutputStream = dataOutputStream;
     }
 
-    public void response200Header(int lengthOfBodyContent, String contentType) {
+    public void response200(final byte[] body, final String contentType) {
+        response200Header(body.length, contentType);
+        responseBody(body);
+    }
+
+    private void response200Header(int contentLength, String contentType) {
         try {
             this.dataOutputStream.writeBytes(HttpVersion.HTTP_1_1.getVersion() + HttpStatusCode.OK.getValue() + NEW_LINE);
             this.dataOutputStream.writeBytes(HeaderName.CONTENT_TYPE.getHeader(contentType) + NEW_LINE);
-            this.dataOutputStream.writeBytes(HeaderName.CONTENT_LENGTH.getHeader(lengthOfBodyContent) + NEW_LINE);
+            this.dataOutputStream.writeBytes(HeaderName.CONTENT_LENGTH.getHeader(contentLength) + NEW_LINE);
             this.dataOutputStream.writeBytes(NEW_LINE);
         } catch (IOException e) {
             logger.error(e.getMessage());
         }
     }
 
-    public void response302Header(String url) {
+    private void responseBody(byte[] body) {
+        try {
+            this.dataOutputStream.write(body, 0, body.length);
+            this.dataOutputStream.flush();
+        } catch (IOException e) {
+            logger.error(e.getMessage());
+        }
+    }
+
+    public void response302(final String url) {
         try {
             this.dataOutputStream.writeBytes(HttpVersion.HTTP_1_1.getVersion() + HttpStatusCode.FOUND.getValue() + NEW_LINE);
             this.dataOutputStream.writeBytes(HeaderName.LOCATION.getHeader(url) + NEW_LINE);
             this.dataOutputStream.writeBytes(NEW_LINE);
-        } catch (IOException e) {
-            logger.error(e.getMessage());
-        }
-    }
-
-    public void responseBody(byte[] body) {
-        try {
-            this.dataOutputStream.write(body, 0, body.length);
             this.dataOutputStream.flush();
         } catch (IOException e) {
             logger.error(e.getMessage());
