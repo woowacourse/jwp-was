9f244df6c4430dd71b68e05bd36cc8f951da84ab
yebink <yebin825@gmail.com>
2020-10-19 20:57:31 +0900

    feat : ThreadPoolExecutor 도입 및 테스트 작성

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -18,7 +18,7 @@ public class RequestHandler implements Runnable {
 
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
 
-    private Socket connection;
+    private final Socket connection;
 
     public RequestHandler(Socket connectionSocket) {
         this.connection = connectionSocket;
