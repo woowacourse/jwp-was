bf3e178223ac3546b45f2363279e511e46501814
yebink <yebin825@gmail.com>
2020-10-21 16:12:54 +0900

    feat : HandlerMapping 에서 정적 파일 처리를 위한 RequestMapping 정보 등록

---
+++ src/main/java/web/servlet/ResourceController.java
@@ -0,0 +1,27 @@
+package web.servlet;
+
+import utils.FileIoUtils;
+import web.HeaderName;
+import web.HttpRequest;
+import web.HttpResponse;
+import web.HttpStatusCode;
+import web.filter.ResourceMatcher;
+
+import java.io.IOException;
+
+public class ResourceController implements Controller {
+    @Override
+    public void doService(HttpRequest httpRequest, HttpResponse httpResponse) {
+        String requestPath = httpRequest.getRequestPath();
+        ResourceMatcher matcher = ResourceMatcher.findResourceMatcher(requestPath);
+
+        try {
+            byte[] body = FileIoUtils.loadFileFromClasspath(matcher.getResourcePath() + requestPath);
+            httpResponse.putHeader(HeaderName.CONTENT_TYPE.getName(), matcher.getContentType());
+            httpResponse.response(HttpStatusCode.OK, body);
+        } catch (IOException e) {
+            throw new IllegalArgumentException(String.format("%s : 해당경로에 파일이 존재하지 않습니다.", requestPath));
+        }
+
+    }
+}
