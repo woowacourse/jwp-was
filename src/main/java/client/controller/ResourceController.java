40d46a2519ea3a8f7b3c31304b19c9b556360a4f
yebink <yebin825@gmail.com>
2020-10-21 18:19:33 +0900

    feat : Resource 요청 처리 방식 변경
     - ResourceController.filePath 추가
     - ContentType 클래스 제거
     - ResourceMatcher -> ContentTypeMatcher로 이름 변경

--- src/main/java/client/controller/ResourceController.java
+++ src/main/java/client/controller/ResourceController.java
@@ -1,24 +1,29 @@
 package client.controller;
 
 import utils.FileIoUtils;
+import utils.ContentTypeMatcher;
 import web.HeaderName;
 import web.controller.Controller;
 import web.request.HttpRequest;
 import web.response.HttpResponse;
 import web.response.HttpStatusCode;
-import utils.ResourceMatcher;
 
 import java.io.IOException;
 
 public class ResourceController implements Controller {
+    private final String filePath;
+
+    public ResourceController(String filePath) {
+        this.filePath = filePath;
+    }
+
     @Override
     public void doService(HttpRequest httpRequest, HttpResponse httpResponse) {
         String requestPath = httpRequest.getRequestPath();
-        ResourceMatcher matcher = ResourceMatcher.findResourceMatcher(requestPath);
 
         try {
-            byte[] body = FileIoUtils.loadFileFromClasspath(matcher.getResourcePath() + requestPath);
-            httpResponse.putHeader(HeaderName.CONTENT_TYPE.getName(), matcher.getContentType());
+            byte[] body = FileIoUtils.loadFileFromClasspath(this.filePath);
+            httpResponse.putHeader(HeaderName.CONTENT_TYPE.getName(), ContentTypeMatcher.findResourceContentType(requestPath));
             httpResponse.response(HttpStatusCode.OK, body);
         } catch (IOException e) {
             throw new IllegalArgumentException(String.format("%s : 해당경로에 파일이 존재하지 않습니다.", requestPath));
