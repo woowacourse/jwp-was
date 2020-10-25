c0df3bdc9311e01a4c51731911e3e0c32cb76ab5
hongbin23 <khbasd@gmail.com>
2020-10-25 23:30:38 +0900

    fix: 비즈니스로직 변경에 따른 테스트코드 수정

--- src/test/java/web/application/common/FilePathMapperTest.java
+++ src/test/java/web/application/common/FilePathMapperTest.java
@@ -1,17 +1,18 @@
 package web.application.common;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+
 import web.server.utils.StaticFileType;
 
 class FilePathMapperTest {
 
     @DisplayName("정적파일의 FullPath를 확인한다")
     @ParameterizedTest
-    @CsvSource(value = {"/index.html, HTML, ./templates/index.html", "/style.css, CSS, ./static/style.css"})
+    @CsvSource(value = {"/index.html, HTML, templates/index.html", "/style.css, CSS, static/style.css"})
     void addPrefix(String path, StaticFileType staticFileType, String actual) {
         FilePathMapper filePathMapper = FilePathMapper.getInstance();
         String filePath = filePathMapper.addPrefix(path, staticFileType);
