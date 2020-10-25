9266607f9336f5d0b361389fd82f30916b5060af
Sony <sonypark0204@gmail.com>
2020-10-25 15:52:28 +0900

    refacotr: LoginController의 return으로 코드 흐름 제어하는 부분 수정

--- src/main/java/http/controller/LoginController.java
+++ src/main/java/http/controller/LoginController.java
@@ -2,7 +2,11 @@ package http.controller;
 
 import java.util.Objects;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import db.DataBase;
+import http.exceptions.UserNotFound;
 import http.request.HttpRequest;
 import http.request.HttpRequestMapping;
 import http.response.HttpResponse;
@@ -10,25 +14,41 @@ import model.User;
 
 public class LoginController extends HttpRequestMappingAbstractController {
 
+    private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
+    private static final String LOGINED_TRUE = "logined=true; Path=/";
+    private static final String LOGINED_FALSE = "logined=false";
+    private static final String LOGIN_SUCCESS_HTML = "/index.html";
+    private static final String LOGIN_FAILED_HTML = "/user/login_failed.html";
+
     public LoginController(HttpRequestMapping httpRequestMapping) {
         super(httpRequestMapping);
     }
 
     @Override
     public void handle(HttpRequest httpRequest, HttpResponse httpResponse) {
-        String userId = httpRequest.getBody().parseBody().get("userId");
-        String password = httpRequest.getBody().parseBody().get("password");
+        try {
+            String userId = httpRequest.getBody().parseBody().get("userId");
+            String password = httpRequest.getBody().parseBody().get("password");
 
-        User user = DataBase.findUserById(userId);
+            validateUser(userId, password);
 
-        if (authenticateUser(user, password)) {
-            httpResponse.setCookie("logined=true; Path=/");
-            httpResponse.redirect("/index.html");
-            return;
+            httpResponse.setCookie(LOGINED_TRUE);
+            httpResponse.redirect(LOGIN_SUCCESS_HTML);
+
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+            httpResponse.setCookie(LOGINED_FALSE);
+            httpResponse.redirect(LOGIN_FAILED_HTML);
+        }
+    }
+
+    private void validateUser(String userId, String password) {
+        User user = DataBase.findUserById(userId).orElseThrow(UserNotFound::new);
+
+        if (!authenticateUser(user, password)) {
+            throw new IllegalArgumentException("로그인에 실패했습니다.");
         }
 
-        httpResponse.setCookie("logined=false");
-        httpResponse.redirect("/user/login_failed.html");
     }
 
     private boolean authenticateUser(User user, String password) {
---
+++ src/main/java/http/exceptions/UserNotFound.java
@@ -0,0 +1,10 @@
+package http.exceptions;
+
+public class UserNotFound extends RuntimeException {
+    public UserNotFound() {
+    }
+
+    public UserNotFound(String message) {
+        super(message);
+    }
+}
