b29de272ea61f51d4ce48d7a8b14b9969438a706
hwookim <gusdn9754@gmail.com>
2020-10-26 16:43:21 +0900

    feat: implement RequestHeader.parseQueryParams

--- src/main/java/webserver/protocol/RequestHeader.java
+++ src/main/java/webserver/protocol/RequestHeader.java
@@ -1,18 +1,44 @@
 package webserver.protocol;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 
 public class RequestHeader {
 
+    private static final int QUERY_PARAM_INDEX = 1;
+    private static final String QUERY_PARAMS_REGEX = "&";
+    private static final String PARAM_REGEX = "=";
+    private static final int QUERY_PARAM_NAME_INDEX = 0;
+    private static final int QUERY_PARAM_VALUE_INDEX = 1;
+
     private final String httpMethod;
     private final String path;
     private final String httpVersion;
+    private final Map<String, String> queryParams;
 
     public RequestHeader(final String httpMethod, final String path, final String httpVersion) {
         validate(httpMethod, path, httpVersion);
         this.httpMethod = httpMethod;
         this.path = path;
         this.httpVersion = httpVersion;
+        this.queryParams = parseQueryParams(path);
+    }
+
+    private Map<String, String> parseQueryParams(final String path) {
+        final String[] pathTokens = path.split("\\?", -1);
+        if (pathTokens.length <= QUERY_PARAM_INDEX) {
+            return new HashMap<>();
+        }
+
+        final String[] allQueryParams = pathTokens[QUERY_PARAM_INDEX].split(QUERY_PARAMS_REGEX, -1);
+        final Map<String, String> queryParams = new HashMap<>();
+        for (final String queryParam : allQueryParams) {
+            final String[] nameAndValue = queryParam.split(PARAM_REGEX, -1);
+            queryParams.put(nameAndValue[QUERY_PARAM_NAME_INDEX], nameAndValue[QUERY_PARAM_VALUE_INDEX]);
+        }
+
+        return queryParams;
     }
 
     private void validate(final String httpMethod, final String path, final String httpVersion) {
@@ -37,5 +63,9 @@ public class RequestHeader {
 
     public String getHttpVersion() {
         return httpVersion;
+    }
+
+    public Map<String, String> getQueryParams() {
+        return queryParams;
     }
 }
--- src/test/java/webserver/protocol/RequestHeaderTest.java
+++ src/test/java/webserver/protocol/RequestHeaderTest.java
@@ -4,6 +4,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
+import java.util.HashMap;
+import java.util.Map;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -42,5 +44,36 @@ class RequestHeaderTest {
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("httpVersion가 유효하지 않습니다: null")
         );
+    }
+
+    @DisplayName("RequestHeader 생성 시 Map<String, String> query 생성")
+    @Test
+    void createTest_queryParams() {
+        final String httpMethod = "GET";
+        final String path = "/user/create?userId=testId&password=testPW&name=testName&email=test%40test.com";
+        final String httpVersion = "HTTP/1.1";
+
+        final RequestHeader requestHeader = new RequestHeader(httpMethod, path, httpVersion);
+
+        Map<String, String> expected = new HashMap<>();
+        expected.put("userId", "testId");
+        expected.put("password", "testPW");
+        expected.put("name", "testName");
+        expected.put("email", "test%40test.com");
+
+        assertThat(requestHeader.getQueryParams()).usingRecursiveComparison().isEqualTo(expected);
+    }
+
+    @Test
+    void createTest_without_queryParams() {
+        final String httpMethod = "GET";
+        final String path = "/index.html";
+        final String httpVersion = "HTTP/1.1";
+
+        final RequestHeader requestHeader = new RequestHeader(httpMethod, path, httpVersion);
+
+        assertThat(requestHeader.getQueryParams())
+            .isInstanceOf(Map.class)
+            .isEmpty();
     }
 }
