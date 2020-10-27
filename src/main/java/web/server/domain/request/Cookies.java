ef747216e12c403f76423a72149fa8918e7422d1
hongbin23 <khbasd@gmail.com>
2020-10-27 14:45:42 +0900

    fix: 쿠키가 없을 때의 예외처리 추가

--- src/main/java/web/server/domain/request/Cookies.java
+++ src/main/java/web/server/domain/request/Cookies.java
@@ -3,7 +3,9 @@ package web.server.domain.request;
 import static java.util.stream.Collectors.*;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -15,11 +17,17 @@ import web.server.domain.exception.CookieNotFoundException;
 @Getter
 public class Cookies {
 
+    private static final Cookies EMPTY_COOKIES = new Cookies(new HashMap<>());
+
     private static final String COOKIES_DELIMITER = ";";
 
     private final Map<String, Cookie> cookies;
 
     public static Cookies from(String cookies) {
+        if (Objects.isNull(cookies) || cookies.isEmpty()) {
+            return EMPTY_COOKIES;
+        }
+
         Map<String, Cookie> cookieMap = Arrays.stream(cookies.split(COOKIES_DELIMITER))
             .map(String::trim)
             .map(Cookie::parseCookie)
