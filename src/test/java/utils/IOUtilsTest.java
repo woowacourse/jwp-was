78b17f262e83de35a8aa224fb2ee821402f97ddf
hwookim <gusdn9754@gmail.com>
2020-10-29 15:34:04 +0900

    test: add test of IOUtils.readHeaderData

--- src/test/java/utils/IOUtilsTest.java
+++ src/test/java/utils/IOUtilsTest.java
@@ -1,5 +1,9 @@
 package utils;
 
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.IOException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
@@ -19,5 +23,22 @@ public class IOUtilsTest {
         BufferedReader br = new BufferedReader(sr);
 
         logger.debug("parse body : {}", IOUtils.readBodyData(br, data.length()));
+    }
+
+    @DisplayName("Http Request에서 header만 정상적으로 읽어오는지 확인")
+    @Test
+    void readHeaderDataTest() throws IOException {
+        final String data = "POST /user/create HTTP/1.1\n"
+            + "Host: localhost:8080\n"
+            + "Connection: keep-alive\n"
+            + "Content-Length: 59\n"
+            + "Content-Type: application/x-www-form-urlencoded\n"
+            + "Accept: */*\n"
+            + "\n"
+            + "userId=javajigi&password=password&name=%EB%B0%95%EC%9E%AC%EC%84%B1&email=javajigi%40slipp.net\n";
+        final StringReader stringReader = new StringReader(data);
+        final BufferedReader bufferedReader = new BufferedReader(stringReader);
+
+        assertThat(IOUtils.readHeaderData(bufferedReader)).hasSize(6);
     }
 }
