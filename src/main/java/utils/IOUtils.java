1757e4169f7c298c2a434fc4e7644dc0a77415e2
hwookim <gusdn9754@gmail.com>
2020-10-29 15:26:01 +0900

    refactor: modify name IOUtils.readData to readBodyData

--- src/main/java/utils/IOUtils.java
+++ src/main/java/utils/IOUtils.java
@@ -14,7 +14,7 @@ public class IOUtils {
      * @return
      * @throws IOException
      */
-    public static String readData(BufferedReader br, int contentLength) throws IOException {
+    public static String readBodyData(BufferedReader br, int contentLength) throws IOException {
         char[] body = new char[contentLength];
         br.read(body, 0, contentLength);
         return String.copyValueOf(body);
--- src/test/java/utils/IOUtilsTest.java
+++ src/test/java/utils/IOUtilsTest.java
@@ -1,5 +1,6 @@
 package utils;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,12 +11,13 @@ import java.io.StringReader;
 public class IOUtilsTest {
     private static final Logger logger = LoggerFactory.getLogger(IOUtilsTest.class);
 
+    @DisplayName("readBodyData 정상 실행")
     @Test
-    public void readData() throws Exception {
+    public void readBodyDataTest() throws Exception {
         String data = "abcd123";
         StringReader sr = new StringReader(data);
         BufferedReader br = new BufferedReader(sr);
 
-        logger.debug("parse body : {}", IOUtils.readData(br, data.length()));
+        logger.debug("parse body : {}", IOUtils.readBodyData(br, data.length()));
     }
 }
