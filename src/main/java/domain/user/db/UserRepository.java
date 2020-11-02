8aec2fee7a58203f31d343d8fea54eafa8b5d8b3
lowoon <sangbo0229@gmail.com>
2020-11-02 15:13:46 +0900

    test: service and controller - user service - login controller - user create controller - user list controller - user read controller

--- src/main/java/domain/user/db/UserRepository.java
+++ src/main/java/domain/user/db/UserRepository.java
@@ -21,4 +21,8 @@ public class UserRepository {
     public static Collection<User> findAll() {
         return users.values();
     }
+
+    public static void clear() {
+        users.clear();
+    }
 }
