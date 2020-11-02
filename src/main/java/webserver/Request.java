ff74539cf28d9a39a8462c2bb1b00a40c7a4b20c
kimhodol <woonjangahn@gmail.com>
2020-11-03 07:21:41 +0900

    feat: add Request.parse()

--- src/main/java/webserver/Request.java
+++ src/main/java/webserver/Request.java
@@ -1,9 +1,36 @@
 package webserver;
 
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Objects;
 
 public class Request {
-    private String request;
-    public Request(InputStream inputStream) {
+    private final String request;
+
+    public Request(InputStream inputStream) throws IOException {
+        if (Objects.isNull(inputStream)) {
+            this.request = "";
+            return;
+        }
+        InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+        this.request = parse(bufferedReader);
+    }
+
+    private String parse(BufferedReader bufferedReader) throws IOException {
+        StringBuilder builder = new StringBuilder();
+        String line = bufferedReader.readLine();
+        while (Objects.nonNull(line) && !line.isEmpty()) {
+            builder.append(line).append(System.lineSeparator());
+            line = bufferedReader.readLine();
+        }
+        return builder.toString();
+    }
+
+    @Override
+    public String toString() {
+        return request;
     }
 }
