91f442a7a9ee5615e4b2f3e97f43e749a07a1300
yebink <yebin825@gmail.com>
2020-11-05 22:43:22 +0900

    feat : HttpSession, HttpSessions 구현 및 테스트

---
+++ src/test/java/web/HttpSessionsTest.java
@@ -0,0 +1,30 @@
+package web;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class HttpSessionsTest {
+
+    @DisplayName("id가 없는 경우 새로운 세션을 생성한다.")
+    @Test
+    void get1() {
+        HttpSessions httpSessions = new HttpSessions();
+
+        HttpSession session = httpSessions.get(null);
+
+        assertThat(session.getId()).isNotNull();
+    }
+
+    @DisplayName("id가 있는 경우 새로운 세션을 생성한다.")
+    @Test
+    void get2() {
+        HttpSessions httpSessions = new HttpSessions();
+
+        HttpSession session = httpSessions.get("bebop");
+
+        assertThat(session.getId()).isEqualTo("bebop");
+    }
+}
