8185e165572133e02dcb8070f8dd3a7259da7627
yebink <yebin825@gmail.com>
2020-11-05 19:39:52 +0900

    feat : Handlebar 적용 구현 및 테스트

--- src/test/java/utils/HandlebarsTest.java
+++ src/test/java/utils/HandlebarsTest.java
@@ -5,15 +5,20 @@ import com.github.jknack.handlebars.Template;
 import com.github.jknack.handlebars.io.ClassPathTemplateLoader;
 import com.github.jknack.handlebars.io.TemplateLoader;
 import model.User;
+import model.Users;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Arrays;
+
 public class HandlebarsTest {
     private static final Logger log = LoggerFactory.getLogger(HandlebarsTest.class);
 
+    @DisplayName("템플릿을 적용한다. - 여러 User")
     @Test
-    void name() throws Exception {
+    void applyTemplate() throws Exception {
         TemplateLoader loader = new ClassPathTemplateLoader();
         loader.setPrefix("/templates");
         loader.setSuffix(".html");
@@ -21,8 +26,12 @@ public class HandlebarsTest {
 
         Template template = handlebars.compile("user/profile");
 
-        User user = new User("javajigi", "password", "자바지기", "javajigi@gmail.com");
-        String profilePage = template.apply(user);
+        User user1 = new User("javajigi", "password", "자바지기", "javajigi@gmail.com");
+        User user2 = new User("elly", "password", "엘리", "elly@gmail.com");
+        User user3 = new User("bebop", "password", "비밥", "bebop@gmail.com");
+
+        Users users = new Users(Arrays.asList(user1, user2, user3));
+        String profilePage = template.apply(users);
         log.debug("ProfilePage : {}", profilePage);
     }
 }
