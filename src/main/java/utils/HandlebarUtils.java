3ebc318ecd4e0bb8b3c0d1092b42aae74dc30245
aegis <aegis1920@gmail.com>
2020-11-06 17:41:20 +0900

    refactor: ClassPathTemplateLoader와 Handlebars 객체를 정적으로 생성하도록 수정

--- src/main/java/utils/HandlebarUtils.java
+++ src/main/java/utils/HandlebarUtils.java
@@ -3,20 +3,13 @@ package utils;
 import com.github.jknack.handlebars.Handlebars;
 import com.github.jknack.handlebars.Template;
 import com.github.jknack.handlebars.io.ClassPathTemplateLoader;
-import com.github.jknack.handlebars.io.TemplateLoader;
 import java.io.IOException;
 
 public class HandlebarUtils {
 
-    private static final String TEMPLATES_DIR = "/templates";
-    private static final String HTML_EXTENSION = ".html";
+    private static final Handlebars handlebars = new Handlebars(new ClassPathTemplateLoader("/templates", ".html"));
 
     public static String apply(String location, Object object) throws IOException {
-        TemplateLoader loader = new ClassPathTemplateLoader();
-        loader.setPrefix(TEMPLATES_DIR);
-        loader.setSuffix(HTML_EXTENSION);
-        Handlebars handlebars = new Handlebars(loader);
-
         Template template = handlebars.compile(location);
         return template.apply(object);
     }
