e1dc02c02bbda06935b94af7351dd5741969859f
kimhodol <woonjangahn@gmail.com>
2020-11-07 17:42:10 +0900

    feat: add getQueryParameters()

--- src/main/java/utils/StringUtils.java
+++ src/main/java/utils/StringUtils.java
@@ -1,9 +1,33 @@
 package utils;
 
+import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class StringUtils {
+    public static final String SPACE = " ";
+    public static final String QUESTION_MARK = "\\?";
+    public static final String AMPERSAND = "&";
+    public static final String EQUAL = "=";
+
     public static String getFilename(String request) {
-        return request
-            .split(System.lineSeparator())[0]
-            .split(" ")[1];
+        return getFirstLine(request)
+            .split(SPACE)[1]
+            .split(QUESTION_MARK)[0];
+    }
+
+    public static String getFirstLine(String request) {
+        return request.split(System.lineSeparator())[0];
+    }
+
+    public static Map<String, String> getQueryParameters(String request) {
+        return Arrays.stream(getFirstLine(request)
+            .split(SPACE)[1]
+            .split(QUESTION_MARK)[1]
+            .split(AMPERSAND))
+            .collect(Collectors.toMap(
+                it -> it.split(EQUAL)[0],
+                it -> it.split(EQUAL)[1])
+            );
     }
 }
--- src/main/java/webserver/Request.java
+++ src/main/java/webserver/Request.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.Map;
 import java.util.Objects;
 
 import utils.StringUtils;
@@ -47,6 +48,10 @@ public class Request {
             return TEMPLATE_PREFIX + filename;
         }
         return STATIC_PREFIX + filename;
+    }
+
+    public Map<String, String> getQueryParameters() {
+        return StringUtils.getQueryParameters(request);
     }
 
     @Override
