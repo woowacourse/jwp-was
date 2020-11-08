52e44dd3e0f5efdfda463bf397c6bb902d8e994e
taejinseok <stj930@gmail.com>
2020-11-08 12:19:50 +0900

    feat: 1단계 - static 이하 정적 자원 반환 기능 구현

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -28,18 +28,57 @@ public class RequestHandler implements Runnable {
 
         try (BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
              DataOutputStream dos = new DataOutputStream(connection.getOutputStream())) {
-            final byte[] body = FileIoUtils.loadFileFromClasspath("./static" + br.readLine().split(" ")[1]);
-            response200Header(dos, body.length);
+            final String requestPath = br.readLine().split(" ")[1];
+            final int extensionIndex = requestPath.lastIndexOf(".");
+            if (extensionIndex != -1 && requestPath.substring(extensionIndex + 1)
+                    .matches("html|ico|css|js|eot|svg|woff|woff2|png|ttf")) {
+                final byte[] body = FileIoUtils.loadFileFromClasspath("./static" + requestPath);
+                response200Header(dos, body.length, requestPath.substring(extensionIndex + 1));
+                responseBody(dos, body);
+                return;
+            }
+            final byte[] body = FileIoUtils.loadFileFromClasspath("./templates" + requestPath);
+            response200Header(dos, body.length, requestPath.substring(extensionIndex + 1));
             responseBody(dos, body);
         } catch (IOException | URISyntaxException e) {
             logger.error(e.getMessage());
         }
     }
 
-    private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
+    private void response200Header(DataOutputStream dos, int lengthOfBodyContent, String extension) {
         try {
+            String type = "text/html";
+            switch (extension) {
+                case "html":
+                    type = "text/html";
+                    break;
+                case "ico":
+                    type = "image/x-icon";
+                    break;
+                case "css":
+                    type = "text/css";
+                    break;
+                case "js":
+                    type = "application/js";
+                    break;
+                case "png":
+                    type = "image/png";
+                    break;
+                case "eot":
+                    type = "application/x-font-eot";
+                    break;
+                case "svg":
+                    type = "image/svg+xml";
+                    break;
+                case "woff":
+                case "woff2":
+                    type = "application/x-font-woff";
+                    break;
+                case "ttf":
+                    type = "application/x-font-ttf";
+            }
             dos.writeBytes("HTTP/1.1 200 OK \r\n");
-            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
+            dos.writeBytes("Content-Type: " + type + ";charset=utf-8\r\n");
             dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
             dos.writeBytes("\r\n");
         } catch (IOException e) {
