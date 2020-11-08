6bf4d513b5e14208f0670a93382ecf1d10b09a4c
taejinseok <stj930@gmail.com>
2020-11-08 10:36:27 +0900

    refactor: 스레드 풀을 통한 서버 스레스 수 한정
    - 매 요청시 마다 새 스레드 생성 하는 대신, 서버에서 관리하는 고정된 스레드만 사용하도록 변경

--- src/main/java/webserver/WebServer.java
+++ src/main/java/webserver/WebServer.java
@@ -2,6 +2,8 @@ package webserver;
 
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,25 +11,29 @@ import org.slf4j.LoggerFactory;
 public class WebServer {
     private static final Logger logger = LoggerFactory.getLogger(WebServer.class);
     private static final int DEFAULT_PORT = 8080;
+    private static final int THREAD_POOL_SIZE = Runtime.getRuntime().availableProcessors();
 
-    public static void main(String args[]) throws Exception {
+    public static void main(String[] args) throws Exception {
+        int port = getPort(args);
+
+        try (ServerSocket listenSocket = new ServerSocket(port)) {
+            logger.info("Web Application Server started {} port.", port);
+            final ExecutorService executorService = Executors.newFixedThreadPool(THREAD_POOL_SIZE);
+
+            Socket connection;
+            while ((connection = listenSocket.accept()) != null) {
+                executorService.execute(new RequestHandler(connection));
+            }
+        }
+    }
+
+    private static int getPort(String[] args) {
         int port = 0;
         if (args == null || args.length == 0) {
             port = DEFAULT_PORT;
         } else {
             port = Integer.parseInt(args[0]);
         }
-
-        // 서버소켓을 생성한다. 웹서버는 기본적으로 8080번 포트를 사용한다.
-        try (ServerSocket listenSocket = new ServerSocket(port)) {
-            logger.info("Web Application Server started {} port.", port);
-
-            // 클라이언트가 연결될때까지 대기한다.
-            Socket connection;
-            while ((connection = listenSocket.accept()) != null) {
-                Thread thread = new Thread(new RequestHandler(connection));
-                thread.start();
-            }
-        }
+        return port;
     }
 }
