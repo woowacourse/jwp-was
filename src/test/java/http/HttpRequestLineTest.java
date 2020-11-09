cb4301e6128ba66006abc967aef519c8a053d0bd
YerinCho <forjustice9839@gmail.com>
2020-11-09 21:01:13 +0900

    test: HttpRequest 테스트 추가  Get, Post요청에 대한 테스트  HttpHeaders 파싱에 대한 테스트

--- src/test/java/http/HttpRequestLineTest.java
+++ src/test/java/http/HttpRequestLineTest.java
@@ -3,11 +3,13 @@ package http;
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class HttpRequestLineTest {
 
     @Test
+    @DisplayName("httpRequestLine 파싱 테스트")
     void from() {
         HttpRequestLine httpRequestLine = HttpRequestLine.from("GET /someUrl.url HTTP/1.1");
 
@@ -16,7 +18,6 @@ class HttpRequestLineTest {
             () -> assertThat(httpRequestLine.getUrl()).isEqualTo("/someUrl.url"),
             () -> assertThat(httpRequestLine.getVersion()).isEqualTo("HTTP/1.1")
         );
-
     }
 
 }
---
+++ src/test/java/http/HttpRequestTest.java
@@ -0,0 +1,50 @@
+package http;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class HttpRequestTest {
+    String testDirectory = "./src/test/resources/";
+
+    @Test
+    @DisplayName("GET 요청에 대한 테스트")
+    void getRequestTest() throws IOException {
+        InputStream in = new FileInputStream(new File(testDirectory + "GET.txt"));
+        HttpRequest httpRequest = new HttpRequest(in);
+
+        assertAll(() -> {
+            assertEquals(HttpMethod.GET, httpRequest.getMethod());
+            assertEquals("/user/create", httpRequest.getUrl());
+            assertEquals("keep-alive", httpRequest.getHeader("Connection"));
+            assertEquals("orange", httpRequest.getParam("userId"));
+            assertEquals("Yerin", httpRequest.getParam("name"));
+            assertFalse(httpRequest.isBodyExist());
+        });
+    }
+
+    @Test
+    @DisplayName("POST 요청에 대한 테스트")
+    void postRequestTest() throws IOException {
+        InputStream in = new FileInputStream(new File(testDirectory + "POST.txt"));
+        HttpRequest httpRequest = new HttpRequest(in);
+
+        assertAll(() -> {
+            assertEquals(HttpMethod.POST, httpRequest.getMethod());
+            assertEquals("/user/create", httpRequest.getUrl());
+            assertEquals("keep-alive", httpRequest.getHeader("Connection"));
+            assertEquals("46", httpRequest.getHeader("Content-Length"));
+            assertEquals("application/x-www-form-urlencoded", httpRequest.getHeader("Content-Type"));
+            assertEquals("orange", httpRequest.getParam("userId"));
+            assertEquals("Yerin", httpRequest.getParam("name"));
+            assertTrue(httpRequest.isBodyExist());
+        });
+    }
+
+}
