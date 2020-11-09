d273e970cd1be3a32b00915d72c675e6bc1cddf5
kimhodol <woonjangahn@gmail.com>
2020-11-09 17:43:57 +0900

    feat: add ContentType, Status enum

---
+++ src/main/java/webserver/Status.java
@@ -0,0 +1,17 @@
+package webserver;
+
+public enum Status {
+    OK(200),
+    FOUND(302);
+
+    private final int code;
+
+    Status(int code) {
+        this.code = code;
+    }
+
+    @Override
+    public String toString() {
+        return code + " " + name();
+    }
+}
