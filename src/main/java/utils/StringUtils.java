268620860ea8946640108cb94f2413afe394aad1
kimhodol <woonjangahn@gmail.com>
2020-11-09 15:41:05 +0900

    feat: add Request.parseBody()

--- src/main/java/utils/StringUtils.java
+++ src/main/java/utils/StringUtils.java
@@ -1,6 +1,7 @@
 package utils;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -10,6 +11,7 @@ public class StringUtils {
     public static final String QUESTION_MARK = "\\?";
     public static final String AMPERSAND = "&";
     public static final String EQUAL = "=";
+    public static final String COLON = ": ";
 
     public static String getFilename(String request) {
         return getFirstLine(request)
@@ -33,7 +35,7 @@ public class StringUtils {
     public static Map<String, String> getQueryParameters(String request) {
         String firstLine = getFirstLine(request);
         if (!firstLine.contains("?")) {
-            return new HashMap<>();
+            return Collections.emptyMap();
         }
         return Arrays.stream(firstLine
             .split(SPACE)[1]
@@ -43,5 +45,14 @@ public class StringUtils {
                 it -> it.split(EQUAL)[0],
                 it -> it.split(EQUAL)[1])
             );
+    }
+
+    public static Map<String, String> getHeader(String request) {
+        return Arrays.stream(request.split(System.lineSeparator()))
+            .skip(1)
+            .collect(Collectors.toMap(
+                it -> it.split(COLON)[0],
+                it -> it.split(COLON)[1]
+            ));
     }
 }
--- src/main/java/webserver/Request.java
+++ src/main/java/webserver/Request.java
@@ -4,25 +4,35 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.Collections;
 import java.util.Map;
 import java.util.Objects;
 
+import utils.IOUtils;
 import utils.StringUtils;
 
 public class Request {
+    public static final String EMPTY = "";
+
     private final String request;
+    private final Map<String, String> header;
     private final Method method;
+    private final String body;
 
     public Request(InputStream inputStream) throws IOException {
         if (Objects.isNull(inputStream)) {
-            this.request = "";
+            this.request = EMPTY;
             this.method = Method.GET;
+            this.header = Collections.emptyMap();
+            this.body = EMPTY;
             return;
         }
         InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
         BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
         this.request = parse(bufferedReader);
         this.method = Method.valueOf(StringUtils.getMethod(request));
+        this.header = StringUtils.getHeader(request);
+        this.body = parseBody(bufferedReader);
     }
 
     private String parse(BufferedReader bufferedReader) throws IOException {
@@ -35,6 +45,14 @@ public class Request {
         return builder.toString();
     }
 
+    private String parseBody(BufferedReader bufferedReader) throws IOException {
+        int contentLength = Integer.parseInt(header.getOrDefault("Content-Length", "0"));
+        if (contentLength == 0) {
+            return EMPTY;
+        }
+        return IOUtils.readData(bufferedReader, contentLength);
+    }
+
     public Method getMethod() {
         return method;
     }
@@ -43,12 +61,19 @@ public class Request {
         return StringUtils.getFilename(request);
     }
 
+    public String getBody() {
+        return body;
+    }
+
     public Map<String, String> getQueryParameters() {
         return StringUtils.getQueryParameters(request);
     }
 
     @Override
     public String toString() {
-        return request;
+        if (body.length() == 0) {
+            return request;
+        }
+        return request + System.lineSeparator() + body;
     }
 }
