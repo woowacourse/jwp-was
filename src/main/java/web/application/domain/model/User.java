4857f5fd058dffe09035c5fa4ff1271a4cca028d
yejiahn <yeppilisa@naver.com>
2020-11-12 15:29:58 +0900

    refactor: Builder 어노테이션 생성자에 적용

--- src/main/java/web/application/domain/model/User.java
+++ src/main/java/web/application/domain/model/User.java
@@ -1,21 +1,25 @@
 package web.application.domain.model;
 
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.ToString;
 
-@AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Getter
 @ToString
-@Builder
 public class User {
 
     private String userId;
     private String password;
     private String name;
     private String email;
+
+    @Builder
+    public User(String userId, String password, String name, String email) {
+        this.userId = userId;
+        this.password = password;
+        this.name = name;
+        this.email = email;
+    }
 
     public boolean hasPasswordSameWith(String password) {
         return this.password.equals(password);
