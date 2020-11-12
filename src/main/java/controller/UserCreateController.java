2f70b69851469f1db72682233bd4567fe8291f75
YerinCho <forjustice9839@gmail.com>
2020-11-13 02:54:15 +0900

    feat: db에 생성한 user 저장

--- src/main/java/controller/UserCreateController.java
+++ src/main/java/controller/UserCreateController.java
@@ -1,14 +1,16 @@
 package controller;
 
+import db.DataBase;
 import http.HttpRequest;
 import model.User;
 
 public class UserCreateController {
-    public static User createUser(HttpRequest httpRequest) {
+    public static void createUser(HttpRequest httpRequest) {
         String userId = httpRequest.getParam("userId");
         String password = httpRequest.getParam("password");
         String name = httpRequest.getParam("name");
         String email = httpRequest.getParam("email");
-        return new User(userId, password, name, email);
+        User user = new User(userId, password, name, email);
+        DataBase.addUser(user);
     }
 }
