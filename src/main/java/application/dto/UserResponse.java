1f8183bd91a701e6b732e470a9510f4b08df2659
김예림 <kiel0103@naver.com>
2020-11-14 14:02:37 +0900

    feat: UserResponse 구현

---
+++ src/main/java/application/dto/UserResponse.java
@@ -0,0 +1,26 @@
+package application.dto;
+
+public class UserResponse {
+
+    private String userId;
+    private String name;
+    private String email;
+
+    public UserResponse(String userId, String name, String email) {
+        this.userId = userId;
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
