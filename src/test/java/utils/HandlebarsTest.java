4299c68fc225f5ef2a9e3d3012931a5fa43bd530
jaeju.jang <jjj0611@gmail.com>
2020-11-20 15:25:39 +0900

    test: handlebar 사용을 위한 학습테스트 작성

--- src/test/java/utils/HandlebarsTest.java
+++ src/test/java/utils/HandlebarsTest.java
@@ -1,5 +1,8 @@
 package utils;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import com.github.jknack.handlebars.Handlebars;
 import com.github.jknack.handlebars.Template;
 import com.github.jknack.handlebars.io.ClassPathTemplateLoader;
@@ -23,6 +26,23 @@ public class HandlebarsTest {
 
         User user = new User("javajigi", "password", "자바지기", "javajigi@gmail.com");
         String profilePage = template.apply(user);
+        log.debug("ProfilePage : {}", profilePage);
+    }
+
+    @Test
+    void applyByMap() throws Exception {
+        TemplateLoader loader = new ClassPathTemplateLoader();
+        loader.setPrefix("/templates");
+        loader.setSuffix(".html");
+        Handlebars handlebars = new Handlebars(loader);
+
+        Template template = handlebars.compile("user/profile");
+
+        Map<String, String> map = new HashMap<>();
+        map.put("name", "자바지기");
+        map.put("email", "javajigi@gmail.com");
+
+        String profilePage = template.apply(map);
         log.debug("ProfilePage : {}", profilePage);
     }
 }
