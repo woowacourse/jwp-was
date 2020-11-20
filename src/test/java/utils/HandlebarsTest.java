9b8092000c185306f3e3aadb8e117a41cb06063a
gunhee lee <dlrjsgml42@naver.com>
2020-11-20 14:09:04 +0900

    feat: HandleBars Template을 통해 리스트를 출력할 수 있도록 구현
    - HandleBars helper 기능을 통해 increase 연산을 선언, 이를 통해 @index에 접근한 후 값을 1 올려서 1부터 시작하는 인덱스 생성. 이를 표에 표시해 줌

--- src/test/java/utils/HandlebarsTest.java
+++ src/test/java/utils/HandlebarsTest.java
@@ -4,16 +4,19 @@ import com.github.jknack.handlebars.Handlebars;
 import com.github.jknack.handlebars.Template;
 import com.github.jknack.handlebars.io.ClassPathTemplateLoader;
 import com.github.jknack.handlebars.io.TemplateLoader;
+import db.DataBase;
 import model.User;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Collection;
+
 public class HandlebarsTest {
-    private static final Logger log = LoggerFactory.getLogger(HandlebarsTest.class);
+    private static final Logger logger = LoggerFactory.getLogger(HandlebarsTest.class);
 
     @Test
-    void name() throws Exception {
+    void profileTest() throws Exception {
         TemplateLoader loader = new ClassPathTemplateLoader();
         loader.setPrefix("/templates");
         loader.setSuffix(".html");
@@ -23,6 +26,22 @@ public class HandlebarsTest {
 
         User user = new User("javajigi", "password", "자바지기", "javajigi@gmail.com");
         String profilePage = template.apply(user);
-        log.debug("ProfilePage : {}", profilePage);
+        logger.debug("ProfilePage : {}", profilePage);
+    }
+
+    @Test
+    void listTest() throws Exception {
+        TemplateLoader loader = new ClassPathTemplateLoader();
+        loader.setPrefix("/templates");
+        loader.setSuffix(".html");
+        Handlebars handlebars = new Handlebars(loader);
+
+        Template template = handlebars.compile("user/list");
+
+        User user = new User("javajigi", "password", "자바지기", "javajigi@gmail.com");
+        DataBase.addUser(user);
+        Collection<User> users = DataBase.findAll();
+        String listPage = template.apply(users);
+        logger.debug("ProfilePage : {}", listPage);
     }
 }
