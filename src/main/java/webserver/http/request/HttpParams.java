c6995aba2739c1e2ddaed2ac8cf7b44c8202f214
kouz <lastpuzly@gmail.com>
2020-11-23 16:42:17 +0900

    refactor: Params에 Value가 존재하지 않을 경우 대응

--- src/main/java/webserver/http/request/HttpParams.java
+++ src/main/java/webserver/http/request/HttpParams.java
@@ -11,6 +11,8 @@ public class HttpParams {
     private static final String VALUE_DELIMITER = "=";
     private static final int KEY = 0;
     private static final int VALUE = 1;
+    private static final int HAS_VALUE = 1;
+    private static final String EMPTY = "";
 
     private final Map<String, String> params;
 
@@ -26,7 +28,12 @@ public class HttpParams {
         Map<String, String> params = Arrays.stream(queryParams.split(PARAM_DELIMITER))
                 .collect(toMap(
                         queryParam -> queryParam.split(VALUE_DELIMITER)[KEY],
-                        queryParam -> queryParam.split(VALUE_DELIMITER)[VALUE]
+                        queryParam -> {
+                            if (queryParam.split(VALUE_DELIMITER).length > HAS_VALUE) {
+                                return queryParam.split(VALUE_DELIMITER)[VALUE];
+                            }
+                            return EMPTY;
+                        }
                 ));
 
         return new HttpParams(params);
