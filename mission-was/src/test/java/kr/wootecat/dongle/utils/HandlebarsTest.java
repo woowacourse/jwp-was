e615a4de6b4b43d14650abd4da0dfa4abab5284b
taejinseok <stj930@gmail.com>
2020-11-24 14:29:15 +0900

    refactor: 멀티 모듈 분리

---
+++ mission-was/src/test/java/kr/wootecat/dongle/utils/HandlebarsTest.java
@@ -0,0 +1,58 @@
+package kr.wootecat.dongle.utils;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.github.jknack.handlebars.Handlebars;
+import com.github.jknack.handlebars.Template;
+import com.github.jknack.handlebars.io.ClassPathTemplateLoader;
+import com.github.jknack.handlebars.io.TemplateLoader;
+
+public class HandlebarsTest {
+    private static final Logger log = LoggerFactory.getLogger(HandlebarsTest.class);
+
+    @Test
+    void name() throws Exception {
+        TemplateLoader loader = new ClassPathTemplateLoader();
+        loader.setPrefix("/templates");
+        loader.setSuffix(".html");
+        Handlebars handlebars = new Handlebars(loader);
+
+        Template template = handlebars.compile("/user/profile");
+
+        User user = new User("javajigi", "password", "자바지기", "javajigi@gmail.com");
+        String profilePage = template.apply(user);
+        log.debug("ProfilePage : {}", profilePage);
+    }
+}
+
+class User {
+    private final String userId;
+    private final String password;
+    private final String name;
+    private final String email;
+
+    public User(String userId, String password, String name, String email) {
+        this.userId = userId;
+        this.password = password;
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
---
+++ mission-was/src/test/java/kr/wootecat/dongle/utils/IOUtilsTest.java
@@ -0,0 +1,41 @@
+package kr.wootecat.dongle.utils;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.StringReader;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class IOUtilsTest {
+    private static final Logger logger = LoggerFactory.getLogger(IOUtilsTest.class);
+
+    @Test
+    public void readData() throws Exception {
+        String data = "abcd123";
+        StringReader sr = new StringReader(data);
+        BufferedReader br = new BufferedReader(sr);
+
+        logger.debug("parse body : {}", IOUtils.readData(br, data.length()));
+    }
+
+    @DisplayName("bufferedReader 학습 테스트 readline")
+    @Test
+    void readLineData() throws IOException {
+        String requestData = "GET /index.html HTTP1/1\r\ncontent-type: application/json;" +
+                "charset=utf-8\r\naccept: application/json;charset=utf-8\r\n\r\n" +
+                "name=one&age=15\r\n";
+        StringReader sr = new StringReader(requestData);
+        BufferedReader br = new BufferedReader(sr);
+
+        logger.info(br.readLine());
+        logger.info(br.readLine());
+        logger.info(br.readLine());
+        logger.info(br.readLine());
+        logger.info(br.readLine());
+        logger.info(br.readLine());
+        logger.info(br.readLine());
+    }
+}
