2f43719e5ac88a2501ce50ebe25d75c345965919
kouz <lastpuzly@gmail.com>
2020-11-24 20:06:57 +0900

    refactor: UserApplication이 WebServer를 사용하도록 변경

--- user/src/main/java/user/UserApplication.java
+++ user/src/main/java/user/UserApplication.java
@@ -1,47 +1,19 @@
 package user;
 
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.util.concurrent.LinkedBlockingQueue;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import java.io.IOException;
 
 import user.controller.UserCreateController;
 import user.controller.UserListController;
 import user.controller.UserLoginController;
-import webserver.RequestHandler;
+import webserver.WebServer;
 import webserver.controller.ControllerMapping;
 import webserver.http.request.HttpMethod;
 import webserver.http.request.RequestMapping;
 
 public class UserApplication {
-    public static final int DEFAULT_PORT = 8080;
-    private static final Logger logger = LoggerFactory.getLogger(UserApplication.class);
-
-    public static void main(String[] args) throws Exception {
-        int port = 0;
-        if (args == null || args.length == 0) {
-            port = DEFAULT_PORT;
-        } else {
-            port = Integer.parseInt(args[0]);
-        }
-
-        LinkedBlockingQueue<Runnable> queue = new LinkedBlockingQueue<>(Integer.MAX_VALUE);
-        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(16, 16, 1L, TimeUnit.SECONDS,
-                queue);
+    public static void main(String[] args) throws IOException {
         initControllerMappings();
-        // 서버소켓을 생성한다. 웹서버는 기본적으로 8080번 포트를 사용한다.
-        try (ServerSocket listenSocket = new ServerSocket(port)) {
-            logger.info("Web Application Server started {} port.", port);
-            // 클라이언트가 연결될때까지 대기한다.
-            Socket connection;
-            while ((connection = listenSocket.accept()) != null) {
-                threadPoolExecutor.execute(new RequestHandler(connection));
-            }
-        }
+        new WebServer(args).run();
     }
 
     private static void initControllerMappings() {
